c8911e2a222382128f274d1a5eb85303c5b45b1d
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-13 15:57:28 -0300

    Fix trait definition

--- math/src/traits.rs
+++ math/src/traits.rs
@@ -1,8 +1,8 @@
 /// A trait for converting an element to its byte representation.
-pub trait ToBytes<T> {
+pub trait ToBytes {
     /// Returns the byte representation of the element in big-endian order.
-    fn to_bytes_be(&self) -> T;
+    fn to_bytes_be(&self) -> Vec<u8>;
 
     /// Returns the byte representation of the element in little-endian order.
-    fn to_bytes_le(&self) -> T;
+    fn to_bytes_le(&self) -> Vec<u8>;
 }
