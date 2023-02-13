6722a66e851f1affa668c8d2b1ea8fbcbb820a64
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-13 17:22:29 -0300

    Update trait

--- math/src/traits.rs
+++ math/src/traits.rs
@@ -1,8 +1,14 @@
 /// A trait for converting an element to its byte representation.
-pub trait ToBytes {
+pub trait ByteConversion {
     /// Returns the byte representation of the element in big-endian order.
     fn to_bytes_be(&self) -> Vec<u8>;
 
     /// Returns the byte representation of the element in little-endian order.
     fn to_bytes_le(&self) -> Vec<u8>;
+
+    /// Returns the element from its byte representation in big-endian order.
+    fn from_bytes_be(bytes: &[u8]) -> Self;
+
+    /// Returns the element from its byte representation in little-endian order.
+    fn from_bytes_le(bytes: &[u8]) -> Self;
 }
