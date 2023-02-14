b6569a190787e4278c15a3b830b1925a5ce18976
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-14 16:05:56 -0300

    Updates errors

--- math/src/errors.rs
+++ math/src/errors.rs
@@ -2,16 +2,14 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ByteConversionError {
-    #[error("from_be_bytes failed: {0}")]
-    FromBEBytesError(String),
-    #[error("from_le_bytes failed: {0}")]
-    FromLEBytesError(String),
-    #[error("to_be_bytes failed: {0}")]
-    ToBEBytesError(String),
-    #[error("to_le_bytes failed: {0}")]
-    ToLEBytesError(String),
-    #[error("try_from_slice failed in from method: {0}")]
-    TryFromSliceError(#[from] std::array::TryFromSliceError),
+    #[error("from_be_bytes failed")]
+    FromBEBytesError,
+    #[error("from_le_bytes failed")]
+    FromLEBytesError,
+    #[error("to_be_bytes failed")]
+    ToBEBytesError,
+    #[error("to_le_bytes failed")]
+    ToLEBytesError,
     #[error("{0}")]
-    Custom(String)
+    Custom(String),
 }
