85cf3d4cdd73073bf753ace011f9490f5ad7113a
Nacho <amigoigna@gmail.com>
2023-02-15 13:56:28 -0300

    Refactor + tests versus other implementations

--- crypto/src/hash/poseidon/mod.rs
+++ crypto/src/hash/poseidon/mod.rs
@@ -2,35 +2,25 @@
 use self::parameters::Parameters;
 
 use super::traits::IsCryptoHash;
-use lambdaworks_math::{
-    elliptic_curve::curves::bls12_381::field_extension::BLS12381PrimeField,
-    field::{element::FieldElement, traits::IsField},
-};
-use std::{ops::{Add, Mul}};
+use lambdaworks_math::field::{element::FieldElement, traits::IsField};
+use std::ops::{Add, Mul};
 
 mod parameters;
-mod test_helper;
+// mod test_helper;
 
 pub struct Poseidon<F: IsField> {
-    pub params: Parameters<F>,
-    pub state: Vec<FieldElement<F>>,
-    pub offset: usize,
+    params: Parameters<F>,
 }
 
 /// Implements hashing for BLS 12381's field.
 /// Alpha = 5 and parameters are predefined for secure implementations
-impl<F:IsField> IsCryptoHash<F> for Poseidon<F> {
-    fn hash_one(
-        input: FieldElement<F>,
-    ) -> Result<FieldElement<F>, String> {
-        self.hash(vec![input], &params)
+impl<F: IsField> IsCryptoHash<F> for Poseidon<F> {
+    fn hash_one(&self, input: FieldElement<F>) -> FieldElement<F> {
+        self.hash(&[input]).unwrap().first().unwrap().clone()
     }
 
-    fn hash_two(
-        left: FieldElement<BLS12381PrimeField>,
-        right: FieldElement<BLS12381PrimeField>,
-    ) -> Result<FieldElement<BLS12381PrimeField>, String> {
-        Self::hash(vec![left, right], &params)
+    fn hash_two(&self, left: FieldElement<F>, right: FieldElement<F>) -> FieldElement<F> {
+        self.hash(&[left, right]).unwrap().first().unwrap().clone()
     }
 }
 
@@ -38,146 +28,171 @@ impl<F> Poseidon<F>
 where
     F: IsField,
 {
-    pub fn new(params: Parameters<F>) -> Self  {
-        Poseidon {
-            offset: 0,
-            params,
-            state: vec![FieldElement::zero(); params.rate + params.capacity],
+    pub fn new(params: Parameters<F>) -> Self {
+        Poseidon { params }
+    }
+
+    pub fn ark(&self, state: &mut [FieldElement<F>], round_number: usize) {
+        for (i, state) in state.iter_mut().enumerate() {
+            *state += self.params.round_constants[round_number + i].clone();
         }
     }
 
-    fn ark(state: &mut Vec<FieldElement<F>>, round_constants: &[FieldElement<F>], round: usize) {
-        for i in 0..state.len() {
-            state[i] += round_constants[round + i].clone();
-        }
-    }
-
-    fn sbox(
-        n_rounds_f: usize,
-        n_rounds_p: usize,
-        state: &mut [FieldElement<F>],
-        i: usize,
-        alpha: u32,
-    ) {
-        if i < n_rounds_f / 2 || i >= n_rounds_f / 2 + n_rounds_p {
+    pub fn sbox(&self, state: &mut [FieldElement<F>], round_number: usize) {
+        if round_number < self.params.n_full_rounds / 2
+            || round_number >= self.params.n_full_rounds / 2 + self.params.n_partial_rounds
+        {
             for current_state in state.iter_mut() {
-                *current_state = current_state.pow(alpha);
+                *current_state = current_state.pow(self.params.alpha);
             }
         } else {
-            state[0] = state[0].pow(alpha);
+            state[0] = state[0].pow(self.params.alpha);
         }
     }
 
-    fn mix(state: &Vec<FieldElement<F>>, mds: &[Vec<FieldElement<F>>]) -> Vec<FieldElement<F>> {
-        let mut new_state: Vec<FieldElement<F>> = Vec::new();
+    pub fn mix(&self, state: &mut [FieldElement<F>]) {
+        let mut new_state: Vec<FieldElement<F>> = Vec::with_capacity(state.len());
         for i in 0..state.len() {
             new_state.push(FieldElement::zero());
             for (j, current_state) in state.iter().enumerate() {
-                let mut mij = mds[i][j].clone();
+                let mut mij = self.params.mds_matrix[i][j].clone();
                 mij = mij.mul(current_state);
                 new_state[i] = new_state[i].clone().add(&mij);
             }
         }
-        new_state.clone()
+        state.clone_from_slice(&new_state[0..state.len()]);
     }
 
-    fn permute(
-        &self,
-        inp: Vec<FieldElement<F>>,
-    ) -> Result<FieldElement<F>, String> {
-        let t = inp.len() + 1;
-        let params = self.params;
-        if inp.is_empty() || inp.len() >= params.n_partial_rounds - 1 {
-            return Err("Wrong inputs length".to_string());
+    fn permute(&self, state: &mut [FieldElement<F>]) {
+        let t = self.params.capacity + self.params.rate;
+        for i in 0..(self.params.n_full_rounds + self.params.n_partial_rounds) {
+            self.ark(state, i * t);
+            self.sbox(state, i);
+            self.mix(state);
         }
-
-        let mut state = vec![FieldElement::zero(); t];
-        state[1..].clone_from_slice(&inp);
-
-        for i in 0..(params.n_full_rounds + params.n_partial_rounds) {
-            Self::ark(&mut state, &params.round_constants, i * t);
-            Self::sbox(
-                params.n_full_rounds,
-                params.n_partial_rounds,
-                &mut state,
-                i,
-                params.alpha,
-            );
-            state = Self::mix(&state, &params.mds_matrix);
-        }
-
-        Ok(state[0].clone())
     }
 
-    fn ensure_permuted(&mut self) {
+    fn ensure_permuted(&self, state: &mut [FieldElement<F>], offset: &mut usize) {
         // offset should be <= rate, so really testing for equality
-        if self.offset >= self.params.rate {
-            self.permute(self.state);
-            self.offset = 0;
+        if *offset >= self.params.rate {
+            self.permute(state);
+            *offset = 0;
         }
     }
 
-    pub fn absorb(&mut self, input: &FieldElement<F>) {
-        self.ensure_permuted();
-        self.state[self.offset] += input.clone();
-        self.offset += 1;
-    }
-
-    pub fn squeeze(&mut self) -> FieldElement<F> {
-        self.ensure_permuted();
-        let result = self.state[self.offset];
-        self.offset += 1;
-        result
-    }
-
-    pub fn hash(&self, inputs: &[FieldElement<F>]) -> Result<Vec<FieldElement<F>>, String>
+    fn hash(&self, inputs: &[FieldElement<F>]) -> Result<Vec<FieldElement<F>>, String>
     where
         F: IsField,
     {
-        if inputs.len() == 0 {
-            return Err("Empty inputs".to_string());
+        let t = self.params.rate + self.params.capacity;
+        if inputs.is_empty() || inputs.len() >= self.params.n_partial_rounds - 1 {
+            return Err("Wrong input length".to_string());
         }
+
+        let mut state = vec![FieldElement::zero(); t];
+        let mut offset: usize = 0;
+
         let n_remaining = inputs.len() % self.params.rate;
         if n_remaining != 0 {
             return Err(format!(
                 "Input length {} must be a multiple of the hash rate {}",
                 inputs.len(),
                 self.params.rate
-            )
-            .to_string());
-        }
-    
-        for input in inputs {
-            self.absorb(input);
+            ));
         }
 
-        let mut result = vec![FieldElement::zero(); self.params.rate];
-        
-        for i in 0..self.params.rate {
-            result[i] = self.squeeze();
+        // absorb
+        for input in inputs {
+            self.ensure_permuted(&mut state, &mut offset);
+            state[offset] += input.clone();
+            offset += 1;
         }
+
+        // squeeze
+        let mut result = vec![FieldElement::zero(); self.params.rate];
+        for result_element in result.iter_mut().take(self.params.rate) {
+            self.ensure_permuted(&mut state, &mut offset);
+            *result_element = state[offset].clone();
+            offset += 1;
+        }
+
         Ok(result)
     }
-    
 }
 
 #[cfg(test)]
 mod tests {
+    use lambdaworks_math::{
+        field::fields::u384_prime_field::{IsMontgomeryConfiguration, MontgomeryBackendPrimeField},
+        unsigned_integer::element::U384,
+    };
+
     use super::*;
+
+    #[derive(Clone, Debug)]
+    pub struct TestFieldConfig;
+    impl IsMontgomeryConfiguration for TestFieldConfig {
+        const MODULUS: U384 = U384::from(
+            "14474011154664525231415395255581126252639794253786371766033694892385558855681",
+        );
+        const MP: u64 = 123;
+        const R2: U384 = U384::from("123");
+    }
+
+    pub type PoseidonTestField = MontgomeryBackendPrimeField<TestFieldConfig>;
+    type TestFieldElement = FieldElement<PoseidonTestField>;
+
+    pub fn load_test_parameters() -> Result<Parameters<PoseidonTestField>, String> {
+        let round_constants_csv = include_str!("s128b/round_constants.csv");
+        let mds_constants_csv = include_str!("s128b/mds_matrix.csv");
+
+        let round_constants = round_constants_csv
+            .split(',')
+            .map(|c| TestFieldElement::new(U384::from(c.trim())))
+            .collect();
+
+        let mut mds_matrix = vec![];
+
+        for line in mds_constants_csv.lines() {
+            let matrix_line = line
+                .split(',')
+                .map(|c| TestFieldElement::new(U384::from(c.trim())))
+                .collect();
+
+            mds_matrix.push(matrix_line);
+        }
+
+        Ok(Parameters {
+            rate: 1,
+            capacity: 2,
+            alpha: 3,
+            n_full_rounds: 8,
+            n_partial_rounds: 83,
+            round_constants,
+            mds_matrix,
+        })
+    }
 
     #[test]
     fn test_poseidon_bls() {
-        let a = FieldElement::<BLS12381PrimeField>::new_base("1");
-        let b = FieldElement::<BLS12381PrimeField>::new_base("2");
-
-        //assert_eq!(
-        //    "0000000000000000000000000000000000000000000000000000000000000001",
-        //    a
-        //);
-
-        let v = Poseidon::hash_one(a.clone()).unwrap();
-        let v = Poseidon::hash_one(b.clone()).unwrap();
-
-        let v = Poseidon::hash_two(a, b).unwrap();
+        let poseidon = Poseidon::new(load_test_parameters().unwrap());
+        let mut state = [
+            TestFieldElement::new(U384::from("7")),
+            TestFieldElement::new(U384::from("98")),
+            TestFieldElement::new(U384::from("0")),
+        ];
+        poseidon.ark(&mut state, 0);
+        let expected = [
+            TestFieldElement::new(U384::from(
+                "10187801339791605336251748402605479409606566396373491958667041943798551150218",
+            )),
+            TestFieldElement::new(U384::from(
+                "8824452141556477327634835943439996420519135454314677708228322513850226510123",
+            )),
+            TestFieldElement::new(U384::from(
+                "5264468709835621148349527988912247104353814123939106227116596276180070073104",
+            )),
+        ];
+        assert_eq!(state, expected);
     }
 }
--- crypto/src/hash/poseidon/parameters.rs
+++ crypto/src/hash/poseidon/parameters.rs
@@ -1,47 +1,35 @@
-// t = 2 means width of input is 1
-// Params: n=381, t=2, alpha=5, M=128, R_F=8, R_P=56
-// Modulus = 4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787
-// Number of S-boxes: 72
-// Number of round constants: 128
-// Round constants for GF(p):
-
-// t = 3 means width of input is 2
-// Params: n=381, t=3, alpha=5, M=128, R_F=8, R_P=56
-// Modulus = 4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787
-// Number of S-boxes: 80
-// Number of round constants: 192
-
 use lambdaworks_math::{
-    elliptic_curve::curves::bls12_381::field_extension::BLS12381PrimeField,
+    elliptic_curve::short_weierstrass::curves::bls12_381::field_extension::BLS12381PrimeField,
     field::{element::FieldElement, traits::IsField},
 };
 
-type PoseidonConstants = (
-    Vec<FieldElement<BLS12381PrimeField>>,
-    Vec<Vec<FieldElement<BLS12381PrimeField>>>,
-);
+type PoseidonConstants<F> = (Vec<FieldElement<F>>, Vec<Vec<FieldElement<F>>>);
 
 pub struct Parameters<F: IsField> {
     pub rate: usize,
     pub capacity: usize,
     pub alpha: u32,
-    /// Full Rounds
     pub n_full_rounds: usize,
-    /// Partial rounds
     pub n_partial_rounds: usize,
     pub round_constants: Vec<FieldElement<F>>,
     pub mds_matrix: Vec<Vec<FieldElement<F>>>,
 }
 
+
+/// Implements hashing for BLS 12381's field.
+/// Alpha = 5 and parameters are predefined for secure implementations
 impl Parameters<BLS12381PrimeField> {
-    pub fn for_two_elements() -> Result<Parameters<BLS12381PrimeField>, String> {
+    // t = 3 means width of input is 2
+    // Params: n=381, t=3, alpha=5, M=128, R_F=8, R_P=56
+    pub fn with_t3() -> Result<Parameters<BLS12381PrimeField>, String> {
         let round_constants_csv = include_str!("t3/round_constants.csv");
         let mds_constants_csv = include_str!("t3/mds_matrix.csv");
+
         let (round_constants, mds_matrix) = Self::parse(round_constants_csv, mds_constants_csv)?;
         Ok(Parameters {
             rate: 2,
-            capacity:1,
-            alpha:5,
+            capacity: 1,
+            alpha: 5,
             n_full_rounds: 8,
             n_partial_rounds: 56,
             round_constants,
@@ -49,15 +37,18 @@ impl Parameters<BLS12381PrimeField> {
         })
     }
 
-    pub fn for_one_element() -> Result<Parameters<BLS12381PrimeField>, String> {
+    // t = 2 means width of input is 1
+    // Params: n=381, t=2, alpha=5, M=128, R_F=8, R_P=56
+    pub fn with_t2() -> Result<Parameters<BLS12381PrimeField>, String> {
         let round_constants_csv = include_str!("t2/round_constants.csv");
         let mds_constants_csv = include_str!("t2/mds_matrix.csv");
+
         let (round_constants, mds_matrix) = Self::parse(round_constants_csv, mds_constants_csv)?;
 
         Ok(Parameters {
             rate: 1,
-            capacity:1,
-            alpha:5,
+            capacity: 1,
+            alpha: 5,
             n_full_rounds: 8,
             n_partial_rounds: 56,
             round_constants,
@@ -65,10 +56,10 @@ impl Parameters<BLS12381PrimeField> {
         })
     }
 
-    fn parse(
+    pub fn parse(
         round_constants_csv: &str,
         mds_constants_csv: &str,
-    ) -> Result<PoseidonConstants, String> {
+    ) -> Result<PoseidonConstants<BLS12381PrimeField>, String> {
         let round_constants = round_constants_csv
             .split(',')
             .map(|c| FieldElement::<BLS12381PrimeField>::new_base(c.trim()))
