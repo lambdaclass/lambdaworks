ee52e7572e30973a943be8ae00fb89fc8f96fbde
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-02-15 18:30:25 -0300

    Tidy up code

--- prover/src/air/composition_poly.rs
+++ prover/src/air/composition_poly.rs
@@ -89,12 +89,8 @@ where
 
 #[cfg(test)]
 mod tests {
-    use winterfell::{
-        math::FieldElement, AirContext, Assertion, ByteWriter, EvaluationFrame, FieldExtension,
-        ProofOptions, TraceInfo, TransitionConstraintDegree,
-    };
-
     use super::*;
+    use winterfell::{FieldExtension, ProofOptions};
 
     #[test]
     fn test_get_coefficients() {
@@ -118,121 +114,7 @@ mod tests {
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
     #[test]
     fn test_composition_poly_simple_computation() {
-        fn build_do_work_trace(start: BaseElement, n: usize) -> TraceTable<BaseElement> {
-            // Instantiate the trace with a given width and length; this will allocate all
-            // required memory for the trace
-            let trace_width = 1;
-            let mut trace = TraceTable::new(trace_width, n);
-
-            // Fill the trace with data; the first closure initializes the first state of the
-            // computation; the second closure computes the next state of the computation based
-            // on its current state.
-            trace.fill(
-                |state| {
-                    state[0] = start;
-                },
-                |_, state| {
-                    state[0] = state[0].exp(3u32.into()) + BaseElement::new(42);
-                },
-            );
-
-            trace
-        }
-
-        // Public inputs for our computation will consist of the starting value and the end result.
-        #[derive(Clone)]
-        struct PublicInputs {
-            start: BaseElement,
-            result: BaseElement,
-        }
-
-        // We need to describe how public inputs can be converted to bytes.
-        impl Serializable for PublicInputs {
-            fn write_into<W: ByteWriter>(&self, target: &mut W) {
-                target.write(self.start);
-                target.write(self.result);
-            }
-        }
-
-        // For a specific instance of our computation, we'll keep track of the public inputs and
-        // the computation's context which we'll build in the constructor. The context is used
-        // internally by the Winterfell prover/verifier when interpreting this AIR.
-        struct WorkAir {
-            context: AirContext<BaseElement>,
-            start: BaseElement,
-            result: BaseElement,
-        }
-
-        impl Air for WorkAir {
-            // First, we'll specify which finite field to use for our computation, and also how
-            // the public inputs must look like.
-            type BaseField = BaseElement;
-            type PublicInputs = PublicInputs;
-
-            // Here, we'll construct a new instance of our computation which is defined by 3 parameters:
-            // starting value, number of steps, and the end result. Another way to think about it is
-            // that an instance of our computation is a specific invocation of the do_work() function.
-            fn new(trace_info: TraceInfo, pub_inputs: PublicInputs, options: ProofOptions) -> Self {
-                // our execution trace should have only one column.
-                assert_eq!(1, trace_info.width());
-
-                // Our computation requires a single transition constraint. The constraint itself
-                // is defined in the evaluate_transition() method below, but here we need to specify
-                // the expected degree of the constraint. If the expected and actual degrees of the
-                // constraints don't match, an error will be thrown in the debug mode, but in release
-                // mode, an invalid proof will be generated which will not be accepted by any verifier.
-                let degrees = vec![TransitionConstraintDegree::new(3)];
-
-                // We also need to specify the exact number of assertions we will place against the
-                // execution trace. This number must be the same as the number of items in a vector
-                // returned from the get_assertions() method below.
-                let num_assertions = 2;
-
-                WorkAir {
-                    context: AirContext::new(trace_info, degrees, num_assertions, options),
-                    start: pub_inputs.start,
-                    result: pub_inputs.result,
-                }
-            }
-
-            // In this method we'll define our transition constraints; a computation is considered to
-            // be valid, if for all valid state transitions, transition constraints evaluate to all
-            // zeros, and for any invalid transition, at least one constraint evaluates to a non-zero
-            // value. The `frame` parameter will contain current and next states of the computation.
-            fn evaluate_transition<E: FieldElement + From<Self::BaseField>>(
-                &self,
-                frame: &EvaluationFrame<E>,
-                _periodic_values: &[E],
-                result: &mut [E],
-            ) {
-                // First, we'll read the current state, and use it to compute the expected next state
-                let current_state = &frame.current()[0];
-                let next_state = current_state.exp(3u32.into()) + E::from(42u32);
-
-                // Then, we'll subtract the expected next state from the actual next state; this will
-                // evaluate to zero if and only if the expected and actual states are the same.
-                result[0] = frame.next()[0] - next_state;
-            }
-
-            // Here, we'll define a set of assertions about the execution trace which must be satisfied
-            // for the computation to be valid. Essentially, this ties computation's execution trace
-            // to the public inputs.
-            fn get_assertions(&self) -> Vec<Assertion<Self::BaseField>> {
-                // for our computation to be valid, value in column 0 at step 0 must be equal to the
-                // starting value, and at the last step it must be equal to the result.
-                let last_step = self.trace_length() - 1;
-                vec![
-                    Assertion::single(0, 0, self.start),
-                    Assertion::single(0, last_step, self.result),
-                ]
-            }
-
-            // This is just boilerplate which is used by the Winterfell prover/verifier to retrieve
-            // the context of the computation.
-            fn context(&self) -> &AirContext<Self::BaseField> {
-                &self.context
-            }
-        }
+        use simple_computation_test_utils::*;
 
         let start = BaseElement::new(3);
         let n = 8;
@@ -284,5 +166,131 @@ mod tests {
         let expected_poly = Polynomial::new(&expected_coeffs);
 
         assert_eq!(get_composition_poly(air, trace, pub_inputs), expected_poly);
+    }
+}
+
+#[cfg(test)]
+pub(crate) mod simple_computation_test_utils {
+    use winterfell::{
+        math::FieldElement, AirContext, Assertion, ByteWriter, EvaluationFrame, ProofOptions,
+        TraceInfo, TransitionConstraintDegree,
+    };
+
+    use super::*;
+
+    pub(crate) fn build_do_work_trace(start: BaseElement, n: usize) -> TraceTable<BaseElement> {
+        // Instantiate the trace with a given width and length; this will allocate all
+        // required memory for the trace
+        let trace_width = 1;
+        let mut trace = TraceTable::new(trace_width, n);
+
+        // Fill the trace with data; the first closure initializes the first state of the
+        // computation; the second closure computes the next state of the computation based
+        // on its current state.
+        trace.fill(
+            |state| {
+                state[0] = start;
+            },
+            |_, state| {
+                state[0] = state[0].exp(3u32.into()) + BaseElement::new(42);
+            },
+        );
+
+        trace
+    }
+
+    // Public inputs for our computation will consist of the starting value and the end result.
+    #[derive(Clone)]
+    pub(crate) struct PublicInputs {
+        pub start: BaseElement,
+        pub result: BaseElement,
+    }
+
+    // We need to describe how public inputs can be converted to bytes.
+    impl Serializable for PublicInputs {
+        fn write_into<W: ByteWriter>(&self, target: &mut W) {
+            target.write(self.start);
+            target.write(self.result);
+        }
+    }
+
+    // For a specific instance of our computation, we'll keep track of the public inputs and
+    // the computation's context which we'll build in the constructor. The context is used
+    // internally by the Winterfell prover/verifier when interpreting this AIR.
+    pub(crate) struct WorkAir {
+        context: AirContext<BaseElement>,
+        start: BaseElement,
+        result: BaseElement,
+    }
+
+    impl Air for WorkAir {
+        // First, we'll specify which finite field to use for our computation, and also how
+        // the public inputs must look like.
+        type BaseField = BaseElement;
+        type PublicInputs = PublicInputs;
+
+        // Here, we'll construct a new instance of our computation which is defined by 3 parameters:
+        // starting value, number of steps, and the end result. Another way to think about it is
+        // that an instance of our computation is a specific invocation of the do_work() function.
+        fn new(trace_info: TraceInfo, pub_inputs: PublicInputs, options: ProofOptions) -> Self {
+            // our execution trace should have only one column.
+            assert_eq!(1, trace_info.width());
+
+            // Our computation requires a single transition constraint. The constraint itself
+            // is defined in the evaluate_transition() method below, but here we need to specify
+            // the expected degree of the constraint. If the expected and actual degrees of the
+            // constraints don't match, an error will be thrown in the debug mode, but in release
+            // mode, an invalid proof will be generated which will not be accepted by any verifier.
+            let degrees = vec![TransitionConstraintDegree::new(3)];
+
+            // We also need to specify the exact number of assertions we will place against the
+            // execution trace. This number must be the same as the number of items in a vector
+            // returned from the get_assertions() method below.
+            let num_assertions = 2;
+
+            WorkAir {
+                context: AirContext::new(trace_info, degrees, num_assertions, options),
+                start: pub_inputs.start,
+                result: pub_inputs.result,
+            }
+        }
+
+        // In this method we'll define our transition constraints; a computation is considered to
+        // be valid, if for all valid state transitions, transition constraints evaluate to all
+        // zeros, and for any invalid transition, at least one constraint evaluates to a non-zero
+        // value. The `frame` parameter will contain current and next states of the computation.
+        fn evaluate_transition<E: winterfell::math::FieldElement + From<Self::BaseField>>(
+            &self,
+            frame: &EvaluationFrame<E>,
+            _periodic_values: &[E],
+            result: &mut [E],
+        ) {
+            // First, we'll read the current state, and use it to compute the expected next state
+            let current_state = &frame.current()[0];
+            let next_state = current_state.exp(3u32.into()) + E::from(42u32);
+
+            // Then, we'll subtract the expected next state from the actual next state; this will
+            // evaluate to zero if and only if the expected and actual states are the same.
+            result[0] = frame.next()[0] - next_state;
+        }
+
+        // Here, we'll define a set of assertions about the execution trace which must be satisfied
+        // for the computation to be valid. Essentially, this ties computation's execution trace
+        // to the public inputs.
+        fn get_assertions(&self) -> Vec<Assertion<Self::BaseField>> {
+            // for our computation to be valid, value in column 0 at step 0 must be equal to the
+            // starting value, and at the last step it must be equal to the result.
+            let last_step = self.trace_length() - 1;
+            vec![
+                Assertion::single(0, 0, self.start),
+                Assertion::single(0, last_step, self.result),
+            ]
+        }
+
+        // This is just boilerplate which is used by the Winterfell prover/verifier to retrieve
+        // the context of the computation.
+        fn context(&self) -> &AirContext<Self::BaseField> {
+            &self.context
+        }
     }
 }
