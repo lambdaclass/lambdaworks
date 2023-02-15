028553841842c46e31a73c6d45309ba524f7c12e
Amin Arria <arria.amin@gmail.com>
2023-02-15 20:05:37 +0100

    Randomize MODULE and values

--- math/benches/field.rs
+++ math/benches/field.rs
@@ -1,59 +1,60 @@
+use const_random::const_random;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::field::fields::u64_prime_field::U64FieldElement;
 
+// Mersenne prime numbers
+// https://www.math.utah.edu/~pa/math/mersenne.html
+const PRIMES: [u64; 39] = [
+    13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279, 2203, 2281, 3217, 4253, 4423, 9689, 9941,
+    11213, 19937, 21701, 23209, 44497, 86243, 110503, 132049, 216091, 756839, 859433, 1257787,
+    1398269, 2976221, 3021377, 6972593, 13466917, 20996011, 24036583, 25964951, 30402457,
+];
+
 fn u64_benchmark(c: &mut Criterion) {
+    const MODULUS: u64 = PRIMES[const_random!(usize) % PRIMES.len()];
     let mut group = c.benchmark_group("u64");
 
     group.bench_function("add", |bench| {
-        let x: U64FieldElement<13> = FieldElement::new(2);
-        let y: U64FieldElement<13> = FieldElement::new(5);
+        let x: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
+        let y: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
         bench.iter(|| black_box(x) + black_box(y));
     });
 
     group.bench_function("mul", |bench| {
-        let x: U64FieldElement<13> = FieldElement::new(2);
-        let y: U64FieldElement<13> = FieldElement::new(5);
+        let x: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
+        let y: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
         bench.iter(|| black_box(x) * black_box(y));
     });
 
     group.bench_function("pow", |bench| {
-        let x: U64FieldElement<13> = FieldElement::new(2);
+        let x: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
         let y: u64 = 5;
         bench.iter(|| black_box(x).pow(black_box(y)));
     });
 
     group.bench_function("sub", |bench| {
-        let x: U64FieldElement<13> = FieldElement::new(2);
-        let y: U64FieldElement<13> = FieldElement::new(5);
+        let x: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
+        let y: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
         bench.iter(|| black_box(x) - black_box(y));
     });
 
-    // group.bench_function("neg", |bench| {
-    // });
-
     group.bench_function("inv", |bench| {
-        let x: U64FieldElement<13> = FieldElement::new(2);
+        let x: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
         bench.iter(|| black_box(x).inv());
     });
 
     group.bench_function("div", |bench| {
-        let x: U64FieldElement<13> = FieldElement::new(2);
-        let y: U64FieldElement<13> = FieldElement::new(5);
+        let x: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
+        let y: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
         bench.iter(|| black_box(x) / black_box(y));
     });
 
     group.bench_function("eq", |bench| {
-        let x: U64FieldElement<13> = FieldElement::new(2);
-        let y: U64FieldElement<13> = FieldElement::new(5);
+        let x: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
+        let y: U64FieldElement<MODULUS> = FieldElement::new(rand::random());
         bench.iter(|| black_box(x) == black_box(y));
     });
-
-    // group.bench_function("from_u64", |bench| {
-    // });
-
-    // group.bench_function("from_base_type", |bench| {
-    // });
 }
 
 criterion_group!(benches, u64_benchmark);
