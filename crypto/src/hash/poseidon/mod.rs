5908348ba32c4f3ab0714023e9e40d4de5d0c7b1
Nacho <amigoigna@gmail.com>
2023-02-15 20:28:05 -0300

    Finish tests

--- crypto/src/hash/poseidon/mod.rs
+++ crypto/src/hash/poseidon/mod.rs
@@ -6,20 +6,19 @@ use lambdaworks_math::field::{element::FieldElement, traits::IsField};
 use std::ops::{Add, Mul};
 
 mod parameters;
-// mod test_helper;
 
 pub struct Poseidon<F: IsField> {
     params: Parameters<F>,
 }
 
-/// Implements hashing for BLS 12381's field.
-/// Alpha = 5 and parameters are predefined for secure implementations
 impl<F: IsField> IsCryptoHash<F> for Poseidon<F> {
     fn hash_one(&self, input: FieldElement<F>) -> FieldElement<F> {
+        // return first element of the state (unwraps to be removed after trait changes to return Result<>)
         self.hash(&[input]).unwrap().first().unwrap().clone()
     }
 
     fn hash_two(&self, left: FieldElement<F>, right: FieldElement<F>) -> FieldElement<F> {
+        // return first element of the state (unwraps to be removed after trait changes to return Result<>)
         self.hash(&[left, right]).unwrap().first().unwrap().clone()
     }
 }
@@ -33,20 +32,25 @@ where
     }
 
     pub fn ark(&self, state: &mut [FieldElement<F>], round_number: usize) {
+        let state_size = state.len();
         for (i, state) in state.iter_mut().enumerate() {
-            *state += self.params.round_constants[round_number + i].clone();
+            *state += self.params.round_constants[round_number * state_size + i].clone();
         }
     }
 
     pub fn sbox(&self, state: &mut [FieldElement<F>], round_number: usize) {
-        if round_number < self.params.n_full_rounds / 2
-            || round_number >= self.params.n_full_rounds / 2 + self.params.n_partial_rounds
-        {
+        let is_full_round = round_number < self.params.n_full_rounds / 2
+            || round_number >= self.params.n_full_rounds / 2 + self.params.n_partial_rounds;
+
+        if is_full_round {
+            // full s-box
             for current_state in state.iter_mut() {
                 *current_state = current_state.pow(self.params.alpha);
             }
         } else {
-            state[0] = state[0].pow(self.params.alpha);
+            // partial s-box
+            let last_state_index = state.len() - 1;
+            state[last_state_index] = state[last_state_index].pow(self.params.alpha);
         }
     }
 
@@ -64,9 +68,8 @@ where
     }
 
     fn permute(&self, state: &mut [FieldElement<F>]) {
-        let t = self.params.capacity + self.params.rate;
         for i in 0..(self.params.n_full_rounds + self.params.n_partial_rounds) {
-            self.ark(state, i * t);
+            self.ark(state, i);
             self.sbox(state, i);
             self.mix(state);
         }
@@ -132,11 +135,11 @@ mod tests {
     #[derive(Clone, Debug)]
     pub struct TestFieldConfig;
     impl IsMontgomeryConfiguration for TestFieldConfig {
-        const MODULUS: U384 = U384::from(
-            "14474011154664525231415395255581126252639794253786371766033694892385558855681",
-        );
-        const MP: u64 = 123;
-        const R2: U384 = U384::from("123");
+        const MODULUS: U384 =
+            U384::from("2000000000000080000000000000000000000000000000000000000000000001");
+        const MP: u64 = 18446744073709551615u64;
+        const R2: U384 =
+            U384::from("C0000FFFFCFFFF800000000C0001FFFFFFFFBFFFF80000000140001FFFFE00");
     }
 
     pub type PoseidonTestField = MontgomeryBackendPrimeField<TestFieldConfig>;
@@ -163,8 +166,8 @@ mod tests {
         }
 
         Ok(Parameters {
-            rate: 1,
-            capacity: 2,
+            rate: 2,
+            capacity: 1,
             alpha: 3,
             n_full_rounds: 8,
             n_partial_rounds: 83,
@@ -175,24 +178,84 @@ mod tests {
 
     #[test]
     fn test_poseidon_bls() {
-        let poseidon = Poseidon::new(load_test_parameters().unwrap());
         let mut state = [
-            TestFieldElement::new(U384::from("7")),
-            TestFieldElement::new(U384::from("98")),
-            TestFieldElement::new(U384::from("0")),
+            TestFieldElement::new(U384::from_u64(7)),
+            TestFieldElement::new(U384::from_u64(98)),
+            TestFieldElement::new(U384::from_u64(0)),
         ];
+        let poseidon = Poseidon::new(load_test_parameters().unwrap());
+
         poseidon.ark(&mut state, 0);
-        let _expected = [
+        let expected = [
             TestFieldElement::new(U384::from(
-                "10187801339791605336251748402605479409606566396373491958667041943798551150218",
+                "16861759ea5568dd39dd92f9562a30b9e58e2ad98109ae4780b7fd8eac77fe8a",
             )),
             TestFieldElement::new(U384::from(
-                "8824452141556477327634835943439996420519135454314677708228322513850226510123",
+                "13827681995D5ADFFFC8397A3D00425A3DA43F76ABF28A64E4AB1A22F275092B",
             )),
             TestFieldElement::new(U384::from(
-                "5264468709835621148349527988912247104353814123939106227116596276180070073104",
+                "BA3956D2FAD4469E7F760A2277DC7CB2CAC75DC279B2D687A0DBE17704A8310",
             )),
         ];
-        // this needs to be asserted once the field definition is in place: assert_eq!(state, expected);
+        assert_eq!(state, expected);
+    }
+
+    #[test]
+    fn test_mix() {
+        let mut state = [
+            TestFieldElement::new(U384::from(
+                "13f891b043b3b740cc3e1b3051127d335f08e488322f360a776b3810b7dc690a",
+            )),
+            TestFieldElement::new(U384::from(
+                "1bd24b7cb99acf0dbea719ff4007bd60105bcefef21ec509d2f8d4f9bb6a3a1a",
+            )),
+            TestFieldElement::new(U384::from(
+                "110853eb2ebee0d940454fe420229a2a0974e666d16c92bab9f36cbd1a0eded",
+            )),
+        ];
+
+        let poseidon = Poseidon::new(load_test_parameters().unwrap());
+
+        poseidon.mix(&mut state);
+
+        let expected = [
+            TestFieldElement::new(U384::from(
+                "1d30b34b465f8cddc8dc468f137891659c7e32b510cf41cec3aac0b26741681d",
+            )),
+            TestFieldElement::new(U384::from(
+                "c445fa4dd2af583994272bede589b06b98fe9cd6d868bf718f6748ba6165620",
+            )),
+            TestFieldElement::new(U384::from(
+                "1ed95ae0ea03bb892691f5200fb5902957ac17b3466afa62be808682801f97f9",
+            )),
+        ];
+        assert_eq!(state, expected);
+    }
+
+    #[test]
+    fn test_permutation() {
+        let poseidon = Poseidon::new(load_test_parameters().unwrap());
+
+        let mut state = [
+            TestFieldElement::new(U384::from_u64(7)),
+            TestFieldElement::new(U384::from_u64(98)),
+            TestFieldElement::new(U384::from_u64(0)),
+        ];
+
+        poseidon.permute(&mut state);
+
+        let expected = [
+            TestFieldElement::new(U384::from(
+                "18700783647721BB9AD092B176BBEB5348401C21132CCF83C30134DFAB5A2DEB",
+            )),
+            TestFieldElement::new(U384::from(
+                "1CC8856652601B3C81139AD5EC13E4A3A8F4A5DB242555521A09E002E7A10B2B",
+            )),
+            TestFieldElement::new(U384::from(
+                "3DCB1CEC811FC2D7401CA7B9B084D167F33B6983D4428C8E0534C9C3CECF46D",
+            )),
+        ];
+
+        assert_eq!(state, expected);
     }
 }
