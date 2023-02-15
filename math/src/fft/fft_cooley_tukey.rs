c5c711e30053a8703a7a24ec4fc6f8e01f0ad446
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-02-15 10:41:07 -0300

    Fixed proptests

--- math/src/fft/fft_cooley_tukey.rs
+++ math/src/fft/fft_cooley_tukey.rs
@@ -61,7 +61,6 @@ pub fn inverse_cooley_tukey<F: IsField>(
 mod fft_test {
     use crate::field::test_fields::u64_test_field::U64TestField;
     use crate::polynomial::Polynomial;
-    use proptest::collection::vec;
     use proptest::prelude::*;
 
     use super::*;
@@ -71,30 +70,23 @@ mod fft_test {
     type FE = FieldElement<F>;
 
     prop_compose! {
-        fn powers_of_two(max_exp: u8)(exp in 0..max_exp) -> u64 { 1 << exp }
-        // max_exp cannot be multiple of 64.
+        fn powers_of_two(max_exp: u8)(exp in 2..max_exp) -> usize { 1 << exp }
+        // max_exp cannot be multiple of the bits that represent a usize, generally 64 or 32.
     }
     prop_compose! {
-        fn field_vec(max_exp: u8)(_pow in powers_of_two(max_exp)) -> Vec<FE> {
-            todo!()
-        }
+        fn field_element()(num in any::<u64>()) -> FE { FE::from(num) }
     }
-
-    // Generates a suitable vector of polynomial coefficients to perform FFT to.
-    // FIXME: this will generate non-powers-of-two polys. Replace with field_vec().
-    fn gen_vec_strategy(max_size: usize) -> BoxedStrategy<Vec<FE>> {
-        let field_element = any::<u64>().prop_map(FE::from);
-        vec(field_element, ..max_size)
-            .prop_filter("size can only be a power of two", |vec| {
-                vec.len().is_power_of_two()
-            })
-            .boxed()
+    prop_compose! {
+        fn field_vec(max_exp: u8)(elem in field_element(), size in powers_of_two(max_exp)) -> Vec<FE> {
+            vec![elem; size]
+        }
     }
 
     proptest! {
         // Property-based test that ensures FFT gives same result as a naive polynomial evaluation.
         #[test]
-        fn test_fft_matches_naive_evaluation(coeffs in gen_vec_strategy(1024)) {
+        fn test_fft_matches_naive_evaluation(coeffs in field_vec(8)) {
+
             let poly = Polynomial::new(&coeffs[..]);
             let omega = F::get_root_of_unity(log2(poly.coefficients().len()).unwrap()).unwrap();
 
@@ -110,7 +102,7 @@ mod fft_test {
     proptest! {
         // Property-based test that ensures IFFT is the inverse operation of FFT.
         #[test]
-        fn test_ifft_composed_fft_is_identity(coeffs in gen_vec_strategy(1024)) {
+        fn test_ifft_composed_fft_is_identity(coeffs in field_vec(8)) {
             let poly = Polynomial::new(&coeffs[..]);
 
             let result = fft(poly.coefficients()).unwrap();
