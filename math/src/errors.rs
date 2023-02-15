c61a20633461d62cde5a1bafc0dea05edda19453
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-15 11:07:51 -0300

    Remove unused errors

--- math/src/errors.rs
+++ math/src/errors.rs
@@ -6,10 +6,4 @@ pub enum ByteConversionError {
     FromBEBytesError,
     #[error("from_le_bytes failed")]
     FromLEBytesError,
-    #[error("to_be_bytes failed")]
-    ToBEBytesError,
-    #[error("to_le_bytes failed")]
-    ToLEBytesError,
-    #[error("{0}")]
-    Custom(String),
 }
