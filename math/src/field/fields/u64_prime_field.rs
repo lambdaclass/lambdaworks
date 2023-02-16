a95c4efc487cb537e302b64ac8a90138cf00238d
fran <franfiuba@gmail.com>
2023-02-16 16:02:47 -0300

    fix

--- math/src/field/fields/u64_prime_field.rs
+++ math/src/field/fields/u64_prime_field.rs
@@ -1,7 +1,9 @@
 use crate::cyclic_group::IsGroup;
 use crate::elliptic_curve::traits::HasPairing;
+use crate::errors::ByteConversionError::{FromBEBytesError, FromLEBytesError};
 use crate::field::element::FieldElement;
 use crate::field::traits::IsField;
+use crate::traits::ByteConversion;
 
 /// Type representing prime fields over unsigned 64-bit integers.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -71,6 +73,26 @@ impl<const MODULUS: u64> IsGroup for U64FieldElement<MODULUS> {
 
     fn operate_with(&self, other: &Self) -> Self {
         *self + *other
+    }
+}
+
+impl<const MODULUS: u64> ByteConversion for U64FieldElement<MODULUS> {
+    fn to_bytes_be(&self) -> Vec<u8> {
+        u64::to_be_bytes(*self.value()).into()
+    }
+
+    fn to_bytes_le(&self) -> Vec<u8> {
+        u64::to_le_bytes(*self.value()).into()
+    }
+
+    fn from_bytes_be(bytes: &[u8]) -> Result<Self, crate::errors::ByteConversionError> {
+        let bytes: [u8; 8] = bytes.try_into().map_err(|_| FromBEBytesError)?;
+        Ok(Self::from(u64::from_be_bytes(bytes)))
+    }
+
+    fn from_bytes_le(bytes: &[u8]) -> Result<Self, crate::errors::ByteConversionError> {
+        let bytes: [u8; 8] = bytes.try_into().map_err(|_| FromLEBytesError)?;
+        Ok(Self::from(u64::from_le_bytes(bytes)))
     }
 }
 
@@ -222,5 +244,29 @@ mod tests {
         let a = FE::new(3);
         let b = FE::new(12);
         assert_eq!(a * b, U64FieldPairing::pairing(&a, &b));
+    }
+
+    #[test]
+    fn to_bytes_from_bytes_be_is_the_identity() {
+        let x = FE::new(12345);
+        assert_eq!(FE::from_bytes_be(&x.to_bytes_be()).unwrap(), x);
+    }
+
+    #[test]
+    fn from_bytes_to_bytes_be_is_the_identity_for_one() {
+        let bytes = vec![0, 0, 0, 0, 0, 0, 0, 1];
+        assert_eq!(FE::from_bytes_be(&bytes).unwrap().to_bytes_be(), bytes);
+    }
+
+    #[test]
+    fn to_bytes_from_bytes_le_is_the_identity() {
+        let x = FE::new(12345);
+        assert_eq!(FE::from_bytes_le(&x.to_bytes_le()).unwrap(), x);
+    }
+
+    #[test]
+    fn from_bytes_to_bytes_le_is_the_identity_for_one() {
+        let bytes = vec![1, 0, 0, 0, 0, 0, 0, 0];
+        assert_eq!(FE::from_bytes_le(&bytes).unwrap().to_bytes_le(), bytes);
     }
 }
