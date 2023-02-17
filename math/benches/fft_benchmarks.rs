db1339771b0e59fefee286960fcba595aeeda0d2
Amin Arria <arria.amin@gmail.com>
2023-02-17 15:27:28 +0100

    Benchmark for fft

--- math/benches/fft_benchmarks.rs
+++ math/benches/fft_benchmarks.rs
@@ -2,11 +2,10 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 mod benchmarks;
 
-fn run_all_benchmarks(c: &mut Criterion) {
-    benchmarks::field::u64_benchmark(c);
+fn run_fft_benchmarks(c: &mut Criterion) {
     benchmarks::fft::fft_benchmark(c);
     benchmarks::fft::inverse_fft_benchmark(c);
 }
 
-criterion_group!(benches, run_all_benchmarks);
+criterion_group!(benches, run_fft_benchmarks);
 criterion_main!(benches);
