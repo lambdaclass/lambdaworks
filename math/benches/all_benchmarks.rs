462ee45048a418aebcb91a60316c18ca5844e708
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-17 17:29:58 -0300

    Restructured benches module

---
+++ math/benches/all_benchmarks.rs
@@ -0,0 +1,10 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+mod benchmarks;
+
+fn run_all_benchmarks(c: &mut Criterion) {
+    benchmarks::field::u64_benchmark(c);
+}
+
+criterion_group!(benches, run_all_benchmarks);
+criterion_main!(benches);
