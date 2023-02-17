2031aace67917f983925231b03b4c158aa415925
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-17 17:51:38 -0300

    Add flamegraph config

--- math/benches/field_benchmarks.rs
+++ math/benches/field_benchmarks.rs
@@ -1,4 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+#[cfg(feature = "benchmark_flamegraph")]
+use pprof::criterion::PProfProfiler;
 
 mod benchmarks;
 
@@ -6,5 +8,12 @@ fn run_field_benchmarks(c: &mut Criterion) {
     benchmarks::field::u64_benchmark(c);
 }
 
+#[cfg(feature = "benchmark_flamegraph")]
+criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler(PProfProfiler::new(100, pprof::criterion::Output::Flamegraph(None)));
+    targets = run_field_benchmarks
+}
+#[cfg(all(not(feature = "benchmark_flamegraph")))]
 criterion_group!(benches, run_field_benchmarks);
 criterion_main!(benches);
