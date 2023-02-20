6540e2dd5337b6756497bf368aa69b7c9f9388be
Amin Arria <arria.amin@gmail.com>
2023-02-20 21:17:04 +0100

    Add benchmark for polynomial

--- math/benches/polynomial_benchmarks.rs
+++ math/benches/polynomial_benchmarks.rs
@@ -2,10 +2,9 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 mod benchmarks;
 
-fn run_all_benchmarks(c: &mut Criterion) {
-    benchmarks::field::u64_benchmark(c);
+fn run_polynomial_benchmarks(c: &mut Criterion) {
     benchmarks::polynomial::polynomial_benchmark(c);
 }
 
-criterion_group!(benches, run_all_benchmarks);
+criterion_group!(benches, run_polynomial_benchmarks);
 criterion_main!(benches);
