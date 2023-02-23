f343e1a22816c0971a2da4f43c2a35e2abba296e
Javier Chatruc <jrchatruc@gmail.com>
2023-02-23 15:18:52 -0300

    Start filling the stark proof struct

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -23,7 +23,10 @@ type U384FieldElement = FieldElement<U384PrimeField>;
 const MODULUS_MINUS_1: U384 =
     U384::from("800000000000011000000000000000000000000000000000000000000000000");
 
-pub fn generate_vec_roots(subgroup_size: u64, coset_factor: u64) -> Vec<U384FieldElement> {
+pub fn generate_vec_roots(
+    subgroup_size: u64,
+    coset_factor: u64,
+) -> (Vec<U384FieldElement>, U384FieldElement) {
     let MODULUS_MINUS_1_FIELD: U384FieldElement = U384FieldElement::new(MODULUS_MINUS_1);
     let subgroup_size_u384: U384FieldElement = subgroup_size.into();
     let generator_field: U384FieldElement = 3.into();
@@ -41,17 +44,21 @@ pub fn generate_vec_roots(subgroup_size: u64, coset_factor: u64) -> Vec<U384Fiel
         numbers.push(ret.clone());
     }
 
-    numbers
+    (numbers, generator_of_subgroup)
 }
 
 #[derive(Debug)]
 pub struct StarkProof {
     // TODO: fill this when we know what a proof entails
+    trace_root: U384FieldElement,
+    composition_poly_root: U384FieldElement,
+    // fri_layer_roots: Vec<U384FieldElement>,
 }
 
 pub use lambdaworks_crypto::merkle_tree::{DefaultHasher, MerkleTree};
 pub type FriMerkleTree = MerkleTree<U384PrimeField, DefaultHasher>;
 
+// TODO: Our Fiat Shamir implementation is not even close to providing what we need for proving/verifying.
 pub fn prove<A>(air: A, trace: TraceTable<A::BaseField>, pub_inputs: A::PublicInputs) -> StarkProof
 where
     A: Air<BaseField = BaseElement>,
@@ -61,29 +68,52 @@ where
     let (mut composition_poly, mut trace_poly) = get_cp_and_tp(air, trace, pub_inputs).unwrap();
 
     // * Generate Coset
-    let roots_of_unity = crate::generate_vec_roots(1024, 1);
+    let (roots_of_unity, primitive_root) = crate::generate_vec_roots(1024, 1);
 
     // * Do Reed-Solomon on the trace and composition polynomials using some blowup factor
     let composition_poly_lde = composition_poly.evaluate_slice(roots_of_unity.as_slice());
+    let trace_poly_lde = trace_poly.evaluate_slice(roots_of_unity.as_slice());
 
     // * Commit to both polynomials using a Merkle Tree
-    let commited_poly_lde = FriMerkleTree::build(composition_poly_lde.as_slice());
+    let commited_composition_poly_lde = FriMerkleTree::build(composition_poly_lde.as_slice());
+    let commited_trace_poly_lde = FriMerkleTree::build(&trace_poly_lde.as_slice());
 
     // * Do FRI on the composition polynomials
     let lde_fri_commitment = crate::fri::fri(&mut composition_poly, &roots_of_unity);
 
     // * Sample q_1, ..., q_m using Fiat-Shamir
     // let q_1 = transcript.challenge();
-    // TODO: Do this with Fiat Shamir
     let q_1: usize = rand::random();
 
     // * For every q_i, do FRI decommitment
     let decommitment = fri_decommit_layers(&lde_fri_commitment, q_1, &mut transcript);
 
     // * For every trace polynomial t_i, provide the evaluations on every q_i, q_i * g, q_i * g^2
+    let evaluation_points = vec![
+        U384FieldElement::from(q_1 as u64),
+        U384FieldElement::from(q_1 as u64) * primitive_root.clone(),
+        U384FieldElement::from(q_1 as u64) * primitive_root.pow(2_usize),
+    ];
 
-    StarkProof {}
+    let trace_evaluations = trace_poly.evaluate_slice(&evaluation_points);
+
+    let trace_root = commited_trace_poly_lde.root.borrow().clone().hash;
+    let composition_poly_root = commited_composition_poly_lde.root.borrow().clone().hash;
+
+    StarkProof {
+        trace_root,
+        composition_poly_root,
+    }
 }
+
+/*
+- Trace merkle tree root
+- Composition poly merkle tree root
+- Fri Layers merkle tree roots
+- Trace Polys merkle paths
+- Composition poly merkle paths
+- Fri layer polys evaluations (in the q_i's and -q_i's)
+*/
 
 pub fn verify() {}
 
