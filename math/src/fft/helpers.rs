1296329406d9040ff947ebc8cefa4dd3b9228e89
Nacho Avecilla <iavecilla@fi.uba.ar>
2023-02-27 08:26:53 -0300

    FFT operation wrappers and evaluation with coset (#93)
    * Add wrappers for fft related operations
    
    * Add function to scale polynomials
    
    * Add property tests for new fft with cosets
    
    * Fix error message for invalid polynomial order
    
    * Fix test (cooley-tukey implementation did not always work)
    
    * Move tests to fft operations file
    
    * Add `blowup_factor` to increase the domain evaluations on ftt.
    
    * Fix test and parameter type for interpolation
    
    * Add newline in gitignore
    
    ---------
    
    Co-authored-by: Javier Chatruc <jrchatruc@gmail.com>
    Co-authored-by: Martin Paulucci <martin.paulucci@lambdaclass.com>

--- math/src/fft/helpers.rs
+++ math/src/fft/helpers.rs
@@ -3,7 +3,7 @@ use super::errors::FFTError;
 pub fn log2(n: usize) -> Result<u64, FFTError> {
     if !n.is_power_of_two() {
         return Err(FFTError::InvalidOrder(
-            "The order of polynomial should a be power of 2".to_string(),
+            "The order of polynomial + 1 should a be power of 2".to_string(),
         ));
     }
     Ok(n.trailing_zeros() as u64)
