4a413692095307daceb5cd77230beab0bed67970
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-02-27 17:56:13 -0300

    Added NR algorithm

--- math/src/fft/fft_cooley_tukey.rs
+++ math/src/fft/fft_cooley_tukey.rs
@@ -29,6 +29,49 @@ pub fn inverse_fft<F: IsField + IsTwoAdicField>(
     Ok(inverse_cooley_tukey(evaluations, omega))
 }
 
+/// In-Place Radix-2 NR DIT FFT algorithm on a slice of type `E` elements.
+/// It's required that the twiddle factors are in bit-reverse order. Else this function will not
+/// return fourier transformed values.
+///
+/// Performs a fast fourier transform with the next attributes:
+/// - In-Place: an auxiliary vector of data isn't needed for the algorithm.
+/// - Radix-2: two elements are operated on by the same thread
+/// - NR: natural to reverse order, meaning that the input is naturally ordered and the output will
+/// be bit-reversed ordered.
+/// - DIT: decimation in time
+fn in_place_nr_2radix_fft<F>(input: &mut Vec<FieldElement<F>>, twiddles: &[FieldElement<F>])
+where
+    F: IsTwoAdicField
+{
+    // divide input in groups, starting with 1, duplicating the number of groups in each stage.
+    let mut group_count = 1;
+    let mut group_size = input.len();
+
+    // for each group, there'll be group_size / 2 butterflies.
+    // a butterfly is the atomic operation of a FFT, e.g: (a, b) = (a + wb, a - wb).
+    // The 0.5 factor is what gives FFT its performance, it recursively halves the problem size
+    // (group size).
+
+    while group_count < input.len() {
+        for group in 0..group_count - 1 {
+            let first_in_group = group * group_size;
+            let last_in_group = first_in_group + group_size/2 - 1;
+
+            for i in first_in_group..=last_in_group {
+                let w = &twiddles[group];
+                let y0 = &input[i] + w * &input[i + group_size/2];
+                let y1 = &input[i] - w * &input[i + group_size/2];
+
+                input[i] = y0;
+                input[i + group_size/2] = y1;
+            }
+        }
+
+        group_count *= 2;
+        group_size /= 2;
+    }
+}
+
 fn cooley_tukey<F: IsField>(
     coeffs: &[FieldElement<F>],
     omega: &FieldElement<F>,
