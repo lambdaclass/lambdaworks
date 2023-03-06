69bcc8539f32175555e8d85434149b45e34d4b70
ajgara <ajgarassino@gmail.com>
2023-03-06 11:36:14 -0300

    Add bls12381 pairing (#121)
    * add bls12 381 twist curve
    
    * add twist curve for bls 12 381
    
    * WIP for the ate pairing for BLS12 381
    
    * add tests fp12 and line
    
    * Ate pairing works for simple test of bilinearity in BLS12 381
    
    * Use frobenius trace instead of r
    
    * Optimize final exponentiation of ate pairing
    
    * Remove old tests
    
    * Fix merge
    
    * Small changes to docstrings
    
    * make ate pairing functions public. Remove unnecesary test curve over Fp12
    
    * small optimization to quadratic extension multiplication and to_fp12_affine
    
    * add karatsuba multiplication for the cubic extension
    
    * Remove old pairings
    
    * Defining equation takes affine points
    
    * Fix clippy and format
    
    ---------
    
    Co-authored-by: Sergio Chouhy <schouhy@gmail.com>
    Co-authored-by: ajgarassino <ajgarassino@eryx.co>
    Co-authored-by: Sergio Chouhy <sergio.chouhy@gmail.com>

--- math/src/cyclic_group.rs
+++ math/src/cyclic_group.rs
@@ -1,3 +1,5 @@
+use crate::unsigned_integer::traits::IsUnsignedInteger;
+
 pub trait IsGroup: Clone + PartialEq + Eq {
     /// Returns the neutral element of the group. The equality
     /// `neutral_element().operate_with(g) == g` must hold
@@ -12,15 +14,15 @@ pub trait IsGroup: Clone + PartialEq + Eq {
     /// Applies the group operation `times` times with itself
     /// The operation can be addition or multiplication depending on
     /// the notation of the particular group.
-    fn operate_with_self(&self, mut exponent: u128) -> Self {
+    fn operate_with_self<T: IsUnsignedInteger>(&self, mut exponent: T) -> Self {
         let mut result = Self::neutral_element();
         let mut base = self.clone();
 
-        while exponent > 0 {
-            if exponent & 1 == 1 {
+        while exponent > T::from(0) {
+            if exponent & T::from(1) == T::from(1) {
                 result = Self::operate_with(&result, &base);
             }
-            exponent >>= 1;
+            exponent = exponent >> 1;
             base = Self::operate_with(&base, &base);
         }
         result
