15a4d70e45d59d7f8055bf8f2314e04b54dec864
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-03-07 17:34:57 -0300

    [WIP] Tried to fix fft with cosets

--- math/src/fft/fft_cooley_tukey.rs
+++ math/src/fft/fft_cooley_tukey.rs
@@ -1,7 +1,7 @@
 use crate::field::{element::FieldElement, traits::IsTwoAdicField};
 
 use super::{
-    bit_reversing::in_place_bit_reverse_permute,
+    bit_reversing::{in_place_bit_reverse_permute, reverse_index},
     errors::FFTError,
     fft_iterative::in_place_nr_2radix_fft,
     helpers::log2,
@@ -12,17 +12,32 @@ use super::{
 /// fast polynomial evaluation.
 pub fn fft_with_blowup<F: IsTwoAdicField>(
     coeffs: &[FieldElement<F>],
+    domain_offset: &FieldElement<F>,
     blowup_factor: usize,
 ) -> Result<Vec<FieldElement<F>>, FFTError> {
-    let domain_size = coeffs.len() * blowup_factor;
-    let order = log2(domain_size)?;
+    let n = coeffs.len();
+    let order = log2(n)?;
+    let domain_size = n * blowup_factor;
+    let domain_order = log2(domain_size)?;
     let twiddles = gen_twiddles_bit_reversed(order)?;
 
     let mut results = coeffs.to_vec();
     results.resize(domain_size, FieldElement::zero());
 
-    in_place_nr_2radix_fft(&mut results[..], &twiddles[..]);
-    in_place_bit_reverse_permute(&mut results);
+    for (i, chunk) in results.chunks_mut(n).enumerate() {
+        let reversed_i = reverse_index(&i, blowup_factor as u64);
+        let root = F::get_root_of_unity(domain_order)?;
+        let offset = root.pow(reversed_i) * domain_offset;
+        let mut factor: FieldElement<F> = 1.into();
+
+        for (chunk_elem, coeff) in chunk.iter_mut().zip(coeffs.iter()) {
+            *chunk_elem = coeff * &factor;
+            factor = factor * &offset;
+        }
+        in_place_nr_2radix_fft(&mut chunk[..], &twiddles[..]);
+    }
+
+    in_place_bit_reverse_permute(&mut results[..]);
 
     Ok(results)
 }
