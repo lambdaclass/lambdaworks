6be4cd5a6846e2e513cb9c4bee068d478b44eb02
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-03-08 15:03:34 -0300

    Added edge case test for bit reverse permutation

--- math/src/fft/bit_reversing.rs
+++ math/src/fft/bit_reversing.rs
@@ -39,4 +39,15 @@ mod test {
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
         );
     }
+
+    #[test]
+    fn bit_reverse_permutation_edge_case() {
+        let mut edge_case = [0];
+
+        in_place_bit_reverse_permute(&mut edge_case[..]);
+        assert_eq!(
+            edge_case[..],
+            [0]
+        );
+    }
 }
