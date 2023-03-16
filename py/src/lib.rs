eb6f8c113fa3e5c0d375f3fa0bd44c81dd68046e
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2023-03-16 18:20:19 -0300

    Implement wrapper for prove and verify

--- py/src/lib.rs
+++ py/src/lib.rs
@@ -1,6 +1,6 @@
 use lambdaworks_math::unsigned_integer::element::U256 as U256Internal;
-use lambdaworks_stark::FE;
-use pyo3::prelude::*;
+use lambdaworks_stark::{ProofConfig as ProofConfigInternal, StarkProof as StarkProofInternal, FE};
+use pyo3::{prelude::*, types::PyList};
 
 #[pyclass]
 pub struct U256(U256Internal);
@@ -14,6 +14,7 @@ impl U256 {
 }
 
 #[pyclass]
+#[derive(Clone)]
 pub struct FieldElement(FE);
 
 #[pymethods]
@@ -28,10 +29,53 @@ impl FieldElement {
     }
 }
 
+#[pyclass]
+pub struct ProofConfig(ProofConfigInternal);
+
+#[pymethods]
+impl ProofConfig {
+    #[new]
+    pub fn new(count_queries: usize, blowup_factor: usize) -> Self {
+        Self(ProofConfigInternal {
+            count_queries,
+            blowup_factor,
+        })
+    }
+}
+
+#[pyclass]
+pub struct StarkProof(StarkProofInternal);
+
+#[pyfunction]
+fn prove(trace: &PyList, proof_config: &ProofConfig) -> PyResult<StarkProof> {
+    // FIXME is there a better way of taking a list of FieldElements as parameters?
+    let trace = {
+        let mut v: Vec<FE> = Vec::with_capacity(trace.len());
+        for pyelem in trace {
+            let fe = FieldElement::extract(pyelem)?;
+            v.push(fe.0);
+        }
+        v
+    };
+
+    Ok(StarkProof(lambdaworks_stark::prove(
+        &trace,
+        &proof_config.0,
+    )))
+}
+
+#[pyfunction]
+fn verify(stark_proof: &StarkProof) -> bool {
+    lambdaworks_stark::verify(&stark_proof.0)
+}
+
 #[pymodule]
 fn lambdaworks_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<U256>()?;
     m.add_class::<FieldElement>()?;
+    m.add_class::<ProofConfig>()?;
+    m.add_function(wrap_pyfunction!(prove, m)?)?;
+    m.add_function(wrap_pyfunction!(verify, m)?)?;
     Ok(())
 }
 
--- py/tests/lamdaworks_py_test.py
+++ py/tests/lamdaworks_py_test.py
@@ -12,6 +12,24 @@ class TestU256(unittest.TestCase):
 class TestFieldElement(unittest.TestCase):
     def test_constructor(self):
         self.assertTrue(FieldElement(U256("1")))
+        
+class TestFibonacci(unittest.TestCase):
+    @classmethod
+    def _fibonacci_trace(cls, v0, v1):
+        N = 32
+        l = [v0, v1]
+        for i in range(2, N):
+            l.append(l[i - 1] + l[i - 2])
+        return l
+
+    def test_prove(self):
+        trace = self.__class__._fibonacci_trace(FieldElement(U256("1")), FieldElement(U256("1")))
+
+        proof_config = ProofConfig(count_queries=30, blowup_factor=4)
+        result = prove(trace, proof_config)
+
+        self.assertTrue(verify(result))
+    
 
 if __name__ == '__main__':
     unittest.main()
