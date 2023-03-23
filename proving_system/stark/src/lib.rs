43cc88803b5c5b86b393ed6c1d55e7adae7018db
Mariano A. Nicolini <mariano.nicolini.91@gmail.com>
2023-03-23 10:20:29 -0300

    Refactor compute_deep_composition_poly function (#200)
    * [WIP] Deep FRI
    
    * Finish implementation
    
    * Document code
    
    * Fix vulnerability tests and add more
    
    * Define basic traits and structs for Air
    
    * Write basic skeleton of ConstraintEvaluator
    
    * Save evaluator progress
    
    * Complete evaluator basic methods
    
    * Fix some compilation errors
    
    * Finish method for computing composition poly from Evaluation Table
    
    * More fixes
    
    * More progress
    
    * Comment out tests
    
    * [WIP] Implement verifier
    
    * Debugging stuff
    
    * Remove debugging stuff as the boundary ood evaluation was correct
    
    * Uncomment important line
    
    * Start compute_deep_composition_poly refactor
    
    * Finish compute_deep_composition_poly refactor
    
    * Refactor trace function to take the length as a parameter
    
    * Fix hardcoded boundary poly degree on verifier
    
    * Some fixes
    
    * Fix transition_divisors implementation
    
    * Fix transition_divisor once more
    
    * Fix calculation of trace primitive root on the verifier
    
    * Test finally working
    
    * Remove unusued num_assertions field
    
    * [WIP] fiat-shamir integration
    
    * Comment transcript.append line in fri function
    
    * Fix fiat-shamir integration bugs
    
    * Fix boundaryconstraints zerofier test
    
    * Fix clippy suggestiojns
    
    * Remove outdated comments
    
    * Rename compute_transition_evaluations
    
    * Add Fibonacci AIR test for a 17 finite field
    
    * Save refactor WIP
    
    * Refactor compute_deep_composition_poly to generalize for a trace with more than one column
    
    * Add comments to dummy transcript challenge calls in verifier
    
    * Use a better variable name
    
    * Remove debug comment
    
    * Add comments and documentation
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Javier Chatruc <jrchatruc@gmail.com>

--- proving_system/stark/src/lib.rs
+++ proving_system/stark/src/lib.rs
@@ -85,6 +85,10 @@ mod tests {
     #[test]
     fn test_prove_fib() {
         let trace = fibonacci_trace([FE::from(1), FE::from(1)], 4);
+        let trace_table = TraceTable {
+            table: trace.clone(),
+            num_cols: 1,
+        };
 
         let context = AirContext {
             options: ProofOptions {
@@ -93,16 +97,11 @@ mod tests {
                 coset_offset: 3,
             },
             trace_length: trace.len(),
-            trace_info: (trace.len(), 1),
+            trace_columns: trace_table.num_cols,
             transition_degrees: vec![1],
             transition_exemptions: vec![trace.len() - 2, trace.len() - 1],
             transition_offsets: vec![0, 1, 2],
             num_transition_constraints: 1,
-        };
-
-        let trace_table = TraceTable {
-            table: trace.clone(),
-            num_cols: 1,
         };
 
         let fibonacci_air = FibonacciAIR::new(trace_table, context);
@@ -116,6 +115,11 @@ mod tests {
     fn test_prove_fib17() {
         let trace = fibonacci_trace([FE17::new(1), FE17::new(1)], 4);
 
+        let trace_table = TraceTable {
+            table: trace.clone(),
+            num_cols: 1,
+        };
+
         let context = AirContext {
             options: ProofOptions {
                 blowup_factor: 2,
@@ -123,16 +127,11 @@ mod tests {
                 coset_offset: 3,
             },
             trace_length: trace.len(),
-            trace_info: (trace.len(), 1),
+            trace_columns: trace_table.num_cols,
             transition_degrees: vec![1],
             transition_exemptions: vec![trace.len() - 2, trace.len() - 1],
             transition_offsets: vec![0, 1, 2],
             num_transition_constraints: 1,
-        };
-
-        let trace_table = TraceTable {
-            table: trace.clone(),
-            num_cols: 1,
         };
 
         let fibonacci_air = Fibonacci17AIR::new(trace_table, context);
--- proving_system/stark/src/verifier.rs
+++ proving_system/stark/src/verifier.rs
@@ -116,6 +116,18 @@ where
     let lde_root_order =
         (air.context().trace_length * air.options().blowup_factor as usize).trailing_zeros();
 
+    // We have to make the call to `transcript.challenge()` a number of times since we need
+    // the transcript to be in the same state as the one in the prover at this stage.
+    // The prover samples coefficients when building the deep composition polynomial. These
+    // sampling is not done in the verifier hence we need to make this dummy calls.
+    // There will be one call for each trace term in the deep composition polynomial + 2 from
+    // the even and odd terms of the H(x) polynomial.
+    let deep_poly_challenges =
+        air.context().transition_offsets.len() * air.context().trace_columns + 2;
+    (0..deep_poly_challenges).for_each(|_| {
+        transcript.challenge();
+    });
+
     // construct vector of betas
     let mut beta_list = Vec::new();
     let count_betas = proof.fri_layers_merkle_roots.len() - 1;
@@ -137,7 +149,7 @@ where
         let last_evaluation_bytes = last_evaluation.to_bytes_be();
         transcript.append(&last_evaluation_bytes);
 
-        let q_i: usize = transcript_to_usize(transcript) % (2_usize.pow(lde_root_order));
+        let q_i = transcript_to_usize(transcript) % (2_usize.pow(lde_root_order));
         transcript.append(&q_i.to_be_bytes());
 
         let fri_decommitment = &proof_i.fri_decommitment;
