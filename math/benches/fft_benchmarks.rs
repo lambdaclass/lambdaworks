d829c8aea40145801b55b887fb929af2315426b3
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-04-11 16:43:09 -0300

    Reverted removal of math all_benchmarks

--- math/benches/fft_benchmarks.rs
+++ math/benches/fft_benchmarks.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 mod benchmarks;
 
-fn run_math_fft_benchmarks(c: &mut Criterion) {
+fn run_fft_benchmarks(c: &mut Criterion) {
     benchmarks::fft::bitrev_permutation_benchmarks(c);
     benchmarks::fft::twiddles_benchmarks(c);
     benchmarks::fft::fft_benchmarks(c);
@@ -10,5 +10,5 @@ fn run_math_fft_benchmarks(c: &mut Criterion) {
     benchmarks::fft::poly_interpolate_fft_benchmarks(c);
 }
 
-criterion_group!(benches, run_math_fft_benchmarks);
+criterion_group!(benches, run_fft_benchmarks);
 criterion_main!(benches);
