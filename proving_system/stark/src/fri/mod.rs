897b08b8a841ff23d46e543a896fe06636c6f215
Javier Chatruc <jrchatruc@gmail.com>
2023-04-11 08:49:21 -0300

    Address comments

--- proving_system/stark/src/fri/mod.rs
+++ proving_system/stark/src/fri/mod.rs
@@ -78,8 +78,8 @@ where
     fri_commitment_list.push(commitment_0);
     let mut degree = p_0.degree();
 
-    let mut last_coef = last_poly.coefficients.get(0).unwrap().clone();
     let zero = FieldElement::zero();
+    let mut last_coef = last_poly.coefficients.get(0).unwrap_or(&zero);
 
     while degree > 0 {
         let beta = transcript_to_field(transcript);
@@ -98,7 +98,7 @@ where
         degree = p_i.degree();
 
         last_poly = p_i.clone();
-        last_coef = last_poly.evaluate(&zero);
+        last_coef = last_poly.coefficients.get(0).unwrap_or(&zero);
 
         last_domain = domain_i.clone();
     }
