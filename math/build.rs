0339634912e80ac940ea04a2543bfe827125add3
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-04-17 16:55:44 -0300

    changes to maike it compile for default target

--- math/build.rs
+++ math/build.rs
@@ -1,6 +1,9 @@
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
-    embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
+    #[cfg(feature = "esp")]
+    {
+        embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
+        embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
+    }
     Ok(())
 }
