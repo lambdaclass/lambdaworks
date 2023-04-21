f5ac4bc7ece119087a6249b9efb790015688243d
github-action-benchmark <github@users.noreply.github.com>
2023-04-21 14:24:16 +0000

    add Benchmark (cargo) benchmark result for d0b0bc96abccd0ef571069a2ad29b905f3c1e461

---
+++ bench/data.js
@@ -0,0 +1,140 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1682087055936,
+  "repoUrl": "https://github.com/lambdaclass/lambdaworks",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "67517699+ilitteri@users.noreply.github.com",
+            "name": "Ivan Litteri",
+            "username": "ilitteri"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d0b0bc96abccd0ef571069a2ad29b905f3c1e461",
+          "message": "Fix typos in `api.md` (#264)",
+          "timestamp": "2023-04-21T14:18:16Z",
+          "tree_id": "8d8c80e4608dd9df1d4e731b545e0152a9db58e3",
+          "url": "https://github.com/lambdaclass/lambdaworks/commit/d0b0bc96abccd0ef571069a2ad29b905f3c1e461"
+        },
+        "date": 1682087055350,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "u64/add",
+            "value": 10,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "u64/mul",
+            "value": 10,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "u64/pow",
+            "value": 53,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "u64/sub",
+            "value": 10,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "u64/inv",
+            "value": 315,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "u64/div",
+            "value": 323,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "u64/eq",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "polynomial/evaluate",
+            "value": 2666,
+            "range": "± 1796",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "polynomial/evaluate_slice",
+            "value": 312190,
+            "range": "± 405615",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "polynomial/add",
+            "value": 3093,
+            "range": "± 707",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "polynomial/neg",
+            "value": 571,
+            "range": "± 252",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "polynomial/sub",
+            "value": 3869,
+            "range": "± 1036",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "polynomial/mul",
+            "value": 3277,
+            "range": "± 754",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "polynomial/div",
+            "value": 2923,
+            "range": "± 872",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "starks/simple_fibonacci",
+            "value": 35102375,
+            "range": "± 73451",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "starks/fibonacci_2_columns",
+            "value": 51253500,
+            "range": "± 2756844",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "starks/fibonacci_f17",
+            "value": 417070,
+            "range": "± 56294",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "starks/quadratic_air",
+            "value": 35695836,
+            "range": "± 277328",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
