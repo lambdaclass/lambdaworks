d9fc3273761103c9862651b9f3fc6d6db6eb7dab
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-04-25 17:14:42 -0300

    Changed param name, coeffs to input

--- fft/src/ops.rs
+++ fft/src/ops.rs
@@ -13,14 +13,14 @@ use super::{
 /// Executes Fast Fourier Transform over elements of a two-adic finite field `F` in a coset. Usually used for
 /// fast polynomial evaluation.
 pub fn fft_with_blowup<F: IsTwoAdicField>(
-    coeffs: &[FieldElement<F>],
+    input: &[FieldElement<F>],
     blowup_factor: usize,
 ) -> Result<Vec<FieldElement<F>>, FFTError> {
-    let domain_size = coeffs.len() * blowup_factor;
+    let domain_size = input.len() * blowup_factor;
     let order = log2(domain_size)?;
     let twiddles = get_twiddles(order, RootsConfig::BitReverse)?;
 
-    let mut results = coeffs.to_vec();
+    let mut results = input.to_vec();
     results.resize(domain_size, FieldElement::zero());
 
     in_place_nr_2radix_fft(&mut results, &twiddles);
@@ -31,13 +31,11 @@ pub fn fft_with_blowup<F: IsTwoAdicField>(
 
 /// Executes Fast Fourier Transform over elements of a two-adic finite field `F`. Usually used for
 /// fast polynomial evaluation.
-pub fn fft<F: IsTwoAdicField>(
-    coeffs: &[FieldElement<F>],
-) -> Result<Vec<FieldElement<F>>, FFTError> {
-    let order = log2(coeffs.len())?;
+pub fn fft<F: IsTwoAdicField>(input: &[FieldElement<F>]) -> Result<Vec<FieldElement<F>>, FFTError> {
+    let order = log2(input.len())?;
     let twiddles = get_twiddles(order, RootsConfig::BitReverse)?;
 
-    let mut results = coeffs.to_vec();
+    let mut results = input.to_vec();
     in_place_nr_2radix_fft(&mut results, &twiddles);
     in_place_bit_reverse_permute(&mut results);
 
@@ -47,17 +45,17 @@ pub fn fft<F: IsTwoAdicField>(
 /// Executes the inverse Fast Fourier Transform over elements of a two-adic finite field `F`.
 /// Usually used for fast polynomial evaluation.
 pub fn inverse_fft<F: IsTwoAdicField>(
-    coeffs: &[FieldElement<F>],
+    input: &[FieldElement<F>],
 ) -> Result<Vec<FieldElement<F>>, FFTError> {
-    let order = log2(coeffs.len())?;
+    let order = log2(input.len())?;
     let twiddles = get_twiddles(order, RootsConfig::BitReverseInversed)?;
 
-    let mut results = coeffs.to_vec();
+    let mut results = input.to_vec();
     in_place_nr_2radix_fft(&mut results, &twiddles);
     in_place_bit_reverse_permute(&mut results);
 
     for elem in &mut results {
-        *elem = elem.clone() / FieldElement::from(coeffs.len() as u64); // required for inverting the DFT matrix.
+        *elem = elem.clone() / FieldElement::from(input.len() as u64); // required for inverting the DFT matrix.
     }
 
     Ok(results)
