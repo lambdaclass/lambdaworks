5b912ead7440ad775c4ef697e2e46d4b86a65344
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-04-26 12:58:21 -0300

    Removed null input edge case handling

--- fft/src/ops.rs
+++ fft/src/ops.rs
@@ -18,10 +18,6 @@ pub fn fft_with_blowup<F: IsTwoAdicField>(
     input: &[FieldElement<F>],
     blowup_factor: usize,
 ) -> Result<Vec<FieldElement<F>>, FFTError> {
-    if input.is_empty() || input == vec![FieldElement::zero(); input.len()] {
-        return Ok(vec![FieldElement::zero()]);
-    }
-
     let mut results = input.to_vec();
     let domain_size = (input.len() * blowup_factor).next_power_of_two();
     results.resize(domain_size, FieldElement::zero());
@@ -38,10 +34,6 @@ pub fn fft_with_blowup<F: IsTwoAdicField>(
 /// Executes Fast Fourier Transform over elements of a two-adic finite field `F`. Usually used for
 /// fast polynomial evaluation.
 pub fn fft<F: IsTwoAdicField>(input: &[FieldElement<F>]) -> Result<Vec<FieldElement<F>>, FFTError> {
-    if input.is_empty() || input == vec![FieldElement::zero(); input.len()] {
-        return Ok(vec![FieldElement::zero()]);
-    }
-
     // if the input size is not a power of two, use zero padding
     let input = helpers::zero_padding(input);
 
@@ -60,10 +52,6 @@ pub fn fft<F: IsTwoAdicField>(input: &[FieldElement<F>]) -> Result<Vec<FieldElem
 pub fn inverse_fft<F: IsTwoAdicField>(
     input: &[FieldElement<F>],
 ) -> Result<Vec<FieldElement<F>>, FFTError> {
-    if input.is_empty() || input == vec![FieldElement::zero(); input.len()] {
-        return Ok(vec![FieldElement::zero()]);
-    }
-
     // if the input size is not a power of two, use zero padding
     let input = helpers::zero_padding(input);
 
