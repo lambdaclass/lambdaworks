1d776c54f9134805af8b64fd2707ea8b5e9a8a8b
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2023-04-26 14:41:28 -0300

    Example reading file with cairo-rs (#269)
    * Start cairo execution trace builder
    
    * Refactor cairo memory
    
    * Implemented flags_and_offsets function for CairoTrace
    
    * Add method for trace representation of instruction flags
    
    * Implement trace representation function for instruction offsets
    
    * Add method to compute dst_addrs column for trace
    
    * cairo module using FE
    
    * Use aux_get_last_nim_of_FE function in all instruction flags TryFrom's
    
    * Refactor CairoMemory to hold FE values
    
    * Add some comments
    
    * compute_res
    
    * compute_res completed
    
    * compute_op1 WIP
    
    * Finish first implementation of build_cairo_trace function
    
    * Start cairo execution trace testing
    
    * Add test for build_cairo_execution_trace
    
    * Save work in progress: debugging cairo trace
    
    * Save work in progress: more debugging
    
    * Fix some bugs
    
    * Remove print
    
    * Finish trace test for simple_program, start call_func test
    
    * Finish second test
    
    * Add documentation and comments in the code
    
    * Make code more idiomatic
    
    * Change auxiliary function name
    
    * Add some more documentation
    
    * Example reading file with cairo-rs
    
    * cairo VM files moved inside starks
    
    * integration_cairo directory removed
    
    * default value for entrypoint is "main"
    
    * function name changed to run_program
    
    * parser.rs file renamed to run.rs
    
    * documentation in run_program
    
    * fmt
    
    ---------
    
    Co-authored-by: Mariano Nicolini <mariano.nicolini.91@gmail.com>
    Co-authored-by: Pablo Deymonnaz <deymonnaz@gmail.com>

---
+++ proving_system/stark/src/cairo_run/run.rs
@@ -0,0 +1,201 @@
+use super::file_writer::FileWriter;
+use cairo_vm::cairo_run::{self, EncodeTraceError};
+use cairo_vm::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
+use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
+use cairo_vm::vm::errors::trace_errors::TraceError;
+use cairo_vm::vm::errors::vm_errors::VirtualMachineError;
+use std::io;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Failed to interact with the file system")]
+    IO(#[from] std::io::Error),
+    #[error("The cairo program execution failed")]
+    Runner(#[from] CairoRunError),
+    #[error(transparent)]
+    EncodeTrace(#[from] EncodeTraceError),
+    #[error(transparent)]
+    VirtualMachine(#[from] VirtualMachineError),
+    #[error(transparent)]
+    Trace(#[from] TraceError),
+}
+
+/// Runs a cairo program in JSON format and returns trace and memory files.
+/// Uses [cairo-rs](https://github.com/lambdaclass/cairo-rs/) project to run the program.
+///
+///  # Params
+///
+/// `entrypoint_function` - the name of the entrypoint function tu run. If `None` is provided, the default value is `main`.
+/// `layout` - type of layout of Cairo.
+/// `filename` - path to the input file.
+/// `trace_path` - path where to store the generated trace file.
+/// `memory_path` - path where to store the generated memory file.
+///
+/// # Returns
+///
+/// Ok(()) in case of succes
+/// `Error` indicating the type of error.
+pub fn run_program(
+    entrypoint_function: Option<&str>,
+    layout: &str,
+    filename: &str,
+    trace_path: &str,
+    memory_path: &str,
+) -> Result<(), Error> {
+    // default value for entrypoint is "main"
+    let entrypoint = entrypoint_function.unwrap_or("main");
+
+    let trace_enabled = true;
+    let mut hint_executor = BuiltinHintProcessor::new_empty();
+    let cairo_run_config = cairo_run::CairoRunConfig {
+        entrypoint,
+        trace_enabled,
+        relocate_mem: true,
+        layout,
+        proof_mode: false,
+        secure_run: None,
+    };
+
+    let program_content = std::fs::read(filename).map_err(Error::IO)?;
+
+    let (cairo_runner, vm) =
+        match cairo_run::cairo_run(&program_content, &cairo_run_config, &mut hint_executor) {
+            Ok(runner) => runner,
+            Err(error) => {
+                eprintln!("{error}");
+                return Err(Error::Runner(error));
+            }
+        };
+
+    let relocated_trace = vm.get_relocated_trace()?;
+
+    let trace_file = std::fs::File::create(trace_path)?;
+    let mut trace_writer =
+        FileWriter::new(io::BufWriter::with_capacity(3 * 1024 * 1024, trace_file));
+
+    cairo_run::write_encoded_trace(relocated_trace, &mut trace_writer)?;
+    trace_writer.flush()?;
+
+    let memory_file = std::fs::File::create(memory_path)?;
+    let mut memory_writer =
+        FileWriter::new(io::BufWriter::with_capacity(5 * 1024 * 1024, memory_file));
+
+    cairo_run::write_encoded_memory(&cairo_runner.relocated_memory, &mut memory_writer)?;
+    memory_writer.flush()?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::air::trace::TraceTable;
+    use crate::cairo_vm::cairo_mem::CairoMemory;
+    use crate::cairo_vm::cairo_trace::CairoTrace;
+    use crate::cairo_vm::execution_trace::build_cairo_execution_trace;
+    use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::MontgomeryConfigStark252PrimeField;
+    use lambdaworks_math::field::{
+        element::FieldElement, fields::montgomery_backed_prime_fields::U256PrimeField,
+    };
+
+    pub type Stark252PrimeField = U256PrimeField<MontgomeryConfigStark252PrimeField>;
+    type FE = FieldElement<Stark252PrimeField>;
+
+    #[test]
+    fn test_parse_cairo_file() {
+        let base_dir = env!("CARGO_MANIFEST_DIR");
+
+        let json_filename = base_dir.to_owned() + "/src/cairo_run/program.json";
+        let dir_trace = base_dir.to_owned() + "/src/cairo_run/program.trace";
+        let dir_memory = base_dir.to_owned() + "/src/cairo_run/program.memory";
+
+        println!("{}", json_filename);
+
+        super::run_program(None, "all_cairo", &json_filename, &dir_trace, &dir_memory).unwrap();
+
+        // read trace from file
+        let raw_trace = CairoTrace::from_file(&dir_trace).unwrap();
+        // read memory from file
+        let memory = CairoMemory::from_file(&dir_memory).unwrap();
+
+        let execution_trace = build_cairo_execution_trace(&raw_trace, &memory);
+
+        // This trace is obtained from Giza when running the prover for the mentioned program.
+        let expected_trace = TraceTable::new_from_cols(&vec![
+            // col 0
+            vec![FE::zero(), FE::zero(), FE::one()],
+            // col 1
+            vec![FE::one(), FE::one(), FE::one()],
+            // col 2
+            vec![FE::one(), FE::one(), FE::zero()],
+            // col 3
+            vec![FE::zero(), FE::zero(), FE::one()],
+            // col 4
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 5
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 6
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 7
+            vec![FE::zero(), FE::zero(), FE::one()],
+            // col 8
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 9
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 10
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 11
+            vec![FE::one(), FE::zero(), FE::zero()],
+            // col 12
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 13
+            vec![FE::zero(), FE::zero(), FE::one()],
+            // col 14
+            vec![FE::one(), FE::one(), FE::zero()],
+            // col 15
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 16
+            vec![FE::from(3), FE::from(3), FE::from(9)],
+            // col 17
+            vec![FE::from(8), FE::from(9), FE::from(9)],
+            // col 18
+            vec![FE::from(8), FE::from(8), FE::from(8)],
+            // col 19
+            vec![FE::from(1), FE::from(3), FE::from(5)],
+            // col 20
+            vec![FE::from(8), FE::from(8), FE::from(6)],
+            // col 21
+            vec![FE::from(7), FE::from(7), FE::from(7)],
+            // col 22
+            vec![FE::from(2), FE::from(4), FE::from(7)],
+            // col 23
+            vec![
+                FE::from(0x480680017fff8000),
+                FE::from(0x400680017fff7fff),
+                FE::from(0x208b7fff7fff7ffe),
+            ],
+            // col 24
+            vec![FE::from(3), FE::from(3), FE::from(9)],
+            // col 25
+            vec![FE::from(9), FE::from(9), FE::from(9)],
+            // col 26
+            vec![FE::from(3), FE::from(3), FE::from(9)],
+            // col 27
+            vec![FE::from(0x8000), FE::from(0x7fff), FE::from(0x7ffe)],
+            // col 28
+            vec![FE::from(0x7fff), FE::from(0x7fff), FE::from(0x7fff)],
+            // col 29
+            vec![FE::from(0x8001), FE::from(0x8001), FE::from(0x7fff)],
+            // col 30
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 31
+            vec![FE::zero(), FE::zero(), FE::zero()],
+            // col 32
+            vec![FE::from(0x1b), FE::from(0x1b), FE::from(0x51)],
+            // col 33 - Selector column
+            // vec![FE::one(), FE::one(), FE::zero()],
+        ]);
+
+        assert_eq!(execution_trace.cols(), expected_trace.cols());
+    }
+}
