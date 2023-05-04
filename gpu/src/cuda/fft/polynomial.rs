794f44e6b66521c95ea0cbafd81853d33eee51ff
matias-gonz <maigonzalez@fi.uba.ar>
2023-05-04 15:24:04 -0300

    Add interpolate_fft_cuda

--- gpu/src/cuda/fft/polynomial.rs
+++ gpu/src/cuda/fft/polynomial.rs
@@ -22,6 +22,27 @@ where
     fft(poly.coefficients(), &twiddles)
 }
 
+/// Returns a new polynomial that interpolates `fft_evals`, which are evaluations using twiddle
+/// factors. This is considered to be the inverse operation of [evaluate_fft_cuda()].
+pub fn interpolate_fft_cuda<F>(
+    fft_evals: &[FieldElement<F>],
+) -> Result<Polynomial<FieldElement<F>>, DriverError>
+where
+    F: IsFFTField,
+    F::BaseType: Unpin,
+{
+    // fft() can zero-pad the coeffs if there aren't 2^k of them (k being any integer).
+    // TODO: twiddle factors need to be handled with too much care, the FFT API shouldn't accept
+    // invalid twiddle factor collections. A better solution is needed.
+    let order = log2(fft_evals.coefficients.len()).unwrap();
+    let twiddles = get_twiddles(order, RootsConfig::BitReverseInverse).unwrap();
+
+    let coeffs = fft(fft_evals.coefficients, &twiddles)?;
+
+    let scale_factor = FieldElement::from(fft_evals.len() as u64).inv();
+    Ok(Polynomial::new(&coeffs).scale_coeffs(&scale_factor))
+}
+
 #[cfg(feature = "cuda")]
 #[cfg(test)]
 mod gpu_tests {}
