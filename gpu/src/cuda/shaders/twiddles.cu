6c028fe6fbed240c2515fbad20cd6f42ac6bb910
matias-gonz <maigonzalez@fi.uba.ar>
2023-05-05 16:02:05 -0300

    Update calc_twiddles_bitrev

--- gpu/src/cuda/shaders/twiddles.cu
+++ gpu/src/cuda/shaders/twiddles.cu
@@ -24,12 +24,12 @@ extern "C"
     // };
 
     __global__ void calc_twiddles_bitrev(p256::Fp *result,
-                                         const p256::Fp _omega)
+                                         const p256::Fp *_omega)
     {
         const uint index = blockIdx.x * blockDim.x + threadIdx.x;
         const uint size = blockDim.x;
 
-        p256::Fp omega = _omega;
+        p256::Fp omega = _omega[0];
         result[index] = omega.pow(reverse_index(index, size));
     };
 
