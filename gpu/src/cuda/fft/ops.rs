2c56e76953b6e96233093a56637dbf1d2fa71d6b
matias-gonz <maigonzalez@fi.uba.ar>
2023-05-05 15:38:19 -0300

    Add missing slice

--- gpu/src/cuda/fft/ops.rs
+++ gpu/src/cuda/fft/ops.rs
@@ -107,7 +107,7 @@ pub fn gen_twiddles<F: IsFFTField>(
             .collect::<Vec<_>>()
             .as_slice(),
     )?;
-    let d_root = device.htod_sync_copy(&CUDAFieldElement::from(&root))?;
+    let d_root = device.htod_sync_copy(&[CUDAFieldElement::from(&root)])?;
 
     device.load_ptx(
         Ptx::from_src(SHADER_PTX_TWIDDLES),
