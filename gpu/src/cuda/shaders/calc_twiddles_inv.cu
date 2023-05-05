dec948af65fedc5013900b93b79b3268b996eb9b
matias-gonz <maigonzalez@fi.uba.ar>
2023-05-05 10:06:50 -0300

    Separate twiddle functions into different files

---
+++ gpu/src/cuda/shaders/calc_twiddles_inv.cu
@@ -0,0 +1,10 @@
+#include "./fp_u256.cuh"
+
+extern "C" __global__ void calc_twiddles_inv(p256::Fp *result,
+                                             const p256::Fp &_omega)
+{
+  const uint index = blockIdx.x * blockDim.x + threadIdx.x;
+
+  p256::Fp omega = _omega;
+  result[index] = omega.pow(index).inverse();
+};
