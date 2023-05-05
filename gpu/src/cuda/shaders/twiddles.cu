f856c09a58162c7762c21b23413483471bf3eb2b
Tomás <tomas.gruner@lambdaclass.com>
2023-05-05 11:56:31 -0300

    Comment out `calc_twiddles*_inv` functions
    These functions slow down compilation to a halt, and are equivalent to calling `calc_twiddles*` with an inverted `_omega`

--- gpu/src/cuda/shaders/twiddles.cu
+++ gpu/src/cuda/shaders/twiddles.cu
@@ -13,14 +13,15 @@ extern "C"
         result[index] = omega.pow(index);
     };
 
-    __global__ void calc_twiddles_inv(p256::Fp *result,
-                                      const p256::Fp &_omega)
-    {
-        const uint index = blockIdx.x * blockDim.x + threadIdx.x;
+    // NOTE: this call is equivalent to calc_twiddles with _omega = omega.inverse()
+    // __global__ void calc_twiddles_inv(p256::Fp *result,
+    //                                   const p256::Fp &_omega)
+    // {
+    //     const uint index = blockIdx.x * blockDim.x + threadIdx.x;
 
-        p256::Fp omega = _omega;
-        result[index] = omega.pow(index).inverse();
-    };
+    //     p256::Fp omega = _omega;
+    //     result[index] = omega.pow(index).inverse();
+    // };
 
     __global__ void calc_twiddles_bitrev(p256::Fp *result,
                                          const p256::Fp &_omega)
@@ -32,13 +33,14 @@ extern "C"
         result[index] = omega.pow(reverse_index(index, size));
     };
 
-    __global__ void calc_twiddles_bitrev_inv(p256::Fp *result,
-                                             const p256::Fp &_omega)
-    {
-        const uint index = blockIdx.x * blockDim.x + threadIdx.x;
-        const uint size = blockDim.x;
+    // NOTE: this call is equivalent to calc_twiddles_bitrev with _omega = omega.inverse()
+    // __global__ void calc_twiddles_bitrev_inv(p256::Fp *result,
+    //                                          const p256::Fp &_omega)
+    // {
+    //     const uint index = blockIdx.x * blockDim.x + threadIdx.x;
+    //     const uint size = blockDim.x;
 
-        p256::Fp omega = _omega;
-        result[index] = omega.pow(reverse_index(index, size)).inverse();
-    };
+    //     p256::Fp omega = _omega;
+    //     result[index] = omega.pow(reverse_index(index, size)).inverse();
+    // };
 }
