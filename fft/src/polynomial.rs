a14304faef8786aeeff745fe5a8047dc1de58179
Tomás <tomas.gruner@lambdaclass.com>
2023-05-08 13:29:50 -0300

    Fix compile error

--- fft/src/polynomial.rs
+++ fft/src/polynomial.rs
@@ -67,10 +67,10 @@ impl<F: IsFFTField> FFTPoly<F> for Polynomial<FieldElement<F>> {
             // TODO: support multiple fields with CUDA
             if F::field_name() == "stark256" {
                 Ok(lambdaworks_gpu::cuda::fft::polynomial::evaluate_fft_cuda(
-                    self,
+                    &coeffs,
                 )?)
             } else {
-                evaluate_fft_cpu(self)
+                evaluate_fft_cpu(&coeffs)
             }
         }
 
