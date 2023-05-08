991d2235acc737710ab39008e7b481af5e528230
MauroFab <maurotoscano2@gmail.com>
2023-05-08 15:50:42 -0300

    Add default types

---
+++ math/src/elliptic_curve/short_weierstrass/curves/bls12_381/default_types.rs
@@ -0,0 +1,15 @@
+use crate::{field::{fields::montgomery_backed_prime_fields::{IsModulus, MontgomeryBackendPrimeField}, element::FieldElement}, unsigned_integer::element::U256};
+
+#[derive(Clone, Debug)]
+pub struct FrConfig;
+
+/// Modulus of bls 12 381 subgroup
+impl IsModulus<U256> for FrConfig {
+    const MODULUS: U256 =
+        U256::from("73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001");
+}
+
+/// FrField using MontgomeryBackend for bls 12 381
+pub type FrField = MontgomeryBackendPrimeField<FrConfig, 4>;
+/// FrElement using MontgomeryBackend for bls 12 381
+pub type FrElement = FieldElement<FrField>;
