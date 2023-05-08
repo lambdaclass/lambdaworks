366ac95febbf6f0b781e8d83b8c710ff6f13ec13
Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>
2023-05-08 16:45:44 -0300

    Srs serialization / Deserealization (#296)
    * Srs serialization
    
    * wip
    
    * Rework serialize curve points
    
    * Added deserialize for non extension fields
    
    * Fix clippy
    
    * Serialize deserialize g2 points
    
    * Fix linter
    
    * Serialization deserialization
    
    * Renamed simple serialize to serializable
    
    * Update description
    
    * Update endianess
    
    * Update endianess
    
    * Fmt
    
    * Update comment
    
    * Add from file for srs
    
    * Refactor
    
    * Fmt
    
    * Improved error handling
    
    * fmt
    
    * Constant refactor
    
    * Deserialize trait comment update
    
    * Error fix
    
    * Read
    
    * Clippy
    
    * Revert
    
    * Raise error

--- crypto/src/commitments/kzg.rs
+++ crypto/src/commitments/kzg.rs
@@ -1,15 +1,17 @@
+use crate::errors::SrsFromFileError;
+
+use super::traits::IsCommitmentScheme;
 use lambdaworks_math::{
     cyclic_group::IsGroup,
-    elliptic_curve::traits::IsPairing,
+    elliptic_curve::{short_weierstrass::errors::DeserializationError, traits::IsPairing},
     field::{element::FieldElement, traits::IsPrimeField},
     msm::msm,
     polynomial::Polynomial,
+    traits::{Deserializable, Serializable},
 };
-use std::marker::PhantomData;
+use std::{marker::PhantomData, mem};
 
-use super::traits::IsCommitmentScheme;
-
-#[derive(Clone)]
+#[derive(PartialEq, Clone, Debug)]
 pub struct StructuredReferenceString<G1Point, G2Point> {
     pub powers_main_group: Vec<G1Point>,
     pub powers_secondary_group: [G2Point; 2],
@@ -26,6 +28,109 @@ where
             powers_main_group: powers_main_group.into(),
             powers_secondary_group: powers_secondary_group.clone(),
         }
+    }
+}
+
+impl<G1Point, G2Point> StructuredReferenceString<G1Point, G2Point>
+where
+    G1Point: IsGroup + Deserializable,
+    G2Point: IsGroup + Deserializable,
+{
+    pub fn from_file(file_path: &str) -> Result<Self, SrsFromFileError> {
+        let bytes = std::fs::read(file_path)?;
+        Ok(Self::deserialize(&bytes)?)
+    }
+}
+
+impl<G1Point, G2Point> Serializable for StructuredReferenceString<G1Point, G2Point>
+where
+    G1Point: IsGroup + Serializable,
+    G2Point: IsGroup + Serializable,
+{
+    fn serialize(&self) -> Vec<u8> {
+        let mut serialized_data: Vec<u8> = Vec::new();
+        // First 4 bytes encodes protocol version
+        let protocol_version: [u8; 4] = [0; 4];
+
+        serialized_data.extend(&protocol_version);
+
+        // Second 8 bytes store the amount of G1 elements to be stored, this is more than can be indexed with a 64-bit architecture, and some millions of terabytes of data if the points were compressed
+        let mut main_group_len_bytes: Vec<u8> = self.powers_main_group.len().to_le_bytes().to_vec();
+
+        // For architectures with less than 64 bits for pointers
+        // We add extra zeros at the end
+        while main_group_len_bytes.len() < 8 {
+            main_group_len_bytes.push(0)
+        }
+
+        serialized_data.extend(&main_group_len_bytes);
+
+        // G1 elements
+        for point in &self.powers_main_group {
+            serialized_data.extend(point.serialize());
+        }
+
+        // G2 elements
+        for point in &self.powers_secondary_group {
+            serialized_data.extend(point.serialize());
+        }
+
+        serialized_data
+    }
+}
+
+impl<G1Point, G2Point> Deserializable for StructuredReferenceString<G1Point, G2Point>
+where
+    G1Point: IsGroup + Deserializable,
+    G2Point: IsGroup + Deserializable,
+{
+    fn deserialize(bytes: &[u8]) -> Result<Self, DeserializationError> {
+        const MAIN_GROUP_LEN_OFFSET: usize = 4;
+        const MAIN_GROUP_OFFSET: usize = 12;
+
+        let main_group_len_u64 = u64::from_le_bytes(
+            // This unwrap can't fail since we are fixing the size of the slice
+            bytes[MAIN_GROUP_LEN_OFFSET..MAIN_GROUP_OFFSET]
+                .try_into()
+                .unwrap(),
+        );
+
+        let main_group_len = usize::try_from(main_group_len_u64)
+            .map_err(|_| DeserializationError::PointerSizeError)?;
+
+        let mut main_group: Vec<G1Point> = Vec::new();
+        let mut secondary_group: Vec<G2Point> = Vec::new();
+
+        let size_g1_point = mem::size_of::<G1Point>();
+        let size_g2_point = mem::size_of::<G2Point>();
+
+        for i in 0..main_group_len {
+            // The second unwrap shouldn't fail since the amount of bytes is fixed
+            let point = G1Point::deserialize(
+                bytes[i * size_g1_point + MAIN_GROUP_OFFSET
+                    ..i * size_g1_point + size_g1_point + MAIN_GROUP_OFFSET]
+                    .try_into()
+                    .unwrap(),
+            )?;
+            main_group.push(point);
+        }
+
+        let g2s_offset = size_g1_point * main_group_len + 12;
+        for i in 0..2 {
+            // The second unwrap shouldn't fail since the amount of bytes is fixed
+            let point = G2Point::deserialize(
+                bytes[i * size_g2_point + g2s_offset
+                    ..i * size_g2_point + g2s_offset + size_g2_point]
+                    .try_into()
+                    .unwrap(),
+            )?;
+            secondary_group.push(point);
+        }
+
+        let secondary_group_slice = [secondary_group[0].clone(), secondary_group[1].clone()];
+
+        let srs = StructuredReferenceString::new(&main_group, &secondary_group_slice);
+        Ok(srs)
     }
 }
 
@@ -164,6 +269,7 @@ mod tests {
         },
         field::element::FieldElement,
         polynomial::Polynomial,
+        traits::{Deserializable, Serializable},
         unsigned_integer::element::U256,
     };
 
@@ -215,5 +321,32 @@ mod tests {
         assert_eq!(y, FieldElement::zero());
         assert_eq!(proof, BLS12381Curve::generator());
         assert!(kzg.verify(&x, &y, &p_commitment, &proof));
+    }
+
+    #[test]
+    fn serialize_deserialize_srs() {
+        let srs = create_srs();
+        let bytes = srs.serialize();
+        let deserialized: StructuredReferenceString<
+            ShortWeierstrassProjectivePoint<BLS12381Curve>,
+            ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+        > = StructuredReferenceString::deserialize(&bytes).unwrap();
+
+        assert_eq!(srs, deserialized);
+    }
+
+    #[test]
+    fn load_srs_from_file() {
+        type TestSrsType = StructuredReferenceString<
+            ShortWeierstrassProjectivePoint<BLS12381Curve>,
+            ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+        >;
+
+        let base_dir = env!("CARGO_MANIFEST_DIR");
+        let srs_file = base_dir.to_owned() + "/src/commitments/test_srs/srs_3_g1_elements.bin";
+
+        let srs = TestSrsType::from_file(&srs_file).unwrap();
+
+        assert_eq!(srs.powers_main_group.len(), 3);
     }
 }
