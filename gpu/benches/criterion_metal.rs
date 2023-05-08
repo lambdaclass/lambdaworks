728b60ef4fcec1446791498e53ca6d6776763538
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-05-08 16:06:13 -0300

    Fix metal benchs names

--- gpu/benches/criterion_metal.rs
+++ gpu/benches/criterion_metal.rs
@@ -65,13 +65,13 @@ fn bitrev_permutation_benchmarks(c: &mut Criterion) {
 }
 
 fn poly_evaluation_benchmarks(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Polynomial");
+    let mut group = c.benchmark_group("Polynomial evaluation");
 
     for poly in SIZE_ORDERS.map(util::rand_poly) {
         group.throughput(criterion::Throughput::Elements(
             poly.coefficients().len() as u64
         ));
-        group.bench_with_input("evaluate_fft_metal", &poly, |bench, poly| {
+        group.bench_with_input("Parallel (Metal)", &poly, |bench, poly| {
             bench.iter_with_large_drop(|| {
                 functions::metal::poly_evaluate_fft(poly);
             });
@@ -82,11 +82,11 @@ fn poly_evaluation_benchmarks(c: &mut Criterion) {
 }
 
 fn poly_interpolation_benchmarks(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Polynomial");
+    let mut group = c.benchmark_group("Polynomial interpolation");
 
     for evals in SIZE_ORDERS.map(util::rand_field_elements) {
         group.throughput(criterion::Throughput::Elements(evals.len() as u64));
-        group.bench_with_input("interpolate_fft_metal", &evals, |bench, evals| {
+        group.bench_with_input("Parallel (Metal)", &evals, |bench, evals| {
             bench.iter_with_large_drop(|| {
                 functions::metal::poly_interpolate_fft(evals);
             });
