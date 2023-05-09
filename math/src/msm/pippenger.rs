08fcfc8e9aa80ac19e63a87ec024518a5805d2c9
Tomás <tomas.gruner@lambdaclass.com>
2023-05-09 16:09:09 -0300

    Clean up the generic params of `pippenger::msm`

--- math/src/msm/pippenger.rs
+++ math/src/msm/pippenger.rs
@@ -1,20 +1,22 @@
-use crate::{
-    cyclic_group::IsGroup,
-    field::{
-        element::FieldElement,
-        fields::montgomery_backed_prime_fields::{IsModulus, MontgomeryBackendPrimeField},
-    },
-    unsigned_integer::element::UnsignedInteger,
-};
-use std::fmt::Debug;
+use crate::{cyclic_group::IsGroup, unsigned_integer::element::UnsignedInteger};
 
-pub fn msm<M, G, const NUM_LIMBS: usize>(
-    cs: &[FieldElement<MontgomeryBackendPrimeField<M, NUM_LIMBS>>],
+/// This function computes the multiscalar multiplication (MSM).
+///
+/// Assume a group G of order r is given.
+/// Let `hidings = [g_1, ..., g_n]` be a tuple of group points in G and
+/// let `cs = [k_1, ..., k_n]` be a tuple of scalars in the Galois field GF(r).
+///
+/// Then, with additive notation, `msm(cs, hidings)` computes k_1 * g_1 + .... + k_n * g_n.
+///
+/// If `hidings` and `cs` are empty, then `msm` returns the zero element of the group.
+///
+/// Panics if `cs` and `hidings` have different lengths.
+pub fn msm<const NUM_LIMBS: usize, G>(
+    cs: &[UnsignedInteger<NUM_LIMBS>],
     hidings: &[G],
     window_size: usize,
 ) -> G
 where
-    M: IsModulus<UnsignedInteger<NUM_LIMBS>> + Clone + Debug,
     G: IsGroup,
 {
     debug_assert_eq!(
@@ -22,7 +24,7 @@ where
         hidings.len(),
         "Slices `cs` and `hidings` must be of the same length to compute `msm`."
     );
-    assert!(window_size < usize::BITS as usize);
+    debug_assert!(window_size < usize::BITS as usize);
     // The number of windows of size `s` is ceil(lambda/s).
     let num_windows = (64 * NUM_LIMBS - 1) / window_size + 1;
 
@@ -42,9 +44,7 @@ where
             cs.iter().zip(hidings).for_each(|(k, p)| {
                 // We truncate the number to the least significative limb.
                 // This is ok because window_size < usize::BITS.
-                let window_unmasked = (dbg!(k.representative())
-                    >> (dbg!(window_idx) * dbg!(window_size)))
-                .limbs[NUM_LIMBS - 1];
+                let window_unmasked = (k >> (window_idx * window_size)).limbs[NUM_LIMBS - 1];
                 let m_ij = window_unmasked & ((1 << window_size) - 1);
                 if dbg!(m_ij) != 0 {
                     let idx = (m_ij - 1) as usize;
@@ -74,12 +74,8 @@ mod tests {
     use crate::msm::{naive, pippenger};
     use crate::{
         elliptic_curve::{
-            short_weierstrass::curves::bls12_381::{
-                curve::BLS12381Curve, field_extension::BLS12381PrimeField,
-            },
-            traits::IsEllipticCurve,
+            short_weierstrass::curves::bls12_381::curve::BLS12381Curve, traits::IsEllipticCurve,
         },
-        field::element::FieldElement,
         unsigned_integer::element::UnsignedInteger,
     };
     use proptest::{collection, prelude::*, prop_assert_eq, prop_compose, proptest};
@@ -89,13 +85,13 @@ mod tests {
     const _MAX_LEN: usize = 30;
 
     prop_compose! {
-        fn bls12381_element()(limbs: [u64; 6]) -> FieldElement::<BLS12381PrimeField> {
-            FieldElement::<BLS12381PrimeField>::new(UnsignedInteger::from_limbs(limbs))
+        fn unsigned_integer()(limbs: [u64; 6]) -> UnsignedInteger<6> {
+            UnsignedInteger::from_limbs(limbs)
         }
     }
 
     prop_compose! {
-        fn bls12381_element_vec()(vec in collection::vec(bls12381_element(), 0.._MAX_LEN)) -> Vec<FieldElement::<BLS12381PrimeField>> {
+        fn unsigned_integer_vec()(vec in collection::vec(unsigned_integer(), 0.._MAX_LEN)) -> Vec<UnsignedInteger<6>> {
             vec
         }
     }
@@ -116,16 +112,14 @@ mod tests {
         #![proptest_config(ProptestConfig {
             cases: _CASES, .. ProptestConfig::default()
           })]
-        // Property-based test that ensures FFT eval. gives same result as a naive polynomial evaluation.
-        // #[test]
-        fn test_pippenger_matches_naive_msm(window_size in 1.._MAX_WSIZE, cs in bls12381_element_vec(), hidings in points_vec()) {
+        // Property-based test that ensures `pippenger::msm` gives same result as `naive::msm`.
+        #[test]
+        fn test_pippenger_matches_naive_msm(window_size in 1.._MAX_WSIZE, cs in unsigned_integer_vec(), hidings in points_vec()) {
             let min_len = cs.len().min(hidings.len());
             let cs = cs[..min_len].to_vec();
             let hidings = hidings[..min_len].to_vec();
 
             let pippenger = pippenger::msm(&cs, &hidings, window_size);
-
-            let cs: Vec<_> = cs.into_iter().map(|x| x.representative()).collect();
             let naive = naive::msm(&cs, &hidings);
 
             prop_assert_eq!(naive, pippenger);
