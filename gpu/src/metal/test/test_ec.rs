90732bfcde7ab231766593fb388b9aa1a30e09b1
matias-gonz <maigonzalez@fi.uba.ar>
2023-05-11 10:58:06 -0300

    Test test_metal_ec_infinity_plus_q_should_equal_q

--- gpu/src/metal/test/test_ec.rs
+++ gpu/src/metal/test/test_ec.rs
@@ -13,8 +13,6 @@ mod test {
         let state = MetalState::new(None).unwrap();
         let pipeline = state.setup_pipeline("bls12381_add").unwrap();
         let mut felt1 = FE::one();
-        //felt1.value().limbs[3] = 1;
-        dbg!(felt1.clone());
 
         let p: &[FE] = &[FE::from(2_u64), FE::from(1_u64), FE::from(3_u64)];
         let p_buffer = state.alloc_buffer_data(p);
@@ -40,5 +38,37 @@ mod test {
         assert_eq!(result[0], FE::from(2_u64));
         assert_eq!(result[1], FE::from(1_u64));
         assert_eq!(result[2], FE::from(3_u64));
+    }
+
+    #[test]
+    fn test_metal_ec_infinity_plus_q_should_equal_q() {
+        let state = MetalState::new(None).unwrap();
+        let pipeline = state.setup_pipeline("bls12381_add").unwrap();
+        let mut felt1 = FE::one();
+
+        let p: &[FE] = &[FE::zero(), FE::one(), FE::zero()];
+        let p_buffer = state.alloc_buffer_data(p);
+        let q: &[FE] = &[FE::from(66_u64), FE::from(85_u64), FE::from(179_u64)];
+        let q_buffer = state.alloc_buffer_data(q);
+        let result: &[FE] = &[FE::from(1), FE::from(1), FE::from(1)];
+        let result_buffer = state.alloc_buffer_data(result);
+
+        let (command_buffer, command_encoder) = state.setup_command(
+            &pipeline,
+            Some(&[(0, &p_buffer), (1, &q_buffer), (2, &result_buffer)]),
+        );
+
+        let threadgroup_size = MTLSize::new(1, 1, 1);
+        let threadgroup_count = MTLSize::new(1, 1, 1);
+        command_encoder.dispatch_thread_groups(threadgroup_count, threadgroup_size);
+        command_encoder.end_encoding();
+        command_buffer.commit();
+        command_buffer.wait_until_completed();
+
+        let result: Vec<FE> = MetalState::retrieve_contents(&result_buffer);
+
+        assert_eq!(result[0], FE::from(66_u64));
+        assert_eq!(result[1], FE::from(85_u64));
+        assert_eq!(result[2], FE::from(179_u64));
     }
 }
