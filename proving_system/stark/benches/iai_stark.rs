0039647b94a0048332f34c23426615e655aaddd2
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-05-11 15:40:44 -0300

    Add inline never lint to bench functions

--- proving_system/stark/benches/iai_stark.rs
+++ proving_system/stark/benches/iai_stark.rs
@@ -1,18 +1,22 @@
 mod functions;
 mod util;
 
+#[inline(never)]
 fn simple_fibonacci_benchmarks() {
     functions::stark::prove_fib(8);
 }
 
+#[inline(never)]
 fn two_col_fibonacci_benchmarks() {
     functions::stark::prove_fib_2_cols();
 }
 
+#[inline(never)]
 fn fibonacci_f17_benchmarks() {
     functions::stark::prove_fib17();
 }
 
+#[inline(never)]
 fn quadratic_air_benchmarks() {
     functions::stark::prove_quadratic();
 }
