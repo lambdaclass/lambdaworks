3f56ebfc38495b270ae46e96fdb6faa00bc95216
Tomás <tomas.gruner@lambdaclass.com>
2023-05-16 15:38:35 -0300

    Fix: benchmarks now bench `prove` call only

--- proving_system/stark/benches/criterion_stark.rs
+++ proving_system/stark/benches/criterion_stark.rs
@@ -1,5 +1,15 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use lambdaworks_stark::prover::prove;
+use criterion::{
+    black_box, criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, BenchmarkId,
+    Criterion, Throughput,
+};
+use functions::stark::{
+    generate_cairo_trace, generate_fib17_proof_params, generate_fib_2_cols_proof_params,
+    generate_fib_proof_params, generate_quadratic_proof_params,
+};
+use lambdaworks_stark::{
+    air::{context::ProofOptions, example::cairo},
+    prover::prove,
+};
 
 mod functions;
 mod util;
@@ -8,52 +18,80 @@ pub fn proof_benchmarks(c: &mut Criterion) {
     let mut group = c.benchmark_group("STARK");
     group.sample_size(10);
 
-    let sizes = [1 << 7]; //, 512, 1024];
+    let sizes = [64, 128, 256, 512];
 
-    // for trace_length in sizes.iter() {
-    //     group.throughput(Throughput::Bytes(*trace_length as u64));
-    //     group.bench_with_input(
-    //         BenchmarkId::new("Simple Fibonacci", trace_length),
-    //         trace_length,
-    //         |b, &t| {
-    //             b.iter(|| black_box(functions::stark::prove_fib(t)));
-    //         },
-    //     );
-    // }
+    for &trace_length in sizes.iter() {
+        let (trace, fibonacci_air) = generate_fib_proof_params(trace_length);
+        group.throughput(Throughput::Elements(trace_length as u64));
+        group.bench_function(BenchmarkId::new("Simple Fibonacci", trace_length), |b| {
+            b.iter(|| black_box(prove(black_box(&trace), black_box(&fibonacci_air))));
+        });
+    }
 
-    // group.bench_function("2 column Fibonacci", |bench| {
-    //     bench.iter(|| black_box(functions::stark::prove_fib_2_cols()));
-    // });
+    for &trace_length in sizes.iter() {
+        let (trace, fibonacci_air) = generate_fib_2_cols_proof_params(trace_length);
+        group.throughput(Throughput::Elements(trace_length as u64));
+        group.bench_function(BenchmarkId::new("2 column Fibonacci", trace_length), |b| {
+            b.iter(|| black_box(prove(black_box(&trace), black_box(&fibonacci_air))));
+        });
+    }
 
-    // group.bench_function("Fibonacci F17", |bench| {
-    //     bench.iter(|| black_box(functions::stark::prove_fib17()));
-    // });
+    for &trace_length in sizes.iter() {
+        let (trace, fibonacci_air) = generate_fib17_proof_params(trace_length);
+        group.throughput(Throughput::Elements(trace_length as u64));
+        group.bench_function(BenchmarkId::new("Fibonacci F17", trace_length), |b| {
+            b.iter(|| black_box(prove(black_box(&trace), black_box(&fibonacci_air))));
+        });
+    }
 
-    // group.bench_function("Quadratic AIR", |bench| {
-    //     bench.iter(|| black_box(functions::stark::prove_quadratic()));
-    // });
-    let (raw_trace, memory, cairo_air) = functions::stark::generate_cairo_fibonacci_5_trace();
-    let tuple = (raw_trace, memory);
+    for &trace_length in sizes.iter() {
+        let (trace, fibonacci_air) = generate_quadratic_proof_params(trace_length);
+        group.throughput(Throughput::Elements(trace_length as u64));
+        group.bench_function(BenchmarkId::new("Quadratic AIR", trace_length), |b| {
+            b.iter(|| black_box(prove(black_box(&trace), black_box(&fibonacci_air))));
+        });
+    }
 
-    group.bench_function("Cairo Fibonacci proof generation - 5 elements", |bench| {
-        bench.iter(|| black_box(prove(black_box(&tuple), black_box(&cairo_air))));
+    run_fibonacci_bench(
+        &mut group,
+        "Cairo Fibonacci proof generation - 5 elements",
+        "fibonacci_5",
+    );
+    run_fibonacci_bench(
+        &mut group,
+        "Cairo Fibonacci proof generation - 10 elements",
+        "fibonacci_10",
+    );
+    run_fibonacci_bench(
+        &mut group,
+        "Cairo Fibonacci proof generation - 30 elements",
+        "fibonacci_30",
+    );
+    run_fibonacci_bench(
+        &mut group,
+        "Cairo Fibonacci proof generation - 50 elements",
+        "fibonacci_50",
+    );
+    run_fibonacci_bench(
+        &mut group,
+        "Cairo Fibonacci proof generation - 100 elements",
+        "fibonacci_100",
+    );
+}
+
+fn run_fibonacci_bench(group: &mut BenchmarkGroup<'_, WallTime>, name: &str, file: &str) {
+    let trace = generate_cairo_trace(file, "all_cairo");
+
+    let proof_options = ProofOptions {
+        blowup_factor: 2,
+        fri_number_of_queries: 5,
+        coset_offset: 3,
+    };
+    let cairo_air = cairo::CairoAIR::new(proof_options, &trace.0);
+
+    group.bench_function(name, |bench| {
+        bench.iter(|| black_box(prove(black_box(&trace), black_box(&cairo_air))));
     });
-
-    // group.bench_function("Cairo Fibonacci proof generation - 10 elements", |bench| {
-    //     bench.iter(|| black_box(functions::stark::prove_cairo_fibonacci_10()));
-    // });
-
-    // group.bench_function("Cairo Fibonacci proof generation - 30 elements", |bench| {
-    //     bench.iter(|| black_box(functions::stark::prove_cairo_fibonacci_30()));
-    // });
-
-    // group.bench_function("Cairo Fibonacci proof generation - 50 elements", |bench| {
-    //     bench.iter(|| black_box(functions::stark::prove_cairo_fibonacci_50()));
-    // });
-
-    // group.bench_function("Cairo Fibonacci proof generation - 100 elements", |bench| {
-    //     bench.iter(|| black_box(functions::stark::prove_cairo_fibonacci_100()));
-    // });
 }
 
 criterion_group!(benches, proof_benchmarks);
--- proving_system/stark/benches/iai_stark.rs
+++ proving_system/stark/benches/iai_stark.rs
@@ -1,14 +1,28 @@
+use lambdaworks_stark::{prover::prove, verifier::verify};
+
 mod functions;
 mod util;
 
 #[inline(never)]
 fn simple_fibonacci_benchmarks() {
-    functions::stark::prove_fib(8);
+    let (trace, fibonacci_air) = functions::stark::generate_fib_proof_params(8);
+
+    let proof = prove(&trace, &fibonacci_air);
+
+    let ok = verify(&proof, &fibonacci_air);
+
+    assert!(ok);
 }
 
 #[inline(never)]
 fn two_col_fibonacci_benchmarks() {
-    functions::stark::prove_fib_2_cols();
+    let (trace, fibonacci_air) = functions::stark::generate_fib_2_cols_proof_params(16);
+
+    let proof = prove(&trace, &fibonacci_air);
+
+    let ok = verify(&proof, &fibonacci_air);
+
+    assert!(ok);
 }
 
 iai::main!(simple_fibonacci_benchmarks, two_col_fibonacci_benchmarks,);
