bf93accc0ea0401499c514634dac064dea4f12eb
Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>
2023-05-25 08:22:13 -0300

    Remove plonk (#379)
    * Remove plonk
    
    * Update gitignore
    
    * Remove DS Store

--- proving_system/plonk/src/test_utils/mod.rs
+++
@@ -1,8 +0,0 @@
-/// A test circuit
-pub mod circuit_1;
-/// A test circuit
-pub mod circuit_2;
-/// Deserialize json to generate test circuits
-pub mod circuit_json;
-/// Useful tools to test plonk over different circuits
-pub mod utils;
