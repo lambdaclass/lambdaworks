5260726c069ab9604b0039d418323b588acff48c
matias-gonz <maigonzalez@fi.uba.ar>
2023-05-29 18:11:06 -0300

    Comment code

--- gpu/src/cuda/shaders/twiddles.cu
+++ gpu/src/cuda/shaders/twiddles.cu
@@ -21,21 +21,25 @@ extern "C"
     __global__ void calc_twiddles(p256::Fp *result,
                                   const p256::Fp &_omega)
     {
+        /*
         int index = threadIdx.x;
 
         p256::Fp omega = _omega;
         result[index] = omega.pow((unsigned)index);
+        */
     };
 
     // NOTE: In order to calculate the inverse twiddles, call with _omega = _omega.inverse()
     __global__ void calc_twiddles_bitrev(p256::Fp *result,
                                          const p256::Fp &_omega)
     {
+        /*
         int index = threadIdx.x;
         int size = blockDim.x;
 
         p256::Fp omega = _omega;
         result[index] = omega.pow(reverse_index((unsigned)index, (unsigned)size));
+        */
     };
 
 }
