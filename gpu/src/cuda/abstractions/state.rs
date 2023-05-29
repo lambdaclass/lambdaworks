4236f4bfc8496ccf8195e59183c79a243ba9d7e2
matias-gonz <maigonzalez@fi.uba.ar>
2023-05-29 17:48:36 -0300

    Update error

--- gpu/src/cuda/abstractions/state.rs
+++ gpu/src/cuda/abstractions/state.rs
@@ -240,9 +240,9 @@ impl<F: IsField> CalcTwiddlesFunction<F> {
         unsafe {
             self.function
                 .clone()
-                .launch(config, (&self.twiddles, &self.omega))
+                .launch(config, (&mut self.twiddles, &self.omega))
         }
-        .map_err(|err| CudaError::Launch(err.to_string()))
+        .map_err(|_err| CudaError::Launch("Error at CalcTwiddlesFunction".to_string()))
     }
 
     pub(crate) fn retrieve_result(self) -> Result<Vec<FieldElement<F>>, CudaError> {
