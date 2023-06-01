b7b26a69a16d3cccf3b2090b059094432f66b5a1
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-06-01 17:55:27 -0300

    trait Compress for elliptic curve points.

--- math/src/elliptic_curve/traits.rs
+++ math/src/elliptic_curve/traits.rs
@@ -50,3 +50,16 @@ pub trait IsPairing {
         Self::compute_batch(&[(p, q)])
     }
 }
+
+pub trait Compress {
+    type G1Point: IsGroup;
+    type G2Point: IsGroup;
+
+    fn compress_g1_point(point: &Self::G1Point) -> Result<[u8; 48], EllipticCurveError>;
+
+    fn decompress_g1_point(input_bytes: &mut [u8; 48])
+        -> Result<Self::G1Point, EllipticCurveError>;
+
+    fn decompress_g2_point(input_bytes: &mut [u8; 96])
+        -> Result<Self::G2Point, EllipticCurveError>;
+}
