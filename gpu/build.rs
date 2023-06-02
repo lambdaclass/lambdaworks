d658199fb75c35c777e87e23efc0dd5a3aa528d9
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-06-02 18:31:09 -0300

    Add metal compilation to build.rs and remove shader

--- gpu/build.rs
+++ gpu/build.rs
@@ -5,9 +5,10 @@ fn compile_cuda_shaders() {
     use walkdir::WalkDir;
     const CUDA_SOURCE_DIR: &str = "src/cuda/shaders";
 
+    // Tell cargo to invalidate the built crate whenever the source changes
     println!("cargo:rerun-if-changed={CUDA_SOURCE_DIR}");
 
-    let nvcc = option_env!("NVCC_PATH").unwrap_or("nvcc");
+    let nvcc = option_env!("CUDA_BIN").unwrap_or("nvcc");
     let nvcc_out_dir = env::var("OUT_DIR").unwrap() + "/cuda/";
 
     let children: Vec<_> = WalkDir::new(CUDA_SOURCE_DIR)
@@ -43,13 +44,41 @@ fn compile_cuda_shaders() {
     });
 }
 
-const METAL_SHADERS_DIR: &str = "src/cuda/shaders";
+#[cfg(feature = "metal")]
+fn compile_metal_shaders() {
+    use std::process::Command;
+    const METAL_SHADERS_DIR: &str = "src/metal/shaders";
 
-fn main() {
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    // Tell cargo to invalidate the built crate whenever the source changes
     println!("cargo:rerun-if-changed={METAL_SHADERS_DIR}");
 
-    // println!("cargo:rustc-env=VAR=VALUE");
+    let input = METAL_SHADERS_DIR.to_owned() + "/all.metal";
+    let output = METAL_SHADERS_DIR.to_owned() + "/lib.metallib";
+
+    let cmd = Command::new("xcrun")
+        .args(&["-sdk", "macosx", "metal"])
+        .arg(&input)
+        .arg("-o")
+        .arg(&output)
+        .spawn()
+        .expect("Failed to spawn process");
+
+    let res = cmd.wait_with_output().expect("Command waiting failed");
+
+    if !res.status.success() {
+        println!();
+        println!("{}", String::from_utf8(res.stdout).unwrap());
+        println!();
+        eprintln!("{}", String::from_utf8(res.stderr).unwrap());
+        println!();
+        panic!("Compilation failed for source '{}'", input);
+    }
+}
+
+fn main() {
     #[cfg(feature = "cuda")]
     compile_cuda_shaders();
+
+    #[cfg(feature = "metal")]
+    compile_metal_shaders();
 }
