bd85124ef2cce79b547af77c52309dceb195bb33
Mario Rugiero <mrugiero@gmail.com>
2023-06-04 13:39:59 -0300

    perf: improve accuracy of field IAI benchmarks
    - Extract FP element sample creation into a `util` function to allow
      exclusion from measurement.
    - Likewise for getting a quadratic residue by squaring the sample.
    - Replace the apprpriate call in IAI and Criterion benches.

--- math/benches/util.rs
+++ math/benches/util.rs
@@ -1,5 +1,11 @@
 use const_random::const_random;
-use lambdaworks_math::{field::fields::u64_prime_field::U64FieldElement, polynomial::Polynomial};
+use lambdaworks_math::{
+    field::element::FieldElement,
+    field::fields::{
+        fft_friendly::stark_252_prime_field::Stark252PrimeField, u64_prime_field::U64FieldElement,
+    },
+    polynomial::Polynomial,
+};
 use rand::random;
 
 // Mersenne prime numbers
@@ -12,6 +18,30 @@ const PRIMES: [u64; 39] = [
 
 const MODULUS: u64 = PRIMES[const_random!(usize) % PRIMES.len()];
 pub type FE = U64FieldElement<MODULUS>;
+
+#[inline(never)]
+#[export_name = "util::fp_get_primes"]
+pub fn get_field_elements() -> (
+    FieldElement<Stark252PrimeField>,
+    FieldElement<Stark252PrimeField>,
+) {
+    let x = FieldElement::<Stark252PrimeField>::from_hex(
+        "0x03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
+    )
+    .unwrap();
+    let y = FieldElement::<Stark252PrimeField>::from_hex(
+        "0x0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
+    )
+    .unwrap();
+    (x, y)
+}
+
+#[inline(never)]
+#[export_name = "util::fp_squared_prime"]
+pub fn get_squared_field_element() -> FieldElement<Stark252PrimeField> {
+    let (x, _) = get_field_elements();
+    &x * &x
+}
 
 #[allow(dead_code)]
 pub fn rand_field_elements(order: u64) -> Vec<FE> {
