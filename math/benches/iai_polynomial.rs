33ab84a20b8ed14d22c15fbbe5e97d60224334ff
Mario Rugiero <mrugiero@gmail.com>
2023-06-05 15:06:13 -0300

    perf: fix polynomial benchmarks

--- math/benches/iai_polynomial.rs
+++ math/benches/iai_polynomial.rs
@@ -1,6 +1,6 @@
 use const_random::const_random;
-use iai_callgrind::black_box;
-use util::{rand_field_elements, rand_poly, FE};
+use core::hint::black_box;
+use util::FE;
 
 mod util;
 
@@ -8,50 +8,50 @@ const ORDER: u64 = const_random!(u64) % 8;
 
 #[inline(never)]
 fn poly_evaluate_benchmarks() {
-    let poly = rand_poly(ORDER);
+    let poly = util::rand_poly(ORDER);
     let x = FE::new(rand::random::<u64>());
-    poly.evaluate(black_box(&x));
+    black_box(poly.evaluate(black_box(&x)));
 }
 
 #[inline(never)]
 fn poly_evaluate_slice_benchmarks() {
-    let poly = rand_poly(ORDER);
-    let inputs = rand_field_elements(ORDER);
-    poly.evaluate_slice(black_box(&inputs));
+    let poly = util::rand_poly(ORDER);
+    let inputs = util::rand_field_elements(ORDER);
+    black_box(poly.evaluate_slice(black_box(&inputs)));
 }
 
 #[inline(never)]
 fn poly_add_benchmarks() {
-    let x_poly = rand_poly(ORDER);
-    let y_poly = rand_poly(ORDER);
-    let _ = black_box(&x_poly) + black_box(&y_poly);
+    let x_poly = util::rand_poly(ORDER);
+    let y_poly = util::rand_poly(ORDER);
+    let _ = black_box(black_box(black_box(&x_poly) + black_box(&y_poly)));
 }
 
 #[inline(never)]
 fn poly_neg_benchmarks() {
-    let x_poly = rand_poly(ORDER);
-    let _ = black_box(x_poly);
+    let x_poly = util::rand_poly(ORDER);
+    let _ = black_box(-black_box(x_poly));
 }
 
 #[inline(never)]
 fn poly_sub_benchmarks() {
-    let x_poly = rand_poly(ORDER);
-    let y_poly = rand_poly(ORDER);
-    let _ = black_box(x_poly) - black_box(y_poly);
+    let x_poly = util::rand_poly(ORDER);
+    let y_poly = util::rand_poly(ORDER);
+    let _ = black_box(black_box(x_poly) - black_box(y_poly));
 }
 
 #[inline(never)]
 fn poly_mul_benchmarks() {
-    let x_poly = rand_poly(ORDER);
-    let y_poly = rand_poly(ORDER);
-    let _ = black_box(x_poly) + black_box(y_poly);
+    let x_poly = util::rand_poly(ORDER);
+    let y_poly = util::rand_poly(ORDER);
+    let _ = black_box(black_box(x_poly) * black_box(y_poly));
 }
 
 #[inline(never)]
 fn poly_div_benchmarks() {
-    let x_poly = rand_poly(ORDER);
-    let y_poly = rand_poly(ORDER);
-    let _ = black_box(x_poly) + black_box(y_poly);
+    let x_poly = util::rand_poly(ORDER);
+    let y_poly = util::rand_poly(ORDER);
+    let _ = black_box(black_box(x_poly) / black_box(y_poly));
 }
 
 iai_callgrind::main!(
