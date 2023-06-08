265ffaabc7f689b397bb62034504679c6a7d4751
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-06-08 17:15:40 -0300

    add docs to merkle tree backend

--- crypto/src/merkle_tree/traits.rs
+++ crypto/src/merkle_tree/traits.rs
@@ -7,8 +7,13 @@ pub trait IsMerkleTreeBackend: Default {
     type Node: PartialEq + Eq + Clone + Default;
     type Data;
 
+    /// This function takes a single variable `Data`
+    /// and converts it to a node
     fn hash_data(&self, leaf: &Self::Data) -> Self::Node;
 
+    /// This function that takes the list of data from
+    /// which the Merkle tree will be built from and
+    /// converts it to a list of leaf nodes 
     fn hash_leaves(&self, unhashed_leaves: &[Self::Data]) -> Vec<Self::Node> {
         unhashed_leaves
             .iter()
@@ -16,5 +21,7 @@ pub trait IsMerkleTreeBackend: Default {
             .collect()
     }
 
+    /// This function takes to children nodes and build a new parent node.
+    /// It will be used in the construction of the Merkle tree.
     fn hash_new_parent(&self, child_1: &Self::Node, child_2: &Self::Node) -> Self::Node;
 }
