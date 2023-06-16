96394e4710871eb6ddadd1cfb6995258bffb7079
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2023-06-16 10:54:42 -0300

    Benches (#434)
    * mul benches
    
    * random mul benches
    
    * optimizations
    
    * bench: add
    
    * bench sub
    
    * benchmark table in the README
    
    * bench invert
    
    * update README
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * bench pow
    
    * Update README.md
    
    * sqrt bench
    
    * clippy in benchs
    
    * SQRT changed to 1000 samples
    
    * lib.rs removed from benches
    
    * sqrt bench time in README
    
    * profile.bench moved to Cargo.toml in the root of the workspace
    
    * suggestion in invert bench
    
    * pinning to a fixed rev of arkwors for benches
    
    * fixed rev
    
    ---------
    
    Co-authored-by: Pablo Deymonnaz <deymonnaz@gmail.com>
    Co-authored-by: Diego K <43053772+diegokingston@users.noreply.github.com>

---
+++ benches/benches/invert.rs
@@ -0,0 +1,67 @@
+use ark_ff::Field;
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
+use lambdaworks_math::traits::ByteConversion;
+use rand::RngCore;
+
+const BENCHMARK_NAME: &str = "invert";
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    // arkworks-ff
+    {
+        use ark_std::{test_rng, UniformRand};
+        use ark_test_curves::starknet_fp::Fq as F;
+
+        let mut rng = test_rng();
+
+        let mut v = Vec::new();
+        for _i in 0..10000 {
+            let a = F::rand(&mut rng);
+            v.push(a);
+        }
+
+        c.bench_function(
+            &format!(
+                "{} | ark-ff - branch: faster-benchmarks-and-starknet-field",
+                BENCHMARK_NAME
+            ),
+            |b| {
+                b.iter_batched(
+                    || v.clone(),
+                    |mut v| {
+                        for mut elem in v.iter_mut() {
+                            black_box(black_box(&mut elem).inverse_in_place());
+                        }
+                    },
+                    BatchSize::LargeInput,
+                );
+            },
+        );
+    }
+
+    // lambdaworks-math
+    {
+        use lambdaworks_math::field::{
+            element::FieldElement, fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
+        };
+        let mut v = Vec::new();
+        let mut buf = [0u8; 32];
+        for _i in 0..10000 {
+            rand::thread_rng().fill_bytes(&mut buf[..]);
+
+            let a = FieldElement::<Stark252PrimeField>::from_bytes_be(&buf).unwrap();
+
+            v.push(a);
+        }
+
+        c.bench_function(&format!("{} | lambdaworks", BENCHMARK_NAME,), |b| {
+            b.iter(|| {
+                for elem in v.iter() {
+                    black_box(black_box(&elem).inv());
+                }
+            });
+        });
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
