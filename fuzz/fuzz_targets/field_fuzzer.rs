a862f5b8773daa7b53bbf05f226f378e7ad894ef
dafifynn <slimbieber@gmail.com>
2023-06-21 17:20:01 -0300

    add result checks

--- fuzz/fuzz_targets/field_fuzzer.rs
+++ fuzz/fuzz_targets/field_fuzzer.rs
@@ -9,6 +9,7 @@ use num_traits::Pow;
 use lambdaworks_math::unsigned_integer::element::UnsignedInteger;
 use std::fmt::Display;
 use ibig::{ibig, modular::ModuloRing, ubig, UBig};
+use num_bigint::BigUint;
 
 fuzz_target!(|values: (u64, u64)| {
 
@@ -31,34 +32,23 @@ fuzz_target!(|values: (u64, u64)| {
     
     assert_eq!(&(add_u64.to_string())[2..], addition.residue().in_radix(16).to_string());
 
-
-    // let sub_u64 = a - b;
-    // if value_u64_a.clone() > value_u64_b.clone() { 
-    //     let substraction = value_u64_a.clone() - value_u64_b.clone();
-    //     assert_eq!(sub_u64, FieldElement::<Stark252PrimeField>::from(substraction));
-    // } else { 
-    //     let substraction = value_u64_b.clone() - value_u64_a.clone();
-    //     assert_eq!(sub_u64, -FieldElement::<Stark252PrimeField>::from(substraction));
-    // }
-
-    // let pow_u64 = &a.pow(value_u64_b.clone());
-    // // let pow = value_u64_a.clone().pow(value_u64_b.clone());
-    // // assert_eq!(pow_u64, FieldElement::<Stark252PrimeField>::from(pow));
+    let sub_u64 = &a - &b;
+    let substraction = ring.from(value_u64_a) - ring.from(value_u64_b);
+    assert_eq!(&(sub_u64.to_string())[2..], substraction.residue().in_radix(16).to_string());
     
-    // let mul_u64 = &a * &b;
-    // let mut multiplication = u128::from(value_u64_a.clone()) * u128::from(value_u64_b.clone());
-    // let multiplication: u64 = multiplication as u64;
-    // println!(" unsignedd{:?}", value_u64_a.clone());
-    // println!(" unsigneddd{:?}", value_u64_b.clone());
-    // println!(" multiplication{:?}", multiplication);
-    // // multiplication fails result
-    // // assert_eq!(mul_u64, FieldElement::<Stark252PrimeField>::from(multiplication ));
-    // if value_u64_b != 0 {
-    //     let division = value_u64_a.clone() / value_u64_b.clone();
-    //     let div_u64 = &a / &b;
-    //     // divition fails result
-    //     // assert_eq!(div_u64, FieldElement::<Stark252PrimeField>::from(division));
-    // }
+    let mul_u64 = &a * &b;
+    let multiplication = ring.from(value_u64_a) * ring.from(value_u64_b);
+    assert_eq!(&(mul_u64.to_string())[2..], multiplication.residue().in_radix(16).to_string());
+
+    let pow_u64 = &a.pow(value_u64_b.clone());
+    // let pow = BigUint::from(value_u64_a).pow(value_u64_b);
+    // assert_eq!(&(pow_u64.to_string())[2..], pow.residue().in_radix(16).to_string());
+    
+    if value_u64_b != 0 {
+        let div_u64 = &a / &b;
+        let division = ring.from(value_u64_a) / ring.from(value_u64_b);
+        assert_eq!(&(div_u64.to_string())[2..], division.residue().in_radix(16).to_string());
+    }
     
     
 });
