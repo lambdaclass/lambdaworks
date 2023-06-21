f014160145e9ae4d59d67d3580e741808b9ed610
dafifynn <slimbieber@gmail.com>
2023-06-21 17:49:53 -0300

    add fixed fuzzer

--- fuzz/fuzz_targets/field_fuzzer.rs
+++ fuzz/fuzz_targets/field_fuzzer.rs
@@ -15,39 +15,42 @@ fuzz_target!(|values: (u64, u64)| {
 
     let (value_u64_a, value_u64_b) = values;
 
-    // let c =  FieldElement::<Stark252PrimeField>::from(2u64);
-    // let d =  FieldElement::<Stark252PrimeField>::from(5u64);
-    // let e = c - d;
-    // println!("resultado de sub {:?}", e.representative() );
-    let d: UBig = "800000000000011000000000000000000000000000000000000000000000001".parse().unwrap();
-
-    let ring = ModuloRing::new(&d);
+    let cairo_prime = 
+        UBig::from_str_radix("800000000000011000000000000000000000000000000000000000000000001", 16).unwrap();
+   
+    let ring = ModuloRing::new(&cairo_prime);
 
 
     let a =  FieldElement::<Stark252PrimeField>::from(value_u64_a);
     let b =  FieldElement::<Stark252PrimeField>::from(value_u64_b);
+    println!();
+
+    let a_expected = ring.from(value_u64_a);
+    let b_expected = ring.from(value_u64_b);
 
     let add_u64 = &a + &b;
-    let addition = ring.from(value_u64_a) + ring.from(value_u64_b);
+    let addition = &a_expected + &b_expected;
     
     assert_eq!(&(add_u64.to_string())[2..], addition.residue().in_radix(16).to_string());
 
     let sub_u64 = &a - &b;
-    let substraction = ring.from(value_u64_a) - ring.from(value_u64_b);
+    let substraction = &a_expected - &b_expected;
     assert_eq!(&(sub_u64.to_string())[2..], substraction.residue().in_radix(16).to_string());
     
     let mul_u64 = &a * &b;
-    let multiplication = ring.from(value_u64_a) * ring.from(value_u64_b);
+    let multiplication = &a_expected * &b_expected;
     assert_eq!(&(mul_u64.to_string())[2..], multiplication.residue().in_radix(16).to_string());
 
-    let pow_u64 = &a.pow(value_u64_b.clone());
-    // let pow = BigUint::from(value_u64_a).pow(value_u64_b);
-    // assert_eq!(&(pow_u64.to_string())[2..], pow.residue().in_radix(16).to_string());
+    let pow = &a.pow(b.representative());
+    let expected_pow = a_expected.pow(&b_expected.residue());
+    assert_eq!(&(pow.to_string())[2..], expected_pow.residue().in_radix(16).to_string());
     
     if value_u64_b != 0 {
-        let div_u64 = &a / &b;
-        let division = ring.from(value_u64_a) / ring.from(value_u64_b);
-        assert_eq!(&(div_u64.to_string())[2..], division.residue().in_radix(16).to_string());
+        
+        let div = &a / &b; 
+        assert_eq!(&div * &b, a.clone());
+        let expected_div = &a_expected / &b_expected;
+        assert_eq!(&(div.to_string())[2..], expected_div.residue().in_radix(16).to_string());
     }
     
     
