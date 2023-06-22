4f9e0c8c3169e5cb398292d0a52e31bc7045d13d
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2023-06-22 15:26:23 -0300

    Update ensure-no_std/src/main.rs

--- ensure-no_std/src/main.rs
+++ ensure-no_std/src/main.rs
@@ -1,14 +1,6 @@
 #![no_std]
 #![no_main]
 
-//use core::panic::PanicInfo;
-
-// /// This function is called on panic.
-//#[panic_handler]
-//fn panic(_info: &PanicInfo) -> ! {
-//    loop {}
-//}
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
