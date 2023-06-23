408812c86eedb72e8bd6d1772de4953d4ecf5d06
Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>
2023-06-23 16:42:12 -0300

    No std ff (#443)
    * WIP
    
    * fix compile
    
    * no_std_initial_version
    
    * no_std_initial_version
    
    * Delete settings.json
    
    * Delete settings.json
    
    * Re add montgomery nbackend
    
    * Update errors in crypto
    
    * Restore rayon as default dep
    
    * Fmt
    
    * missing conditional compile on import
    
    * Remove unused commit
    
    * Remove no_std as default
    
    * CI no_std
    
    * Update ci
    
    * Update ci
    
    * fix typo
    
    * fix typo
    
    * ensure-no_std crate
    
    * ensure-no_std to ci
    
    * clean, ensure std to CI
    
    * Add wasm target to ci
    
    * Lint CI
    
    * From to bytes for montgomery backend
    
    * Bit conversion
    
    * byte conversion fix
    
    * Fix
    
    * Final version
    
    * Fmt
    
    * Re added tests for no std
    
    * Re added tests for no std
    
    * Update ensure-no_std/src/main.rs
    
    * Update math/src/lib.rs
    
    Co-authored-by: Tomás <47506558+MegaRedHand@users.noreply.github.com>
    
    * Fix from in no_std
    
    * Disabled to_bytes from ByteConversion with no std
    
    * Fix clippy
    
    * Fix ensure no std
    
    * Fix ensure no std
    
    * Revert unnecesary change in byte conversion
    
    * Fix no std tests
    
    * Move CI tests no std to git CI
    
    * Fix bench
    
    ---------
    
    Co-authored-by: Pablo Deymonnaz <pdeymon@fi.uba.ar>
    Co-authored-by: Tomás <47506558+MegaRedHand@users.noreply.github.com>

--- math/src/elliptic_curve/short_weierstrass/mod.rs
+++ math/src/elliptic_curve/short_weierstrass/mod.rs
@@ -1,7 +1,5 @@
 /// Implementation of particular cases of elliptic curves.
 pub mod curves;
-/// Shared errors
-pub mod errors;
 /// Structs for points
 pub mod point;
 /// Common behaviour for Elliptic curves.
