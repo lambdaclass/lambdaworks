49b1376922a2cb9f5bbc96a80602909820fb9b67
MauroFab <maurotoscano2@gmail.com>
2023-06-26 15:47:22 -0300

    Add generalization

--- crypto/src/merkle_tree/backends/batch_512_bits.rs
+++ crypto/src/merkle_tree/backends/batch_512_bits.rs
@@ -4,7 +4,7 @@ use lambdaworks_math::{
     field::{element::FieldElement, traits::IsField},
     traits::ByteConversion,
 };
-use sha3::{Digest};
+use sha3::{Digest, digest::{generic_array::GenericArray, OutputSizeUser}};
 
 use crate::merkle_tree::traits::IsMerkleTreeBackend;
 
@@ -27,6 +27,7 @@ impl<F, D: Digest> IsMerkleTreeBackend for Batch512BitsTree<F, D>
 where
     F: IsField,
     FieldElement<F>: ByteConversion,
+    [u8; 64]: From<GenericArray<u8, <D as OutputSizeUser>::OutputSize>>
 {
     type Node = [u8; 64];
     type Data = Vec<FieldElement<F>>;
@@ -56,6 +57,7 @@ mod tests {
     use lambdaworks_math::field::{
         element::FieldElement, fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
     };
+    use sha2::Sha512;
     use sha3::{Keccak512, Sha3_512};
 
     use crate::merkle_tree::{backends::batch_512_bits::Batch512BitsTree, merkle::MerkleTree};
@@ -95,5 +97,22 @@ mod tests {
         let merkle_tree = MerkleTree::<Batch512BitsTree<F, Keccak512>>::build(&values);
         let proof = merkle_tree.get_proof_by_pos(0).unwrap();
         assert!(proof.verify::<Batch512BitsTree<F, Keccak512>>(&merkle_tree.root, 0, &values[0]));
+    }
+
+    #[test]
+    fn hash_data_field_element_backend_works_with_sha2_512() {
+        let values = [
+            vec![FE::from(2u64), FE::from(11u64)],
+            vec![FE::from(3u64), FE::from(14u64)],
+            vec![FE::from(4u64), FE::from(7u64)],
+            vec![FE::from(5u64), FE::from(3u64)],
+            vec![FE::from(6u64), FE::from(5u64)],
+            vec![FE::from(7u64), FE::from(16u64)],
+            vec![FE::from(8u64), FE::from(19u64)],
+            vec![FE::from(9u64), FE::from(21u64)],
+        ];
+        let merkle_tree = MerkleTree::<Batch512BitsTree<F, Sha512>>::build(&values);
+        let proof = merkle_tree.get_proof_by_pos(0).unwrap();
+        assert!(proof.verify::<Batch512BitsTree<F, Sha512>>(&merkle_tree.root, 0, &values[0]));
     }
 }
