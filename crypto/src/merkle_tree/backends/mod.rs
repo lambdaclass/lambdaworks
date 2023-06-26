552a6e0f2df57b029c31e8ffac96549c790bb922
MauroFab <maurotoscano2@gmail.com>
2023-06-26 16:15:30 -0300

    Add default types

--- crypto/src/merkle_tree/backends/mod.rs
+++ crypto/src/merkle_tree/backends/mod.rs
@@ -2,3 +2,6 @@ pub mod batch_256_bits;
 pub mod batch_512_bits;
 pub mod hash_256_bits;
 pub mod hash_512_bits;
+/// Configurations for merkle trees
+/// Setting generics to some value
+pub mod types;
