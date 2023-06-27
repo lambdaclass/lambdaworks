f85fb5660d3701cdab8225a86cd749bc9e22854f
gabrielbosio <gabrielbosio95@gmail.com>
2023-06-27 19:09:34 -0300

    Remove state import in state module

--- math/src/fft/gpu/cuda/state.rs
+++ math/src/fft/gpu/cuda/state.rs
@@ -1,5 +1,5 @@
 use crate::{
-    fft::{errors::FFTError, gpu::cuda::state::CudaState},
+    fft::errors::FFTError,
     field::{
         element::FieldElement,
         traits::{IsFFTField, RootsConfig},
