b9a2aa83d5d686633c0e250b2c64a395b20f4d1b
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-06-28 11:56:35 -0300

    Refactor; Add error comparison

--- fuzz/fuzz_targets/polynomial_fft_diff_fuzzer.rs
+++ fuzz/fuzz_targets/polynomial_fft_diff_fuzzer.rs
@@ -4,6 +4,7 @@ use lambdaworks_math::{
     fft::{
         gpu::metal::polynomial::{evaluate_fft_metal, interpolate_fft_metal},
         polynomial::{evaluate_fft_cpu, interpolate_fft_cpu},
+        errors::FFTError
     },
     field::{
         fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
@@ -12,43 +13,47 @@ use lambdaworks_math::{
     unsigned_integer::element::UnsignedInteger
 };
 
-fuzz_target!(|values: (Vec<[u64;4]>, Vec<[u64;4]>)| {
-    let (mut input_raw, mut twiddles_raw) = values;
-    let mut inputs = Vec::new();
-    let mut twiddles = Vec::new();
+use lambdaworks_gpu::metal::abstractions::errors::MetalError;
 
-    if input_raw.len() == 0 {
-        input_raw.push([1u64;4]);
+fn compare_errors(metal_error: MetalError, fft_error: FFTError) -> bool {
+    match (metal_error, fft_error) {
+        (MetalError::RootOfUnityError(_, nth_metal), FFTError::RootOfUnityError(nth_cpu)) => nth_metal == nth_cpu,
+        (MetalError::InputError(len_metal), FFTError::InputError(len_cpu)) => len_metal == len_cpu,
+        _ => false
+    }
+}
+
+fuzz_target!(|data: Vec<[u64;4]>| {
+    let mut inputs_raw = data;
+    let mut inputs = Vec::new();
+
+    if inputs_raw.len() == 0 {
+        inputs_raw.push([1u64;4]);
     }
 
-    for i in 0..input_raw.len() {
-        let input_value = UnsignedInteger::<4>::from_limbs(input_raw[i]);
+    for i in 0..inputs_raw.len() {
+        let input_value = UnsignedInteger::<4>::from_limbs(inputs_raw[i]);
         inputs.push(FieldElement::<Stark252PrimeField>::from_raw(&input_value))
     }
 
-    if twiddles_raw.len() == 0 {
-        twiddles_raw.push([1u64;4]);
-    }
-
-    for i in 0..twiddles_raw.len() {
-        let twiddle_value = UnsignedInteger::<4>::from_limbs(twiddles_raw[i]);
-        twiddles.push(FieldElement::<Stark252PrimeField>::from_raw(&twiddle_value))
-    }
-
-    let evaluated_fft_cpu = evaluate_fft_cpu(&inputs);
-    let evaluated_fft_metal = evaluate_fft_metal(&inputs);
-
-    match evaluated_fft_cpu {
-        Ok(fft_evals_cpu) => {
-            let fft_evals_metal = evaluated_fft_metal.unwrap();
-            match interpolate_fft_cpu(&fft_evals_cpu) {
-                Ok(interpolated_cpu) => {
-                    assert_eq!(interpolate_fft_metal(&fft_evals_metal).unwrap(), interpolated_cpu)
-                },
-                Err(_) => {assert!(interpolate_fft_metal(&fft_evals_metal).is_err())}
-            }
+    let (fft_eval_metal, fft_eval_cpu) = match (evaluate_fft_metal(&inputs), evaluate_fft_cpu(&inputs)) {
+        (Ok(fft_eval_metal), Ok(fft_eval_cpu)) => {
+            assert_eq!(fft_eval_metal, fft_eval_cpu);
+            (fft_eval_metal.clone(), fft_eval_cpu.clone())
         },
-        Err(_) => assert!(evaluated_fft_metal.is_err())
+        (Err(err_metal), Err(err_cpu)) => {
+            assert!(compare_errors(err_metal, err_cpu));
+            (inputs.clone(), inputs)
+        },
+        (metal, cpu) => panic!("Evaluate results didn't match. metal.is_err(): {}, cpu.is_err(): {}", metal.is_err(), cpu.is_err())
+    };
+
+    match (interpolate_fft_metal(&fft_eval_metal), interpolate_fft_cpu(&fft_eval_cpu)) {
+        (Ok(interpolated_metal), Ok(interpolated_cpu)) => {
+            assert_eq!(interpolated_metal, interpolated_cpu);
+        },
+        (Err(err_metal), Err(err_cpu)) => assert!(compare_errors(err_metal, err_cpu)),
+        (metal, cpu) => panic!("Interpolate results didn't match. metal.is_err(): {}, cpu.is_err(): {}", metal.is_err(), cpu.is_err())
     };
 });
 
