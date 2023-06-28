b1b2b24d4b20f6ab5022cb4500fee79dbd056dff
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-06-28 09:40:12 -0300

    Minor changes

--- fuzz/fuzz_targets/polynomial_fft_diff_fuzzer.rs
+++ fuzz/fuzz_targets/polynomial_fft_diff_fuzzer.rs
@@ -1,8 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 use lambdaworks_math::{
-    gpu::metal::fft::ops::fft as fft_metal, 
-    fft::ops::fft as fft_cpu, 
+    gpu::metal::fft::polynomial::{evaluate_fft_metal, interpolate_fft_metal}, 
+    polynomial::Polynomial,
     field::{
         fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
         element::FieldElement
@@ -10,15 +10,15 @@ use lambdaworks_math::{
     unsigned_integer::element::UnsignedInteger
 };
 
-use lambdaworks_gpu::metal::abstractions::state::MetalState;
 
 fuzz_target!(|values: (Vec<[u64;4]>, Vec<[u64;4]>)| {
+    println!("{:?}", values);
     let (mut input_raw, mut twiddles_raw) = values;
     let mut inputs = Vec::new();
     let mut twiddles = Vec::new();
 
     if input_raw.len() == 0 {
-        input_raw.push([0u64;4]);
+        input_raw.push([1u64;4]);
     }
 
     for i in 0..input_raw.len() {
@@ -27,20 +27,18 @@ fuzz_target!(|values: (Vec<[u64;4]>, Vec<[u64;4]>)| {
     }
 
     if twiddles_raw.len() == 0 {
-        twiddles_raw.push([0u64;4]);
+        twiddles_raw.push([1u64;4]);
     }
-
 
     for i in 0..twiddles_raw.len() {
         let twiddle_value = UnsignedInteger::<4>::from_limbs(twiddles_raw[i]);
         twiddles.push(FieldElement::<Stark252PrimeField>::from_raw(&twiddle_value))
     }
 
-    let state = MetalState::new(None).unwrap();
+    let evaluated_fields = evaluate_fft_metal(&inputs).unwrap();
+    let interpolated_fields = interpolate_fft_metal(&evaluated_fields).unwrap();
 
-    match fft_cpu(&inputs, &twiddles) {
-        Ok(fft_result) => assert_eq!(fft_result, fft_metal(&inputs, &twiddles, &state).unwrap()),
-        Err(_) => assert!(fft_metal(&inputs, &twiddles, &state).is_err())
-    }
+    let polinomial_inputs =  Polynomial { coefficients: (*inputs).to_vec() };
+    assert_eq!(interpolated_fields, polinomial_inputs);
 });
 
