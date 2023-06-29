cfa85ed085b56dca3d0d43500e76c6ecd38f5cad
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-06-29 18:57:20 -0300

    Add batch poseidon template

---
+++ crypto/src/merkle_tree/backends/batch_poseidon.rs
@@ -0,0 +1,57 @@
+use lambdaworks_math::{
+    field::{
+        element::FieldElement,
+        fields::montgomery_backed_prime_fields::{IsModulus, MontgomeryBackendPrimeField},
+    },
+    unsigned_integer::element::UnsignedInteger,
+};
+use std::fmt::Debug;
+
+use crate::{hash::poseidon::Poseidon, merkle_tree::traits::IsMerkleTreeBackend};
+
+use super::poseidon::TreePoseidon;
+
+#[derive(Clone)]
+pub struct BatchPoseidon<M, const NUM_LIMBS: usize>
+where
+    M: IsModulus<UnsignedInteger<NUM_LIMBS>> + Clone + Debug,
+{
+    hasher: Poseidon<MontgomeryBackendPrimeField<M, NUM_LIMBS>>,
+}
+
+impl<M, const NUM_LIMBS: usize> Default for BatchPoseidon<M, NUM_LIMBS>
+where
+    M: IsModulus<UnsignedInteger<NUM_LIMBS>> + Clone + Debug,
+{
+    fn default() -> Self {
+        let hasher = Poseidon::default();
+        BatchPoseidon { hasher }
+    }
+}
+
+impl<M, const NUM_LIMBS: usize> IsMerkleTreeBackend for TreePoseidon<M, NUM_LIMBS>
+where
+    M: IsModulus<UnsignedInteger<NUM_LIMBS>> + Clone + Debug,
+{
+    type Node = FieldElement<MontgomeryBackendPrimeField<M, NUM_LIMBS>>;
+    type Data = Vec<FieldElement<MontgomeryBackendPrimeField<M, NUM_LIMBS>>>;
+
+    fn hash_data(&self, input: &Self::Data) -> Self::Node {
+        let mut hasher = D::new();
+        for element in input.iter() {
+            hasher.update(element.to_bytes_be());
+        }
+        let mut result_hash = [0_u8; 32];
+        result_hash.copy_from_slice(&hasher.finalize());
+        result_hash
+    }
+
+    fn hash_new_parent(&self, left: &[u8; 32], right: &[u8; 32]) -> [u8; 32] {
+        let mut hasher = D::new();
+        hasher.update(left);
+        hasher.update(right);
+        let mut result_hash = [0_u8; 32];
+        result_hash.copy_from_slice(&hasher.finalize());
+        result_hash
+    }
+}
--- crypto/src/merkle_tree/backends/mod.rs
+++ crypto/src/merkle_tree/backends/mod.rs
@@ -1,5 +1,6 @@
 pub mod batch_256_bits;
 pub mod batch_512_bits;
+pub mod batch_poseidon;
 pub mod hash_256_bits;
 pub mod hash_512_bits;
 pub mod poseidon;
