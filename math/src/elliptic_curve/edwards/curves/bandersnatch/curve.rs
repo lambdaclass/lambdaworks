2c9ea52359c0845e7d8f8caa39072e11e0f95bb6
MatteoMer <matteo.merv@gmail.com>
2023-07-17 12:30:49 +0200

    switching from SW to TE

--- math/src/elliptic_curve/edwards/curves/bandersnatch/curve.rs
+++ math/src/elliptic_curve/edwards/curves/bandersnatch/curve.rs
@@ -1,8 +1,8 @@
 
 use crate::elliptic_curve::traits::IsEllipticCurve;
-use crate::elliptic_curve::short_weierstrass::point::ShortWeierstrassProjectivePoint;
+use crate::elliptic_curve::edwards::point::EdwardsProjectivePoint;
 use crate::{
-    elliptic_curve::short_weierstrass::traits::IsShortWeierstrass, field::element::FieldElement,
+    elliptic_curve::edwards::traits::IsEdwards, field::element::FieldElement,
 };
 pub use super::field_extension::FqField;
 
@@ -13,27 +13,29 @@ pub struct BandersnatchCurve;
 
 impl IsEllipticCurve for BandersnatchCurve {
     type BaseField = BaseBandersnatchFieldElement;
-    type PointRepresentation = ShortWeierstrassProjectivePoint<Self>;
+    type PointRepresentation = EdwardsProjectivePoint<Self>;
 
     // Values are from https://github.com/arkworks-rs/curves/blob/5a41d7f27a703a7ea9c48512a4148443ec6c747e/ed_on_bls12_381_bandersnatch/src/curves/mod.rs#L120
+    // Converted to Hex
     fn generator() -> Self::PointRepresentation {
         Self::PointRepresentation::new([
-            FieldElement::<Self::BaseField>::new_base("4450F9122AE57D5CBD002014EF530AD63F83A04D7022AEAD47AC4AEA0A73F2F7"),
-            FieldElement::<Self::BaseField>::new_base("1BFF80EF062FE4B1592598412A2B272627F74E164A63E0BE795A3EB57763C1B2"),
+            FieldElement::<Self::BaseField>::new_base("29C132CC2C0B34C5743711777BBE42F32B79C022AD998465E1E71866A252AE18"),
+            FieldElement::<Self::BaseField>::new_base("2A6C669EDA123E0F157D8B50BADCD586358CAD81EEE464605E3167B6CC974166"),
             FieldElement::one()
         ])
     }
 }
 
-impl IsShortWeierstrass for BandersnatchCurve {
+impl IsEdwards for BandersnatchCurve {
     fn a() -> FieldElement<Self::BaseField> {
-        FieldElement::<Self::BaseField>::new_base("17D15ECBE9F0FC8B8C4A118E26DA26E5E32913D24F20541DE0720F8033267935")
+        FieldElement::<Self::BaseField>::new_base("-5")
     }
 
-    fn b() -> FieldElement<Self::BaseField> {
-        FieldElement::<Self::BaseField>::new_base("415FCB20D12E60A811194E6B4D5D4625FA2D24F0AB8C256907BB3D28D5E8BBBF")
+    fn d() -> FieldElement<Self::BaseField> {
+        FieldElement::<Self::BaseField>::new_base("6389C12633C267CBC66E3BF86BE3B6D8CB66677177E54F92B369F2F5188D58E7")
     }
 }
+
 
 
 #[cfg(test)]
@@ -50,14 +52,14 @@ mod tests {
     #[allow(clippy::upper_case_acronyms)]
     type FEE = FieldElement<BaseBandersnatchFieldElement>;
 
-    fn point_1() -> ShortWeierstrassProjectivePoint<BandersnatchCurve> {
+    fn point_1() -> EdwardsProjectivePoint<BandersnatchCurve> {
         let x = FEE::new_base("9697B44AA99E68EA78A5E48044A88E43B4ECF5D3B1833BEA4ED1F96653C4FC2");
         let y = FEE::new_base("393C141D1A492C2289B2528D0C468EEB87FAF4C35964D2470EB262F76B3E0C8");    
 
         BandersnatchCurve::create_point_from_affine(x, y).unwrap()
         }
 
-    fn f1f2g_fixed() -> ShortWeierstrassProjectivePoint<BandersnatchCurve> {
+    fn f1f2g_fixed() -> EdwardsProjectivePoint<BandersnatchCurve> {
         let x = FEE::new_base("248BED2600CB615A6715105E8C33A48E2669775385260EDCCFFA0540A9204865");
         let y = FEE::new_base("5DEB792D33432583D251F8EE85F7090A7BDDBC2113A10323650361F99BAF7254");    
 
@@ -69,6 +71,7 @@ mod tests {
         let f1f2 = point_1();
 
         let g = BandersnatchCurve::generator();
+
         // let f1f2g = EdwardsAffine::from_str(
         //     "(16530491029447613915334753043669938793793987372416328257719459807614119987301, \
         //      42481140308370805476764840229335460092474682686441442216596889726548353970772)",
@@ -80,7 +83,15 @@ mod tests {
         // let f1f2g_fixed = f1f2g_fixed();
 
         assert_eq!(g.clone(), g.clone());
-
         assert_eq!(f1f2.clone() , f1f2.clone());
+    }
+
+    #[test]
+    fn test_create_valid_point_works(){
+        let p = BandersnatchCurve::generator();
+        
+
+        assert_eq!(p,p.clone());
+
     }
 }
--- math/src/elliptic_curve/short_weierstrass/curves/mod.rs
+++ math/src/elliptic_curve/short_weierstrass/curves/mod.rs
@@ -1,5 +1,4 @@
 pub mod bls12_377;
 pub mod bls12_381;
-pub mod bandersnatch;
 pub mod test_curve_1;
 pub mod test_curve_2;
