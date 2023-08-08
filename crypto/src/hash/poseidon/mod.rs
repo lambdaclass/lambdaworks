6341733728e9598ee1e6ca578b9d7ad771e6f2ae
feltroid Prime <feltroidprime@protonmail.com>
2023-08-08 21:38:13 +0200

    StarkPoseidon test ok

--- crypto/src/hash/poseidon/mod.rs
+++ crypto/src/hash/poseidon/mod.rs
@@ -1,47 +1,57 @@
-mod parameters;
 mod cairo_poseidon_constants;
+mod parameters;
 use self::parameters::PermutationParameters;
 
-use lambdaworks_math::field::{element::FieldElement, traits::{IsPrimeField}};
+use lambdaworks_math::field::{element::FieldElement, traits::IsPrimeField};
 use std::ops::{Add, Mul};
-
 
 pub struct Poseidon<F: IsPrimeField> {
     params: PermutationParameters<F>,
     // Suggestion: Add the state here
 }
 
-impl<F: IsPrimeField> Poseidon<F>
-{
+impl<F: IsPrimeField> Poseidon<F> {
     pub fn new_with_params(params: PermutationParameters<F>) -> Self {
         Poseidon { params }
     }
 
-    pub fn ark(&self, state: &mut [FieldElement<F>], round_number: usize) {
-        for i in 0..state.len() {
-            state[i] = &state[i] + &self.params.add_round_constants[round_number][i]
+    pub fn hades_permutation(&self, state: &mut [FieldElement<F>]) {
+        let mut round_number = 0;
+        for _ in 0..self.params.n_full_rounds / 2 {
+            self.full_round(state, round_number);
+            round_number += 1;
+        }
+        for _ in 0..self.params.n_partial_rounds {
+            self.partial_round(state, round_number);
+            round_number += 1;
+        }
+        for _ in 0..self.params.n_full_rounds / 2 {
+            self.full_round(state, round_number);
+            round_number += 1;
         }
     }
 
-    pub fn sbox(&self, state: &mut [FieldElement<F>], round_number: usize) {
-        let is_full_round = round_number < self.params.n_full_rounds / 2
-            || round_number >= self.params.n_full_rounds / 2 + self.params.n_partial_rounds;
-
-        if is_full_round {
-            // full s-box
-            for current_state in state.iter_mut() {
-                *current_state = current_state.pow(self.params.alpha);
-            }
-        } else {
-            // partial s-box
-            let last_state_index = state.len() - 1;
-            state[last_state_index] = state[last_state_index].pow(self.params.alpha);
+    pub fn full_round(&self, state: &mut [FieldElement<F>], round_number: usize) {
+        for i in 0..self.params.state_size {
+            state[i] = &state[i] + &self.params.round_constants[round_number][i];
+            state[i] = state[i].pow(self.params.alpha);
         }
+        self.mix(state);
+    }
+    pub fn partial_round(&self, state: &mut [FieldElement<F>], round_number: usize) {
+        for i in 0..self.params.state_size {
+            state[i] = &state[i] + &self.params.round_constants[round_number][i];
+        }
+
+        state[self.params.state_size - 1] =
+            state[self.params.state_size - 1].pow(self.params.alpha);
+
+        self.mix(state);
     }
 
     pub fn mix(&self, state: &mut [FieldElement<F>]) {
-        let mut new_state: Vec<FieldElement<F>> = Vec::with_capacity(state.len());
-        for i in 0..state.len() {
+        let mut new_state: Vec<FieldElement<F>> = Vec::with_capacity(self.params.state_size);
+        for i in 0..self.params.state_size {
             new_state.push(FieldElement::zero());
             for (j, current_state) in state.iter().enumerate() {
                 let mut mij = self.params.mds_matrix[i][j].clone();
@@ -49,14 +59,39 @@ impl<F: IsPrimeField> Poseidon<F>
                 new_state[i] = new_state[i].clone().add(&mij);
             }
         }
-        state.clone_from_slice(&new_state[0..state.len()]);
+        state.clone_from_slice(&new_state[0..self.params.state_size]);
     }
+}
 
-    fn permute(&self, state: &mut [FieldElement<F>]) {
-        for i in 0..(self.params.n_full_rounds + self.params.n_partial_rounds) {
-            self.ark(state, i);
-            self.sbox(state, i);
-            self.mix(state);
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::hash::poseidon::parameters::{DefaultPoseidonParams, PermutationParameters};
+    use lambdaworks_math::field::{
+        element::FieldElement, fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
+    };
+
+    #[test]
+    fn test_hades_permutation() {
+        let params = PermutationParameters::new_with(DefaultPoseidonParams::CairoStark252);
+
+        let poseidon = Poseidon::new_with_params(params);
+
+        // Initialize a state to test. The exact contents will depend on your specific use case.
+        let mut state: Vec<FieldElement<Stark252PrimeField>> = vec![
+            FieldElement::<Stark252PrimeField>::from_hex("0x9").unwrap(),
+            FieldElement::<Stark252PrimeField>::from_hex("0xb").unwrap(),
+            FieldElement::<Stark252PrimeField>::from_hex("0x2").unwrap(),
+        ];
+
+        poseidon.hades_permutation(&mut state);
+
+        // Compare the result to the expected output. You will need to know the expected output for your specific test case.
+        let expected_state = FieldElement::<Stark252PrimeField>::from_hex(
+            "0x510f3a3faf4084e3b1e95fd44c30746271b48723f7ea9c8be6a9b6b5408e7e6",
+        )
+        .unwrap();
+
+        assert_eq!(state[0], expected_state);
     }
 }
--- crypto/src/hash/poseidon/parameters.rs
+++ crypto/src/hash/poseidon/parameters.rs
@@ -1,16 +1,17 @@
 use lambdaworks_math::field::{element::FieldElement as FE, traits::IsPrimeField};
 
-use crate::hash::poseidon::cairo_poseidon_constants::add_round_constants::ADD_ROUND_CONSTANTS_HEXSTRINGS;
+use crate::hash::poseidon::cairo_poseidon_constants::round_constants::ROUND_CONSTANTS_HEXSTRINGS;
 
 pub struct PermutationParameters<F: IsPrimeField> {
-        /// Exponent for the S box
-        pub alpha: u32,
-        pub n_full_rounds: usize,
-        pub n_partial_rounds: usize,
-        pub add_round_constants: Vec<Vec<FE<F>>>,
-        pub mds_matrix: Vec<Vec<FE<F>>>,
+    /// Exponent for the S box
+    pub alpha: u32,
+    pub n_full_rounds: usize,
+    pub n_partial_rounds: usize,
+    pub round_constants: Vec<Vec<FE<F>>>,
+    pub mds_matrix: Vec<Vec<FE<F>>>,
+    pub state_size: usize,
 }
-pub enum DefaultPoseidonParams{
+pub enum DefaultPoseidonParams {
     /// Poseidon as used by Cairo
     /// with three inputs
     CairoStark252,
@@ -18,47 +19,48 @@ pub enum DefaultPoseidonParams{
 
 /// Parameters for Poseidon
 /// Mds constants and rounds constants should be used for the shared field, even if it technically can work for any field with the same configuration
-impl <F> PermutationParameters<F> where
-F: IsPrimeField  { 
-    pub fn new_with(params: DefaultPoseidonParams) -> Self{
+impl<F> PermutationParameters<F>
+where
+    F: IsPrimeField,
+{
+    pub fn new_with(params: DefaultPoseidonParams) -> Self {
         match params {
-            DefaultPoseidonParams::CairoStark252 => 
-                Self::cairo_stark_params()
+            DefaultPoseidonParams::CairoStark252 => Self::cairo_stark_params(),
         }
     }
 
-    fn cairo_stark_params() -> PermutationParameters<F>{
-
-        let add_round_constants: Vec<Vec<FE<F>>> = 
-            ADD_ROUND_CONSTANTS_HEXSTRINGS
-                .iter()
-                .map(
-                    |[x0,x1,x2]| 
-                    [ 
-                        FE::<F>::from_hex(x0).unwrap(),
-                        FE::<F>::from_hex(x1).unwrap(),
-                        FE::<F>::from_hex(x2).unwrap()
-                    ].to_vec()
-                )
-                .collect();
+    fn cairo_stark_params() -> PermutationParameters<F> {
+        let round_constants: Vec<Vec<FE<F>>> = ROUND_CONSTANTS_HEXSTRINGS
+            .iter()
+            .map(|[x0, x1, x2]| {
+                [
+                    FE::<F>::from_hex(x0).unwrap(),
+                    FE::<F>::from_hex(x1).unwrap(),
+                    FE::<F>::from_hex(x2).unwrap(),
+                ]
+                .to_vec()
+            })
+            .collect();
 
         let mds_matrix = [
-            [FE::<F>::from(3),FE::<F>::from(1),FE::<F>::from(1)].to_vec(),
-            [FE::<F>::from(1),-FE::one(),FE::<F>::from(1)].to_vec(),
-            [FE::<F>::from(1),FE::<F>::from(1),-FE::<F>::from(2)].to_vec()
-        ].to_vec();
+            [FE::<F>::from(3), FE::<F>::from(1), FE::<F>::from(1)].to_vec(),
+            [FE::<F>::from(1), -FE::one(), FE::<F>::from(1)].to_vec(),
+            [FE::<F>::from(1), FE::<F>::from(1), -FE::<F>::from(2)].to_vec(),
+        ]
+        .to_vec();
 
         const RATE: usize = 2;
         const CAPACITY: usize = 1;
         const ALPHA: u32 = 3;
         const N_FULL_ROUNDS: usize = 8;
-        const N_PARTIAL_ROUNDS: usize = 83;     
-        Self{
+        const N_PARTIAL_ROUNDS: usize = 83;
+        Self {
             alpha: ALPHA,
             n_full_rounds: N_FULL_ROUNDS,
             n_partial_rounds: N_PARTIAL_ROUNDS,
-            add_round_constants,
+            round_constants,
             mds_matrix,
+            state_size: RATE + CAPACITY,
         }
     }
 }
