9f3887734c5d41ef2d61f495d14a5a3fa5bc141d
Sumit kumar <106421807+startup-dreamer@users.noreply.github.com>
2023-08-31 20:35:22 +0530

    Fix: from_hex_unchecked implementation for field elements (#510)
    * Fix: from_hex_unchecked implementation for field elements
    
    * Update math/src/field/element.rs
    
    * Fix linter
    
    * fmt
    
    ---------
    
    Co-authored-by: Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>

--- math/src/unsigned_integer/element.rs
+++ math/src/unsigned_integer/element.rs
@@ -440,23 +440,29 @@ impl<const NUM_LIMBS: usize> UnsignedInteger<NUM_LIMBS> {
 
     /// Creates an `UnsignedInteger` from a hexstring
     /// # Panics
-    /// Panics if value is not a hexstring. Shouldn't start with `0x`
+    /// Panics if value is not a hexstring. It can contain `0x` or not.
     pub const fn from_hex_unchecked(value: &str) -> Self {
         let mut result = [0u64; NUM_LIMBS];
         let mut limb = 0;
         let mut limb_index = NUM_LIMBS - 1;
         let mut shift = 0;
-        let value = value.as_bytes();
-        let mut i: usize = value.len();
-        while i > 0 {
-            i -= 1;
-            limb |= match value[i] {
-                c @ b'0'..=b'9' => (c as u64 - '0' as u64) << shift,
-                c @ b'a'..=b'f' => (c as u64 - 'a' as u64 + 10) << shift,
-                c @ b'A'..=b'F' => (c as u64 - 'A' as u64 + 10) << shift,
-                _ => {
-                    panic!("Malformed hex expression.")
-                }
+
+        let value_bytes = value.as_bytes();
+
+        // Remove "0x" if it's at the beginning of the string
+        let mut i = 0;
+        if value_bytes.len() > 2 && value_bytes[0] == b'0' && value_bytes[1] == b'x' {
+            i = 2;
+        }
+
+        let mut j = value_bytes.len();
+        while j > i {
+            j -= 1;
+            limb |= match value_bytes[j] {
+                c @ b'0'..=b'9' => (c as u64 - b'0' as u64) << shift,
+                c @ b'a'..=b'f' => (c as u64 - b'a' as u64 + 10) << shift,
+                c @ b'A'..=b'F' => (c as u64 - b'A' as u64 + 10) << shift,
+                _ => panic!("Malformed hex expression."),
             };
             shift += 4;
             if shift == 64 && limb_index > 0 {
@@ -466,8 +472,8 @@ impl<const NUM_LIMBS: usize> UnsignedInteger<NUM_LIMBS> {
                 shift = 0;
             }
         }
-        result[limb_index] = limb;
 
+        result[limb_index] = limb;
         UnsignedInteger { limbs: result }
     }
 
@@ -1045,6 +1051,12 @@ mod tests_u384 {
     #[test]
     fn construct_new_integer_from_hex_1() {
         let a = U384::from_hex_unchecked("1");
+        assert_eq!(a.limbs, [0, 0, 0, 0, 0, 1]);
+    }
+
+    #[test]
+    fn construct_new_integer_from_zero_x_1() {
+        let a = U384::from_hex_unchecked("0x1");
         assert_eq!(a.limbs, [0, 0, 0, 0, 0, 1]);
     }
 
