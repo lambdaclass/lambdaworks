bc91cf8b421eaf6010b87adabdfb7e92550bd9c5
MauroFab <maurotoscano2@gmail.com>
2023-09-19 18:45:19 -0300

    Fix fuzzer not building

--- fuzz/deserialize_stark_proof/fuzz_targets/deserialize.rs
+++ fuzz/deserialize_stark_proof/fuzz_targets/deserialize.rs
@@ -1,6 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use lambdaworks_stark::starks::proof::stark::StarkProof;
+use stark_platinum_prover::proof::stark::StarkProof;
 use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::Stark252PrimeField;
 use lambdaworks_math::traits::{Deserializable, Serializable};
 
