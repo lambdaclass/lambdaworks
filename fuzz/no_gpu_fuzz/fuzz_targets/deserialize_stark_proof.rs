b5614cd82906772a4e1b536b44922fe4fb7e0030
MauroFab <maurotoscano2@gmail.com>
2023-09-19 18:58:08 -0300

    Move deserialize fuzzer to no gpu fuzzers

--- fuzz/no_gpu_fuzz/fuzz_targets/deserialize_stark_proof.rs
+++ fuzz/no_gpu_fuzz/fuzz_targets/deserialize_stark_proof.rs
@@ -2,11 +2,11 @@
 use libfuzzer_sys::fuzz_target;
 use stark_platinum_prover::proof::stark::StarkProof;
 use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::Stark252PrimeField;
-use lambdaworks_math::traits::{Deserializable, Serializable};
+use lambdaworks_math::traits::Deserializable;
 
 
 fuzz_target!(|data: Vec<u8>| {
     
-    let proof = StarkProof::<Stark252PrimeField>::deserialize(&data);
+    let _proof = StarkProof::<Stark252PrimeField>::deserialize(&data);
 
 });
