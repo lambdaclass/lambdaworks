22a87895fcf324d28a2d2c891ae6e02969b13cbf
PatStiles <pdstiles78@gmail.com>
2023-09-21 03:30:27 -0500

    fix representative field all tests passing

--- math/src/field/fields/u32_mersenne_prime_field.rs
+++ math/src/field/fields/u32_mersenne_prime_field.rs
@@ -1,13 +1,42 @@
 use core::ops::BitXorAssign;
 
-use crate::field::{errors::FieldError, traits::IsField};
+use crate::{
+    errors::CreationError,
+    field::{
+        errors::FieldError,
+        traits::{IsField, IsPrimeField},
+    },
+};
 
+/// Represents a 31 bit integer value
+/// Invariants:
+///      31st bit is clear
+///      n < MODULUS
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Mersenne31Field;
 
-pub const MERSENNE_31_PRIME_FIELD_ORDER: u32 = (1 << 31) - 1;
+impl Mersenne31Field {
+    fn weak_reduce(n: u32) -> u32 {
+        // To reduce 'n' to 31 bits we clear its MSB, then add it back in its reduced form.
+        let msb = n & (1 << 31);
+        let msb_reduced = msb >> 31;
+        let res = msb ^ n;
 
-//TODO implement Montgomery Backend
+        // assert msb_reduced fits within 31 bits
+        debug_assert!((res >> 31) == 0 && (msb_reduced >> 1) == 0);
+        res + msb_reduced
+    }
+
+    fn as_representative(n: &u32) -> u32 {
+        if *n == MERSENNE_31_PRIME_FIELD_ORDER {
+            0
+        } else {
+            *n
+        }
+    }
+}
+
+pub const MERSENNE_31_PRIME_FIELD_ORDER: u32 = (1 << 31) - 1;
 
 //NOTE: This implementation was inspired by and borrows from the work done by the Plonky3 team
 // https://github.com/Plonky3/Plonky3/blob/main/mersenne-31/src/lib.rs
@@ -24,19 +53,23 @@ impl IsField for Mersenne31Field {
         let msb = sum & (1 << 31);
         sum.bitxor_assign(msb);
         sum += u32::from(msb != 0);
-        Self::from_base_type(sum)
+        //assert 31 bit clear
+        debug_assert!((sum >> 31) == 0);
+        Self::as_representative(&sum)
     }
 
     /// Returns the multiplication of `a` and `b`.
+    // Note: for powers of 2 we can perform bit shifting this would involve overriding the trait implementation
     fn mul(a: &u32, b: &u32) -> u32 {
         let prod = u64::from(*a) * u64::from(*b);
-        let prof_lo = (prod as u32) & ((1 << 31) - 1);
+        let prod_lo = (prod as u32) & ((1 << 31) - 1);
         let prod_hi = (prod >> 31) as u32;
-        Self::add(
-            &Self::from_base_type(prof_lo),
-            &Self::from_base_type(prod_hi),
-        )
+        //assert prod_hi and prod_lo 31 bit clear
+        debug_assert!((prod_lo >> 31) == 0 && (prod_hi >> 31) == 0);
+        Self::add(&prod_lo, &prod_hi)
     }
+
+    //todo: override power implementation to delegate to bitshifting if multiple of 2
 
     // Need to optimize
     fn sub(a: &u32, b: &u32) -> u32 {
@@ -45,7 +78,8 @@ impl IsField for Mersenne31Field {
 
     /// Returns the additive inverse of `a`.
     fn neg(a: &u32) -> u32 {
-        Self::sub(&MERSENNE_31_PRIME_FIELD_ORDER, a)
+        // NOTE: MODULUS known to have 31 bit clear
+        MERSENNE_31_PRIME_FIELD_ORDER - a
     }
 
     /// Returns the multiplicative inverse of `a`.
@@ -53,14 +87,13 @@ impl IsField for Mersenne31Field {
         if *a == Self::zero() {
             return Err(FieldError::InvZeroError);
         }
-        let p1 = *a;
-        let p1_1 = Self::mul(&Self::pow(&p1, 2u32), &p1);
+        let p1_1 = Self::mul(&Self::pow(a, 4u32), a);
         let p4 = Self::mul(&Self::square(&p1_1), &p1_1);
-        let p8 = Self::mul(&Self::pow(&p4, 8u32), &p4);
-        let p16 = Self::mul(&Self::pow(&p8, 16u32), &p8);
-        let p24 = Self::mul(&Self::pow(&p16, 16u32), &p8);
-        let p28 = Self::mul(&Self::pow(&p24, 8u32), &p4);
-        let p29_1 = Self::mul(&Self::pow(&p28, 6u32), &p1_1);
+        let p8 = Self::mul(&Self::pow(&p4, 16u32), &p4);
+        let p16 = Self::mul(&Self::pow(&p8, 256u32), &p8);
+        let p24 = Self::mul(&Self::pow(&p16, 256u32), &p8);
+        let p28 = Self::mul(&Self::pow(&p24, 16u32), &p4);
+        let p29_1 = Self::mul(&Self::pow(&p28, 8u32), &p1_1);
         Ok(p29_1)
     }
 
@@ -72,7 +105,7 @@ impl IsField for Mersenne31Field {
 
     /// Returns a boolean indicating whether `a` and `b` are equal or not.
     fn eq(a: &u32, b: &u32) -> bool {
-        a == b
+        Self::as_representative(a) == Self::representative(b)
     }
 
     /// Returns the additive neutral element.
@@ -87,156 +120,255 @@ impl IsField for Mersenne31Field {
 
     /// Returns the element `x * 1` where 1 is the multiplicative neutral element.
     fn from_u64(x: u64) -> u32 {
-        let x_u32: u32 = x.try_into().expect("Too large to be canonical Mersenne31");
-        Self::from_base_type(x_u32)
+        let (lo, hi) = (x as u32 as u64, x >> 32);
+        // 2^32 = 2 (mod Mersenne 31 bit prime)
+        // t <= (2^32 - 1) + 2 * (2^32 - 1) = 3 * 2^32 - 3 = 6 * 2^31 - 3
+        let t = lo + 2 * hi;
+
+        const MASK: u64 = (1 << 31) - 1;
+        let (lo, hi) = ((t & MASK) as u32, (t >> 31) as u32);
+        // 2^31 = 1 mod Mersenne31
+        // lo < 2^31, hi < 6, so lo + hi < 2^32.
+        Self::weak_reduce(lo + hi)
     }
 
     /// Takes as input an element of BaseType and returns the internal representation
     /// of that element in the field.
     fn from_base_type(x: u32) -> u32 {
-        //NOTE: we could abstract this check to a ::new() function of a wrapper struct for u32. I decided to go for the simplest form.
-        debug_assert!(x < MERSENNE_31_PRIME_FIELD_ORDER);
+        Self::weak_reduce(x)
+    }
+}
+
+impl IsPrimeField for Mersenne31Field {
+    type RepresentativeType = u32;
+
+    // Since our invariant guarantees that `value` fits in 31 bits, there is only one possible
+    // `value` that is not canonical, namely 2^31 - 1 = p = 0.
+    fn representative(x: &u32) -> u32 {
         debug_assert!((x >> 31) == 0);
-        x
+        Self::as_representative(x)
+    }
+
+    fn field_bit_size() -> usize {
+        ((MERSENNE_31_PRIME_FIELD_ORDER - 1).ilog2() + 1) as usize
+    }
+
+    fn from_hex(hex_string: &str) -> Result<Self::BaseType, CreationError> {
+        let mut hex_string = hex_string;
+        // Remove 0x if it's on the string
+        let mut char_iterator = hex_string.chars();
+        if hex_string.len() > 2
+            && char_iterator.next().unwrap() == '0'
+            && char_iterator.next().unwrap() == 'x'
+        {
+            hex_string = &hex_string[2..];
+        }
+        u32::from_str_radix(hex_string, 16).map_err(|_| CreationError::InvalidHexString)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    type Fp = Mersenne31Field;
+    type F = Mersenne31Field;
 
     #[test]
-    fn one_plus_one_is_two() {
-        let num1 = Fp::one();
-        let num2 = Fp::one();
-        let num3 = Fp::add(&num1, &num2);
-        assert_eq!(num3, 2u32);
+    fn from_hex_for_b_is_11() {
+        assert_eq!(F::from_hex("B").unwrap(), 11);
     }
 
     #[test]
-    fn neg_one_plus_one_is_zero() {
-        let num1 = Fp::neg(&Fp::one());
-        let num2 = Fp::one();
-        let num3 = Fp::add(&num1, &num2);
-        assert_eq!(num3, Fp::zero());
+    fn from_hex_for_0x1_a_is_26() {
+        assert_eq!(F::from_hex("0x1a").unwrap(), 26);
     }
 
     #[test]
-    fn neg_one_plus_two_is_one() {
-        let num1 = Fp::neg(&Fp::one());
-        let num2 = Fp::from_base_type(2u32);
-        let num3 = Fp::add(&num1, &num2);
-        assert_eq!(num3, Fp::one());
+    fn bit_size_of_field_is_31() {
+        assert_eq!(
+            <F as crate::field::traits::IsPrimeField>::field_bit_size(),
+            31
+        );
     }
 
     #[test]
-    fn neg_one_plus_neg_one_is_() {
-        let num1 = Fp::from_base_type(MERSENNE_31_PRIME_FIELD_ORDER - 1);
-        let num2 = Fp::one();
-        let num3 = Fp::add(&num1, &num2);
-        assert_eq!(num3, Fp::zero());
+    fn one_plus_1_is_2() {
+        let a = F::one();
+        let b = F::one();
+        let c = F::add(&a, &b);
+        assert_eq!(c, 2u32);
     }
 
     #[test]
-    fn sub_test_1() {
-        let num1 = Fp::one();
-        let num2 = Fp::one();
-        let num3 = Fp::sub(&num1, &num2);
-        assert_eq!(num3, Fp::zero())
+    fn neg_1_plus_1_is_0() {
+        let a = F::neg(&F::one());
+        let b = F::one();
+        let c = F::add(&a, &b);
+        assert_eq!(c, F::zero());
     }
 
     #[test]
-    fn sub_test_2() {
-        let num1 = Fp::from_base_type(2u32);
-        let num2 = Fp::from_base_type(2u32);
-        let num3 = Fp::sub(&num1, &num2);
-        assert_eq!(num3, Fp::zero());
+    fn neg_1_plus_2_is_1() {
+        let a = F::neg(&F::one());
+        let b = F::from_base_type(2u32);
+        let c = F::add(&a, &b);
+        assert_eq!(c, F::one());
     }
 
     #[test]
-    fn sub_test_3() {
-        let num1 = Fp::neg(&Fp::one());
-        let num2 = Fp::neg(&Fp::one());
-        let num3 = Fp::sub(&num1, &num2);
-        assert_eq!(num3, Fp::zero());
+    fn max_order_plus_1_is_0() {
+        let a = F::from_base_type(MERSENNE_31_PRIME_FIELD_ORDER - 1);
+        let b = F::one();
+        let c = F::add(&a, &b);
+        assert_eq!(c, F::zero());
     }
 
     #[test]
-    fn sub_test_4() {
-        let num1 = Fp::from_base_type(2u32);
-        let num2 = Fp::one();
-        let num3 = Fp::sub(&num1, &num2);
-        assert_eq!(num3, Fp::one());
+    fn comparing_13_and_13_are_equal() {
+        let a = F::from_base_type(13);
+        let b = F::from_base_type(13);
+        assert_eq!(a, b);
     }
 
     #[test]
-    fn sub_test_5() {
-        let num1 = Fp::neg(&Fp::one());
-        let num2 = Fp::zero();
-        let num3 = Fp::sub(&num1, &num2);
-        assert_eq!(num3, Fp::neg(&Fp::one()));
-    }
-
-    /*
-    #[test]
-    fn neg_test() {
-        let num1 = Fp::from_base_type();
-        let num2 = Fp::neg();
-        assert_eq!(num2, Fp::from_base_type());
+    fn comparing_13_and_8_they_are_not_equal() {
+        let a = F::from_base_type(13);
+        let b = F::from_base_type(8);
+        assert_ne!(a, b);
     }
 
     #[test]
-    fn mul_test_1() {
-        let num1 = Fp::from_base_type();
-        let num2 = Fp::from_base_type();
-        let num3 = Fp::mul(&num1, &num2);
-        assert_eq!(num3, Fp::from_base_type());
+    fn one_sub_1_is_0() {
+        let a = F::one();
+        let b = F::one();
+        let c = F::sub(&a, &b);
+        assert_eq!(c, F::zero());
     }
 
     #[test]
-    fn mul_test_2() {
-        let num1 = Fp::from_base_type();
-        let num2 = Fp::from_base_type();
-        let num3 = Fp::mul(&num1, &num2);
-        assert_eq!(num3, Fp::from_base_type());
+    fn zero_sub_1_is_order_minus_1() {
+        let a = F::zero();
+        let b = F::one();
+        let c = F::sub(&a, &b);
+        assert_eq!(c, MERSENNE_31_PRIME_FIELD_ORDER - 1);
     }
 
     #[test]
-    fn pow_test() {
-        let num1 = Fp::from_base_type();
-        let num2 = Fp::pow(&num1, 65537u64);
-        assert_eq!(num2, Fp::from_base_type());
+    fn neg_1_sub_neg_1_is_0() {
+        let a = F::neg(&F::one());
+        let b = F::neg(&F::one());
+        let c = F::sub(&a, &b);
+        assert_eq!(c, F::zero());
     }
-    */
 
     #[test]
-    fn inv_0_test() {
-        let result = Fp::inv(&Fp::zero());
+    fn neg_1_sub_1_is_neg_1() {
+        let a = F::neg(&F::one());
+        let b = F::zero();
+        let c = F::sub(&a, &b);
+        assert_eq!(c, F::neg(&F::one()));
+    }
+
+    #[test]
+    fn mul_neutral_element() {
+        let a = F::from_base_type(1);
+        let b = F::from_base_type(2);
+        let c = F::mul(&a, &b);
+        assert_eq!(c, F::from_base_type(2));
+    }
+
+    #[test]
+    fn mul_2_3_is_6() {
+        let a = F::from_base_type(2);
+        let b = F::from_base_type(3);
+        assert_eq!(a * b, F::from_base_type(6));
+    }
+
+    #[test]
+    fn mul_order_neg_1() {
+        let a = F::from_base_type(MERSENNE_31_PRIME_FIELD_ORDER - 1);
+        let b = F::from_base_type(MERSENNE_31_PRIME_FIELD_ORDER - 1);
+        let c = F::mul(&a, &b);
+        assert_eq!(c, F::from_base_type(1));
+    }
+
+    #[test]
+    fn pow_p_neg_1() {
+        assert_eq!(
+            F::pow(&F::from_base_type(2), MERSENNE_31_PRIME_FIELD_ORDER - 1),
+            F::one()
+        )
+    }
+
+    #[test]
+    fn inv_0_error() {
+        let result = F::inv(&F::zero());
         assert!(matches!(result, Err(FieldError::InvZeroError)));
     }
 
     #[test]
-    fn inv_2_test() {
-        let result = Fp::inv(&Fp::from_base_type(2u32));
-        assert!(matches!(result, Err(FieldError::InvZeroError)));
+    fn inv_2() {
+        let result = F::inv(&F::from_base_type(2u32)).unwrap();
+        // sage: 1 / F(2) = 1073741824
+        assert_eq!(result, 1073741824);
+    }
+
+    #[test]
+    fn pow_2_3() {
+        assert_eq!(F::pow(&F::from_base_type(2), 3_u64), 8)
+    }
+
+    #[test]
+    fn div_1() {
+        assert_eq!(F::div(&F::from_base_type(2), &F::from_base_type(1)), 2)
+    }
+
+    #[test]
+    fn div_4_2() {
+        assert_eq!(F::div(&F::from_base_type(4), &F::from_base_type(2)), 2)
+    }
+
+    // 1431655766
+    #[test]
+    fn div_4_3() {
+        // sage: F(4) / F(3) = 1431655766
+        assert_eq!(
+            F::div(&F::from_base_type(4), &F::from_base_type(3)),
+            1431655766
+        )
+    }
+
+    #[test]
+    fn two_plus_its_additive_inv_is_0() {
+        let two = F::from_base_type(2);
+
+        assert_eq!(F::add(&two, &F::neg(&two)), F::zero())
     }
 
     #[test]
     fn from_u64_test() {
-        let num = Fp::from_u64(1u64);
-        assert_eq!(num, Fp::one());
+        let num = F::from_u64(1u64);
+        assert_eq!(num, F::one());
     }
 
     #[test]
-    #[should_panic]
-    fn from_u64_max_test() {
-        Fp::from_u64(u64::MAX);
+    fn creating_a_field_element_from_its_representative_returns_the_same_element_1() {
+        let change = 1;
+        let f1 = F::from_base_type(MERSENNE_31_PRIME_FIELD_ORDER + change);
+        let f2 = F::from_base_type(Mersenne31Field::representative(&f1));
+        assert_eq!(f1, f2);
+    }
+
+    #[test]
+    fn creating_a_field_element_from_its_representative_returns_the_same_element_2() {
+        let change = 8;
+        let f1 = F::from_base_type(MERSENNE_31_PRIME_FIELD_ORDER + change);
+        let f2 = F::from_base_type(Mersenne31Field::representative(&f1));
+        assert_eq!(f1, f2);
     }
 
     #[test]
     fn from_base_type_test() {
-        let num2 = Fp::from_base_type(1u32);
-        assert_eq!(num2, Fp::one());
+        let b = F::from_base_type(1u32);
+        assert_eq!(b, F::one());
     }
 }
