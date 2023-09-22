d9a850fd94038dd854d35e468c7e13bc6b1c58e4
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-09-22 10:55:56 -0300

    add test

--- provers/stark/src/transcript.rs
+++ provers/stark/src/transcript.rs
@@ -171,127 +171,6 @@ mod tests {
 
     use crate::transcript::{IsStarkTranscript, StoneProverTranscript};
 
-    // #[test]
-    // fn test_stark_prime_field_random_to_field_32() {
-    //     #[rustfmt::skip]
-    //     let mut randomness: [u8; 32] = [
-    //         248, 0, 0, 0, 0, 0, 0, 0,
-    //         0, 0, 0, 0, 0, 0, 0, 0,
-    //         0, 0, 0, 0, 0, 0, 0, 0,
-    //         0, 0, 0, 0, 0, 0, 0, 32,
-    //     ];
-    //
-    //     type FE = FieldElement<Stark252PrimeField>;
-    //     let field_element: FE = randomness_to_field(&mut randomness);
-    //     let expected_fe = FE::from(32u64);
-    //     assert_eq!(field_element, expected_fe)
-    // }
-    //
-    // #[test]
-    // fn test_stark_prime_field_random_to_fiel_repeated_f_and_zero() {
-    //     #[rustfmt::skip]
-    //     let mut randomness: [u8; 32] = [
-    //         255, 0, 255, 0, 255, 0, 255, 0,
-    //         255, 0, 255, 0, 255, 0, 255, 0,
-    //         255, 0, 255, 0, 255, 0, 255, 0,
-    //         255, 0, 255, 0, 255, 0, 255, 0,
-    //     ];
-    //
-    //     type FE = FieldElement<Stark252PrimeField>;
-    //
-    //     // 251 bits should be used (252 of StarkField - 1) to avoid duplicates
-    //     // This leaves a 7
-    //     let expected_fe = FE::from_hex_unchecked(
-    //         "\
-    //         0700FF00FF00FF00\
-    //         FF00FF00FF00FF00\
-    //         FF00FF00FF00FF00\
-    //         FF00FF00FF00FF00",
-    //     );
-    //
-    //     let field_element: FE = randomness_to_field(&mut randomness);
-    //
-    //     assert_eq!(field_element, expected_fe)
-    // }
-    //
-    // #[test]
-    // fn test_241_bit_random_to_field() {
-    //     #[derive(Clone, Debug)]
-    //     pub struct TestModulus;
-    //     impl IsModulus<U256> for TestModulus {
-    //         const MODULUS: U256 = U256::from_hex_unchecked(
-    //             "\
-    //             0001000000000011\
-    //             0000000000000000\
-    //             0000000000000000\
-    //             0000000000000001",
-    //         );
-    //     }
-    //
-    //     pub type TestField = U256PrimeField<TestModulus>;
-    //
-    //     #[rustfmt::skip]
-    //     let mut randomness: [u8; 32] = [
-    //         255, 255, 255, 1, 2, 3, 4, 5,
-    //         6, 7, 8, 1, 2, 3, 4, 5,
-    //         6, 7, 8, 1, 2, 3, 4, 5,
-    //         6, 7, 8, 1, 2, 3, 4, 5,
-    //     ];
-    //
-    //     type FE = FieldElement<TestField>;
-    //
-    //     let expected_fe = FE::from_hex_unchecked(
-    //         "\
-    //         0000FF0102030405\
-    //         0607080102030405\
-    //         0607080102030405\
-    //         0607080102030405",
-    //     );
-    //
-    //     let field_element: FE = randomness_to_field(&mut randomness);
-    //
-    //     assert_eq!(field_element, expected_fe);
-    // }
-    //
-    // #[test]
-    // fn test_249_bit_random_to_field() {
-    //     #[derive(Clone, Debug)]
-    //     pub struct TestModulus;
-    //     impl IsModulus<U256> for TestModulus {
-    //         const MODULUS: U256 = U256::from_hex_unchecked(
-    //             "\
-    //             0200000000000011\
-    //             0000000000000000\
-    //             0000000000000000\
-    //             0000000000000001",
-    //         );
-    //     }
-    //
-    //     pub type TestField = U256PrimeField<TestModulus>;
-    //
-    //     #[rustfmt::skip]
-    //     let mut randomness: [u8; 32] = [
-    //         255, 0, 255, 0, 255, 0, 255, 0,
-    //         255, 0, 255, 0, 255, 0, 255, 0,
-    //         255, 0, 255, 0, 255, 0, 255, 0,
-    //         255, 0, 255, 0, 255, 0, 255, 0,
-    //     ];
-    //
-    //     let expected_fe = FE::from_hex_unchecked(
-    //         "\
-    //             0100FF00FF00FF00\
-    //             FF00FF00FF00FF00\
-    //             FF00FF00FF00FF00\
-    //             FF00FF00FF00FF00",
-    //     );
-    //
-    //     type FE = FieldElement<TestField>;
-    //
-    //     let field_element: FE = randomness_to_field(&mut randomness);
-    //
-    //     assert_eq!(field_element, expected_fe)
-    // }
-
     use std::num::ParseIntError;
 
     type FE = FieldElement<Stark252PrimeField>;
@@ -301,22 +180,6 @@ mod tests {
             .step_by(2)
             .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
             .collect()
-    }
-
-    pub fn send_field_element(s: &str) -> Vec<u8> {
-        // Taken from serialize_be method, but reverses the limbs for
-        // compatibility with the stone prover.
-        let a = FE::from_hex_unchecked(s);
-        let limbs = a.value().limbs;
-        let mut bytes: [u8; 32] = [0; 32];
-
-        for i in (0..4).rev() {
-            let limb_bytes = limbs[i].to_be_bytes();
-            for j in 0..8 {
-                bytes[i * 8 + j] = limb_bytes[j]
-            }
-        }
-        bytes.to_vec()
     }
 
     #[test]
@@ -397,7 +260,12 @@ mod tests {
     }
 
     #[test]
-    fn fibonacci_transcript_replicate() {
+    fn test_transcript_compatibility_with_stone_prover_1() {
+        // This corresponds to the following run.
+        // Air: `Fibonacci2ColsShifted`
+        // `trace_length`: 4
+        // `blowup_factor`: 2
+        // `fri_number_of_queries`: 1
         let mut transcript = StoneProverTranscript::new(&[0xca, 0xfe, 0xca, 0xfe]);
         // Send hash of trace commitment
         transcript.append_bytes(
@@ -474,64 +342,90 @@ mod tests {
         transcript.append_bytes(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x30, 0x4d]); // Eight bytes
                                                                                     // Sample query indices
         assert_eq!(transcript.sample_u64(8), 0);
+    }
 
+    #[test]
+    fn test_transcript_compatibility_with_stone_prover_2() {
+        // This corresponds to the following run.
+        // Air: `Fibonacci2ColsShifted`
+        // `trace_length`: 4
+        // `blowup_factor`: 6
+        // `fri_number_of_queries`: 2
+        let mut transcript = StoneProverTranscript::new(&[0xfa, 0xfa, 0xfa, 0xee]);
+        // Send hash of trace commitment
+        transcript.append_bytes(
+            &decode_hex("99d8d4342895c4e35a084f8ea993036be06f51e7fa965734ed9c7d41104f0848")
+                .unwrap(),
+        );
+        // Sample challenge to collapse the constraints for the composition polynomial
+        assert_eq!(
+            transcript.sample_field_element(),
+            FE::from_hex_unchecked(
+                "3fc675742e0692558bb95f36bd34bdfe050697ed0d849e5369808685e548441"
+            )
+        );
+        // Send hash of composition poly commitment H(z)
+        transcript.append_bytes(
+            &decode_hex("2f4b599828a3f1ac458202ce06ec223bc9f4ad9ac758030109d40eebcf5776fd")
+                .unwrap(),
+        );
+        // Sample challenge Z to compute t_j(z), H(z)
+        assert_eq!(
+            transcript.sample_field_element(),
+            FE::from_hex_unchecked(
+                "7298af9e2574933e62e51b107b8ef52f253d20644fc7250e9af118b02bc8a71"
+            )
+        );
+        // Append t_j(z), H(z)
         transcript.append_field_element(&FE::from_hex_unchecked(
-            "643e5520c60d06219b27b34da0856a2c23153efe9da75c6036f362c8f19615e",
+            "6791c8cdbd981f7db9786d702b21b87f4128a6941f35683d8b10faafcab83d5",
         ));
         transcript.append_field_element(&FE::from_hex_unchecked(
-            "165d7fb12913882268bb8cf470c81f42349fde7dec7b0a90526d142d6a61205",
+            "3cd6d8a23d01db66ea4911d6d7b09595b674f0507278fbf1f15cd85aa4ba72d",
         ));
         transcript.append_field_element(&FE::from_hex_unchecked(
-            "1bc1aadf39f2faee64d84cb25f7a95d3dceac1016258a39fc90c9d370e69ea2",
+            "3123deded538b40c1faa7988310f315860a43e320ae70f8f86eaeadf3828a10",
         ));
         transcript.append_field_element(&FE::from_hex_unchecked(
-            "69a2804ed6ec78ed9744730b8f37e0bdcb6021821384f56fad92ebd2959edf4",
-        ));
-
-        transcript.append_bytes(
-            &decode_hex("0160a780da72e50c596b9b6712bd040475d30777a4fef2c9f9be3a7fbaa98072")
-                .unwrap(),
-        );
-        transcript.append_bytes(
-            &decode_hex("993b044db22444c0c0ebf1095b9a51faeb001c9b4dea36abe905f7162620dbbd")
-                .unwrap(),
-        );
-        transcript.append_bytes(
-            &decode_hex("5017abeca33fa82576b5c5c2c61792693b48c9d4414a407eef66b6029dae07ea")
-                .unwrap(),
-        );
-
-        transcript.append_field_element(&FE::from_hex_unchecked(
-            "483069de80bf48a1b5ca2f55bdeb9ec3ed1b7bf9c794c3c8832f14928124cbb",
+            "4d2edcc28870d79cbbb87181ffcb5942f7fa1c7b5f5bd5794c43452700e00d7",
         ));
         transcript.append_field_element(&FE::from_hex_unchecked(
-            "1cf5d5ed8348c3dee617bceff2d59cb14099d2978b1f7f928027dbbded1d66f",
+            "5c244407085950973147074ee245bd1c7ed6d8a019df997aab1928a4a9a1e19",
         ));
-
+        // Sample challenge Gamma to collapse the terms of the deep composition polynomial (batch open).
+        // Powers of this challenge are used if more than two terms.
+        assert_eq!(
+            transcript.sample_field_element(),
+            FE::from_hex_unchecked(
+                "12f2b9edda6bb334bdf340d99eb0e6815e57aabffb48359117f71e7d0159d93"
+            )
+        );
+        // FRI: Sample challenge Zeta to split the polynomial in half
+        assert_eq!(
+            transcript.sample_field_element(),
+            FE::from_hex_unchecked(
+                "7549307d78354156552667acf19a0ae978d4ec4954d210e23d9979672987dc"
+            )
+        );
+        // FRI: Send hash of commitment at Layer 1
         transcript.append_bytes(
-            &decode_hex("6a23307160a636ea45c08f6b56e7585a850b5e14170a6c63f4d166a2220a7c2f")
+            &decode_hex("97decf0ad3cd590e7e5a4f85b3d4fa8c02c6d4b5343388c4536127dc8ef0fbf2")
                 .unwrap(),
         );
-        transcript.append_bytes(
-            &decode_hex("7950888c0355c204a1e83ecbee77a0a6a89f93d41cc2be6b39ddd1e727cc9650")
-                .unwrap(),
+        // FRI: Sample challenge to split the polynomial in half
+        assert_eq!(
+            transcript.sample_field_element(),
+            FE::from_hex_unchecked(
+                "4b79e806108567fd0f670ded2be5468009aaefeb993b346579c4f295fa3ddd0"
+            )
         );
-        transcript.append_bytes(
-            &decode_hex("58befe2c5de74cc5a002aa82ea219c5b242e761b45fd266eb95521e9f53f44eb")
-                .unwrap(),
-        );
-
+        // Send field element at final layer of FRI
         transcript.append_field_element(&FE::from_hex_unchecked(
-            "724fcd17f8649ed5e180d4e98ba7e8900c8da2643f5ed548773b145230cf12d",
+            "7b8aa43aef4d3f2d476608251cffc9fa1c655bedecbcac49e4cafb012c7edf4",
         ));
-
-        transcript.append_bytes(
-            &decode_hex("f1f135fc9228ae46afe83d108b256dda8a6ad63e05d630be1f8b461bf2dccf3d")
-                .unwrap(),
-        );
-        transcript.append_bytes(
-            &decode_hex("3fdabd3f5fae2bf405d423417141678f4b9afa5666b00790baac61116c5ea8af")
-                .unwrap(),
-        );
+        // Send proof of work
+        transcript.append_bytes(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x3b, 0xb8]);
+        assert_eq!(transcript.sample_u64(128), 28);
+        assert_eq!(transcript.sample_u64(128), 31);
     }
 }
