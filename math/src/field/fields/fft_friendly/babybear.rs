b22606971f144f193792c1a772ade2e6d051dae4
mdvillagra <mdvillagra@gmail.com>
2023-09-25 16:02:40 -0400

    added isFFT trait to babybear field with test

--- math/src/field/fields/fft_friendly/babybear.rs
+++ math/src/field/fields/fft_friendly/babybear.rs
@@ -4,7 +4,7 @@ use crate::{
         fields::montgomery_backed_prime_fields::{IsModulus, MontgomeryBackendPrimeField},
         traits::IsFFTField,
     },
-    unsigned_integer::element::{U64, UnsignedInteger},
+    unsigned_integer::element::{UnsignedInteger, U64},
 };
 
 pub type U64MontgomeryBackendPrimeField<T> = MontgomeryBackendPrimeField<T, 1>;
@@ -19,14 +19,13 @@ impl IsModulus<U64> for MontgomeryConfigBabybear31PrimeField {
 pub type Babybear31PrimeField =
     U64MontgomeryBackendPrimeField<MontgomeryConfigBabybear31PrimeField>;
 
+//a two-adic primitive root of unity is 21^(2^24)
 // 21^(2^24)=1 mod 2013265921
 // 2^27(2^4-1)+1 where n=27 (two-adicity) and k=2^4+1
 impl IsFFTField for Babybear31PrimeField {
     const TWO_ADICITY: u64 = 27;
 
-    const TWO_ADIC_PRIMITVE_ROOT_OF_UNITY: Self::BaseType = UnsignedInteger{
-        limbs: [21],
-    };
+    const TWO_ADIC_PRIMITVE_ROOT_OF_UNITY: Self::BaseType = UnsignedInteger { limbs: [21] };
 
     fn field_name() -> &'static str {
         "babybear31"
@@ -60,7 +59,13 @@ impl Ord for FieldElement<Babybear31PrimeField> {
 #[cfg(test)]
 mod test_babybear_31_bytes_ops {
     use super::Babybear31PrimeField;
-    use crate::{field::element::FieldElement, traits::ByteConversion};
+    use crate::{
+        field::{
+            element::FieldElement,
+            traits::IsFFTField,
+        },
+        traits::ByteConversion,
+    };
 
     #[test]
     #[cfg(feature = "std")]
@@ -89,7 +94,6 @@ mod test_babybear_31_bytes_ops {
     }
 
     #[test]
-
     fn byte_serialization_and_deserialization_works_le() {
         let element = FieldElement::<Babybear31PrimeField>::from_hex_unchecked(
             "\
@@ -102,7 +106,6 @@ mod test_babybear_31_bytes_ops {
     }
 
     #[test]
-
     fn byte_serialization_and_deserialization_works_be() {
         let element = FieldElement::<Babybear31PrimeField>::from_hex_unchecked(
             "\
@@ -112,5 +115,21 @@ mod test_babybear_31_bytes_ops {
         let bytes = element.to_bytes_be();
         let from_bytes = FieldElement::<Babybear31PrimeField>::from_bytes_be(&bytes).unwrap();
         assert_eq!(element, from_bytes);
+    }
+
+    #[test]
+    fn test_two_adic_primitive_root_of_unity() {
+        let root = FieldElement::<Babybear31PrimeField>::from_hex_unchecked(
+            &Babybear31PrimeField::TWO_ADIC_PRIMITVE_ROOT_OF_UNITY.to_string(),
+        );
+        let result = root.pow(u64::pow(2, 24));
+
+        //checks that Babybear31PrimeField::TWO_ADIC_PRIMITVE_ROOT_OF_UNITY is a root of unity
+        assert_eq!(result, FieldElement::<Babybear31PrimeField>::one());
+
+        //checks that Babybear31PrimeField::TWO_ADIC_PRIMITVE_ROOT_OF_UNITY is primitive
+        for i in 2..u64::pow(2, 24) {
+            assert_ne!(root.pow(i) , FieldElement::<Babybear31PrimeField>::one());
+        }
     }
 }
