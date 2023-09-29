ca179a79e1bb3332a72458dee29831c1a14306a6
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-09-29 07:29:13 -0300

    rename backend definitions

--- crypto/src/merkle_tree/backends/types.rs
+++ crypto/src/merkle_tree/backends/types.rs
@@ -5,26 +5,26 @@ use super::{
     batch::BatchBackend, hash::Backend
 };
 
-// Trees definitions
+// Field element data backend definitions
 
 // - With 256 bit
-pub type Sha3_256Tree<F> = Backend<F, Sha3_256, 32>;
-pub type Keccak256Tree<F> = Backend<F, Keccak256, 32>;
-pub type Sha2_256Tree<F> = Backend<F, Sha256, 32>;
+pub type Sha3_256Backend<F> = Backend<F, Sha3_256, 32>;
+pub type Keccak256Backend<F> = Backend<F, Keccak256, 32>;
+pub type Sha2_256Backend<F> = Backend<F, Sha256, 32>;
 
 // - With 512 bit
-pub type Sha3_512Tree<F> = Backend<F, Sha3_512, 64>;
-pub type Keccak512Tree<F> = Backend<F, Keccak512, 64>;
-pub type Sha2_512Tree<F> = Backend<F, Sha512, 64>;
+pub type Sha3_512Backend<F> = Backend<F, Sha3_512, 64>;
+pub type Keccak512Backend<F> = Backend<F, Keccak512, 64>;
+pub type Sha2_512Backend<F> = Backend<F, Sha512, 64>;
 
-// Batch trees definitions
+// Vector field element data backend definitions
 
 // - With 256 bit
-pub type BatchSha3_256Tree<F> = BatchBackend<F, Sha3_256, 32>;
-pub type BatchKeccak256Tree<F> = BatchBackend<F, Keccak256, 32>;
-pub type BatchSha2_256Tree<F> = BatchBackend<F, Sha256, 32>;
+pub type BatchSha3_256Backend<F> = BatchBackend<F, Sha3_256, 32>;
+pub type BatchKeccak256Backend<F> = BatchBackend<F, Keccak256, 32>;
+pub type BatchSha2_256Backend<F> = BatchBackend<F, Sha256, 32>;
 
 // - With 512 bit
-pub type BatchSha3_512Tree<F> = BatchBackend<F, Sha3_512, 64>;
-pub type BatchKeccak512Tree<F> = BatchBackend<F, Keccak512, 64>;
-pub type BatchSha2_512Tree<F> = BatchBackend<F, Sha512, 64>;
+pub type BatchSha3_512Backend<F> = BatchBackend<F, Sha3_512, 64>;
+pub type BatchKeccak512Backend<F> = BatchBackend<F, Keccak512, 64>;
+pub type BatchSha2_512Backend<F> = BatchBackend<F, Sha512, 64>;
