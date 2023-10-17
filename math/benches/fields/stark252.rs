ae198dac31a09f7a4604ebd5e67a9d2c89e0dd20
PatStiles <pdstiles78@gmail.com>
2023-10-17 03:02:22 -0500

    fmt

--- math/benches/fields/stark252.rs
+++ math/benches/fields/stark252.rs
@@ -2,13 +2,19 @@ use std::hint::black_box;
 
 use criterion::Criterion;
 use lambdaworks_math::{
-    field::{fields::{
-        fft_friendly::stark_252_prime_field::{
-            MontgomeryConfigStark252PrimeField, Stark252PrimeField,
+    field::{
+        element::FieldElement,
+        fields::{
+            fft_friendly::stark_252_prime_field::{
+                MontgomeryConfigStark252PrimeField, Stark252PrimeField,
+            },
+            montgomery_backed_prime_fields::IsModulus,
         },
-        montgomery_backed_prime_fields::IsModulus,
-    }, element::FieldElement},
-    unsigned_integer::{element::{U256, UnsignedInteger}, montgomery::MontgomeryAlgorithms},
+    },
+    unsigned_integer::{
+        element::{UnsignedInteger, U256},
+        montgomery::MontgomeryAlgorithms,
+    },
 };
 use rand::random;
 
@@ -28,16 +34,19 @@ pub fn rand_field_elements(num: usize) -> Vec<(F, F)> {
 }
 
 pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
-    let input: Vec<Vec<(F,F)>> = [1,10,100,1000,10000,100000,1000000].into_iter().map(|i| rand_field_elements(i)).collect::<Vec<_>>();
+    let input: Vec<Vec<(F, F)>> = [1, 10, 100, 1000, 10000, 100000, 1000000]
+        .into_iter()
+        .map(|i| rand_field_elements(i))
+        .collect::<Vec<_>>();
     let mut group = c.benchmark_group("Stark FP operations");
-    
+
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("add {:?}", &i.len()), &i, |bench, i| {
-                bench.iter(|| {
-                    for (x, y) in i {
-                        black_box(black_box(x) + black_box(y));
-                    }
-                });
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(x) + black_box(y));
+                }
+            });
         });
     }
 
@@ -47,7 +56,7 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
                 for (x, y) in i {
                     black_box(black_box(x) * black_box(y));
                 }
-            }); 
+            });
         });
     }
 
@@ -65,15 +74,15 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
     // normally computed at compile time.
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("sos_square {:?}", &i.len()), &i, |bench, i| {
-                bench.iter(|| {
-                    for (x, _) in i {
-                        MontgomeryAlgorithms::sos_square(
-                            black_box(black_box(x.value())),
-                            &<MontgomeryConfigStark252PrimeField as IsModulus<U256>>::MODULUS,
-                            &Stark252PrimeField::MU,
-                        );
-                    }
-                });
+            bench.iter(|| {
+                for (x, _) in i {
+                    MontgomeryAlgorithms::sos_square(
+                        black_box(black_box(x.value())),
+                        &<MontgomeryConfigStark252PrimeField as IsModulus<U256>>::MODULUS,
+                        &Stark252PrimeField::MU,
+                    );
+                }
+            });
         });
     }
 
@@ -89,11 +98,11 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("square with pow {:?}", &i.len()), &i, |bench, i| {
-                bench.iter(|| {
-                    for (x, _) in i {
-                        black_box(black_box(x).pow(2_u64));
-                    }
-                });
+            bench.iter(|| {
+                for (x, _) in i {
+                    black_box(black_box(x).pow(2_u64));
+                }
+            });
         });
     }
 
@@ -108,13 +117,17 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
     }
 
     for i in input.clone().into_iter() {
-        group.bench_with_input(format!("pow {:?}", &i.len()), &(i, 5u64), |bench, (i, a)| {
-            bench.iter(|| {
-                for (x, _) in i {
-                    black_box(black_box(x).pow(*a));
-                }
-            });
-        });
+        group.bench_with_input(
+            format!("pow {:?}", &i.len()),
+            &(i, 5u64),
+            |bench, (i, a)| {
+                bench.iter(|| {
+                    for (x, _) in i {
+                        black_box(black_box(x).pow(*a));
+                    }
+                });
+            },
+        );
     }
 
     for i in input.clone().into_iter() {
@@ -129,31 +142,31 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("inv {:?}", &i.len()), &i, |bench, i| {
-                bench.iter(|| { 
-                    for (x, _) in i {
-                        black_box(black_box(x).inv().unwrap());
-                    }
-                });
+            bench.iter(|| {
+                for (x, _) in i {
+                    black_box(black_box(x).inv().unwrap());
+                }
+            });
         });
     }
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("div {:?}", &i.len()), &i, |bench, i| {
-                bench.iter(|| {
-                    for (x, y) in i {
-                        black_box(black_box(x) / black_box(y));
-                    }
-                });
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(x) / black_box(y));
+                }
+            });
         });
     }
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("eq {:?}", &i.len()), &i, |bench, i| {
-                bench.iter(|| {
-                    for (x, y) in i {
-                        black_box(black_box(x) == black_box(y));
-                    }
-                });
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(x) == black_box(y));
+                }
+            });
         });
     }
 
@@ -170,29 +183,31 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("sqrt squared {:?}", &i.len()), &i, |bench, i| {
             let i: Vec<F> = i.iter().map(|(x, _)| x * x).collect();
-                bench.iter(|| {
-                    for x in &i {
-                        black_box(black_box(x).sqrt());
-                    }
-                });
+            bench.iter(|| {
+                for x in &i {
+                    black_box(black_box(x).sqrt());
+                }
+            });
         });
     }
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("bitand {:?}", &i.len()), &i, |bench, i| {
             // Note: we should strive to have the number of limbs be generic... ideally this benchmark group itself should have a generic type that we call into from the main runner.
-            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> = i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
+            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> =
+                i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
             bench.iter(|| {
                 for (x, y) in &i {
                     black_box(black_box(*x) & black_box(*y));
-                }   
+                }
             });
         });
     }
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("bitor {:?}", &i.len()), &i, |bench, i| {
-            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> = i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
+            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> =
+                i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
             bench.iter(|| {
                 for (x, y) in &i {
                     black_box(black_box(*x) | black_box(*y));
@@ -203,7 +218,8 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("bitxor {:?}", &i.len()), &i, |bench, i| {
-            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> = i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
+            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> =
+                i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
             bench.iter(|| {
                 for (x, y) in &i {
                     black_box(black_box(*x) ^ black_box(*y));
