b1f3f5d242e52e732b85fcf25dddd704a8de9b66
mdvillagra <mdvillagra@gmail.com>
2023-10-20 08:06:38 -0300

    added `pub` keyword to modules

--- math/src/polynomial/mod.rs
+++ math/src/polynomial/mod.rs
@@ -3,9 +3,9 @@ use crate::field::traits::IsField;
 use std::ops;
 
 // pub mod multivariate;
-mod multilinear_poly;
-mod multilinear_term;
-mod multivariate_term;
+pub mod multilinear_poly;
+pub mod multilinear_term;
+pub mod multivariate_term;
 pub mod term;
 
 /// Represents the polynomial c_0 + c_1 * X + c_2 * X^2 + ... + c_n * X^n
