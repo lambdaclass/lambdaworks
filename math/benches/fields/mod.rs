9abc20e1fda8d6ae27fdc0927d8794a4042c5444
PatStiles <pdstiles78@gmail.com>
2023-10-20 11:59:20 -0500

    refactor benchmarks

--- math/benches/fields/mod.rs
+++ math/benches/fields/mod.rs
@@ -1,1 +1,2 @@
 pub mod stark252;
+pub mod u64_goldilocks;
--- math/benches/fields/u64_goldilocks.rs
+++ math/benches/fields/u64_goldilocks.rs
@@ -1,44 +1,31 @@
 use std::hint::black_box;
 
 use criterion::Criterion;
-use lambdaworks_math::{
-    field::{
+use lambdaworks_math::field::{
         element::FieldElement,
-        fields::{
-            fft_friendly::stark_252_prime_field::{
-                MontgomeryConfigStark252PrimeField, Stark252PrimeField,
-            },
-            montgomery_backed_prime_fields::IsModulus,
-        },
-    },
-    unsigned_integer::{
-        element::{UnsignedInteger, U256},
-        montgomery::MontgomeryAlgorithms,
-    },
-};
+        fields::u64_goldilocks_field::Goldilocks64Field,
+    };
 use rand::random;
 
-pub type F = FieldElement<Stark252PrimeField>;
+pub type F = FieldElement<Goldilocks64Field>;
 
 #[inline(never)]
 #[no_mangle]
-#[export_name = "util::rand_field_elements"]
+#[export_name = "util::rand_goldilocks_field_elements"]
 pub fn rand_field_elements(num: usize) -> Vec<(F, F)> {
     let mut result = Vec::with_capacity(num);
     for _ in 0..result.capacity() {
-        let rand_a = UnsignedInteger { limbs: random() };
-        let rand_b = UnsignedInteger { limbs: random() };
-        result.push((F::new(rand_a), F::new(rand_b)));
+        result.push((F::new(random()), F::new(random())));
     }
     result
 }
 
-pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
+pub fn u64_goldilocks_ops_benchmarks(c: &mut Criterion) {
     let input: Vec<Vec<(F, F)>> = [1, 10, 100, 1000, 10000, 100000, 1000000]
         .into_iter()
         .map(rand_field_elements)
         .collect::<Vec<_>>();
-    let mut group = c.benchmark_group("Stark FP operations");
+    let mut group = c.benchmark_group("u64 Goldilocks FP operations");
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("add {:?}", &i.len()), &i, |bench, i| {
@@ -65,22 +52,6 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
             bench.iter(|| {
                 for (x, _) in i {
                     black_box(black_box(x).pow(1_u64));
-                }
-            });
-        });
-    }
-
-    // The non-boxed constants are intentional as they are
-    // normally computed at compile time.
-    for i in input.clone().into_iter() {
-        group.bench_with_input(format!("sos_square {:?}", &i.len()), &i, |bench, i| {
-            bench.iter(|| {
-                for (x, _) in i {
-                    MontgomeryAlgorithms::sos_square(
-                        black_box(black_box(x.value())),
-                        &<MontgomeryConfigStark252PrimeField as IsModulus<U256>>::MODULUS,
-                        &Stark252PrimeField::MU,
-                    );
                 }
             });
         });
@@ -194,7 +165,7 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("bitand {:?}", &i.len()), &i, |bench, i| {
             // Note: we should strive to have the number of limbs be generic... ideally this benchmark group itself should have a generic type that we call into from the main runner.
-            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> =
+            let i: Vec<(u64, u64)> =
                 i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
             bench.iter(|| {
                 for (x, y) in &i {
@@ -206,7 +177,7 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("bitor {:?}", &i.len()), &i, |bench, i| {
-            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> =
+            let i: Vec<(u64, u64)> =
                 i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
             bench.iter(|| {
                 for (x, y) in &i {
@@ -218,7 +189,7 @@ pub fn starkfield_ops_benchmarks(c: &mut Criterion) {
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("bitxor {:?}", &i.len()), &i, |bench, i| {
-            let i: Vec<(UnsignedInteger<4>, UnsignedInteger<4>)> =
+            let i: Vec<(u64, u64)> =
                 i.iter().map(|(x, y)| (*x.value(), *y.value())).collect();
             bench.iter(|| {
                 for (x, y) in &i {
