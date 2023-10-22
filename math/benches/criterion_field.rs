e1ae220a520dad385fda52597d23572e0d0558de
PatStiles <pdstiles78@gmail.com>
2023-10-22 03:55:43 -0500

    add mersenne montgomery bench

--- math/benches/criterion_field.rs
+++ math/benches/criterion_field.rs
@@ -7,10 +7,12 @@ use fields::{
     u64_goldilocks_montgomery::u64_goldilocks_montgomery_ops_benchmarks,
 };
 use fields::mersenne31::mersenne31_ops_benchmarks;
+use fields::mersenne31_montgomery::mersenne31_mont_ops_benchmarks;
+use fields::stark252::starkfield_ops_benchmarks;
 
 criterion_group!(
     name = field_benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets = starkfield_ops_benchmarks, mersenne31_ops_benchmarks, u64_goldilocks_ops_benchmarks, u64_goldilocks_montgomery_ops_benchmarks
+    targets = starkfield_ops_benchmarks, mersenne31_ops_benchmarks, mersenne31_mont_ops_benchmarks, u64_goldilocks_ops_benchmarks, u64_goldilocks_montgomery_ops_benchmarks
 );
 criterion_main!(field_benches);
