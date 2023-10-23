4dab08d85c9cd22f4e6911fe87b0095134b896db
mdvillagra <mdvillagra@gmail.com>
2023-10-22 21:41:14 -0300

    round 0 of prover

--- crypto/src/gadgets/sumcheck/prover.rs
+++ crypto/src/gadgets/sumcheck/prover.rs
@@ -1,18 +1,47 @@
+use std::ops::AddAssign;
+
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::field::traits::IsField;
-use lambdaworks_math::polynomial::multilinear_poly;
+use lambdaworks_math::polynomial::{
+    multilinear_poly::MultilinearPolynomial, multilinear_term::MultiLinearMonomial,
+};
 
-pub enum ProverMessage<T: IsField> {
-    Sum(FieldElement<T>),
-    Polynomial(Polynomial<FieldElement<T>>),
+pub enum ProverMessage<F: IsField>
+where
+    <F as IsField>::BaseType: Send + Sync,
+{
+    Sum(FieldElement<F>),
+    Polynomial(MultilinearPolynomial<F>),
 }
 
-pub fn sumcheck_prover<F: IsField>(round: u64, p: Polynomial<FieldElement<F>>) -> ProverMessage<F> {
+pub fn sumcheck_prover<F: IsField>(round: u64, poly: MultilinearPolynomial<F>) -> ProverMessage<F>
+where
+    <F as IsField>::BaseType: Send + Sync,
+{
     if round == 0 {
+        println!("poly has {} variables", poly.n_vars);
         let mut acc = FieldElement::<F>::zero();
-        for i in 0..p.coefficients.len() {
-            acc += eval_binary_multilinear_polynomial(i as u64, &p);
+
+        for value in 0..2u64.pow(poly.n_vars as u32) {
+            let mut assign_numbers: Vec<u64> = Vec::new();
+            let mut assign_value = value;
+            
+            for _bit in 0..poly.n_vars {
+                assign_numbers.push(assign_value % 2);
+                assign_value = assign_value >> 1;
+            }
+
+            println!("{:?}", assign_numbers);
+
+            let assign = assign_numbers
+                .iter()
+                .map(|x| FieldElement::<F>::from(*x))
+                .collect::<Vec<FieldElement<F>>>();
+            
+
+            acc.add_assign(poly.evaluate(&assign));
         }
+
         ProverMessage::Sum(acc)
     } else if round == 1 {
         ProverMessage::Sum(FieldElement::<F>::one())
@@ -21,98 +50,32 @@ pub fn sumcheck_prover<F: IsField>(round: u64, p: Polynomial<FieldElement<F>>) -
     }
 }
 
-/// point evaluation for multilinear polynomial for binary value assignments
-/// number of elements of the polynomial must be a power of 2
-fn eval_binary_multilinear_polynomial<T: IsField>(
-    var_assignment: u64,
-    p: &Polynomial<FieldElement<T>>,
-) -> FieldElement<T> {
-    assert_eq!(
-        p.coefficients.len() & p.coefficients.len() - 1,
-        0,
-        "the number of coefficientes of the polynomial is not a power of 2"
-    );
-
-    let mut eval = p.coefficients[0].clone();
-
-    for i in 1..p.coefficients.len() {
-        let mut var = i as u64;
-        let mut assign = var_assignment;
-
-        let mut mult = FieldElement::<T>::one();
-        while var > 0 {
-            if var % 2 == 1 && assign % 2 == 0 {
-                mult = FieldElement::<T>::zero();
-                break;
-            }
-            var = var >> 1;
-            assign = assign >> 1;
-        }
-
-        eval += p.coefficients[i].clone() * mult;
-    }
-    eval
-}
-
 #[cfg(test)]
 mod test_prover {
+    use std::vec;
+
     use super::*;
     use lambdaworks_math::field::fields::fft_friendly::babybear::Babybear31PrimeField;
 
     #[test]
     fn test_sumcheck_initial_value() {
-        //1 + x_1 + x_2 + x_2 x_1
-        let poly = Polynomial::new(&[
-            FieldElement::<Babybear31PrimeField>::one(),
-            FieldElement::<Babybear31PrimeField>::one(),
-            FieldElement::<Babybear31PrimeField>::one(),
-            FieldElement::<Babybear31PrimeField>::one(),
-        ]);
+        //Test the polynomial 1 + x_0 + x_1 + x_0 x_1
+        let constant =
+            MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![]));
+        let x0: MultiLinearMonomial<lambdaworks_math::field::fields::montgomery_backed_prime_fields::MontgomeryBackendPrimeField<lambdaworks_math::field::fields::fft_friendly::babybear::MontgomeryConfigBabybear31PrimeField, 1>> =
+            MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![0]));
+        let x1 =
+            MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![1]));
+        let x01 =
+            MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![0, 1]));
+
+        let poly = MultilinearPolynomial::new(vec![constant]);
 
         let msg = match sumcheck_prover(0, poly) {
             ProverMessage::Sum(x) => x,
             _ => FieldElement::<Babybear31PrimeField>::zero(),
         };
 
-        assert_eq!(
-            msg,
-            FieldElement::<Babybear31PrimeField>::from_hex_unchecked("9")
-        );
-    }
-
-    #[test]
-    fn test_polynomial_evaluation() {
-        //1 + x_1 + x_2 + x_2 x_1
-        let poly = Polynomial::new(&[
-            FieldElement::<Babybear31PrimeField>::one(),
-            FieldElement::<Babybear31PrimeField>::one(),
-            FieldElement::<Babybear31PrimeField>::one(),
-            FieldElement::<Babybear31PrimeField>::one(),
-        ]);
-        //x_2 = x_1 = 0
-        assert_eq!(
-            eval_binary_multilinear_polynomial(0, &poly),
-            FieldElement::<Babybear31PrimeField>::one()
-        );
-        //x_2 = 0, x_1 = 1
-        assert_eq!(
-            eval_binary_multilinear_polynomial(1, &poly),
-            FieldElement::<Babybear31PrimeField>::one()
-                + FieldElement::<Babybear31PrimeField>::one()
-        );
-        //x_2 = 1 x_1 = 0
-        assert_eq!(
-            eval_binary_multilinear_polynomial(2, &poly),
-            FieldElement::<Babybear31PrimeField>::one()
-                + FieldElement::<Babybear31PrimeField>::one()
-        );
-        //x_2 = 1 x_1 = 1
-        assert_eq!(
-            eval_binary_multilinear_polynomial(3, &poly),
-            FieldElement::<Babybear31PrimeField>::one()
-                + FieldElement::<Babybear31PrimeField>::one()
-                + FieldElement::<Babybear31PrimeField>::one()
-                + FieldElement::<Babybear31PrimeField>::one()
-        );
+        assert_eq!(msg, FieldElement::<Babybear31PrimeField>::from(1));
     }
 }
