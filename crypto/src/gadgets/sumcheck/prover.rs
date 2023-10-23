34a3e9536e8a43a30dd2d821c736b1209bfa1e2e
mdvillagra <mdvillagra@gmail.com>
2023-10-23 16:52:40 -0300

    prover round 0 finished

--- crypto/src/gadgets/sumcheck/prover.rs
+++ crypto/src/gadgets/sumcheck/prover.rs
@@ -2,7 +2,7 @@ use std::ops::AddAssign;
 
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::field::traits::IsField;
-use lambdaworks_math::polynomial::{multilinear_poly, Polynomial};
+use lambdaworks_math::polynomial::multilinear_poly::MultilinearPolynomial;
 
 pub enum ProverMessage<F: IsField>
 where
@@ -17,7 +17,6 @@ where
     <F as IsField>::BaseType: Send + Sync,
 {
     if round == 0 {
-        println!("poly has {} variables", poly.n_vars);
         let mut acc = FieldElement::<F>::zero();
 
         for value in 0..2u64.pow(poly.n_vars as u32) {
@@ -28,8 +27,6 @@ where
                 assign_numbers.push(assign_value % 2);
                 assign_value = assign_value >> 1;
             }
-
-            println!("{:?}", assign_numbers);
 
             let assign = assign_numbers
                 .iter()
@@ -52,26 +49,28 @@ mod test_prover {
     use std::vec;
 
     use super::*;
-    use lambdaworks_math::{field::fields::fft_friendly::babybear::Babybear31PrimeField, polynomial::Polynomial};
+    use lambdaworks_math::{
+        field::fields::fft_friendly::babybear::Babybear31PrimeField,
+        polynomial::multilinear_term::MultiLinearMonomial,
+    };
 
     #[test]
     fn test_sumcheck_initial_value() {
         //Test the polynomial 1 + x_0 + x_1 + x_0 x_1
         let constant =
             MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![]));
-        let x0: MultiLinearMonomial<lambdaworks_math::field::fields::montgomery_backed_prime_fields::MontgomeryBackendPrimeField<lambdaworks_math::field::fields::fft_friendly::babybear::MontgomeryConfigBabybear31PrimeField, 1>> =
-            MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![0]));
+        let x0 = MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![0]));
         let x1 = MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![1]));
         let x01 =
             MultiLinearMonomial::new((FieldElement::<Babybear31PrimeField>::from(1), vec![0, 1]));
 
-        let poly = MultilinearPolynomial::new(vec![constant]);
+        let poly = MultilinearPolynomial::new(vec![constant,x0,x1,x01]);
 
         let msg = match sumcheck_prover(0, poly) {
             ProverMessage::Sum(x) => x,
             _ => FieldElement::<Babybear31PrimeField>::zero(),
         };
 
-        assert_eq!(msg, FieldElement::<Babybear31PrimeField>::from(1));
+        assert_eq!(msg, FieldElement::<Babybear31PrimeField>::from(9));
     }
 }
