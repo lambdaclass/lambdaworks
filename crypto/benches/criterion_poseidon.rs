4a475c99ec4f399b5d324ab73d72348be7db5e0e
Theprotocolwhisperer <chrismata10@gmail.com>
2023-10-23 16:42:53 -0500

    (rebase): Fix Clippy Issue

--- crypto/benches/criterion_poseidon.rs
+++ crypto/benches/criterion_poseidon.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use lambdaworks_crypto::hash::poseidon::shared_tests::run_tests;
 
 fn poseidon_benchmarks(c: &mut Criterion) {
-    c.bench_function("poseidon_tests", |b| b.iter(|| run_tests()));
+    c.bench_function("poseidon_tests", |b| b.iter(|| run_tests));
 }
 
 criterion_group!(poseidon, poseidon_benchmarks);
