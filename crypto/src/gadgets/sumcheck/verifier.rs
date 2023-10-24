9dc2b3fbf64f6ec65e6361e4c0056a0b05489daf
Wisdom Ogwu <iammadab@gmail.com>
2023-10-24 08:17:14 +0100

    add a function to determine the last round in the protocol

--- crypto/src/gadgets/sumcheck/verifier.rs
+++ crypto/src/gadgets/sumcheck/verifier.rs
@@ -10,8 +10,8 @@ where
 {
     /// Represents the polynomial whose sum over boolean hypercube is computed
     poly: MultilinearPolynomial<F>,
-    /// Current Round
-    /// this value will only advance when a previous round has been successfully verified
+    /// Current Round: this value will only advance when a previous round has been
+    /// successfully verified
     round: u64,
     /// Accumulated challenges over the course of the protocol
     challenges: Vec<FieldElement<F>>,
@@ -34,12 +34,27 @@ where
     }
 
     /// Verify the ith round of the protocol, advance round if successful
-    pub fn verify_round(round: u64, prover_message: ProverMessage<F>) -> Result<bool, String> {
+    pub fn verify_round(
+        &mut self,
+        round: u64,
+        prover_message: ProverMessage<F>,
+    ) -> Result<bool, String> {
+        // verify that the poly is univariate
+        // evaluate the poly at 0 and 1, ensure the sum is the same as the round sum
+        // to check if the poly is the true poly, then we need to generate some random challenge
+        // both the prover polynomial and the true polynomial at that point
+        // if we can't do the true polynomial, then we have to save the value then apply sum check again
         todo!()
     }
 
-    /// Generate challenge for current rount
-    pub fn generate_challenge() -> Option<FieldElement<F>> {
+    /// Generate challenge for current round
+    pub fn generate_challenge(&mut self) -> Option<FieldElement<F>> {
         todo!()
+    }
+
+    /// Determines what the last round of the protocol will be given the poly
+    fn last_round(&self) -> u64 {
+        // in sumcheck, we have number of variable rounds
+        self.poly.n_vars as u64
     }
 }
