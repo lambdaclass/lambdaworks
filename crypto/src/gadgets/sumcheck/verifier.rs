17c8a6c6b1c6b16e7c4952ddb4bcac92890cbd68
Wisdom Ogwu <iammadab@gmail.com>
2023-10-24 07:52:14 +0100

    add method to instantiate verifier

--- crypto/src/gadgets/sumcheck/verifier.rs
+++ crypto/src/gadgets/sumcheck/verifier.rs
@@ -1,9 +1,12 @@
 use lambdaworks_math::field::element::FieldElement;
-use lambdaworks_math::field::traits::IsPrimeField;
+use lambdaworks_math::field::traits::{IsField, IsPrimeField};
 use lambdaworks_math::polynomial::multilinear_poly::MultilinearPolynomial;
 
 /// Sumcheck Verifier
-pub struct Verifier<F: IsPrimeField> {
+pub struct Verifier<F: IsPrimeField>
+where
+    <F as IsField>::BaseType: Send + Sync,
+{
     /// Represents the polynomial whose sum over boolean hypercube is computed
     poly: MultilinearPolynomial<F>,
     /// Current Round
@@ -12,4 +15,19 @@ pub struct Verifier<F: IsPrimeField> {
     challenges: Vec<FieldElement<F>>,
     /// Claimed sum for the current round
     round_sum: FieldElement<F>,
+}
+
+impl<F: IsPrimeField> Verifier<F>
+where
+    <F as IsField>::BaseType: Send + Sync,
+{
+    /// Instantiate new sumcheck verifier
+    pub fn new(poly: MultilinearPolynomial<F>, claimed_sum: FieldElement<F>) -> Verifier<F> {
+        Verifier {
+            poly,
+            round: 1,
+            challenges: vec![],
+            round_sum: claimed_sum,
+        }
+    }
 }
