cc0f4fec0af6c27d02fbf44707812fe6c7b2dd27
MauroFab <maurotoscano2@gmail.com>
2023-10-25 15:14:16 -0300

    Rework bench

--- benches/benches/point.rs
+++ benches/benches/point.rs
@@ -1,139 +1,79 @@
-use std::ops::AddAssign;
+use std::ops::{AddAssign, Add};
 
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
-use hex::ToHex;
-use lambdaworks_math::{field::{fields::fft_friendly::stark_252_prime_field::Stark252PrimeField, element::FieldElement}, elliptic_curve::{short_weierstrass::{curves::stark_curve::StarkCurve, point::ShortWeierstrassProjectivePoint}, traits::FromAffine}, cyclic_group::IsGroup};
-use starknet_curve::{
-    AffinePoint, ProjectivePoint,
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use lambdaworks_math::{
+    cyclic_group::IsGroup,
+    elliptic_curve::{short_weierstrass::curves::stark_curve::StarkCurve, traits::IsEllipticCurve},
 };
+use starknet_curve::{curve_params::GENERATOR, AffinePoint, ProjectivePoint};
 
 const BENCHMARK_NAME: &str = "point";
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let point = AffinePoint::from_x(42u64.into()).unwrap();
+    let starknet_rs_affine_generator = GENERATOR;
 
-    let mut initial_projective_point = ProjectivePoint::from(&point);
-    let second_project_point = initial_projective_point.clone();
-
-
-    let mut projective_point = initial_projective_point.clone();
-    // We loop to have a higher variance of numbers, and make the time of the clones not relevant
-    for _i in 0..10000{
-        projective_point.add_assign(&second_project_point);
-    }
-    println!("Starknet RS result: ");
-
-    print!("{}",
-            AffinePoint::from(&projective_point).x.to_string().encode_hex::<String>()
-        );
-
-    {
-        c.bench_function(
-            &format!(
-                "{} Point Addition | Starknet RS ",
-                BENCHMARK_NAME
-            ),
-            |b| {
-                b.iter(|| {
-                    let mut projective_point = initial_projective_point.clone();
-                    // We loop to have a higher variance of numbers, and make the time of the clones not relevant
-                    for _i in 0..1000{
-                        projective_point.add_assign(&second_project_point);
-                    }
-                    projective_point
-                });
-            },
-        );
-    }
-
-    let x = FieldElement::<Stark252PrimeField>::from(42);
-
-    let y = FieldElement::<Stark252PrimeField>::from_hex_unchecked("011743d4867c1261920c023e4f6529a69aa0dc6df18100835078fba5d83b9dfa");
-
-    let initial_projective_point = ShortWeierstrassProjectivePoint::<StarkCurve>::from_affine(x, y).unwrap();
-    let second_projective_point = initial_projective_point.clone();
-
-
-    let mut projective_point = initial_projective_point.clone();
+    let starknet_rs_initial_projective = ProjectivePoint::from_affine_point(&GENERATOR.add(&GENERATOR));
+    // This is the code we are going to bench
+    // We test it once outside the bench to check the result matches with Lambdaworks
+    let mut projective_point_rs = starknet_rs_initial_projective;
     for _i in 0..10000 {
-        projective_point = black_box(projective_point.operate_with(black_box(&second_projective_point)));
+        projective_point_rs.add_assign(&starknet_rs_affine_generator);
     }
-    println!("Lambdaworks result: {}", projective_point.to_affine().x().to_string());
+
+
+    let starknet_rs_x = AffinePoint::from(&projective_point_rs).x;
+    println!("Starknet RS result - X: {:#x} ", starknet_rs_x);
+    let starknet_rs_y = AffinePoint::from(&projective_point_rs).y;
+    println!("Starknet RS result - Y: {:#x} ", starknet_rs_y);
 
     {
         c.bench_function(
-            &format!(
-                "{} Point Addition | Lambdaworks",
-                BENCHMARK_NAME
-            ),
+            &format!("{} 10k Operations with Affine (Add) | Starknet RS ", BENCHMARK_NAME),
             |b| {
                 b.iter(|| {
-                    let mut projective_point = initial_projective_point.clone();
+                    let mut projective_point_rs = starknet_rs_initial_projective;
                     for _i in 0..10000 {
-                        projective_point = black_box(projective_point.operate_with(black_box(&second_projective_point)));
+                        projective_point_rs.add_assign(black_box(&starknet_rs_affine_generator));
+                    }
+                    projective_point_rs
+                });
+            },
+        );
+    }
+
+    let lambdaworks_affine_generator = StarkCurve::generator();
+
+    // This is the code we are going to bench
+    // We test it once outside the bench to check the result matches with Starknet RS
+    let lambdaworks_rs_initial_projective = StarkCurve::generator().operate_with(&StarkCurve::generator());
+
+    let mut projective_point = lambdaworks_rs_initial_projective.clone();
+    for _i in 0..10000 {
+        projective_point =
+            black_box(projective_point.operate_with(black_box(&lambdaworks_affine_generator)));
+    }
+
+    let lambdaworks_x = projective_point.to_affine().x().to_string();
+    let lambdaworks_y = projective_point.to_affine().y().to_string();
+    println!("Lambdaworks result - X: {}", lambdaworks_x);
+    println!("Lambdaworks result - Y: {}", lambdaworks_y);
+
+    {
+        c.bench_function(
+            &format!("{} 10k Operations with Affine (Add) | Lambdaworks", BENCHMARK_NAME),
+            |b| {
+                b.iter(|| {
+                    let mut projective_point = lambdaworks_rs_initial_projective.clone();
+                    for _i in 0..10000 {
+                        projective_point =
+                            projective_point.operate_with(black_box(&lambdaworks_affine_generator));
                     }
                     projective_point
                 });
             },
         );
     }
-
-   
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
-
-
-/*
-pub fn criterion_benchmark(c: &mut Criterion) {
-    let point = AffinePoint::from_x(42u8.into()).unwrap();
-
-    let mut initial_projective_point = ProjectivePoint::from(&point);
-    let second_project_point = initial_projective_point.clone();
-    {
-        c.bench_function(
-            &format!(
-                "{} Point Addition | Starkent RS ",
-                BENCHMARK_NAME
-            ),
-            |b| {
-                b.iter(|| {
-                    let mut projective_point = initial_projective_point.clone();
-                    for _i in 0..1000{
-                        black_box(projective_point).add_assign(black_box(&black_box(second_project_point)))
-                    }
-                   
-                });
-            },
-        );
-    }
-
-    let x = FieldElement::<Stark252PrimeField>::from(42);
-
-    let y = FieldElement::<Stark252PrimeField>::from_hex_unchecked("011743d4867c1261920c023e4f6529a69aa0dc6df18100835078fba5d83b9dfa");
-
-    let mut initial_projective_point = ShortWeierstrassProjectivePoint::<StarkCurve>::from_affine(x, y).unwrap();
-    let second_projective_point = initial_projective_point.clone();
-
-    {
-        c.bench_function(
-            &format!(
-                "{} Point Addition | Lambdaworks",
-                BENCHMARK_NAME
-            ),
-            |b| {
-                b.iter(|| {
-                    let mut projective_point = initial_projective_point.clone();
-                    for _i in 0..1000 {
-                        black_box(projective_point.operate_with(black_box(&second_projective_point)));
-                    }
-                });
-            },
-        );
-    }
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
-*/
