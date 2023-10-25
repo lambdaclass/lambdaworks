9f7307ac2cfce7da1d3ae61bbc763f427d634cd5
MauroFab <maurotoscano2@gmail.com>
2023-10-25 17:05:30 -0300

    Test

--- benches/benches/point.rs
+++ benches/benches/point.rs
@@ -97,7 +97,7 @@ pub fn point_add_projective_affine(c: &mut Criterion) {
                     let mut projective_point = lambdaworks_rs_initial_projective.clone();
                     for _i in 0..10000 {
                         projective_point =
-                            projective_point.operate_with(black_box(&lambdaworks_affine_generator));
+                            projective_point.operate_with_affine(black_box(&lambdaworks_affine_generator));
                     }
                     projective_point
                 });
