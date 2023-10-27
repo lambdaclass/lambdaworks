592b9760348dcda20cc73469df57a92442345546
PatStiles <33334338+PatStiles@users.noreply.github.com>
2023-10-27 08:03:49 -0500

    Feat(field): add Mini goldilocks Field (#622)
    * init
    
    * finish trait functions need to debug canon
    
    * add montgomery field
    
    * add benches + fix clippy
    
    * fix tests
    
    * clean up and bench fix
    
    * add extension and hook up fuzzer
    
    * fmt
    
    * add fuzzing
    
    * fuzz against plonky3
    
    * fix lint by adding deny statement
    
    * fix comments + rm fuzz crash corpus
    
    * fix nt
    
    * add fuzz
    
    * refactor benchmarks
    
    * rm extraneous fuzz
    
    * fmt
    
    * fmt + clippy
    
    * Update fuzz/no_gpu_fuzz/fuzz_targets/field_mini_goldilocks.rs
    
    * Update fuzz/no_gpu_fuzz/Cargo.toml
    
    * Update fuzz/no_gpu_fuzz/Cargo.toml
    
    * Polish bench
    
    * Add seed to montgomery goldilocks
    
    ---------
    
    Co-authored-by: Mariano A. Nicolini <mariano.nicolini.91@gmail.com>

--- math/src/field/fields/mod.rs
+++ math/src/field/fields/mod.rs
@@ -3,5 +3,7 @@ pub mod fft_friendly;
 pub mod montgomery_backed_prime_fields;
 /// Implementation of the Goldilocks Prime field (p = 2^448 - 2^224 - 1)
 pub mod p448_goldilocks_prime_field;
+/// Implementation of the u64 Goldilocks Prime field (p = 2^64 - 2^32 + 1)
+pub mod u64_goldilocks_field;
 /// Implementation of prime fields over 64 bit unsigned integers.
 pub mod u64_prime_field;
