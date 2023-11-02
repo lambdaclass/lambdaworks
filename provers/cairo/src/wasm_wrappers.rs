f169cba1926c2d9054cb3ae7a82733c4244c1b89
MauroFab <maurotoscano2@gmail.com>
2023-11-02 19:00:42 -0300

    Compatibility with main

--- provers/cairo/src/wasm_wrappers.rs
+++ provers/cairo/src/wasm_wrappers.rs
@@ -1,4 +1,4 @@
-use super::air::{CairoAIR, PublicInputs};
+use super::air::CairoAIR;
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::Stark252PrimeField;
 use serde::{Deserialize, Serialize};
@@ -20,17 +20,35 @@ pub struct FE(FieldElement<Stark252PrimeField>);
 pub struct MemoryMap(HashMap<FE, FE>);
 
 #[wasm_bindgen]
-pub fn verify_cairo_proof_wasm(
-    proof: &[u8],
-    pub_input_serialized: &[u8],
-    proof_options: &ProofOptions,
-) -> bool {
-    let pub_input: PublicInputs = serde_cbor::from_slice(pub_input_serialized).unwrap();
-    let proof: StarkProof<Stark252PrimeField> = serde_cbor::from_slice(proof).unwrap();
+pub fn verify_cairo_proof_wasm(proof_bytes: &[u8], proof_options: &ProofOptions) -> bool {
+    let bytes = proof_bytes;
+
+    // This logic is the same as main verify, with only error handling changing. In wasm, we simply return a false if the proof is invalid, instead of rising an error.
+
+    // Proof len was stored as an u32, 4u8 needs to be read
+    let proof_len = u32::from_le_bytes(bytes[0..4].try_into().unwrap()) as usize;
+
+    let bytes = &bytes[4..];
+    if bytes.len() < proof_len {
+        return false;
+    }
+
+    let Ok((proof, _)) =
+        bincode::serde::decode_from_slice(&bytes[0..proof_len], bincode::config::standard())
+    else {
+        return false;
+    };
+    let bytes = &bytes[proof_len..];
+
+    let Ok((pub_inputs, _)) =
+        bincode::serde::decode_from_slice(&bytes, bincode::config::standard())
+    else {
+        return false;
+    };
 
     Verifier::verify::<CairoAIR>(
         &proof,
-        &pub_input,
+        &pub_inputs,
         proof_options,
         StoneProverTranscript::new(&[]),
     )
