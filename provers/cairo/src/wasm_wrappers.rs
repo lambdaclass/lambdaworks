b9477f800474f9772308f77b778b582f78b93ba1
MauroFab <maurotoscano2@gmail.com>
2023-11-03 12:59:13 -0300

    Fix

--- provers/cairo/src/wasm_wrappers.rs
+++ provers/cairo/src/wasm_wrappers.rs
@@ -41,7 +41,7 @@ pub fn verify_cairo_proof_wasm(proof_bytes: &[u8], proof_options: &ProofOptions)
     let bytes = &bytes[proof_len..];
 
     let Ok((pub_inputs, _)) =
-        bincode::serde::decode_from_slice(&bytes, bincode::config::standard())
+        bincode::serde::decode_from_slice(bytes, bincode::config::standard())
     else {
         return false;
     };
