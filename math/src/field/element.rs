7b22a26f4eaf00842544b8dfbaf4018df98a6600
jmunoz <juanmunoz890@gmail.com>
2023-11-03 11:16:20 -0300

    keep only string on merkle cli

--- math/src/field/element.rs
+++ math/src/field/element.rs
@@ -1,10 +1,7 @@
 use crate::errors::CreationError;
 use crate::field::errors::FieldError;
 use crate::field::traits::IsField;
-#[cfg(all(
-    feature = "lambdaworks-serde-binary",
-    not(feature = "lambdaworks-serde-string")
-))]
+#[cfg(feature = "lambdaworks-serde-binary")]
 use crate::traits::ByteConversion;
 use crate::unsigned_integer::element::UnsignedInteger;
 use crate::unsigned_integer::montgomery::MontgomeryAlgorithms;
@@ -472,8 +469,7 @@ impl<F: IsPrimeField> Serialize for FieldElement<F> {
         S: Serializer,
     {
         let mut state = serializer.serialize_struct("FieldElement", 1)?;
-        let data = self.value().to_bytes_be();
-        state.serialize_field("value", &data)?;
+        state.serialize_field("value", &F::representative(self.value()).to_string())?;
         state.end()
     }
 }
@@ -566,7 +562,7 @@ impl<'de, F: IsPrimeField> Deserialize<'de> for FieldElement<F> {
             where
                 M: MapAccess<'de>,
             {
-                let mut value: Option<Vec<u8>> = None;
+                let mut value = None;
                 while let Some(key) = map.next_key()? {
                     match key {
                         Field::Value => {
@@ -576,38 +572,6 @@ impl<'de, F: IsPrimeField> Deserialize<'de> for FieldElement<F> {
                             value = Some(map.next_value()?);
                         }
                     }
-                }
-                let value = value.ok_or_else(|| de::Error::missing_field("value"))?;
-                let val = F::BaseType::from_bytes_be(&value).unwrap();
-                Ok(FieldElement::from_raw(&val))
-            }
-
-            fn visit_seq<S>(self, mut seq: S) -> Result<FieldElement<F>, S::Error>
-            where
-                S: SeqAccess<'de>,
-            {
-                let mut value: Option<Vec<u8>> = None;
-                while let Some(val) = seq.next_element()? {
-                    if value.is_some() {
-                        return Err(de::Error::duplicate_field("value"));
-                    }
-                    value = Some(val);
-                }
-                let value = value.ok_or_else(|| de::Error::missing_field("value"))?;
-                let val = F::BaseType::from_bytes_be(&value).unwrap();
-                Ok(FieldElement::from_raw(&val))
-            }
-
-            fn visit_seq<S>(self, mut seq: S) -> Result<FieldElement<F>, S::Error>
-            where
-                S: SeqAccess<'de>,
-            {
-                let mut value = None;
-                while let Some(val) = seq.next_element()? {
-                    if value.is_some() {
-                        return Err(de::Error::duplicate_field("value"));
-                    }
-                    value = Some(val);
                 }
                 let value = value.ok_or_else(|| de::Error::missing_field("value"))?;
                 Ok(FieldElement::from_hex(value).unwrap())
