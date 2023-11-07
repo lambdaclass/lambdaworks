af1c05065d271039a5b6bdb97d4a04d89c078f56
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-11-07 17:08:02 -0300

    Fix permutation argument Cairo constraints

--- provers/cairo/src/air.rs
+++ provers/cairo/src/air.rs
@@ -14,7 +14,7 @@ use stark_platinum_prover::{
     frame::Frame,
     proof::{options::ProofOptions, stark::StarkProof},
     prover::{IsStarkProver, Prover, ProvingError},
-    trace::TraceTable,
+    trace::{StepView, TraceTable},
     traits::AIR,
     transcript::{IsStarkTranscript, StoneProverTranscript},
     verifier::{IsStarkVerifier, Verifier},
@@ -1015,54 +1015,83 @@ fn compute_operand_constraints(constraints: &mut [Felt252], frame: &Frame<Stark2
 }
 
 fn compute_register_constraints(constraints: &mut [Felt252], frame: &Frame<Stark252PrimeField>) {
-    let curr = frame.get_row(0);
-    let next = frame.get_row(1);
+    let curr = frame.get_evaluation_step(0);
+    let next = frame.get_evaluation_step(1);
 
     let one = Felt252::one();
     let two = Felt252::from(2);
 
-    // ap and fp constraints
-    constraints[NEXT_AP] =
-        curr[FRAME_AP] + curr[F_AP_ADD] * curr[FRAME_RES] + curr[F_AP_ONE] + curr[F_OPC_CALL] * two
-            - next[FRAME_AP];
+    let ap = curr.get_evaluation_element(0, FRAME_AP);
+    let next_ap = next.get_evaluation_element(0, FRAME_AP);
+    let ap_add = curr.get_evaluation_element(0, F_AP_ADD);
+    let res = curr.get_evaluation_element(0, FRAME_RES);
+    let ap_one = curr.get_evaluation_element(0, F_AP_ONE);
+    let opc_call = curr.get_evaluation_element(0, F_OPC_CALL);
 
-    constraints[NEXT_FP] = curr[F_OPC_RET] * curr[FRAME_DST]
-        + curr[F_OPC_CALL] * (curr[FRAME_AP] + two)
-        + (one - curr[F_OPC_RET] - curr[F_OPC_CALL]) * curr[FRAME_FP]
-        - next[FRAME_FP];
+    let opc_ret = curr.get_evaluation_element(0, F_OPC_RET);
+    let dst = curr.get_evaluation_element(0, FRAME_DST);
+    let fp = curr.get_evaluation_element(0, FRAME_FP);
+    let next_fp = next.get_evaluation_element(0, FRAME_FP);
+
+    let t1 = curr.get_evaluation_element(0, FRAME_T1);
+    let pc_jnz = curr.get_evaluation_element(0, F_PC_JNZ);
+    let pc = curr.get_evaluation_element(0, FRAME_PC);
+    let next_pc = next.get_evaluation_element(0, FRAME_PC);
+
+    let t0 = curr.get_evaluation_element(0, FRAME_T0);
+    let op1 = curr.get_evaluation_element(0, FRAME_OP1);
+    let pc_abs = curr.get_evaluation_element(0, F_PC_ABS);
+    let pc_rel = curr.get_evaluation_element(0, F_PC_REL);
+
+    // ap and fp constraints
+    constraints[NEXT_AP] = ap + ap_add * res + ap_one + opc_call * two - next_ap;
+
+    constraints[NEXT_FP] =
+        opc_ret * dst + opc_call * (ap + two) + (one - opc_ret - opc_call) * fp - next_fp;
 
     // pc constraints
-    constraints[NEXT_PC_1] = (curr[FRAME_T1] - curr[F_PC_JNZ])
-        * (next[FRAME_PC] - (curr[FRAME_PC] + frame_inst_size(curr)));
+    constraints[NEXT_PC_1] = (t1 - pc_jnz) * (next_pc - (pc + frame_inst_size(curr)));
 
-    constraints[NEXT_PC_2] = curr[FRAME_T0] * (next[FRAME_PC] - (curr[FRAME_PC] + curr[FRAME_OP1]))
-        + (one - curr[F_PC_JNZ]) * next[FRAME_PC]
-        - ((one - curr[F_PC_ABS] - curr[F_PC_REL] - curr[F_PC_JNZ])
-            * (curr[FRAME_PC] + frame_inst_size(curr))
-            + curr[F_PC_ABS] * curr[FRAME_RES]
-            + curr[F_PC_REL] * (curr[FRAME_PC] + curr[FRAME_RES]));
+    constraints[NEXT_PC_2] = t0 * (next_pc - (pc + op1)) + (one - pc_jnz) * next_pc
+        - ((one - pc_abs - pc_rel - pc_jnz) * (pc + frame_inst_size(curr))
+            + pc_abs * res
+            + pc_rel * (pc + res));
 
-    constraints[T0] = curr[F_PC_JNZ] * curr[FRAME_DST] - curr[FRAME_T0];
-    constraints[T1] = curr[FRAME_T0] * curr[FRAME_RES] - curr[FRAME_T1];
+    constraints[T0] = pc_jnz * dst - t0;
+    constraints[T1] = t0 * res - t1;
 }
 
 fn compute_opcode_constraints(constraints: &mut [Felt252], frame: &Frame<Stark252PrimeField>) {
-    let curr = frame.get_row(0);
+    let curr = frame.get_evaluation_step(0);
     let one = Felt252::one();
 
-    constraints[MUL_1] = curr[FRAME_MUL] - curr[FRAME_OP0] * curr[FRAME_OP1];
+    let mul = curr.get_evaluation_element(0, FRAME_MUL);
+    let op0 = curr.get_evaluation_element(0, FRAME_OP0);
+    let op1 = curr.get_evaluation_element(0, FRAME_OP1);
 
-    constraints[MUL_2] = curr[F_RES_ADD] * (curr[FRAME_OP0] + curr[FRAME_OP1])
-        + curr[F_RES_MUL] * curr[FRAME_MUL]
-        + (one - curr[F_RES_ADD] - curr[F_RES_MUL] - curr[F_PC_JNZ]) * curr[FRAME_OP1]
-        - (one - curr[F_PC_JNZ]) * curr[FRAME_RES];
+    let res_add = curr.get_evaluation_element(0, F_RES_ADD);
+    let res_mul = curr.get_evaluation_element(0, F_RES_MUL);
+    let pc_jnz = curr.get_evaluation_element(0, F_PC_JNZ);
+    let res = curr.get_evaluation_element(0, FRAME_RES);
 
-    constraints[CALL_1] = curr[F_OPC_CALL] * (curr[FRAME_DST] - curr[FRAME_FP]);
+    let opc_call = curr.get_evaluation_element(0, F_OPC_CALL);
+    let dst = curr.get_evaluation_element(0, FRAME_DST);
+    let fp = curr.get_evaluation_element(0, FRAME_FP);
+    let pc = curr.get_evaluation_element(0, FRAME_FP);
 
-    constraints[CALL_2] =
-        curr[F_OPC_CALL] * (curr[FRAME_OP0] - (curr[FRAME_PC] + frame_inst_size(curr)));
+    let opc_aeq = curr.get_evaluation_element(0, F_OPC_AEQ);
 
-    constraints[ASSERT_EQ] = curr[F_OPC_AEQ] * (curr[FRAME_DST] - curr[FRAME_RES]);
+    constraints[MUL_1] = mul - op0 * op1;
+
+    constraints[MUL_2] =
+        res_add * (op0 + op1) + res_mul * mul + (one - res_add - res_mul - pc_jnz) * op1
+            - (one - pc_jnz) * res;
+
+    constraints[CALL_1] = opc_call * (dst - fp);
+
+    constraints[CALL_2] = opc_call * (op0 - (pc + frame_inst_size(curr)));
+
+    constraints[ASSERT_EQ] = opc_aeq * (dst - res);
 }
 
 fn memory_is_increasing(
@@ -1070,69 +1099,56 @@ fn memory_is_increasing(
     frame: &Frame<Stark252PrimeField>,
     builtin_offset: usize,
 ) {
-    let curr = frame.get_row(0);
-    let next = frame.get_row(1);
+    let curr = frame.get_evaluation_step(0);
+    let next = frame.get_evaluation_step(1);
     let one = FieldElement::one();
 
-    constraints[MEMORY_INCREASING_0] = (curr[MEMORY_ADDR_SORTED_0 - builtin_offset]
-        - curr[MEMORY_ADDR_SORTED_1 - builtin_offset])
-        * (curr[MEMORY_ADDR_SORTED_1 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_0 - builtin_offset]
-            - one);
+    let mem_addr_sorted_0 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_0 - builtin_offset);
+    let mem_addr_sorted_1 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_1 - builtin_offset);
+    let mem_addr_sorted_2 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_2 - builtin_offset);
+    let mem_addr_sorted_3 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_3 - builtin_offset);
+    let mem_addr_sorted_4 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_4 - builtin_offset);
+    let mem_addr_sorted_0 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_0 - builtin_offset);
+    let next_mem_addr_sorted_0 =
+        next.get_evaluation_element(0, MEMORY_ADDR_SORTED_0 - builtin_offset);
 
-    constraints[MEMORY_INCREASING_1] = (curr[MEMORY_ADDR_SORTED_1 - builtin_offset]
-        - curr[MEMORY_ADDR_SORTED_2 - builtin_offset])
-        * (curr[MEMORY_ADDR_SORTED_2 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_1 - builtin_offset]
-            - one);
+    let mem_val_sorted_0 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_0 - builtin_offset);
+    let mem_val_sorted_1 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_1 - builtin_offset);
+    let mem_val_sorted_2 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_2 - builtin_offset);
+    let mem_val_sorted_3 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_3 - builtin_offset);
+    let mem_val_sorted_4 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_4 - builtin_offset);
+    let next_mem_val_sorted_0 =
+        next.get_evaluation_element(0, MEMORY_VALUES_SORTED_0 - builtin_offset);
 
-    constraints[MEMORY_INCREASING_2] = (curr[MEMORY_ADDR_SORTED_2 - builtin_offset]
-        - curr[MEMORY_ADDR_SORTED_3 - builtin_offset])
-        * (curr[MEMORY_ADDR_SORTED_3 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_2 - builtin_offset]
-            - one);
+    constraints[MEMORY_INCREASING_0] =
+        (mem_addr_sorted_0 - mem_addr_sorted_1) * (mem_addr_sorted_1 - mem_addr_sorted_0 - one);
 
-    constraints[MEMORY_INCREASING_3] = (curr[MEMORY_ADDR_SORTED_3 - builtin_offset]
-        - curr[MEMORY_ADDR_SORTED_4 - builtin_offset])
-        * (curr[MEMORY_ADDR_SORTED_4 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_3 - builtin_offset]
-            - one);
+    constraints[MEMORY_INCREASING_1] =
+        (mem_addr_sorted_1 - mem_addr_sorted_2) * (mem_addr_sorted_2 - mem_addr_sorted_1 - one);
 
-    constraints[MEMORY_INCREASING_4] = (curr[MEMORY_ADDR_SORTED_4 - builtin_offset]
-        - next[MEMORY_ADDR_SORTED_0 - builtin_offset])
-        * (next[MEMORY_ADDR_SORTED_0 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_4 - builtin_offset]
-            - one);
+    constraints[MEMORY_INCREASING_2] =
+        (mem_addr_sorted_2 - mem_addr_sorted_3) * (mem_addr_sorted_3 - mem_addr_sorted_2 - one);
 
-    constraints[MEMORY_CONSISTENCY_0] = (curr[MEMORY_VALUES_SORTED_0 - builtin_offset]
-        - curr[MEMORY_VALUES_SORTED_1 - builtin_offset])
-        * (curr[MEMORY_ADDR_SORTED_1 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_0 - builtin_offset]
-            - one);
+    constraints[MEMORY_INCREASING_3] =
+        (mem_addr_sorted_3 - mem_addr_sorted_4) * (mem_addr_sorted_4 - mem_addr_sorted_3 - one);
 
-    constraints[MEMORY_CONSISTENCY_1] = (curr[MEMORY_VALUES_SORTED_1 - builtin_offset]
-        - curr[MEMORY_VALUES_SORTED_2 - builtin_offset])
-        * (curr[MEMORY_ADDR_SORTED_2 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_1 - builtin_offset]
-            - one);
+    constraints[MEMORY_INCREASING_4] = (mem_addr_sorted_4 - next_mem_addr_sorted_0)
+        * (next_mem_addr_sorted_0 - mem_addr_sorted_4 - one);
 
-    constraints[MEMORY_CONSISTENCY_2] = (curr[MEMORY_VALUES_SORTED_2 - builtin_offset]
-        - curr[MEMORY_VALUES_SORTED_3 - builtin_offset])
-        * (curr[MEMORY_ADDR_SORTED_3 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_2 - builtin_offset]
-            - one);
+    constraints[MEMORY_CONSISTENCY_0] =
+        (mem_val_sorted_0 - mem_val_sorted_1) * (mem_addr_sorted_1 - mem_addr_sorted_0 - one);
 
-    constraints[MEMORY_CONSISTENCY_3] = (curr[MEMORY_VALUES_SORTED_3 - builtin_offset]
-        - curr[MEMORY_VALUES_SORTED_4 - builtin_offset])
-        * (curr[MEMORY_ADDR_SORTED_4 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_3 - builtin_offset]
-            - one);
+    constraints[MEMORY_CONSISTENCY_1] =
+        (mem_val_sorted_1 - mem_val_sorted_2) * (mem_addr_sorted_2 - mem_addr_sorted_1 - one);
 
-    constraints[MEMORY_CONSISTENCY_4] = (curr[MEMORY_VALUES_SORTED_4 - builtin_offset]
-        - next[MEMORY_VALUES_SORTED_0 - builtin_offset])
-        * (next[MEMORY_ADDR_SORTED_0 - builtin_offset]
-            - curr[MEMORY_ADDR_SORTED_4 - builtin_offset]
-            - one);
+    constraints[MEMORY_CONSISTENCY_2] =
+        (mem_val_sorted_2 - mem_val_sorted_3) * (mem_addr_sorted_3 - mem_addr_sorted_2 - one);
+
+    constraints[MEMORY_CONSISTENCY_3] =
+        (mem_val_sorted_3 - mem_val_sorted_4) * (mem_addr_sorted_4 - mem_addr_sorted_3 - one);
+
+    constraints[MEMORY_CONSISTENCY_4] = (mem_val_sorted_4 - next_mem_val_sorted_0)
+        * (next_mem_addr_sorted_0 - mem_addr_sorted_4 - one);
 }
 
 fn permutation_argument(
@@ -1141,41 +1157,42 @@ fn permutation_argument(
     rap_challenges: &CairoRAPChallenges,
     builtin_offset: usize,
 ) {
-    let curr = frame.get_row(0);
-    let next = frame.get_row(1);
+    let curr = frame.get_evaluation_step(0);
+    let next = frame.get_evaluation_step(1);
+
     let z = &rap_challenges.z_memory;
     let alpha = &rap_challenges.alpha_memory;
 
-    let p0 = &curr[PERMUTATION_ARGUMENT_COL_0 - builtin_offset];
-    let p0_next = &next[PERMUTATION_ARGUMENT_COL_0 - builtin_offset];
-    let p1 = &curr[PERMUTATION_ARGUMENT_COL_1 - builtin_offset];
-    let p2 = &curr[PERMUTATION_ARGUMENT_COL_2 - builtin_offset];
-    let p3 = &curr[PERMUTATION_ARGUMENT_COL_3 - builtin_offset];
-    let p4 = &curr[PERMUTATION_ARGUMENT_COL_4 - builtin_offset];
+    let p0 = curr.get_evaluation_element(0, PERMUTATION_ARGUMENT_COL_0 - builtin_offset);
+    let next_p0 = next.get_evaluation_element(0, PERMUTATION_ARGUMENT_COL_0 - builtin_offset);
+    let p1 = curr.get_evaluation_element(0, PERMUTATION_ARGUMENT_COL_1 - builtin_offset);
+    let p2 = curr.get_evaluation_element(0, PERMUTATION_ARGUMENT_COL_2 - builtin_offset);
+    let p3 = curr.get_evaluation_element(0, PERMUTATION_ARGUMENT_COL_3 - builtin_offset);
+    let p4 = curr.get_evaluation_element(0, PERMUTATION_ARGUMENT_COL_4 - builtin_offset);
 
-    let ap0_next = &next[MEMORY_ADDR_SORTED_0 - builtin_offset];
-    let ap1 = &curr[MEMORY_ADDR_SORTED_1 - builtin_offset];
-    let ap2 = &curr[MEMORY_ADDR_SORTED_2 - builtin_offset];
-    let ap3 = &curr[MEMORY_ADDR_SORTED_3 - builtin_offset];
-    let ap4 = &curr[MEMORY_ADDR_SORTED_4 - builtin_offset];
+    let next_ap0 = next.get_evaluation_element(0, MEMORY_ADDR_SORTED_0 - builtin_offset);
+    let ap1 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_1 - builtin_offset);
+    let ap2 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_2 - builtin_offset);
+    let ap3 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_3 - builtin_offset);
+    let ap4 = curr.get_evaluation_element(0, MEMORY_ADDR_SORTED_4 - builtin_offset);
 
-    let vp0_next = &next[MEMORY_VALUES_SORTED_0 - builtin_offset];
-    let vp1 = &curr[MEMORY_VALUES_SORTED_1 - builtin_offset];
-    let vp2 = &curr[MEMORY_VALUES_SORTED_2 - builtin_offset];
-    let vp3 = &curr[MEMORY_VALUES_SORTED_3 - builtin_offset];
-    let vp4 = &curr[MEMORY_VALUES_SORTED_4 - builtin_offset];
+    let next_vp0 = next.get_evaluation_element(0, MEMORY_VALUES_SORTED_0 - builtin_offset);
+    let vp1 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_1 - builtin_offset);
+    let vp2 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_2 - builtin_offset);
+    let vp3 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_3 - builtin_offset);
+    let vp4 = curr.get_evaluation_element(0, MEMORY_VALUES_SORTED_4 - builtin_offset);
 
-    let a0_next = &next[FRAME_PC];
-    let a1 = &curr[FRAME_DST_ADDR];
-    let a2 = &curr[FRAME_OP0_ADDR];
-    let a3 = &curr[FRAME_OP1_ADDR];
-    let a4 = &curr[EXTRA_ADDR];
+    let next_a0 = next.get_evaluation_element(0, FRAME_PC);
+    let a1 = curr.get_evaluation_element(0, FRAME_DST_ADDR);
+    let a2 = curr.get_evaluation_element(0, FRAME_OP0_ADDR);
+    let a3 = curr.get_evaluation_element(0, FRAME_OP1_ADDR);
+    let a4 = curr.get_evaluation_element(0, EXTRA_ADDR);
 
-    let v0_next = &next[FRAME_INST];
-    let v1 = &curr[FRAME_DST];
-    let v2 = &curr[FRAME_OP0];
-    let v3 = &curr[FRAME_OP1];
-    let v4 = &curr[EXTRA_VAL];
+    let next_v0 = next.get_evaluation_element(0, FRAME_INST);
+    let v1 = curr.get_evaluation_element(0, FRAME_DST);
+    let v2 = curr.get_evaluation_element(0, FRAME_OP0);
+    let v3 = curr.get_evaluation_element(0, FRAME_OP1);
+    let v4 = curr.get_evaluation_element(0, EXTRA_VAL);
 
     constraints[PERMUTATION_ARGUMENT_0] =
         (z - (ap1 + alpha * vp1)) * p1 - (z - (a1 + alpha * v1)) * p0;
@@ -1186,7 +1203,7 @@ fn permutation_argument(
     constraints[PERMUTATION_ARGUMENT_3] =
         (z - (ap4 + alpha * vp4)) * p4 - (z - (a4 + alpha * v4)) * p3;
     constraints[PERMUTATION_ARGUMENT_4] =
-        (z - (ap0_next + alpha * vp0_next)) * p0_next - (z - (a0_next + alpha * v0_next)) * p4;
+        (z - (next_ap0 + alpha * next_vp0)) * next_p0 - (z - (next_a0 + alpha * next_v0)) * p4;
 }
 
 fn permutation_argument_range_check(
@@ -1243,8 +1260,9 @@ fn permutation_argument_range_check(
     constraints[RANGE_CHECK_3] = (z - ap0_next) * p0_next - (z - a0_next) * p3;
 }
 
-fn frame_inst_size(frame_row: &[Felt252]) -> Felt252 {
-    frame_row[F_OP_1_VAL] + Felt252::one()
+fn frame_inst_size(step: &StepView<Stark252PrimeField>) -> Felt252 {
+    let op1_val = step.get_evaluation_element(0, F_OP_1_VAL);
+    op1_val + Felt252::one()
 }
 
 fn range_check_builtin(
