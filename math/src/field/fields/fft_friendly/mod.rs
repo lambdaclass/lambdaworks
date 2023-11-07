335422b1605dca7ff8050434c13d7a3062aca279
PatStiles <pdstiles78@gmail.com>
2023-11-07 00:24:25 -0600

    fix ci

--- math/src/field/fields/fft_friendly/mod.rs
+++ math/src/field/fields/fft_friendly/mod.rs
@@ -2,7 +2,7 @@
 pub mod babybear;
 /// Implementation of two-adic Prime Field over 256 bit unsigned integers.
 pub mod stark_252_prime_field;
-/// Implemenation of the Mersenne Prime field p = 2^31 - 1
-pub mod u64_mersenne_montgomery_field;
 /// Implemenation of the Goldilocks Prime Field p = 2^64 - 2^32 + 1
 pub mod u64_goldilocks;
+/// Implemenation of the Mersenne Prime field p = 2^31 - 1
+pub mod u64_mersenne_montgomery_field;
