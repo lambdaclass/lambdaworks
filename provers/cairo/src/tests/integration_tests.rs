acd7913b349355ced319785b34a0510d407684f7
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-11-10 15:32:40 -0300

    Remove useless tests

--- provers/cairo/src/tests/integration_tests.rs
+++ provers/cairo/src/tests/integration_tests.rs
@@ -1,11 +1,7 @@
 use crate::{
-    air::{
-        generate_cairo_proof, verify_cairo_proof, CairoAIR, MemorySegment, MemorySegmentMap,
-        PublicInputs, FRAME_DST_ADDR, FRAME_OP0_ADDR, FRAME_OP1_ADDR, FRAME_PC,
-    },
+    air::{generate_cairo_proof, verify_cairo_proof, CairoAIR},
     cairo_layout::CairoLayout,
-    execution_trace::build_main_trace,
-    runner::run::{generate_prover_args, run_program},
+    runner::run::generate_prover_args,
     tests::utils::{
         cairo0_program_path, test_prove_cairo_program, test_prove_cairo_program_from_trace,
     },
@@ -19,7 +15,6 @@ use stark_platinum_prover::{
         options::{ProofOptions, SecurityLevel},
         stark::StarkProof,
     },
-    trace::TraceTable,
     traits::AIR,
     transcript::StoneProverTranscript,
 };
@@ -85,33 +80,6 @@ fn test_prove_cairo_fibonacci_1000() {
 //     test_prove_cairo1_program(&cairo1_program_path("fibonacci_cairo1_mod.casm"), layout);
 // }
 
-// #[test_log::test]
-// fn test_prove_cairo_lt_comparison() {
-//     let layout = CairoLayout::Small;
-//     test_prove_cairo_program(&cairo0_program_path("lt_comparison.json"), &None, layout);
-// }
-
-// #[cfg_attr(feature = "metal", ignore)]
-// #[test_log::test]
-// fn test_prove_cairo_compare_lesser_array() {
-//     let layout = CairoLayout::Small;
-//     test_prove_cairo_program(
-//         &cairo0_program_path("compare_lesser_array.json"),
-//         &None,
-//         layout,
-//     );
-// }
-
-// #[test_log::test]
-// fn test_prove_cairo_output_and_rc_program() {
-//     let layout = CairoLayout::Small;
-//     test_prove_cairo_program(
-//         &cairo0_program_path("signed_div_rem.json"),
-//         &Some(1837..1853),
-//         layout,
-//     );
-// }
-
 #[test_log::test]
 fn test_verifier_rejects_proof_of_a_slightly_different_program() {
     let program_content = std::fs::read(cairo0_program_path("simple_program.json")).unwrap();
@@ -149,117 +117,24 @@ fn test_verifier_rejects_proof_with_different_range_bounds() {
     assert!(!verify_cairo_proof(&proof, &pub_inputs, &proof_options));
 }
 
-// #[test_log::test]
-// fn test_verifier_rejects_proof_with_changed_range_check_value() {
-//     // In this test we change the range-check value in the trace, so the constraint
-//     // that asserts that the sum of the rc decomposed values is equal to the
-//     // range-checked value won't hold, and the verifier will reject the proof.
-//     let program_content = std::fs::read(cairo0_program_path("rc_program.json")).unwrap();
-//     let (main_trace, pub_inputs) =
-//         generate_prover_args(&program_content, CairoLayout::Small).unwrap();
+#[test_log::test]
+fn test_verifier_rejects_proof_with_different_security_params() {
+    let program_content = std::fs::read(cairo0_program_path("fibonacci_5.json")).unwrap();
+    let (main_trace, pub_inputs) =
+        generate_prover_args(&program_content, CairoLayout::Plain).unwrap();
 
-//     // The malicious value, we change the previous value to a 35.
-//     let malicious_rc_value = Felt252::from(35);
+    let proof_options_prover = ProofOptions::new_secure(SecurityLevel::Conjecturable80Bits, 3);
 
-//     let proof_options = ProofOptions::default_test_options();
+    let proof = generate_cairo_proof(&main_trace, &pub_inputs, &proof_options_prover).unwrap();
 
-//     let mut malicious_trace_columns = main_trace.columns();
-//     let n_cols = malicious_trace_columns.len();
-//     let mut last_column = malicious_trace_columns.last().unwrap().clone();
-//     last_column[0] = malicious_rc_value;
-//     malicious_trace_columns[n_cols - 1] = last_column;
+    let proof_options_verifier = ProofOptions::new_secure(SecurityLevel::Conjecturable128Bits, 3);
 
-//     let malicious_trace = TraceTable::from_columns(malicious_trace_columns, 1);
-//     let proof = generate_cairo_proof(&malicious_trace, &pub_inputs, &proof_options).unwrap();
-//     assert!(!verify_cairo_proof(&proof, &pub_inputs, &proof_options));
-// }
-
-// #[test_log::test]
-// fn test_verifier_rejects_proof_with_overflowing_range_check_value() {
-//     // In this test we manually insert a value greater than 2^128 in the range-check builtin segment.
-
-//     // This value is greater than 2^128, and the verifier should reject the proof built with it.
-//     let overflowing_rc_value = Felt252::from_hex("0x100000000000000000000000000000001").unwrap();
-//     let program_content = std::fs::read(cairo0_program_path("rc_program.json")).unwrap();
-//     let (register_states, mut malicious_memory, program_size, _) =
-//         run_program(None, CairoLayout::Small, &program_content).unwrap();
-
-//     // The malicious value is inserted in memory here.
-//     malicious_memory.data.insert(27, overflowing_rc_value);
-
-//     // These is the regular setup for generating the trace and the Cairo AIR, but now
-//     // we do it with the malicious memory
-//     let proof_options = ProofOptions::default_test_options();
-//     let memory_segments = MemorySegmentMap::from([(MemorySegment::RangeCheck, 27..29)]);
-
-//     let mut pub_inputs = PublicInputs::from_regs_and_mem(
-//         &register_states,
-//         &malicious_memory,
-//         program_size,
-//         &memory_segments,
-//     );
-
-//     let malicious_trace = build_main_trace(&register_states, &malicious_memory, &mut pub_inputs);
-
-//     let proof = generate_cairo_proof(&malicious_trace, &pub_inputs, &proof_options).unwrap();
-//     assert!(!verify_cairo_proof(&proof, &pub_inputs, &proof_options));
-// }
-
-// #[test_log::test]
-// fn test_verifier_rejects_proof_with_changed_output() {
-//     let program_content = std::fs::read(cairo0_program_path("output_program.json")).unwrap();
-//     let (main_trace, pub_inputs) =
-//         generate_prover_args(&program_content, &Some(27..28), CairoLayout::Small).unwrap();
-
-//     // The malicious value, we change the previous value to a 100.
-//     let malicious_output_value = Felt252::from(100);
-
-//     let mut output_col_idx = None;
-//     let mut output_row_idx = None;
-//     for (i, row) in main_trace.rows().iter().enumerate() {
-//         let output_col_found = [FRAME_PC, FRAME_DST_ADDR, FRAME_OP0_ADDR, FRAME_OP1_ADDR]
-//             .iter()
-//             .find(|&&col_idx| row[col_idx] != Felt252::from(19));
-//         if output_col_found.is_some() {
-//             output_col_idx = output_col_found;
-//             output_row_idx = Some(i);
-//             break;
-//         }
-//     }
-
-//     let output_col_idx = *output_col_idx.unwrap();
-//     let output_row_idx = output_row_idx.unwrap();
-
-//     let proof_options = ProofOptions::default_test_options();
-
-//     let mut malicious_trace_columns = main_trace.columns();
-//     let mut output_value_column = malicious_trace_columns[output_col_idx + 4].clone();
-//     output_value_column[output_row_idx] = malicious_output_value;
-//     malicious_trace_columns[output_col_idx + 4] = output_value_column;
-
-//     let malicious_trace = TraceTable::from_columns(malicious_trace_columns, 1);
-//     let proof = generate_cairo_proof(&malicious_trace, &pub_inputs, &proof_options).unwrap();
-//     assert!(!verify_cairo_proof(&proof, &pub_inputs, &proof_options));
-// }
-
-// #[test_log::test]
-// fn test_verifier_rejects_proof_with_different_security_params() {
-//     let program_content = std::fs::read(cairo0_program_path("output_program.json")).unwrap();
-//     let (main_trace, pub_inputs) =
-//         generate_prover_args(&program_content, &None, CairoLayout::Small).unwrap();
-
-//     let proof_options_prover = ProofOptions::new_secure(SecurityLevel::Conjecturable80Bits, 3);
-
-//     let proof = generate_cairo_proof(&main_trace, &pub_inputs, &proof_options_prover).unwrap();
-
-//     let proof_options_verifier = ProofOptions::new_secure(SecurityLevel::Conjecturable128Bits, 3);
-
-//     assert!(!verify_cairo_proof(
-//         &proof,
-//         &pub_inputs,
-//         &proof_options_verifier
-//     ));
-// }
+    assert!(!verify_cairo_proof(
+        &proof,
+        &pub_inputs,
+        &proof_options_verifier
+    ));
+}
 
 #[test]
 fn check_simple_cairo_trace_evaluates_to_zero() {
