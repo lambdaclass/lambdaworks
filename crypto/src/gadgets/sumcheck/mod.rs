aceceabcd502508b916346bc750f05ae4146a393
Wisdom Ogwu <iammadab@gmail.com>
2023-11-19 09:49:26 +0100

    add padded evals

--- crypto/src/gadgets/sumcheck/mod.rs
+++ crypto/src/gadgets/sumcheck/mod.rs
@@ -13,7 +13,7 @@ struct Sumcheck {}
 impl Sumcheck {
     fn prove<F: IsPrimeField>(
         poly: MultilinearPolynomial<F>,
-        _sum: FieldElement<F>,
+        sum: FieldElement<F>,
     ) -> Result<SumcheckProof<F>, String>
     where
         <F as IsField>::BaseType: Send + Sync,
@@ -44,9 +44,13 @@ impl Sumcheck {
         transcript.append(&claimed_sum.to_bytes_be());
         add_poly_to_transcript(&proof.poly, &mut transcript);
 
-        for poly in proof.uni_polys {
-            let p_0 = poly.evaluate(&[FieldElement::zero()]);
-            let p_1 = poly.evaluate(&[FieldElement::one()]);
+        for (round, poly) in proof.uni_polys.iter().enumerate() {
+            // verify that p(0) + p(1) = claimed_sum
+            let padded_0 = add_padding_to_evaluation_point(round, FieldElement::zero());
+            let padded_1 = add_padding_to_evaluation_point(round, FieldElement::one());
+            let p_0 = poly.evaluate(padded_0.as_slice());
+            let p_1 = poly.evaluate(padded_1.as_slice());
+
             if claimed_sum != p_0 + p_1 {
                 return false;
             }
@@ -56,13 +60,24 @@ impl Sumcheck {
             // update the claimed sum, by evaluating at sampled challenge
             let challenge_bytes = transcript.challenge();
             let challenge = FieldElement::<F>::from_bytes_be(&challenge_bytes).unwrap();
-            claimed_sum = poly.evaluate(&[challenge.clone()]);
+            let mut padded_challenge = add_padding_to_evaluation_point(round, challenge.clone());
+
+            claimed_sum = poly.evaluate(padded_challenge.as_slice());
             challenges.push(challenge);
         }
 
         let poly_at_challenge_eval = proof.poly.evaluate(challenges.as_slice());
         poly_at_challenge_eval == claimed_sum
     }
+}
+
+fn add_padding_to_evaluation_point<F: IsPrimeField>(
+    pad_num: usize,
+    point: FieldElement<F>,
+) -> Vec<FieldElement<F>> {
+    let mut padding = vec![FieldElement::zero(); pad_num];
+    padding.push(point);
+    padding
 }
 
 #[cfg(test)]
@@ -77,7 +92,7 @@ mod test {
     type FE = FieldElement<F>;
 
     #[test]
-    fn test_sumcheck_prover_verifier() {
+    fn test_sumcheck_prover_verifier_correct_sum() {
         // p = 2ab + 3bc
         // [a, b, c] = [0, 1, 2]
         // sum over boolean hypercube = 10
@@ -87,7 +102,6 @@ mod test {
         ]);
         let proof = Sumcheck::prove(p, FE::from(10)).unwrap();
         assert_eq!(proof.uni_polys.len(), 3);
-
         assert!(Sumcheck::verify(proof));
     }
 }
