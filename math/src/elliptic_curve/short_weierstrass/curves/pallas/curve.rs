11975277fc1221b8773e82aa201c7a2b56ea5391
mdvillagra <mdvillagra@gmail.com>
2023-11-21 11:55:59 -0300

    starting with pallas

---
+++ math/src/elliptic_curve/short_weierstrass/curves/pallas/curve.rs
@@ -0,0 +1,6 @@
+use crate::field::fields::pallas_field::Pallas255PrimeField;
+use crate::elliptic_curve::short_weierstrass::point::ShortWeierstrassProjectivePoint;
+use crate::elliptic_curve::traits::IsEllipticCurve;
+use crate::{
+    elliptic_curve::short_weierstrass::traits::IsShortWeierstrass, field::element::FieldElement,
+};
