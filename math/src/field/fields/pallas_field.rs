ed8f0f5a4b838e9ab93f427548abd3d172e5b45f
mdvillagra <mdvillagra@gmail.com>
2023-11-21 09:34:02 -0300

    added montgomery backend

--- math/src/field/fields/pallas_field.rs
+++ math/src/field/fields/pallas_field.rs
@@ -3,7 +3,15 @@ use crate::{
         element::FieldElement,
         fields::montgomery_backed_prime_fields::{IsModulus, MontgomeryBackendPrimeField},
     },
-    unsigned_integer::element::U64,
+    unsigned_integer::element::{UnsignedInteger, U256},
 };
 
 pub type PallasMontgomeryBackendPrimeField<T> = MontgomeryBackendPrimeField<T, 4>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MontgomeryConfigPallas255PrimeField;
+impl IsModulus<U256> for MontgomeryConfigPallas255PrimeField {
+    const MODULUS: U256 = U256::from_hex_unchecked(
+        "40000000000000000000000000000000224698fc094cf91b992d30ed00000001",
+    );
+}
