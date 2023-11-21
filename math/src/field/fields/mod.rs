895cfcffa43ecc0d7bd57bc834e5703bae1b4f5f
mdvillagra <mdvillagra@gmail.com>
2023-11-21 10:07:37 -0300

    fix

--- math/src/field/fields/mod.rs
+++ math/src/field/fields/mod.rs
@@ -1,7 +1,5 @@
 /// Implementation of two-adic prime fields to use with the Fast Fourier Transform (FFT).
 pub mod fft_friendly;
-/// Implementation of the 32-bit Mersenne Prime field (p = 2^31 - 1)
-pub mod mersenne31;
 pub mod montgomery_backed_prime_fields;
 /// Implementation of the Goldilocks Prime field (p = 2^448 - 2^224 - 1)
 pub mod p448_goldilocks_prime_field;
