c384b38c89db927f8e4af7aa8179e65ed0816343
Marcos Villagra <mdvillagra@gmail.com>
2023-11-23 19:28:09 -0300

    added fix

--- math/src/field/fields/fft_friendly/babybear.rs
+++ math/src/field/fields/fft_friendly/babybear.rs
@@ -106,6 +106,7 @@ mod tests {
         }
     }
 
+    #[cfg(all(feature = "std", not(feature = "instruments")))]
     mod test_babybear_31_fft {
         use super::*;
         use crate::fft::cpu::roots_of_unity::{
@@ -119,7 +120,7 @@ mod tests {
         use crate::polynomial::Polynomial;
         use proptest::{collection, prelude::*, std_facade::Vec};
 
-        #[cfg(not(any(feature = "metal", feature = "cuda")))]
+        #[cfg(not(any(feature = "metal",feature = "cuda")))]
         fn gen_fft_and_naive_evaluation<F: IsFFTField>(
             poly: Polynomial<FieldElement<F>>,
         ) -> (Vec<FieldElement<F>>, Vec<FieldElement<F>>) {
@@ -134,7 +135,7 @@ mod tests {
             (fft_eval, naive_eval)
         }
 
-        #[cfg(not(any(feature = "metal", feature = "cuda")))]
+        #[cfg(not(any(feature = "metal",feature = "cuda")))]
         fn gen_fft_coset_and_naive_evaluation<F: IsFFTField>(
             poly: Polynomial<FieldElement<F>>,
             offset: FieldElement<F>,
@@ -154,7 +155,7 @@ mod tests {
             (fft_eval, naive_eval)
         }
 
-        #[cfg(not(any(feature = "metal", feature = "cuda")))]
+        #[cfg(not(any(feature = "metal",feature = "cuda")))]
         fn gen_fft_and_naive_interpolate<F: IsFFTField>(
             fft_evals: &[FieldElement<F>],
         ) -> (Polynomial<FieldElement<F>>, Polynomial<FieldElement<F>>) {
@@ -168,7 +169,7 @@ mod tests {
             (fft_poly, naive_poly)
         }
 
-        #[cfg(not(any(feature = "metal", feature = "cuda")))]
+        #[cfg(not(any(feature = "metal",feature = "cuda")))]
         fn gen_fft_and_naive_coset_interpolate<F: IsFFTField>(
             fft_evals: &[FieldElement<F>],
             offset: &FieldElement<F>,
@@ -182,7 +183,7 @@ mod tests {
             (fft_poly, naive_poly)
         }
 
-        #[cfg(not(any(feature = "metal", feature = "cuda")))]
+        #[cfg(not(any(feature = "metal",feature = "cuda")))]
         fn gen_fft_interpolate_and_evaluate<F: IsFFTField>(
             poly: Polynomial<FieldElement<F>>,
         ) -> (Polynomial<FieldElement<F>>, Polynomial<FieldElement<F>>) {
