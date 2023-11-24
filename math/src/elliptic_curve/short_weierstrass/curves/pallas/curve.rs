54be68b1369d29cc8b43bbed0f2bc01dffc3f402
Marcos Villagra <mdvillagra@gmail.com>
2023-11-24 12:08:17 -0300

    curve created

--- math/src/elliptic_curve/short_weierstrass/curves/pallas/curve.rs
+++ math/src/elliptic_curve/short_weierstrass/curves/pallas/curve.rs
@@ -4,3 +4,28 @@ use crate::elliptic_curve::traits::IsEllipticCurve;
 use crate::{
     elliptic_curve::short_weierstrass::traits::IsShortWeierstrass, field::element::FieldElement,
 };
+
+pub struct PallasCurve;
+
+impl IsEllipticCurve for PallasCurve {
+    type BaseField = Pallas255PrimeField;
+    type PointRepresentation = ShortWeierstrassProjectivePoint<Self>;
+
+    fn generator() -> Self::PointRepresentation {
+        Self::PointRepresentation::new([
+            -FieldElement::<Self::BaseField>::one(),
+            FieldElement::<Self::BaseField>::from_raw([2, 0, 0, 0]),
+            FieldElement::one()
+        ])
+    }
+}
+
+impl IsShortWeierstrass for BLS12381Curve {
+    fn a() -> FieldElement<Self::BaseField> {
+        FieldElement::from(0)
+    }
+
+    fn b() -> FieldElement<Self::BaseField> {
+        FieldElement::from(5)
+    }
+}
