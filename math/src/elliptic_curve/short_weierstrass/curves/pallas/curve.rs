a645ac38003216d74458954a08945862f080f653
Marcos Villagra <mdvillagra@gmail.com>
2023-11-24 12:26:29 -0300

    adding tests

--- math/src/elliptic_curve/short_weierstrass/curves/pallas/curve.rs
+++ math/src/elliptic_curve/short_weierstrass/curves/pallas/curve.rs
@@ -1,6 +1,6 @@
-use crate::field::fields::pallas_field::Pallas255PrimeField;
 use crate::elliptic_curve::short_weierstrass::point::ShortWeierstrassProjectivePoint;
 use crate::elliptic_curve::traits::IsEllipticCurve;
+use crate::field::fields::pallas_field::Pallas255PrimeField;
 use crate::{
     elliptic_curve::short_weierstrass::traits::IsShortWeierstrass, field::element::FieldElement,
 };
@@ -15,7 +15,7 @@ impl IsEllipticCurve for PallasCurve {
         Self::PointRepresentation::new([
             -FieldElement::<Self::BaseField>::one(),
             FieldElement::<Self::BaseField>::from_raw([2, 0, 0, 0]),
-            FieldElement::one()
+            FieldElement::one(),
         ])
     }
 }
@@ -27,5 +27,38 @@ impl IsShortWeierstrass for BLS12381Curve {
 
     fn b() -> FieldElement<Self::BaseField> {
         FieldElement::from(5)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        cyclic_group::IsGroup, elliptic_curve::traits::EllipticCurveError,
+        field::element::FieldElement,
+    };
+
+    use super::PallasCurve;
+
+    #[allow(clippy::upper_case_acronyms)]
+    type FEE = FieldElement<BLS12381PrimeField>;
+
+    fn point_1() -> ShortWeierstrassProjectivePoint<PallasCurve> {
+        let x = FEE::new_base("36bb494facde72d0da5c770c4b16d9b2d45cfdc27604a25a1a80b020798e5b0dbd4c6d939a8f8820f042a29ce552ee5");
+        let y = FEE::new_base("7acf6e49cc000ff53b06ee1d27056734019c0a1edfa16684da41ebb0c56750f73bc1b0eae4c6c241808a5e485af0ba0");
+        BLS12381Curve::create_point_from_affine(x, y).unwrap()
+    }
+
+    fn point_1_times_5() -> ShortWeierstrassProjectivePoint<PallasCurve> {
+        let x = FEE::new_base("32bcce7e71eb50384918e0c9809f73bde357027c6bf15092dd849aa0eac274d43af4c68a65fb2cda381734af5eecd5c");
+        let y = FEE::new_base("11e48467b19458aabe7c8a42dc4b67d7390fdf1e150534caadddc7e6f729d8890b68a5ea6885a21b555186452b954d88");
+        BLS12381Curve::create_point_from_affine(x, y).unwrap()
+    }
+
+    #[test]
+    fn adding_five_times_point_1_works() {
+        let point_1 = point_1();
+        let point_1_times_5 = point_1_times_5();
+        assert_eq!(point_1.operate_with_self(5_u16), point_1_times_5);
     }
 }
