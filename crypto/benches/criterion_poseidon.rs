9c84e6a571ebd1af471cd2dba4f69bfeedf8c2b1
Theprotocolwhisperer <chrismata10@gmail.com>
2023-12-06 13:13:30 -0500

    (fix): Updating imports

--- crypto/benches/criterion_poseidon.rs
+++ crypto/benches/criterion_poseidon.rs
@@ -1,8 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use lambdaworks_crypto::hash::poseidon::starknet::{
-    parameters::{DefaultPoseidonParams, PermutationParameters},
-    Poseidon,
-};
+use lambdaworks_crypto::hash::poseidon::starknet::PoseidonCairoStark252;
+use lambdaworks_crypto::hash::poseidon::Poseidon;
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::Stark252PrimeField;
 use std::time::Duration;
@@ -17,10 +15,8 @@ fn poseidon_benchmarks(c: &mut Criterion) {
         // `(1 << 20) + 1` exploits worst cases in terms of rounding up to powers of 2.
         .take((1 << 20) + 1)
         .collect();
-    let params = PermutationParameters::new_with(DefaultPoseidonParams::CairoStark252);
-    let poseidon = Poseidon::new_with_params(params);
     group.bench_with_input("build", points_x.as_slice(), |bench, points_x| {
-        bench.iter_with_large_drop(|| Poseidon::hash_many(&poseidon, points_x));
+        bench.iter_with_large_drop(|| PoseidonCairoStark252::hash_many(points_x));
     });
 }
 criterion_group!(poseidon, poseidon_benchmarks);
