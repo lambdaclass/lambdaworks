c33f548899340268b2ac22dbb84e3287e2611056
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-12-18 16:18:54 -0300

    Comment code to run only fibonacci AIR tests

--- provers/cairo/src/tests/utils.rs
+++ provers/cairo/src/tests/utils.rs
@@ -1,49 +1,49 @@
-use crate::{
-    air::{generate_cairo_proof, verify_cairo_proof},
-    cairo_layout::CairoLayout,
-    runner::run::generate_prover_args,
-    runner::run::generate_prover_args_from_trace,
-};
-use stark_platinum_prover::proof::options::ProofOptions;
-use std::time::Instant;
+// use crate::{
+//     air::{generate_cairo_proof, verify_cairo_proof},
+//     cairo_layout::CairoLayout,
+//     runner::run::generate_prover_args,
+//     runner::run::generate_prover_args_from_trace,
+// };
+// use stark_platinum_prover::proof::options::ProofOptions;
+// use std::time::Instant;
 
-pub fn cairo0_program_path(program_name: &str) -> String {
-    const CARGO_DIR: &str = env!("CARGO_MANIFEST_DIR");
-    const CAIRO0_BASE_REL_PATH: &str = "/cairo_programs/cairo0/";
-    let program_base_path = CARGO_DIR.to_string() + CAIRO0_BASE_REL_PATH;
-    program_base_path + program_name
-}
+// pub fn cairo0_program_path(program_name: &str) -> String {
+//     const CARGO_DIR: &str = env!("CARGO_MANIFEST_DIR");
+//     const CAIRO0_BASE_REL_PATH: &str = "/cairo_programs/cairo0/";
+//     let program_base_path = CARGO_DIR.to_string() + CAIRO0_BASE_REL_PATH;
+//     program_base_path + program_name
+// }
 
-pub fn cairo1_program_path(program_name: &str) -> String {
-    const CARGO_DIR: &str = env!("CARGO_MANIFEST_DIR");
-    const CAIRO1_BASE_REL_PATH: &str = "/cairo_programs/cairo1/";
-    let program_base_path = CARGO_DIR.to_string() + CAIRO1_BASE_REL_PATH;
-    program_base_path + program_name
-}
+// pub fn cairo1_program_path(program_name: &str) -> String {
+//     const CARGO_DIR: &str = env!("CARGO_MANIFEST_DIR");
+//     const CAIRO1_BASE_REL_PATH: &str = "/cairo_programs/cairo1/";
+//     let program_base_path = CARGO_DIR.to_string() + CAIRO1_BASE_REL_PATH;
+//     program_base_path + program_name
+// }
 
-/// Loads the program in path, runs it with the Cairo VM, and makes a proof of it
-pub fn test_prove_cairo_program(file_path: &str, layout: CairoLayout) {
-    let proof_options = ProofOptions::default_test_options();
-    let timer = Instant::now();
-    println!("Making proof ...");
+// /// Loads the program in path, runs it with the Cairo VM, and makes a proof of it
+// pub fn test_prove_cairo_program(file_path: &str, layout: CairoLayout) {
+//     let proof_options = ProofOptions::default_test_options();
+//     let timer = Instant::now();
+//     println!("Making proof ...");
 
-    let program_content = std::fs::read(file_path).unwrap();
-    let (main_trace, pub_inputs) = generate_prover_args(&program_content, layout).unwrap();
-    let proof = generate_cairo_proof(&main_trace, &pub_inputs, &proof_options).unwrap();
-    println!("  Time spent in proving: {:?} \n", timer.elapsed());
+//     let program_content = std::fs::read(file_path).unwrap();
+//     let (main_trace, pub_inputs) = generate_prover_args(&program_content, layout).unwrap();
+//     let proof = generate_cairo_proof(&main_trace, &pub_inputs, &proof_options).unwrap();
+//     println!("  Time spent in proving: {:?} \n", timer.elapsed());
 
-    assert!(verify_cairo_proof(&proof, &pub_inputs, &proof_options));
-}
+//     assert!(verify_cairo_proof(&proof, &pub_inputs, &proof_options));
+// }
 
-pub fn test_prove_cairo_program_from_trace(trace_bin_path: &str, memory_bin_path: &str) {
-    let proof_options = ProofOptions::default_test_options();
-    let (main_trace, pub_inputs) =
-        generate_prover_args_from_trace(trace_bin_path, memory_bin_path).unwrap();
+// pub fn test_prove_cairo_program_from_trace(trace_bin_path: &str, memory_bin_path: &str) {
+//     let proof_options = ProofOptions::default_test_options();
+//     let (main_trace, pub_inputs) =
+//         generate_prover_args_from_trace(trace_bin_path, memory_bin_path).unwrap();
 
-    // println
-    let timer = Instant::now();
-    println!("Making proof ...");
-    let proof = generate_cairo_proof(&main_trace, &pub_inputs, &proof_options).unwrap();
-    println!("  Time spent in proving: {:?} \n", timer.elapsed());
-    assert!(verify_cairo_proof(&proof, &pub_inputs, &proof_options));
-}
+//     // println
+//     let timer = Instant::now();
+//     println!("Making proof ...");
+//     let proof = generate_cairo_proof(&main_trace, &pub_inputs, &proof_options).unwrap();
+//     println!("  Time spent in proving: {:?} \n", timer.elapsed());
+//     assert!(verify_cairo_proof(&proof, &pub_inputs, &proof_options));
+// }
--- provers/stark/src/tests/integration_tests.rs
+++ provers/stark/src/tests/integration_tests.rs
@@ -4,13 +4,13 @@ use lambdaworks_math::field::{
 
 use crate::{
     examples::{
-        dummy_air::{self, DummyAIR},
-        fibonacci_2_cols_shifted::{self, Fibonacci2ColsShifted},
-        fibonacci_2_columns::{self, Fibonacci2ColsAIR},
-        fibonacci_rap::{fibonacci_rap_trace, FibonacciRAP, FibonacciRAPPublicInputs},
-        quadratic_air::{self, QuadraticAIR, QuadraticPublicInputs},
+        // dummy_air::{self, DummyAIR},
+        // fibonacci_2_cols_shifted::{self, Fibonacci2ColsShifted},
+        // fibonacci_2_columns::{self, Fibonacci2ColsAIR},
+        // fibonacci_rap::{fibonacci_rap_trace, FibonacciRAP, FibonacciRAPPublicInputs},
+        // quadratic_air::{self, QuadraticAIR, QuadraticPublicInputs},
         simple_fibonacci::{self, FibonacciAIR, FibonacciPublicInputs},
-        simple_periodic_cols::{self, SimplePeriodicAIR, SimplePeriodicPublicInputs},
+        //     simple_periodic_cols::{self, SimplePeriodicAIR, SimplePeriodicPublicInputs},
     },
     proof::options::ProofOptions,
     prover::{IsStarkProver, Prover},
@@ -77,179 +77,179 @@ fn test_prove_fib17() {
     ));
 }
 
-#[test_log::test]
-fn test_prove_simple_periodic_8() {
-    let trace = simple_periodic_cols::simple_periodic_trace::<Stark252PrimeField>(8);
+// #[test_log::test]
+// fn test_prove_simple_periodic_8() {
+//     let trace = simple_periodic_cols::simple_periodic_trace::<Stark252PrimeField>(8);
 
-    let proof_options = ProofOptions::default_test_options();
+//     let proof_options = ProofOptions::default_test_options();
 
-    let pub_inputs = SimplePeriodicPublicInputs {
-        a0: Felt252::one(),
-        a1: Felt252::from(8),
-    };
+//     let pub_inputs = SimplePeriodicPublicInputs {
+//         a0: Felt252::one(),
+//         a1: Felt252::from(8),
+//     };
 
-    let proof = Prover::prove::<SimplePeriodicAIR<Stark252PrimeField>>(
-        &trace,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[]),
-    )
-    .unwrap();
-    assert!(Verifier::verify::<SimplePeriodicAIR<Stark252PrimeField>>(
-        &proof,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[]),
-    ));
-}
+//     let proof = Prover::prove::<SimplePeriodicAIR<Stark252PrimeField>>(
+//         &trace,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[]),
+//     )
+//     .unwrap();
+//     assert!(Verifier::verify::<SimplePeriodicAIR<Stark252PrimeField>>(
+//         &proof,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[]),
+//     ));
+// }
 
-#[test_log::test]
-fn test_prove_simple_periodic_32() {
-    let trace = simple_periodic_cols::simple_periodic_trace::<Stark252PrimeField>(32);
+// #[test_log::test]
+// fn test_prove_simple_periodic_32() {
+//     let trace = simple_periodic_cols::simple_periodic_trace::<Stark252PrimeField>(32);
 
-    let proof_options = ProofOptions::default_test_options();
+//     let proof_options = ProofOptions::default_test_options();
 
-    let pub_inputs = SimplePeriodicPublicInputs {
-        a0: Felt252::one(),
-        a1: Felt252::from(32768),
-    };
+//     let pub_inputs = SimplePeriodicPublicInputs {
+//         a0: Felt252::one(),
+//         a1: Felt252::from(32768),
+//     };
 
-    let proof = Prover::prove::<SimplePeriodicAIR<Stark252PrimeField>>(
-        &trace,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[]),
-    )
-    .unwrap();
-    assert!(Verifier::verify::<SimplePeriodicAIR<Stark252PrimeField>>(
-        &proof,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[]),
-    ));
-}
+//     let proof = Prover::prove::<SimplePeriodicAIR<Stark252PrimeField>>(
+//         &trace,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[]),
+//     )
+//     .unwrap();
+//     assert!(Verifier::verify::<SimplePeriodicAIR<Stark252PrimeField>>(
+//         &proof,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[]),
+//     ));
+// }
 
-#[test_log::test]
-fn test_prove_fib_2_cols() {
-    let trace = fibonacci_2_columns::compute_trace([Felt252::from(1), Felt252::from(1)], 16);
+// #[test_log::test]
+// fn test_prove_fib_2_cols() {
+//     let trace = fibonacci_2_columns::compute_trace([Felt252::from(1), Felt252::from(1)], 16);
 
-    let proof_options = ProofOptions::default_test_options();
+//     let proof_options = ProofOptions::default_test_options();
 
-    let pub_inputs = FibonacciPublicInputs {
-        a0: Felt252::one(),
-        a1: Felt252::one(),
-    };
+//     let pub_inputs = FibonacciPublicInputs {
+//         a0: Felt252::one(),
+//         a1: Felt252::one(),
+//     };
 
-    let proof = Prover::prove::<Fibonacci2ColsAIR<Stark252PrimeField>>(
-        &trace,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[]),
-    )
-    .unwrap();
-    assert!(Verifier::verify::<Fibonacci2ColsAIR<Stark252PrimeField>>(
-        &proof,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[])
-    ));
-}
+//     let proof = Prover::prove::<Fibonacci2ColsAIR<Stark252PrimeField>>(
+//         &trace,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[]),
+//     )
+//     .unwrap();
+//     assert!(Verifier::verify::<Fibonacci2ColsAIR<Stark252PrimeField>>(
+//         &proof,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[])
+//     ));
+// }
 
-#[test_log::test]
-fn test_prove_fib_2_cols_shifted() {
-    let trace = fibonacci_2_cols_shifted::compute_trace(FieldElement::one(), 16);
+// #[test_log::test]
+// fn test_prove_fib_2_cols_shifted() {
+//     let trace = fibonacci_2_cols_shifted::compute_trace(FieldElement::one(), 16);
 
-    let claimed_index = 14;
-    let claimed_value = trace.get_row(claimed_index)[0];
-    let proof_options = ProofOptions::default_test_options();
+//     let claimed_index = 14;
+//     let claimed_value = trace.get_row(claimed_index)[0];
+//     let proof_options = ProofOptions::default_test_options();
 
-    let pub_inputs = fibonacci_2_cols_shifted::PublicInputs {
-        claimed_value,
-        claimed_index,
-    };
+//     let pub_inputs = fibonacci_2_cols_shifted::PublicInputs {
+//         claimed_value,
+//         claimed_index,
+//     };
 
-    let proof = Prover::prove::<Fibonacci2ColsShifted<_>>(
-        &trace,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[]),
-    )
-    .unwrap();
-    assert!(Verifier::verify::<Fibonacci2ColsShifted<_>>(
-        &proof,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[])
-    ));
-}
+//     let proof = Prover::prove::<Fibonacci2ColsShifted<_>>(
+//         &trace,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[]),
+//     )
+//     .unwrap();
+//     assert!(Verifier::verify::<Fibonacci2ColsShifted<_>>(
+//         &proof,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[])
+//     ));
+// }
 
-#[test_log::test]
-fn test_prove_quadratic() {
-    let trace = quadratic_air::quadratic_trace(Felt252::from(3), 4);
+// #[test_log::test]
+// fn test_prove_quadratic() {
+//     let trace = quadratic_air::quadratic_trace(Felt252::from(3), 4);
 
-    let proof_options = ProofOptions::default_test_options();
+//     let proof_options = ProofOptions::default_test_options();
 
-    let pub_inputs = QuadraticPublicInputs {
-        a0: Felt252::from(3),
-    };
+//     let pub_inputs = QuadraticPublicInputs {
+//         a0: Felt252::from(3),
+//     };
 
-    let proof = Prover::prove::<QuadraticAIR<Stark252PrimeField>>(
-        &trace,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[]),
-    )
-    .unwrap();
-    assert!(Verifier::verify::<QuadraticAIR<Stark252PrimeField>>(
-        &proof,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[])
-    ));
-}
+//     let proof = Prover::prove::<QuadraticAIR<Stark252PrimeField>>(
+//         &trace,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[]),
+//     )
+//     .unwrap();
+//     assert!(Verifier::verify::<QuadraticAIR<Stark252PrimeField>>(
+//         &proof,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[])
+//     ));
+// }
 
-#[test_log::test]
-fn test_prove_rap_fib() {
-    let steps = 16;
-    let trace = fibonacci_rap_trace([Felt252::from(1), Felt252::from(1)], steps);
+// #[test_log::test]
+// fn test_prove_rap_fib() {
+//     let steps = 16;
+//     let trace = fibonacci_rap_trace([Felt252::from(1), Felt252::from(1)], steps);
 
-    let proof_options = ProofOptions::default_test_options();
+//     let proof_options = ProofOptions::default_test_options();
 
-    let pub_inputs = FibonacciRAPPublicInputs {
-        steps,
-        a0: Felt252::one(),
-        a1: Felt252::one(),
-    };
+//     let pub_inputs = FibonacciRAPPublicInputs {
+//         steps,
+//         a0: Felt252::one(),
+//         a1: Felt252::one(),
+//     };
 
-    let proof = Prover::prove::<FibonacciRAP<Stark252PrimeField>>(
-        &trace,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[]),
-    )
-    .unwrap();
-    assert!(Verifier::verify::<FibonacciRAP<Stark252PrimeField>>(
-        &proof,
-        &pub_inputs,
-        &proof_options,
-        StoneProverTranscript::new(&[])
-    ));
-}
+//     let proof = Prover::prove::<FibonacciRAP<Stark252PrimeField>>(
+//         &trace,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[]),
+//     )
+//     .unwrap();
+//     assert!(Verifier::verify::<FibonacciRAP<Stark252PrimeField>>(
+//         &proof,
+//         &pub_inputs,
+//         &proof_options,
+//         StoneProverTranscript::new(&[])
+//     ));
+// }
 
-#[test_log::test]
-fn test_prove_dummy() {
-    let trace_length = 16;
-    let trace = dummy_air::dummy_trace(trace_length);
+// #[test_log::test]
+// fn test_prove_dummy() {
+//     let trace_length = 16;
+//     let trace = dummy_air::dummy_trace(trace_length);
 
-    let proof_options = ProofOptions::default_test_options();
+//     let proof_options = ProofOptions::default_test_options();
 
-    let proof =
-        Prover::prove::<DummyAIR>(&trace, &(), &proof_options, StoneProverTranscript::new(&[]))
-            .unwrap();
-    assert!(Verifier::verify::<DummyAIR>(
-        &proof,
-        &(),
-        &proof_options,
-        StoneProverTranscript::new(&[])
-    ));
-}
+//     let proof =
+//         Prover::prove::<DummyAIR>(&trace, &(), &proof_options, StoneProverTranscript::new(&[]))
+//             .unwrap();
+//     assert!(Verifier::verify::<DummyAIR>(
+//         &proof,
+//         &(),
+//         &proof_options,
+//         StoneProverTranscript::new(&[])
+//     ));
+// }
