11bd682d8583b852875510befd74c6ffa24cddc0
Strohy <anujkumarsharma1808@gmail.com>
2024-01-06 16:40:12 +0530

    feat: algorithm to avoid branching

--- math/src/cyclic_group.rs
+++ math/src/cyclic_group.rs
@@ -1,4 +1,5 @@
 use crate::unsigned_integer::traits::IsUnsignedInteger;
+use crate::subtle::ConditionallySelectable;
 
 pub trait IsGroup: Clone + PartialEq + Eq {
     /// Returns the neutral element of the group. The equality
@@ -30,24 +31,21 @@ pub trait IsGroup: Clone + PartialEq + Eq {
     }
     #[cfg(feature = "constant-time")]
     fn operate_with_self<T: IsUnsignedInteger>(&self, exponent: T) -> Self {
-        let mut result = Self::neutral_element();
-        let mut base = self.clone();
+        let mut r0 = Self::neutral_element();
+        let mut r1 = self.clone();
 
         let num_bits = std::mem::size_of::<T>() * 8;
 
         for i in (0..num_bits).rev() {
             let mask = T::from(1) << i;
-            let bit = (exponent & mask) >> i;
+            let bit = ((exponent & mask) >> i) == T::from(1);
 
-            if bit == T::from(1) {
-                result = Self::operate_with(&result, &base);
-                base = Self::operate_with(&base, &base);
-            } else {
-                base = Self::operate_with(&base, &result);
-                result = Self::operate_with(&result, &result);
-            }
+            // conditional_swap(r0, r1, bit);
+            r1 = Self::operate_with(r0, r1);            // r1 = r0 + r1;
+            r0 = Self::operate_with(r0, r0);            // r0 = 2r0;
+            // cconditional_swap(r0, r1, bit);
         }
-        result
+        r0
     }
 
     /// Applies the group operation between `self` and `other`.
