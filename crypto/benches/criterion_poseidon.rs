a488de1c9873b3252243f69fa143cd4194222fac
Theprotocolwhisperer <chrismata10@gmail.com>
2024-01-08 13:15:25 -0300

    (rebase): Fixing fmt error

--- crypto/benches/criterion_poseidon.rs
+++ crypto/benches/criterion_poseidon.rs
@@ -9,11 +9,10 @@ fn poseidon_benchmarks(c: &mut Criterion) {
     let y = FieldElement::<Stark252PrimeField>::from_hex("0x789101").unwrap();
     let mut group = c.benchmark_group("Poseidon Benchmark");
 
-    // Benchmark with black_box is 0.41% faster 
+    // Benchmark with black_box is 0.41% faster
     group.bench_function("Hashing with black_box", |bench| {
         bench.iter(|| black_box(PoseidonCairoStark252::hash(&x, &y)))
     });
-
 }
 criterion_group!(poseidon, poseidon_benchmarks);
 criterion_main!(poseidon);
