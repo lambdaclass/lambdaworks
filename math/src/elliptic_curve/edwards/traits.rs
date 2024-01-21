9cfb4d6ca92e52e201acec49f63aa149538f2940
Rahul Ghangas <rahulghangas0@gmail.com>
2024-01-21 22:44:31 +0530

    chore: define IsEdwards trait using macro

--- math/src/elliptic_curve/edwards/traits.rs
+++ math/src/elliptic_curve/edwards/traits.rs
@@ -1,18 +1,28 @@
 use crate::elliptic_curve::traits::IsEllipticCurve;
 use crate::field::element::FieldElement;
 use core::fmt::Debug;
-/// Trait to add elliptic curves behaviour to a struct.
-pub trait IsEdwards: IsEllipticCurve + Clone + Debug {
-    fn a() -> FieldElement<Self::BaseField>;
+macro_rules! define_edwards_trait {
+    ($($bound:ident),*) => {
+        /// Trait to add elliptic curves behaviour to a struct.
+        pub trait IsEdwards: $($bound +)* {
+            fn a() -> FieldElement<Self::BaseField>;
 
-    fn d() -> FieldElement<Self::BaseField>;
+            fn d() -> FieldElement<Self::BaseField>;
 
-    fn defining_equation(
-        x: &FieldElement<Self::BaseField>,
-        y: &FieldElement<Self::BaseField>,
-    ) -> FieldElement<Self::BaseField> {
-        (Self::a() * x.pow(2_u16) + y.pow(2_u16))
-            - FieldElement::<Self::BaseField>::one()
-            - Self::d() * x.pow(2_u16) * y.pow(2_u16)
-    }
+            fn defining_equation(
+                x: &FieldElement<Self::BaseField>,
+                y: &FieldElement<Self::BaseField>,
+            ) -> FieldElement<Self::BaseField> {
+                (Self::a() * x.pow(2_u16) + y.pow(2_u16))
+                    - FieldElement::<Self::BaseField>::one()
+                    - Self::d() * x.pow(2_u16) * y.pow(2_u16)
+            }
+        }
+    };
 }
+
+#[cfg(not(feature = "constant-time"))]
+define_edwards_trait!(IsEllipticCurve, Clone, Debug);
+
+#[cfg(feature = "constant-time")]
+define_edwards_trait!(IsEllipticCurve, Clone, Debug, Copy);
