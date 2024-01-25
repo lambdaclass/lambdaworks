da8945d80aff93e2ec3e52699f51836affa8b9fc
Irfan Bozkurt <irfanbozkurt123@gmail.com>
2024-01-26 02:48:52 +0300

    add to examples folder + add one simple example

--- provers/groth16/circom-adapter/src/integration_tests.rs
+++ provers/groth16/circom-adapter/src/integration_tests.rs
@@ -3,16 +3,36 @@ use std::fs;
 use crate::*;
 use lambdaworks_groth16::*;
 
-const TEST_DIR: &str = "src/test_files/";
+const TEST_DIR: &str = "test_files";
 
 // Proves & verifies a Poseidon circuit with 1 input and 2 outputs. The input is decimal 100.
 #[test]
 fn poseidon_parse_prove_verify() {
-    let test_dir = format!("{TEST_DIR}poseidon/");
+    let test_dir = format!("{TEST_DIR}/poseidon");
 
     let (qap, w) = circom_to_lambda(
-        &fs::read_to_string(format!("{test_dir}test.r1cs.json")).expect("Error reading the file"),
-        &fs::read_to_string(format!("{test_dir}witness.json")).expect("Error reading the file"),
+        &fs::read_to_string(format!("{test_dir}/test.r1cs.json")).expect("Error reading the file"),
+        &fs::read_to_string(format!("{test_dir}/witness.json")).expect("Error reading the file"),
+    );
+
+    let (pk, vk) = setup(&qap);
+
+    let accept = verify(
+        &vk,
+        &Prover::prove(&w, &qap, &pk),
+        &w[..qap.num_of_public_inputs],
+    );
+    assert!(accept);
+}
+
+// Proves & verifies Vitalik's famous "x^3 + x + 5 = 35" example
+#[test]
+fn vitalik_parse_prove_verify() {
+    let test_dir = format!("{TEST_DIR}/vitalik_example");
+
+    let (qap, w) = circom_to_lambda(
+        &fs::read_to_string(format!("{test_dir}/test.r1cs.json")).expect("Error reading the file"),
+        &fs::read_to_string(format!("{test_dir}/witness.json")).expect("Error reading the file"),
     );
 
     let (pk, vk) = setup(&qap);
