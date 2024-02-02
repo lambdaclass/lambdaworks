04a63ed4082f777b02d524b59a9f404e33fa1194
PatStiles <pdstiles78@gmail.com>
2024-02-02 12:18:44 -0600

    clippy

--- math/benches/elliptic_curves/iai_bls12_377.rs
+++ math/benches/elliptic_curves/iai_bls12_377.rs
@@ -12,6 +12,7 @@ use rand::{rngs::StdRng, Rng, SeedableRng};
 #[allow(dead_code)]
 type G1 = ShortWeierstrassProjectivePoint<BLS12377Curve>;
 
+#[allow(dead_code)]
 pub fn rand_points_g1() -> (G1, G1, u128, u128) {
     let mut rng = StdRng::seed_from_u64(42);
     let a_val = rng.gen();
@@ -21,21 +22,25 @@ pub fn rand_points_g1() -> (G1, G1, u128, u128) {
     (a, b, a_val, b_val)
 }
 
+#[allow(dead_code)]
 pub fn bls12_377_operate_with_g1() {
     let (a, b, _, _) = rand_points_g1();
     let _ = black_box(black_box(&a).operate_with(black_box(&b)));
 }
 
+#[allow(dead_code)]
 pub fn bls12_377_operate_with_self_g1() {
     let (a, _, _, b_val) = rand_points_g1();
     let _ = black_box(black_box(&a).operate_with_self(black_box(b_val)));
 }
 
+#[allow(dead_code)]
 pub fn bls12_377_double_g1() {
     let (a, _, _, _) = rand_points_g1();
     let _ = black_box(black_box(&a).operate_with_self(black_box(2u64)));
 }
 
+#[allow(dead_code)]
 pub fn bls12_377_neg_g1() {
     let (a, _, _, _) = rand_points_g1();
     let _ = black_box(black_box(&a).neg());
