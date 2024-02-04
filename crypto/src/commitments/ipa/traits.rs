920360a94cb77765da3021863ab565eef35abf76
cliraa <113949669+cliraa@users.noreply.github.com>
2024-02-04 16:47:57 -0400

    Update traits.rs

--- crypto/src/commitments/ipa/traits.rs
+++ crypto/src/commitments/ipa/traits.rs
@@ -1,37 +1,43 @@
-use lambdaworks_math::polynomial::Polynomial;
-use lambdaworks_math::elliptic_curve::short_weierstrass::point::ShortWeierstrassProjectivePoint;
-use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::curve::BLS12381Curve;
-use crate::commitments::ipa::ipa::FrElement;
 use crate::commitments::ipa::ipa::Proof;
+use lambdaworks_math::elliptic_curve::short_weierstrass::point::ShortWeierstrassProjectivePoint;
+use lambdaworks_math::elliptic_curve::short_weierstrass::traits::IsShortWeierstrass;
+use lambdaworks_math::elliptic_curve::traits::IsEllipticCurve;
+use lambdaworks_math::field::element::FieldElement;
+use lambdaworks_math::field::traits::IsField;
+use lambdaworks_math::field::traits::IsPrimeField;
+use lambdaworks_math::polynomial::Polynomial;
 
-pub trait IsCommitmentSchemeIPA {
-
+pub trait IsCommitmentSchemeIPA<F: IsField + IsPrimeField, E: IsEllipticCurve + IsShortWeierstrass>
+{
     type CommitmentIPA;
     type OpenIPA;
     type VerifyOpen;
 
-    fn commit<E: lambdaworks_math::elliptic_curve::traits::IsEllipticCurve>(
+    fn commit(
         &self,
-        p: &Polynomial<FrElement>, r: FrElement
+        p: &Polynomial<FieldElement<F>>,
+        zr: FieldElement<F>,
+        neutral_element: ShortWeierstrassProjectivePoint<E>,
     ) -> Self::CommitmentIPA;
 
     fn open(
         &mut self,
-        a: &[FrElement],
-        b: &[FrElement],
-        u: &[FrElement],
-        _u: &ShortWeierstrassProjectivePoint<BLS12381Curve>,
+        a: &[FieldElement<F>],
+        b: &[FieldElement<F>],
+        u: &[FieldElement<F>],
+        _u: &ShortWeierstrassProjectivePoint<E>,
+        neutral_element: ShortWeierstrassProjectivePoint<E>,
     ) -> Self::OpenIPA;
 
     fn verify_open(
         &self,
-        x: &FrElement,
-        v: &FrElement,
-        _p: &ShortWeierstrassProjectivePoint<BLS12381Curve>,
-        p: &Proof<BLS12381Curve>,
-        r: &FrElement,
-        u: &[FrElement],
-        _u: &ShortWeierstrassProjectivePoint<BLS12381Curve>,
+        x: &FieldElement<F>,
+        v: &FieldElement<F>,
+        _p: &ShortWeierstrassProjectivePoint<E>,
+        p: &Proof<F, E>,
+        r: &FieldElement<F>,
+        u: &[FieldElement<F>],
+        _u: &ShortWeierstrassProjectivePoint<E>,
+        neutral_element: ShortWeierstrassProjectivePoint<E>,
     ) -> Self::VerifyOpen;
-
 }
