7d606252c9ff0774e755f4bb18a72a90f1ef95c1
Ubuntu <ubuntu@ip-172-31-16-208.ec2.internal>
2024-02-21 18:41:20 +0000

    rm unneeded method

--- math/src/gpu/icicle/bls12_377.rs
+++ math/src/gpu/icicle/bls12_377.rs
@@ -27,10 +27,6 @@ impl BLS12377FieldElement {
     fn from_icicle(icicle: &curve::BaseField) -> Result<Self, ByteConversionError> {
         Self::from_bytes_le(&icicle.to_bytes_le())
     }
-
-    fn from_icicle_scalar(icicle: &curve::ScalarField) -> Result<Self, ByteConversionError> {
-        Self::from_bytes_le(&icicle.to_bytes_le())
-    }
 }
 
 impl ShortWeierstrassProjectivePoint<BLS12377Curve> {
