32540e2d6beba507659bd2606ac15fad469b75b5
PatStiles <pdstiles78@gmail.com>
2024-02-22 21:30:58 -0600

    add additional curve tests

--- math/src/elliptic_curve/montgomery/curves/curve25519.rs
+++ math/src/elliptic_curve/montgomery/curves/curve25519.rs
@@ -59,20 +59,19 @@ mod tests {
     /*
        sage script
        p = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed
-       K = GF(p)
-       A = K(0x76d06)
-       B = K(0x01)
-       E = EllipticCurve(K, ((3 - A^2)/(3 * B^2), (2 * A^3 - 9 * A)/(27 * B^3)))
-       def to_weierstrass(A, B, x, y):
-           return (x/B + A/(3*B), y/B)
-       def to_montgomery(A, B, u, v):
-           return (B * (u - A/(3*B)), B*v)
-       G = E(*to_weierstrass(A, B, K(0x09), K(0x20ae19a1b8a086b4e01edd2c7748d14c923d4d7e6d7c61b229e9c5a27eced3d9)))
+       Fp = GF(p)
+       a = K(0x76d06)
+       b = K(0x01)
+       E = EllipticCurve(Fp, ((3 - a^2)/(3 * b^2), (2 * a^3 - 9 * a)/(27 * b^3)))
+       def to_weierstrass(a, b, x, y):
+           return (x/b + a/(3*b), y/b)
+       def to_montgomery(a, b, u, v):
+           return (b * (u - a/(3*b)), b*v)
        E.set_order(0x1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed * 0x08)
 
        P = E.random_point()
        P = (55982355253182784578671123550301779825433794365936117526811944583306035556801 : 7252143264526750761856340913670505401463091321304403727596372170490063892550 : 1)
-       to_montgomery(A, B, K(55982355253182784578671123550301779825433794365936117526811944583306035556801), K(7252143264526750761856340913670505401463091321304403727596372170490063892550)) = (36683673713630085341409292715520461849888796921662690186902347248653847121264,
+       to_montgomery(a, b, K(55982355253182784578671123550301779825433794365936117526811944583306035556801), K(7252143264526750761856340913670505401463091321304403727596372170490063892550)) = (36683673713630085341409292715520461849888796921662690186902347248653847121264,
     7252143264526750761856340913670505401463091321304403727596372170490063892550)
        hex(36683673713630085341409292715520461849888796921662690186902347248653847121264) = 0x511a3939af1f568fadea95dd734fe90412af04a637afc3b11813d66741e24d70
        hex(7252143264526750761856340913670505401463091321304403727596372170490063892550) = 0x100891500644f6850e25d95925ab209aaf7e5e1fa5d8036f07725063e0543046
@@ -86,20 +85,20 @@ mod tests {
     /*
        sage script
        p = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed
-       K = GF(p)
-       A = K(0x76d06)
-       B = K(0x01)
-       E = EllipticCurve(K, ((3 - A^2)/(3 * B^2), (2 * A^3 - 9 * A)/(27 * B^3)))
-       def to_weierstrass(A, B, x, y):
-           return (x/B + A/(3*B), y/B)
-       def to_montgomery(A, B, u, v):
-           return (B * (u - A/(3*B)), B*v)
-       G = E(*to_weierstrass(A, B, K(0x09), K(0x20ae19a1b8a086b4e01edd2c7748d14c923d4d7e6d7c61b229e9c5a27eced3d9)))
+       Fp = GF(p)
+       a = K(0x76d06)
+       b = K(0x01)
+       E = EllipticCurve(Fp, ((3 - a^2)/(3 * b^2), (2 * a^3 - 9 * a)/(27 * b^3)))
+       def to_weierstrass(a, b, x, y):
+           return (x/b + a/(3*b), y/b)
+       def to_montgomery(a, b, u, v):
+           return (b * (u - a/(3*b)), b*v)
+       G = E(*to_weierstrass(a, b, K(0x09), K(0x20ae19a1b8a086b4e01edd2c7748d14c923d4d7e6d7c61b229e9c5a27eced3d9)))
        E.set_order(0x1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed * 0x08)
 
        P = E.random_point()
        P * 5 = (43673264053381013043093028227158132460078739296636781789931124335052711458784 : 32008223164755653184586471723570822084978824108746555469179824380913641587957 : 1)
-       to_montgomery(A, B, K(43673264053381013043093028227158132460078739296636781789931124335052711458784), K(32008223164755653184586471723570822084978824108746555469179824380913641587957)) = (24374582513828313805831197392376814484533741852363354450021527000400523023247,
+       to_montgomery(a, b, K(43673264053381013043093028227158132460078739296636781789931124335052711458784), K(32008223164755653184586471723570822084978824108746555469179824380913641587957)) = (24374582513828313805831197392376814484533741852363354450021527000400523023247,
     32008223164755653184586471723570822084978824108746555469179824380913641587957)
        hex(24374582513828313805831197392376814484533741852363354450021527000400523023247) = 0x35e38659ceab3f043e0f37a5872c1d8d484e6eca6014589b453bf375201dd38f
        hex(32008223164755653184586471723570822084978824108746555469179824380913641587957) = 0x46c403265a55dd5e2baffc0c0b64e18ab378d312d01768eed674a9fcbe4404f5
@@ -130,6 +129,34 @@ mod tests {
         );
         assert_eq!(*p.z(), FE::new_base("1"));
     }
+
+    #[test]
+    fn addition_with_neutral_element_returns_same_element() {
+        let p = point();
+        assert_eq!(
+            *p.x(),
+            FE::new_base("0x511a3939af1f568fadea95dd734fe90412af04a637afc3b11813d66741e24d70")
+        );
+        assert_eq!(
+            *p.y(),
+            FE::new_base("0x100891500644f6850e25d95925ab209aaf7e5e1fa5d8036f07725063e0543046")
+        );
+
+        let neutral_element = ShortWeierstrassProjectivePoint::<Curve25519>::neutral_element();
+
+        assert_eq!(p.operate_with(&neutral_element), p);
+    }
+
+    #[test]
+    fn neutral_element_plus_neutral_element_is_neutral_element() {
+        let neutral_element = ShortWeierstrassProjectivePoint::<BN254Curve>::neutral_element();
+
+        assert_eq!(
+            neutral_element.operate_with(&neutral_element),
+            neutral_element
+        );
+    }
+
 
     #[test]
     fn create_invalid_points_returns_an_error() {
