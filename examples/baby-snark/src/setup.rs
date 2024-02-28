34a8d1a114852d894cd09c9dfe947777fb45ba88
Manuel Iñaki Bilbao <bilbaomanuel98@gmail.com>
2024-02-28 12:20:41 -0300

    Babysnark Setup (#819)
    * Create setup function
    
    Setup function has g1, g2, beta, gamma and tau generated.
    It also start the verifying key struct construction (WIP)
    
    * Complete VetifyingKey
    
    * Fix length of operations in VerifyingKey
    
    * Add ProvingKey
    
    * Modify last 2 items of VerifyingKey
    
    * Add return values
    
    * Format babysnark/setup.rs
    
    * Fix imports
    
    Remove unused imports and add lambdaworks-crypto dependency
    
    ---------
    
    Co-authored-by: fborello-lambda <federicoborello@lambdaclass.com>

--- examples/baby-snark/src/setup.rs
+++ examples/baby-snark/src/setup.rs
@@ -1,16 +1,12 @@
 use lambdaworks_math::{
     cyclic_group::IsGroup,
-    elliptic_curve::{
-        short_weierstrass::{
-            curves::bls12_381::curve::BLS12381Curve,
-            point::ShortWeierstrassProjectivePoint,
-        },
-        traits::IsEllipticCurve,
-    },
+    elliptic_curve::traits::{IsEllipticCurve, IsPairing},
 };
 
 use crate::{
-    common::{ sample_fr_elem, Curve, FrElement, G1Point, G2Point, PairingOutput, TwistedCurve },
+    common::{
+        sample_fr_elem, Curve, FrElement, G1Point, G2Point, Pairing, PairingOutput, TwistedCurve,
+    },
     ssp::SquareSpanProgram,
 };
 
@@ -23,10 +19,10 @@ pub struct VerifyingKey {
     pub t_tau_g2: G2Point,
     // e(g1, g2)^-1
     pub inv_pairing_g1_g2: PairingOutput,
-    // β * γ * g2
-    pub beta_gamma_g2: G2Point,
-    // γ * g1
-    pub gamma_g1: G1Point,
+    // β * γ * g1
+    pub beta_gamma_g1: G1Point,
+    // γ * g2
+    pub gamma_g2: G2Point,
 }
 
 pub struct ProvingKey {
@@ -56,4 +52,81 @@ impl ToxicWaste {
     }
 }
 
+fn setup(u: SquareSpanProgram) -> (ProvingKey, VerifyingKey) {
+    let g1: G1Point = Curve::generator();
+    let g2: G2Point = TwistedCurve::generator();
 
+    let tw = ToxicWaste::new();
+
+    let u_tau = u.u_poly.iter().map(|p| p.evaluate(&tw.tau));
+
+    let vk = VerifyingKey {
+        u_tau_g1: u_tau
+            .clone()
+            .enumerate()
+            .filter_map(|(i, ui)| {
+                if i < u.num_of_public_inputs {
+                    Some(g1.operate_with_self(ui.representative()))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+        u_tau_g2: u_tau
+            .clone()
+            .enumerate()
+            .filter_map(|(i, ui)| {
+                if i < u.num_of_public_inputs {
+                    Some(g2.operate_with_self(ui.representative()))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+        t_tau_g2: g2
+            .operate_with_self((tw.tau.pow(u.num_of_gates) - FrElement::one()).representative()),
+        inv_pairing_g1_g2: Pairing::compute(&g1, &g2).unwrap().inv().unwrap(),
+        beta_gamma_g1: g1.operate_with_self((&tw.beta * &tw.gamma).representative()),
+        gamma_g2: g2.operate_with_self(tw.gamma.representative()),
+    };
+
+    let pk = ProvingKey {
+        k_powers_of_tau_g1: (0..u.num_of_gates)
+            .map(|k| g1.operate_with_self(tw.tau.pow(k).representative()))
+            .collect(),
+        u_tau_g1: u_tau
+            .clone()
+            .enumerate()
+            .filter_map(|(i, ui)| {
+                if i >= u.num_of_public_inputs && i <= u.num_of_gates {
+                    Some(g1.operate_with_self(ui.representative()))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+        u_tau_g2: u_tau
+            .clone()
+            .enumerate()
+            .filter_map(|(i, ui)| {
+                if i >= u.num_of_public_inputs && i <= u.num_of_gates {
+                    Some(g2.operate_with_self(ui.representative()))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+        beta_u_tau_g1: u_tau
+            .clone()
+            .enumerate()
+            .filter_map(|(i, ui)| {
+                if i >= u.num_of_public_inputs && i <= u.num_of_gates {
+                    Some(g1.operate_with_self((ui * (&tw.beta)).representative()))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+    };
+    (pk, vk)
+}
