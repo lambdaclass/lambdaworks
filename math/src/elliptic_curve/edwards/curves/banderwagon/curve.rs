9ed8603b5b7188d5ebe5a8976f9105264e5d6e52
rupam-04 <rpmdey2004@gmail.com>
2024-03-25 22:01:34 +0530

    feat: added sage scripts as comments in test

--- math/src/elliptic_curve/edwards/curves/banderwagon/curve.rs
+++ math/src/elliptic_curve/edwards/curves/banderwagon/curve.rs
@@ -62,6 +62,20 @@ mod tests {
 
     #[test]
     fn test_scalar_mul() {
+        /*
+        Here the coordinates in sage are in Weierstrass form, so we need to convert them to Edwards form.
+        The formula is taken from https://github.com/zhenfeizhang/bandersnatch/blob/main/bandersnatch/script/bandersnatch.sage
+        sage: p=52435875175126190479447740508185965837690552500527637822603658699938581184513
+        sage: Fp = GF(p)
+        sage: ban = EllipticCurve(Fp, [-3763200000,-78675968000000])
+        sage: G = ban(0xa76451786f95a802c0982bbd0abd68e41b92adc86c8859b4f44679b21658710,0x44d150c8b4bd14f79720d021a839e7b7eb4ee43844b30243126a72ac2375490a)
+        sage: G
+        (4732093294267640299242820317528400560681136891967543338160850811774078125840 : 31127102290931869693084292284935581507759552409643462510093198106308390504714 : 1)
+        sage: P = G*5
+        sage: P
+        (29008922875876791132439276322366220655270680966305110461148260308934935549949 : 5321989321016801167609012824343080578288974189043484058186624183847078047548 : 1)
+        */
+
         let g = BanderwagonCurve::generator();
         let result1 = g.operate_with_self(5u16);
 
@@ -84,6 +98,17 @@ mod tests {
 
     #[test]
     fn test_create_valid_point_works() {
+        /*
+        Here the coordinates in sage are in Weierstrass form, so we need to convert them to Twisted Edwards form.
+        The formula is taken from https://github.com/zhenfeizhang/bandersnatch/blob/main/bandersnatch/script/bandersnatch.sage
+        sage: p=52435875175126190479447740508185965837690552500527637822603658699938581184513
+        sage: Fp = GF(p)
+        sage: ban = EllipticCurve(Fp, [-3763200000,-78675968000000])
+        sage: G = ban(0xa76451786f95a802c0982bbd0abd68e41b92adc86c8859b4f44679b21658710,0x44d150c8b4bd14f79720d021a839e7b7eb4ee43844b30243126a72ac2375490a)
+        sage: G
+        (4732093294267640299242820317528400560681136891967543338160850811774078125840 : 31127102290931869693084292284935581507759552409643462510093198106308390504714 : 1)
+        */
+
         let p = BanderwagonCurve::generator();
 
         assert_eq!(p, p.clone());
@@ -113,6 +138,20 @@ mod tests {
 
     #[test]
     fn equality_works() {
+        /*
+        Here the coordinates in sage are in Weierstrass form, so we need to convert them to Twisted Edwards form.
+        The formula is taken from https://github.com/zhenfeizhang/bandersnatch/blob/main/bandersnatch/script/bandersnatch.sage
+        sage: p=52435875175126190479447740508185965837690552500527637822603658699938581184513
+        sage: Fp = GF(p)
+        sage: ban = EllipticCurve(Fp, [-3763200000,-78675968000000])
+        sage: G = ban(0xa76451786f95a802c0982bbd0abd68e41b92adc86c8859b4f44679b21658710,0x44d150c8b4bd14f79720d021a839e7b7eb4ee43844b30243126a72ac2375490a)
+        sage: G
+        (4732093294267640299242820317528400560681136891967543338160850811774078125840 : 31127102290931869693084292284935581507759552409643462510093198106308390504714 : 1)
+        sage: R = G*2
+        sage: R
+        (31951609130618743256244397091892446950585800848077116798961915903626008441884 : 44003096583616910216940410849480500633572695092183489174446167302668306571218 : 1)
+        */
+        
         let g = BanderwagonCurve::generator();
         let g2 = g.operate_with(&g);
         assert_ne!(&g2, &g);
@@ -121,6 +160,20 @@ mod tests {
 
     #[test]
     fn operate_with_self_works_1() {
+        /*
+        Here the coordinates in sage are in Weierstrass form, so we need to convert them to Twisted Edwards form.
+        The formula is taken from https://github.com/zhenfeizhang/bandersnatch/blob/main/bandersnatch/script/bandersnatch.sage
+        sage: p=52435875175126190479447740508185965837690552500527637822603658699938581184513
+        sage: Fp = GF(p)
+        sage: ban = EllipticCurve(Fp, [-3763200000,-78675968000000])
+        sage: G = ban(0xa76451786f95a802c0982bbd0abd68e41b92adc86c8859b4f44679b21658710,0x44d150c8b4bd14f79720d021a839e7b7eb4ee43844b30243126a72ac2375490a)
+        sage: G
+        (4732093294267640299242820317528400560681136891967543338160850811774078125840 : 31127102290931869693084292284935581507759552409643462510093198106308390504714 : 1)
+        sage: Q = G*3
+        sage: Q
+        (31019807805656217859737735480197534678630587750605614610381590357187303463025 : 7443365003011992916088981749186597178295868666409019489786056142248668879353 : 1)
+        */
+
         let g = BanderwagonCurve::generator();
         assert_eq!(
             g.operate_with(&g).operate_with(&g),
