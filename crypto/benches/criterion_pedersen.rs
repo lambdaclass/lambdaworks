4002c68409b86263fd19ba987e93500a6b8ebe69
MauroFab <maurotoscano2@gmail.com>
2024-03-26 12:34:39 -0300

    Add random in bench

--- crypto/benches/criterion_pedersen.rs
+++ crypto/benches/criterion_pedersen.rs
@@ -2,12 +2,20 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use lambdaworks_crypto::hash::pedersen::Pedersen;
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::Stark252PrimeField;
-
-use starknet_types_core::hash::Pedersen;
+use lambdaworks_math::traits::ByteConversion;
+use rand::rngs::StdRng;
+use rand::{RngCore, SeedableRng};
 
 fn pedersen_benchmarks(c: &mut Criterion) {
-    let x = FieldElement::<Stark252PrimeField>::from_hex("0x123456").unwrap();
-    let y = FieldElement::<Stark252PrimeField>::from_hex("0x789101").unwrap();
+
+    let mut rng = StdRng::from_entropy();
+    let mut felt1: [u8; 32] = Default::default();
+    rng.fill_bytes(&mut felt1);
+    let mut felt2: [u8; 32] = Default::default();
+    rng.fill_bytes(&mut felt2);
+    
+    let x = FieldElement::<Stark252PrimeField>::from_bytes_be(&felt1).unwrap();
+    let y = FieldElement::<Stark252PrimeField>::from_bytes_be(&felt2).unwrap();
     let mut group = c.benchmark_group("Pedersen Benchmark");
 
     // Benchmark with black_box is 0.41% faster
