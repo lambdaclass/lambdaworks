baed56929510eeee9fa807def831c63aaf7b5718
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-04-09 14:53:44 +0300

    Fix fields fuzzers

--- fuzz/no_gpu_fuzz/fuzz_targets/field/from_raw.rs
+++ fuzz/no_gpu_fuzz/fuzz_targets/field/from_raw.rs
@@ -1,48 +1,56 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
-use lambdaworks_math::field::{
-    element::FieldElement, 
-    fields::fft_friendly::stark_252_prime_field::Stark252PrimeField
-};
 use ibig::{modular::ModuloRing, UBig};
+use lambdaworks_math::field::{
+    element::FieldElement, fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
+};
 use lambdaworks_math::unsigned_integer::element::UnsignedInteger;
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|values: (u64, u64)| {
-
     let (value_u64_a, value_u64_b) = values;
-    let prime = 
-        UBig::from_str_radix("800000000000011000000000000000000000000000000000000000000000001", 16).unwrap();
+    let prime = UBig::from_str_radix(
+        "800000000000011000000000000000000000000000000000000000000000001",
+        16,
+    )
+    .unwrap();
     let ring = ModuloRing::new(&prime);
 
     let value_a = UnsignedInteger::from(value_u64_a);
     let value_b = UnsignedInteger::from(value_u64_b);
 
-    let a = FieldElement::<Stark252PrimeField>::from_raw(&value_a);
-    let b = FieldElement::<Stark252PrimeField>::from_raw(&value_b);
+    let a = FieldElement::<Stark252PrimeField>::from_raw(value_a);
+    let b = FieldElement::<Stark252PrimeField>::from_raw(value_b);
 
     let _a_expected = ring.from(value_u64_a);
     let _b_expected = ring.from(value_u64_b);
 
     let _add_u64 = &a + &b;
-    
+
     let _sub_u64 = &a - &b;
-    
+
     let _mul_u64 = &a * &b;
-    
+
     let _pow = &a.pow(b.representative());
-    
+
     if value_u64_b != 0 {
-        
-        let div = &a / &b; 
+        let div = &a / &b;
         assert_eq!(&div * &b, a.clone());
     }
 
     for n in [&a, &b] {
         match n.sqrt() {
             Some((fst_sqrt, snd_sqrt)) => {
-                assert_eq!(fst_sqrt.square(), snd_sqrt.square(), "Squared roots don't match each other");
-                assert_eq!(n, &fst_sqrt.square(), "Squared roots don't match original number");
+                assert_eq!(
+                    fst_sqrt.square(),
+                    snd_sqrt.square(),
+                    "Squared roots don't match each other"
+                );
+                assert_eq!(
+                    n,
+                    &fst_sqrt.square(),
+                    "Squared roots don't match original number"
+                );
             }
             None => {}
         };
@@ -62,10 +70,22 @@ fuzz_target!(|values: (u64, u64)| {
     assert_eq!(&a * &b, &b * &a, "Commutative mul property failed");
 
     let c = &a * &b;
-    assert_eq!((&a + &b) + &c, &a + (&b + &c), "Associative add property failed");
-    assert_eq!((&a * &b) * &c, &a * (&b * &c), "Associative mul property failed");
+    assert_eq!(
+        (&a + &b) + &c,
+        &a + (&b + &c),
+        "Associative add property failed"
+    );
+    assert_eq!(
+        (&a * &b) * &c,
+        &a * (&b * &c),
+        "Associative mul property failed"
+    );
 
-    assert_eq!(&a * (&b + &c), &a * &b + &a * &c, "Distributive property failed");
+    assert_eq!(
+        &a * (&b + &c),
+        &a * &b + &a * &c,
+        "Distributive property failed"
+    );
 
     assert_eq!(&a - &a, zero, "Inverse add a failed");
     assert_eq!(&b - &b, zero, "Inverse add b failed");
@@ -76,8 +96,4 @@ fuzz_target!(|values: (u64, u64)| {
     if b != zero {
         assert_eq!(&b * b.inv().unwrap(), one, "Inverse mul b failed");
     }
-    
-    
 });
-
-
--- fuzz/no_gpu_fuzz/fuzz_targets/field/mersenne31.rs
+++ fuzz/no_gpu_fuzz/fuzz_targets/field/mersenne31.rs
@@ -1,25 +1,23 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
 use lambdaworks_math::field::{
-    element::FieldElement, 
-    fields::{
-        mersenne31::field::{Mersenne31Field, MERSENNE_31_PRIME_FIELD_ORDER},
-    }
+    element::FieldElement,
+    fields::mersenne31::field::{Mersenne31Field, MERSENNE_31_PRIME_FIELD_ORDER},
 };
+use libfuzzer_sys::fuzz_target;
+use p3_field::{AbstractField, Field, PrimeField32, PrimeField64};
 use p3_mersenne_31::Mersenne31;
-use p3_field::{Field, PrimeField32, PrimeField64, AbstractField};
 
 fuzz_target!(|values: (u32, u32)| {
     // Note: we filter values outside of order as it triggers an assert within plonky3 disallowing values n >= Self::Order
     if values.0 >= MERSENNE_31_PRIME_FIELD_ORDER || values.1 >= MERSENNE_31_PRIME_FIELD_ORDER {
-        return
+        return;
     }
 
     let (value_u32_a, value_u32_b) = values;
-   
-    let a =  FieldElement::<Mersenne31Field>::from(value_u32_a as u64);
-    let b =  FieldElement::<Mersenne31Field>::from(value_u32_b as u64);
+
+    let a = FieldElement::<Mersenne31Field>::from(value_u32_a as u64);
+    let b = FieldElement::<Mersenne31Field>::from(value_u32_b as u64);
 
     // Note: if we parse using from_canonical_u32 fails due to check that n < Self::Order
     let a_expected = Mersenne31::from_canonical_u32(value_u32_a);
@@ -27,13 +25,13 @@ fuzz_target!(|values: (u32, u32)| {
 
     let add_u32 = &a + &b;
     let addition = a_expected + b_expected;
-    
+
     assert_eq!(add_u32.representative(), addition.as_canonical_u32());
 
     let sub_u32 = &a - &b;
     let substraction = a_expected - b_expected;
     assert_eq!(sub_u32.representative(), substraction.as_canonical_u32());
-    
+
     let mul_u32 = &a * &b;
     let multiplication = a_expected * b_expected;
     assert_eq!(mul_u32.representative(), multiplication.as_canonical_u32());
@@ -41,9 +39,9 @@ fuzz_target!(|values: (u32, u32)| {
     let pow = &a.pow(b.representative());
     let expected_pow = a_expected.exp_u64(b_expected.as_canonical_u64());
     assert_eq!(pow.representative(), expected_pow.as_canonical_u32());
-    
+
     if value_u32_b != 0 && b.inv().is_ok() && b_expected.try_inverse().is_some() {
-        let div = &a / &b; 
+        let div = &a / &b;
         assert_eq!(&div * &b, a.clone());
         let expected_div = a_expected / b_expected;
         assert_eq!(div.representative(), expected_div.as_canonical_u32());
@@ -52,8 +50,16 @@ fuzz_target!(|values: (u32, u32)| {
     for n in [&a, &b] {
         match n.sqrt() {
             Some((fst_sqrt, snd_sqrt)) => {
-                assert_eq!(fst_sqrt.square(), snd_sqrt.square(), "Squared roots don't match each other");
-                assert_eq!(n, &fst_sqrt.square(), "Squared roots don't match original number");
+                assert_eq!(
+                    fst_sqrt.square(),
+                    snd_sqrt.square(),
+                    "Squared roots don't match each other"
+                );
+                assert_eq!(
+                    n,
+                    &fst_sqrt.square(),
+                    "Squared roots don't match original number"
+                );
             }
             None => {}
         };
@@ -73,10 +79,22 @@ fuzz_target!(|values: (u32, u32)| {
     assert_eq!(&a * &b, &b * &a, "Commutative mul property failed");
 
     let c = &a * &b;
-    assert_eq!((&a + &b) + &c, &a + (&b + &c), "Associative add property failed");
-    assert_eq!((&a * &b) * &c, &a * (&b * &c), "Associative mul property failed");
+    assert_eq!(
+        (&a + &b) + &c,
+        &a + (&b + &c),
+        "Associative add property failed"
+    );
+    assert_eq!(
+        (&a * &b) * &c,
+        &a * (&b * &c),
+        "Associative mul property failed"
+    );
 
-    assert_eq!(&a * (&b + &c), &a * &b + &a * &c, "Distributive property failed");
+    assert_eq!(
+        &a * (&b + &c),
+        &a * &b + &a * &c,
+        "Distributive property failed"
+    );
 
     assert_eq!(&a - &a, zero, "Inverse add a failed");
     assert_eq!(&b - &b, zero, "Inverse add b failed");
