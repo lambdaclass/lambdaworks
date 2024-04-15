3eb6ec6cc149fca529b1aa088b51ee545964a8f6
jotabulacios <juanpablo@Juans-MacBook-Air.local>
2024-04-15 17:27:41 -0300

    add fuzzer vs. pathfinder and starknet

---
+++ fuzz/no_gpu_fuzz/fuzz_targets/hash/pedersen_fuzz.rs
@@ -0,0 +1,54 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use lambdaworks_crypto::hash::pedersen::Pedersen;
+use lambdaworks_crypto::hash::pedersen::PedersenStarkCurve;
+use lambdaworks_math::field::{
+    element::FieldElement, 
+    fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
+};
+use lambdaworks_math::traits::ByteConversion;
+use lambdaworks_crypto::hash::poseidon::Poseidon;
+use pathfinder_crypto::MontFelt;
+use pathfinder_crypto::Felt;
+use pathfinder_crypto::hash::pedersen::pedersen_hash;
+use num_bigint::BigInt;
+use num_traits::ToPrimitive;
+use std::str::FromStr;
+fn convert_dec_to_hex(dec_str: &str) -> String {
+    let dec = BigInt::from_str(dec_str)
+        .unwrap();  
+    format!("0x{:x}", dec)  
+}
+
+fuzz_target!(|data: ([u8; 32], [u8; 32])| {
+    let (bytes_a, bytes_b) = data;
+
+    let lw_x = FieldElement::<Stark252PrimeField>::from_bytes_be(&bytes_a).unwrap();
+    let lw_y = FieldElement::<Stark252PrimeField>::from_bytes_be(&bytes_b).unwrap();
+    let pedersen_hash =PedersenStarkCurve::hash(&lw_x, &lw_y).to_string();
+
+    let mut mont_x = lw_x.value().limbs;
+    let mut mont_y = lw_y.value().limbs;
+
+    // In order use the same field elements for starknet-rs and pathfinder, we have to reverse
+    // the limbs order respect to the lambdaworks implementation.
+    mont_x.reverse();
+    mont_y.reverse();
+
+    let pf_x = Felt::from(MontFelt(mont_x));
+    let pf_y = Felt::from(MontFelt(mont_y));
+    
+    let pathfinder_hash = Felt::from(pathfinder_crypto::hash::pedersen_hash(pf_x,pf_y)).to_hex_str();
+    assert_eq!(pedersen_hash, pathfinder_hash, "Lambdaworks and Pathfinder hashes don't match each other");
+
+// Starknet-rs 
+ 
+    let sn_ff_x = starknet_ff::FieldElement::from_mont(mont_x);
+    let sn_ff_y = starknet_ff::FieldElement::from_mont(mont_y);
+    let starknet_hash =convert_dec_to_hex(&starknet_crypto::pedersen_hash(&sn_ff_x,& sn_ff_y).to_string());
+
+    assert_eq!(pedersen_hash, starknet_hash, "Lambdaworks and Starknet hashes don't match each other");
+
+});
+
