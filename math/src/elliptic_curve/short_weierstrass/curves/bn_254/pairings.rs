7e1434abb815e03313b87f117bb77536dc19f09c
jotabulacios <jbulacios@fi.uba.ar>
2024-07-26 10:20:01 -0300

    save work

--- math/src/elliptic_curve/short_weierstrass/curves/bn_254/pairings.rs
+++ math/src/elliptic_curve/short_weierstrass/curves/bn_254/pairings.rs
@@ -3,14 +3,12 @@ use super::curve::MILLER_LOOP_CONSTANT;
 // we think that MILLER_LOOP_CONSTANT = 6x+2 = 29793968203157093288
 // with x = 496566136719284888
 // see https://hackmd.io/@Wimet/ry7z1Xj-2
-
 use super::{
     curve::BN254Curve,
     field_extension::{BN254PrimeField, Degree12ExtensionField, Degree2ExtensionField},
     twist::BN254TwistCurve,
 };
 use crate::{cyclic_group::IsGroup, elliptic_curve::traits::IsPairing, errors::PairingError};
-
 use crate::{
     elliptic_curve::short_weierstrass::{
         curves::bn_254::field_extension::{Degree6ExtensionField, LevelTwoResidue},
@@ -20,21 +18,15 @@ use crate::{
     field::{element::FieldElement, extensions::cubic::HasCubicNonResidue},
     unsigned_integer::element::{UnsignedInteger, U256},
 };
-
 // We have to find the SUBGROUP_ORDER and see where it's used.
 pub const SUBGROUP_ORDER: U256 =
     U256::from_hex_unchecked("TODO");
-
 // Need implementation of NAF representation
-
-
 pub struct BN254AtePairing;
-
 impl IsPairing for BN254AtePairing {
     type G1Point = ShortWeierstrassProjectivePoint<BN254Curve>;
     type G2Point = ShortWeierstrassProjectivePoint<BN254TwistCurve>;
     type OutputField = Degree12ExtensionField;
-
     // Compute the product of the ate pairings for a list of point pairs.
     // To optimize the pairing computation, we compute first all the miller
     // loops and multiply each other (so that we can then do the final expon).
@@ -43,7 +35,6 @@ impl IsPairing for BN254AtePairing {
     ) -> Result<FieldElement<Self::OutputField>, PairingError> {
         let mut result = FieldElement::one();
         for (p, q) in pairs {
-
             // We think we can remove the condition !p.is_in_subgroup() because
             // the subgroup oF G1 is G1 (see BN254 for the rest of us).
             // We have to implement is_in_subgroup() for the TwistesCurve in curve.rs
@@ -59,10 +50,25 @@ impl IsPairing for BN254AtePairing {
         Ok(final_exponentiation(&result))
     }
 }
-
-
-
-
-// 
-
-
+// TODO
+// We need a function that computes the double of a G2 point and its tangent line.
+// In the implementation of bls381, this function also changes the t's and accumulator's (f) values.
+// Initially t = Q, accumulator (f) = 1. See https://eprint.iacr.org/2010/354.pdf.
+// Question: In https://eprint.iacr.org/2010/354.pdf P shouldn't be ProjectivePoint (Q is ok).
+fn double_accumulate_line (
+    t: &mut ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+    p: &ShortWeierstrassProjectivePoint<BLS12381Curve>,
+    accumulator: &mut FieldElement<Degree12ExtensionField>,
+){
+    // TODO
+}
+//TODO
+// We need a function that computes the addition of two G2 points and the line through them.
+fn add_accumulate_line(
+    t: &mut ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+    q: &ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+    p: &ShortWeierstrassProjectivePoint<BLS12381Curve>,
+    accumulator: &mut FieldElement<Degree12ExtensionField>,
+) {
+    // TODO
+}
