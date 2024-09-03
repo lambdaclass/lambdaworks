4409b55e48ed40806de6cb0eadfe6e79f031b77c
diegokingston <dkingston@fi.uba.ar>
2024-09-03 13:13:42 -0300

    format

--- math/src/field/fields/montgomery_backed_prime_fields.rs
+++ math/src/field/fields/montgomery_backed_prime_fields.rs
@@ -1239,7 +1239,7 @@ mod tests_u256_prime_fields {
         let minus_3_squared = minus_3.square();
         let minus_3_pow_2 = minus_3.pow(2_u32);
         let nine = SecpMontElement::from_hex_unchecked("0x9");
-        
+
         assert_eq!(minus_3_mul_minus_3, nine);
         assert_eq!(minus_3_squared, nine);
         assert_eq!(minus_3_pow_2, nine);
