2546ca2d06a56ac1da2f463f9798e469976b3bf0
Nicole <nicole@Nicoles-MacBook-Air.local>
2024-10-16 13:35:11 -0300

    add tests and comments

--- math/src/circle/cfft.rs
+++ math/src/circle/cfft.rs
@@ -2,81 +2,122 @@ extern crate alloc;
 use crate::field::{element::FieldElement, fields::mersenne31::field::Mersenne31Field};
 
 #[cfg(feature = "alloc")]
+/// fft in place algorithm used to evaluate a polynomial of degree 2^n - 1 in 2^n points.
+/// Input must be of size 2^n for some n.
 pub fn cfft(
     input: &mut [FieldElement<Mersenne31Field>],
     twiddles: Vec<Vec<FieldElement<Mersenne31Field>>>,
 ) {
+    // If the input size is 2^n, then log_2_size is n.
     let log_2_size = input.len().trailing_zeros();
-    
+
+    // The cfft has n layers.
     (0..log_2_size).for_each(|i| {
+        // In each layer i we split the current input in chunks of size 2^{i+1}.
         let chunk_size = 1 << i + 1;
         let half_chunk_size = 1 << i;
         input.chunks_mut(chunk_size).for_each(|chunk| {
+            // We split each chunk in half, calling the first half hi_part and the second hal low_part.
             let (hi_part, low_part) = chunk.split_at_mut(half_chunk_size);
-            hi_part.into_iter().zip(low_part).enumerate().for_each( |(j, (hi, low))| {
-                let temp = *low * twiddles[i as usize][j];
-                *low = *hi - temp;
-                *hi = *hi + temp;    
-            });
+
+            // We apply the corresponding butterfly for every element j of the high and low part.
+            hi_part
+                .into_iter()
+                .zip(low_part)
+                .enumerate()
+                .for_each(|(j, (hi, low))| {
+                    let temp = *low * twiddles[i as usize][j];
+                    *low = *hi - temp;
+                    *hi = *hi + temp;
+                });
         });
     });
-} 
-
+}
 
 #[cfg(feature = "alloc")]
+/// The inverse fft algorithm used to interpolate 2^n points.
+/// Input must be of size 2^n for some n.
 pub fn icfft(
     input: &mut [FieldElement<Mersenne31Field>],
     twiddles: Vec<Vec<FieldElement<Mersenne31Field>>>,
 ) {
+    // If the input size is 2^n, then log_2_size is n.
     let log_2_size = input.len().trailing_zeros();
-    
-    println!("{:?}", twiddles);
-    
+
+    // The icfft has n layers.
     (0..log_2_size).for_each(|i| {
+        // In each layer i we split the current input in chunks of size 2^{n - i}.
         let chunk_size = 1 << log_2_size - i;
         let half_chunk_size = chunk_size >> 1;
         input.chunks_mut(chunk_size).for_each(|chunk| {
+            // We split each chunk in half, calling the first half hi_part and the second hal low_part.
             let (hi_part, low_part) = chunk.split_at_mut(half_chunk_size);
-            hi_part.into_iter().zip(low_part).enumerate().for_each( |(j, (hi, low))| {
-                let temp = *hi + *low;
-                *low = (*hi - *low) * twiddles[i as usize][j];
-                *hi = temp;
-            });
-        });
-    });    
-} 
 
-// From [0, 2, 4, 6, 7, 5, 3, 1] to [0, 1, 2, 3, 4, 5, 6, 7]
-pub fn order_cfft_result_naive(input: &mut [FieldElement<Mersenne31Field>]) -> Vec<FieldElement<Mersenne31Field>> {
+            // We apply the corresponding butterfly for every element j of the high and low part.
+            hi_part
+                .into_iter()
+                .zip(low_part)
+                .enumerate()
+                .for_each(|(j, (hi, low))| {
+                    let temp = *hi + *low;
+                    *low = (*hi - *low) * twiddles[i as usize][j];
+                    *hi = temp;
+                });
+        });
+    });
+}
+
+/// This function permutes a slice of field elements to order the result of the cfft in the natural way.
+/// We call the natural order to [P(x0, y0), P(x1, y1), P(x2, y2), ...],
+/// where (x0, y0) is the first point of the corresponding coset.
+/// The cfft doesn't return the evaluations in the natural order.
+/// For example, if we apply the cfft to 8 coefficients of a polynomial of degree 7 we'll get the evaluations in this order:
+/// [P(x0, y0), P(x2, y2), P(x4, y4), P(x6, y6), P(x7, y7), P(x5, y5), P(x3, y3), P(x1, y1)],
+/// where the even indices are found first in ascending order and then the odd indices in descending order.
+/// This function permutes the slice [0, 2, 4, 6, 7, 5, 3, 1] into [0, 1, 2, 3, 4, 5, 6, 7].
+pub fn order_cfft_result_naive(
+    input: &mut [FieldElement<Mersenne31Field>],
+) -> Vec<FieldElement<Mersenne31Field>> {
     let mut result = Vec::new();
     let length = input.len();
-    for i in (0..length/2) {
-        result.push(input[i]);
-        result.push(input[length - i - 1]);
+    for i in 0..length / 2 {
+        result.push(input[i]); // We push the left index.
+        result.push(input[length - i - 1]); // We push the right index.
     }
     result
 }
 
-// From [0, 1, 2, 3, 4, 5, 6, 7] to [0, 2, 4, 6, 7, 5, 3, 1]
-pub fn order_icfft_input_naive(input: &mut [FieldElement<Mersenne31Field>]) -> Vec<FieldElement<Mersenne31Field>> {
+/// This function permutes a slice of field elements to order the input of the icfft in a specific way.
+/// For example, if we want to interpolate 8 points we should input them in the icfft in this order:
+/// [(x0, y0), (x2, y2), (x4, y4), (x6, y6), (x7, y7), (x5, y5), (x3, y3), (x1, y1)],
+/// where the even indices are found first in ascending order and then the odd indices in descending order.
+/// This function permutes the slice [0, 1, 2, 3, 4, 5, 6, 7] into [0, 2, 4, 6, 7, 5, 3, 1].
+pub fn order_icfft_input_naive(
+    input: &mut [FieldElement<Mersenne31Field>],
+) -> Vec<FieldElement<Mersenne31Field>> {
     let mut result = Vec::new();
-    (0..input.len()).step_by(2).for_each( |i| {
+
+    // We push the even indices.
+    (0..input.len()).step_by(2).for_each(|i| {
         result.push(input[i]);
     });
-    (1..input.len()).step_by(2).rev().for_each( |i| {
+
+    // We push the odd indices.
+    (1..input.len()).step_by(2).rev().for_each(|i| {
         result.push(input[i]);
     });
     result
 }
 
+// We are not using this fucntion.
 pub fn reverse_cfft_index(index: usize, length: usize) -> usize {
-    if index < (length >> 1) { // index < length / 2
+    if index < (length >> 1) {
+        // index < length / 2
         index << 1 // index * 2
     } else {
         (((length - 1) - index) << 1) + 1
     }
 }
-
 
 pub fn cfft_4(
     input: &mut [FieldElement<Mersenne31Field>],
@@ -147,27 +188,16 @@ pub fn cfft_8(
     stage3.into_iter().map(|elem| elem * f).collect()
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
     type FE = FieldElement<Mersenne31Field>;
 
     #[test]
-    fn ordering_4() {
-        let expected_slice = [
-            FE::from(0),
-            FE::from(1),
-            FE::from(2),
-            FE::from(3),
-        ];
+    fn ordering_cfft_result_works_for_4_points() {
+        let expected_slice = [FE::from(0), FE::from(1), FE::from(2), FE::from(3)];
 
-        let mut slice = [
-            FE::from(0),
-            FE::from(2),
-            FE::from(3),
-            FE::from(1),
-        ];
+        let mut slice = [FE::from(0), FE::from(2), FE::from(3), FE::from(1)];
 
         let res = order_cfft_result_naive(&mut slice);
 
@@ -175,7 +205,7 @@ mod tests {
     }
 
     #[test]
-    fn ordering() {
+    fn ordering_cfft_result_works_for_16_points() {
         let expected_slice = [
             FE::from(0),
             FE::from(1),
@@ -232,7 +262,7 @@ mod tests {
     }
 
     #[test]
-    fn from_natural_to_icfft_input_order() {
+    fn from_natural_to_icfft_input_order_works() {
         let mut slice = [
             FE::from(0),
             FE::from(1),
@@ -275,6 +305,4 @@ mod tests {
 
         assert_eq!(res, expected_slice)
     }
-
-
 }
--- math/src/circle/twiddles.rs
+++ math/src/circle/twiddles.rs
@@ -16,18 +16,28 @@ pub fn get_twiddles(
     domain: Coset,
     config: TwiddlesConfig,
 ) -> Vec<Vec<FieldElement<Mersenne31Field>>> {
+    // We first take the half coset.
     let half_domain_points = Coset::get_coset_points(&Coset::half_coset(domain.clone()));
 
+    // The first set of twiddles are all the y coordinates of the half coset.
     let mut twiddles: Vec<Vec<FieldElement<Mersenne31Field>>> =
         vec![half_domain_points.iter().map(|p| p.y).collect()];
 
     if domain.log_2_size >= 2 {
-        twiddles.push(half_domain_points.iter().take(half_domain_points.len() / 2 ).map(|p| p.x).collect());
+        // The second set of twiddles are the x coordinates of the first half of the half coset.
+        twiddles.push(
+            half_domain_points
+                .iter()
+                .take(half_domain_points.len() / 2)
+                .map(|p| p.x)
+                .collect(),
+        );
         for _ in 0..(domain.log_2_size - 2) {
+            // The rest of the sets of twiddles are the "square" of the x coordinates of the first half of the previous set.
             let prev = twiddles.last().unwrap();
             let cur = prev
                 .iter()
-                .take(prev.len() / 2 )
+                .take(prev.len() / 2)
                 .map(|x| x.square().double() - FieldElement::<Mersenne31Field>::one())
                 .collect();
             twiddles.push(cur);
@@ -40,7 +50,7 @@ pub fn get_twiddles(
             FieldElement::<Mersenne31Field>::inplace_batch_inverse(x).unwrap();
         });
     } else {
-        // For the evaluation, we need the vector of twiddles but in the inverse order.
+        // For the evaluation, we need reverse the order of the vector of twiddles.
         twiddles.reverse();
     }
     twiddles
@@ -76,9 +86,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn twiddles_vectors_lenght() {
+    fn evaluation_twiddles_vectors_length_is_correct() {
         let domain = Coset::new_standard(3);
         let config = TwiddlesConfig::Evaluation;
+        let twiddles = get_twiddles(domain, config);
+        for i in 0..twiddles.len() - 1 {
+            assert_eq!(2 * twiddles[i].len(), twiddles[i + 1].len())
+        }
+    }
+
+    #[test]
+    fn interpolation_twiddles_vectors_length_is_correct() {
+        let domain = Coset::new_standard(3);
+        let config = TwiddlesConfig::Interpolation;
         let twiddles = get_twiddles(domain, config);
         for i in 0..twiddles.len() - 1 {
             assert_eq!(twiddles[i].len(), 2 * twiddles[i + 1].len())
