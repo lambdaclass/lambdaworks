f897b9bb0764a709966b549c8fe886746d3dacdb
jotabulacios <jbulacios@fi.uba.ar>
2024-10-25 17:40:34 -0300

    add generator reference

--- math/src/elliptic_curve/short_weierstrass/curves/bls12_377/curve.rs
+++ math/src/elliptic_curve/short_weierstrass/curves/bls12_377/curve.rs
@@ -28,11 +28,12 @@ impl IsEllipticCurve for BLS12377Curve {
     type BaseField = BLS12377PrimeField;
     type PointRepresentation = ShortWeierstrassProjectivePoint<Self>;
 
+    // generator values are taken from https://neuromancer.sk/std/bls/BLS12-377
     fn generator() -> Self::PointRepresentation {
         Self::PointRepresentation::new([
             FieldElement::<Self::BaseField>::new_base("8848defe740a67c8fc6225bf87ff5485951e2caa9d41bb188282c8bd37cb5cd5481512ffcd394eeab9b16eb21be9ef"),
             FieldElement::<Self::BaseField>::new_base("1914a69c5102eff1f674f5d30afeec4bd7fb348ca3e52d96d182ad44fb82305c2fe3d3634a9591afd82de55559c8ea6"),
-            FieldElement::one() // I have doubts about the second coordinate
+            FieldElement::one()
         ])
     }
 }
