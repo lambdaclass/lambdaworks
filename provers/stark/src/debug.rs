2ce8b2ea2dd6547848b63db2246912b06066dd1b
Joaquin Carletti <56092489+ColoCarletti@users.noreply.github.com>
2024-12-20 13:02:13 -0300

    Refactor STARK for small fields (#945)
    * add enum for evaluate transitions constraints
    
    * change constraints evaluator fn
    
    * fix fib example
    
    * fix simple fib and dummy air examples
    
    * fix examples
    
    * fix mem example
    
    * crate new methods for evaluation_context
    
    ---------
    
    Co-authored-by: jotabulacios <45471455+jotabulacios@users.noreply.github.com>

--- provers/stark/src/debug.rs
+++ provers/stark/src/debug.rs
@@ -1,5 +1,5 @@
 use super::domain::Domain;
-use super::traits::AIR;
+use super::traits::{TransitionEvaluationContext, AIR};
 use crate::{frame::Frame, trace::LDETraceTable};
 use lambdaworks_math::{
     field::{
@@ -93,7 +93,9 @@ pub fn validate_trace<A: AIR>(
             .iter()
             .map(|col| col[step].clone())
             .collect();
-        let evaluations = air.compute_transition_prover(&frame, &periodic_values, rap_challenges);
+        let transition_evaluation_context =
+            TransitionEvaluationContext::new_prover(&frame, &periodic_values, rap_challenges);
+        let evaluations = air.compute_transition(&transition_evaluation_context);
 
         // Iterate over each transition evaluation. When the evaluated step is not from
         // the exemption steps corresponding to the transition, it should have zero as a
