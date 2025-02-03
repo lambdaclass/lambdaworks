d78d18a2444668ead647d3b45e9071163b84af53
Nicole Graus <nicole.graus@lambdaclass.com>
2025-02-03 12:58:13 -0300

    Add quartic extension baby bear u32 (#956)
    * add quartic extension for baby bear u32. Add comments in mu and r2 generator function
    
    * add bench for Fp4E using u32
    
    * fix typo and restore all benches
    
    * fix clippy
    
    * cargo check
    
    * fix tests
    
    * fix clippy
    
    * fix crate import
    
    * fix documentation
    
    * use num instead of capacity in loop
    
    ---------
    
    Co-authored-by: Nicole <nicole@Nicoles-MacBook-Air.local>
    Co-authored-by: jotabulacios <jbulacios@fi.uba.ar>
    Co-authored-by: Diego K <43053772+diegokingston@users.noreply.github.com>
    Co-authored-by: jotabulacios <45471455+jotabulacios@users.noreply.github.com>

--- math/benches/criterion_field.rs
+++ math/benches/criterion_field.rs
@@ -5,7 +5,11 @@ mod fields;
 use fields::mersenne31::{mersenne31_extension_ops_benchmarks, mersenne31_ops_benchmarks};
 use fields::mersenne31_montgomery::mersenne31_mont_ops_benchmarks;
 use fields::{
-    baby_bear::{babybear_ops_benchmarks, babybear_ops_benchmarks_f64, babybear_p3_ops_benchmarks},
+    baby_bear::{
+        babybear_extension_ops_benchmarks_p3, babybear_p3_ops_benchmarks,
+        babybear_u32_extension_ops_benchmarks, babybear_u32_ops_benchmarks,
+        babybear_u64_extension_ops_benchmarks, babybear_u64_ops_benchmarks,
+    },
     stark252::starkfield_ops_benchmarks,
     u64_goldilocks::u64_goldilocks_ops_benchmarks,
     u64_goldilocks_montgomery::u64_goldilocks_montgomery_ops_benchmarks,
@@ -14,7 +18,18 @@ use fields::{
 criterion_group!(
     name = field_benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets =babybear_ops_benchmarks,babybear_ops_benchmarks_f64, babybear_p3_ops_benchmarks,mersenne31_extension_ops_benchmarks,mersenne31_ops_benchmarks,
-    starkfield_ops_benchmarks,u64_goldilocks_ops_benchmarks,u64_goldilocks_montgomery_ops_benchmarks,mersenne31_mont_ops_benchmarks
+
+    targets = babybear_u32_ops_benchmarks,
+              babybear_u32_extension_ops_benchmarks,
+              babybear_u64_ops_benchmarks,
+              babybear_u64_extension_ops_benchmarks,
+              babybear_p3_ops_benchmarks,
+              babybear_extension_ops_benchmarks_p3,
+              mersenne31_ops_benchmarks,
+              mersenne31_extension_ops_benchmarks,
+              mersenne31_mont_ops_benchmarks,
+              starkfield_ops_benchmarks,
+              u64_goldilocks_ops_benchmarks,
+              u64_goldilocks_montgomery_ops_benchmarks,
 );
 criterion_main!(field_benches);
--- math/benches/fields/baby_bear.rs
+++ math/benches/fields/baby_bear.rs
@@ -1,6 +1,10 @@
 use criterion::Criterion;
 use std::hint::black_box;
 
+use lambdaworks_math::field::fields::fft_friendly::{
+    quartic_babybear::Degree4BabyBearExtensionField,
+    quartic_babybear_u32::Degree4BabyBearU32ExtensionField,
+};
 use lambdaworks_math::field::{
     element::FieldElement,
     fields::{
@@ -10,6 +14,7 @@ use lambdaworks_math::field::{
 };
 
 use p3_baby_bear::BabyBear;
+use p3_field::extension::BinomialExtensionField;
 use p3_field::{Field, FieldAlgebra};
 
 use rand::random;
@@ -18,11 +23,63 @@ use rand::Rng;
 pub type U32Babybear31PrimeField = U32MontgomeryBackendPrimeField<2013265921>;
 pub type F = FieldElement<U32Babybear31PrimeField>;
 pub type F64 = FieldElement<Babybear31PrimeField>;
+pub type Fp4Eu32 = FieldElement<Degree4BabyBearU32ExtensionField>;
+pub type Fp4E = FieldElement<Degree4BabyBearExtensionField>;
+type EF4 = BinomialExtensionField<BabyBear, 4>;
 
-pub fn rand_field_elements(num: usize) -> Vec<(F, F)> {
+pub fn rand_field_elements_u32(num: usize) -> Vec<(F, F)> {
     let mut result = Vec::with_capacity(num);
-    for _ in 0..result.capacity() {
+    for _ in 0..num {
         result.push((F::from(random::<u64>()), F::from(random::<u64>())));
+    }
+    result
+}
+
+pub fn rand_field_elements_u64(num: usize) -> Vec<(F64, F64)> {
+    let mut result = Vec::with_capacity(num);
+    for _ in 0..num {
+        result.push((F64::from(random::<u64>()), F64::from(random::<u64>())));
+    }
+    result
+}
+
+pub fn rand_babybear_u64_fp4_elements(num: usize) -> Vec<(Fp4E, Fp4E)> {
+    let mut result = Vec::with_capacity(num);
+    for _ in 0..num {
+        result.push((
+            Fp4E::new([
+                F64::from(random::<u64>()),
+                F64::from(random::<u64>()),
+                F64::from(random::<u64>()),
+                F64::from(random::<u64>()),
+            ]),
+            Fp4E::new([
+                F64::from(random::<u64>()),
+                F64::from(random::<u64>()),
+                F64::from(random::<u64>()),
+                F64::from(random::<u64>()),
+            ]),
+        ));
+    }
+    result
+}
+pub fn rand_babybear_u32_fp4_elements(num: usize) -> Vec<(Fp4Eu32, Fp4Eu32)> {
+    let mut result = Vec::with_capacity(num);
+    for _ in 0..num {
+        result.push((
+            Fp4Eu32::new([
+                F::from(random::<u64>()),
+                F::from(random::<u64>()),
+                F::from(random::<u64>()),
+                F::from(random::<u64>()),
+            ]),
+            Fp4Eu32::new([
+                F::from(random::<u64>()),
+                F::from(random::<u64>()),
+                F::from(random::<u64>()),
+                F::from(random::<u64>()),
+            ]),
+        ));
     }
     result
 }
@@ -34,10 +91,17 @@ fn rand_babybear_elements_p3(num: usize) -> Vec<(BabyBear, BabyBear)> {
         .collect()
 }
 
-pub fn babybear_ops_benchmarks(c: &mut Criterion) {
+fn rand_babybear_fp4_elements_p3(num: usize) -> Vec<(EF4, EF4)> {
+    let mut rng = rand::thread_rng();
+    (0..num)
+        .map(|_| (rng.gen::<EF4>(), rng.gen::<EF4>()))
+        .collect()
+}
+
+pub fn babybear_u32_ops_benchmarks(c: &mut Criterion) {
     let input: Vec<Vec<(F, F)>> = [1000000]
         .into_iter()
-        .map(rand_field_elements)
+        .map(rand_field_elements_u32)
         .collect::<Vec<_>>();
     let mut group = c.benchmark_group("BabyBear operations using Lambdaworks u32");
 
@@ -92,14 +156,7 @@ pub fn babybear_ops_benchmarks(c: &mut Criterion) {
     }
 }
 
-pub fn rand_field_elements_u64(num: usize) -> Vec<(F64, F64)> {
-    let mut result = Vec::with_capacity(num);
-    for _ in 0..result.capacity() {
-        result.push((F64::from(random::<u64>()), F64::from(random::<u64>())));
-    }
-    result
-}
-pub fn babybear_ops_benchmarks_f64(c: &mut Criterion) {
+pub fn babybear_u64_ops_benchmarks(c: &mut Criterion) {
     let input: Vec<Vec<(F64, F64)>> = [1000000]
         .into_iter()
         .map(rand_field_elements_u64)
@@ -156,6 +213,130 @@ pub fn babybear_ops_benchmarks_f64(c: &mut Criterion) {
         });
     }
 }
+pub fn babybear_u32_extension_ops_benchmarks(c: &mut Criterion) {
+    let input: Vec<Vec<(Fp4Eu32, Fp4Eu32)>> = [1000000]
+        .into_iter()
+        .map(rand_babybear_u32_fp4_elements)
+        .collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("BabyBear u32 Fp4 operations");
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Addition of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(x) + black_box(y));
+                }
+            });
+        });
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(
+            format!("Multiplication of Fp4 {:?}", &i.len()),
+            &i,
+            |bench, i| {
+                bench.iter(|| {
+                    for (x, y) in i {
+                        black_box(black_box(x) * black_box(y));
+                    }
+                });
+            },
+        );
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Square of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, _) in i {
+                    black_box(black_box(x).square());
+                }
+            });
+        });
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Inverse of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(x) / black_box(y));
+                }
+            });
+        });
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Division of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, _) in i {
+                    black_box(black_box(x).inv().unwrap());
+                }
+            });
+        });
+    }
+}
+pub fn babybear_u64_extension_ops_benchmarks(c: &mut Criterion) {
+    let input: Vec<Vec<(Fp4E, Fp4E)>> = [1000000]
+        .into_iter()
+        .map(rand_babybear_u64_fp4_elements)
+        .collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("BabyBear u64 Fp4 operations");
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Addition of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(x) + black_box(y));
+                }
+            });
+        });
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(
+            format!("Multiplication of Fp4 {:?}", &i.len()),
+            &i,
+            |bench, i| {
+                bench.iter(|| {
+                    for (x, y) in i {
+                        black_box(black_box(x) * black_box(y));
+                    }
+                });
+            },
+        );
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Square of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, _) in i {
+                    black_box(black_box(x).square());
+                }
+            });
+        });
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Inverse of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(x) / black_box(y));
+                }
+            });
+        });
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Division of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, _) in i {
+                    black_box(black_box(x).inv().unwrap());
+                }
+            });
+        });
+    }
+}
 
 pub fn babybear_p3_ops_benchmarks(c: &mut Criterion) {
     let input: Vec<Vec<(BabyBear, BabyBear)>> = [1000000]
@@ -206,6 +387,68 @@ pub fn babybear_p3_ops_benchmarks(c: &mut Criterion) {
 
     for i in input.clone().into_iter() {
         group.bench_with_input(format!("Division {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(*x) / black_box(*y));
+                }
+            });
+        });
+    }
+}
+
+pub fn babybear_extension_ops_benchmarks_p3(c: &mut Criterion) {
+    let input_sizes = [1000000];
+    let input: Vec<Vec<(EF4, EF4)>> = input_sizes
+        .into_iter()
+        .map(rand_babybear_fp4_elements_p3)
+        .collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("BabyBear Fp4 operations using Plonky3");
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Addition of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, y) in i {
+                    black_box(black_box(*x) + black_box(*y));
+                }
+            });
+        });
+    }
+    for i in input.clone().into_iter() {
+        group.bench_with_input(
+            format!("Multiplication of Fp4 {:?}", &i.len()),
+            &i,
+            |bench, i| {
+                bench.iter(|| {
+                    for (x, y) in i {
+                        black_box(black_box(*x) * black_box(*y));
+                    }
+                });
+            },
+        );
+    }
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Square of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, _) in i {
+                    black_box(black_box(x).square());
+                }
+            });
+        });
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Inverse of Fp4 {:?}", &i.len()), &i, |bench, i| {
+            bench.iter(|| {
+                for (x, _) in i {
+                    black_box(black_box(x).inverse());
+                }
+            });
+        });
+    }
+
+    for i in input.clone().into_iter() {
+        group.bench_with_input(format!("Division of Fp4 {:?}", &i.len()), &i, |bench, i| {
             bench.iter(|| {
                 for (x, y) in i {
                     black_box(black_box(*x) / black_box(*y));
