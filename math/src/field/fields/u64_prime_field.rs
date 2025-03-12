ba1c84920433b7210656fb9338eb1c59382cbe40
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2025-03-12 11:57:53 -0300

    impl HasDefaultTranscript for u64prmeField

--- math/src/field/fields/u64_prime_field.rs
+++ math/src/field/fields/u64_prime_field.rs
@@ -4,7 +4,7 @@ use crate::errors::CreationError;
 use crate::errors::DeserializationError;
 use crate::field::element::FieldElement;
 use crate::field::errors::FieldError;
-use crate::field::traits::{IsFFTField, IsField, IsPrimeField};
+use crate::field::traits::{HasDefaultTranscript, IsFFTField, IsField, IsPrimeField};
 use crate::traits::{ByteConversion, Deserializable};
 
 /// Type representing prime fields over unsigned 64-bit integers.
@@ -150,6 +150,20 @@ impl<const MODULUS: u64> Deserializable for FieldElement<U64PrimeField<MODULUS>>
         Self: Sized,
     {
         Self::from_bytes_be(bytes).map_err(|x| x.into())
+    }
+}
+
+
+impl<const MODULUS: u64> HasDefaultTranscript for U64PrimeField<MODULUS> {
+    fn get_random_field_element_from_seed(seed: [u8; 32]) -> Option<FieldElement<Self>> {
+        let mut sample = u64::from_be_bytes(seed[24..32].try_into().unwrap());
+        let mask = u64::MAX >> MODULUS.leading_zeros();
+        sample &= mask;
+
+        if sample > MODULUS {
+            return None;
+        }
+        Some(U64FieldElement::new(sample))
     }
 }
 
