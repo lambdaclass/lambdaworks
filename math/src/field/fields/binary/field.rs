f6a01b9f7fa1c7db174f859089c0e0c57524d0a4
jotabulacios <jbulacios@fi.uba.ar>
2025-03-20 14:22:52 -0300

    add docs and update README

--- math/src/field/fields/binary/field.rs
+++ math/src/field/fields/binary/field.rs
@@ -1,7 +1,16 @@
 use core::fmt;
 use core::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub};
+use std::cmp::Ordering;
 use std::iter::{Product, Sum};
 
+// Implementation of binary field and its arithmetic in finite fields of the form GF(2^(2^n)) by constructing
+// a tower of binary field extensions. The basic idea is to represent an element of the field as a polynomial
+// with coefficients in GF(2) (the binary field) where the coefficients are stored as bits in a `u128` integer.
+// The tower structure is built recursively, with each level representing an extension of the previous field.
+
+// For more details, see:
+// - Lambdaclass blog post about the use of binary fields in SNARKs: https://blog.lambdaclass.com/snarks-on-binary-fields-binius/
+// - Vitalik Buterin’s Binius: https://vitalik.eth.limo/general/2024/04/29/binius.html
 #[derive(Debug)]
 pub enum BinaryFieldError {
     /// Attempt to compute inverse of zero
@@ -9,6 +18,12 @@ pub enum BinaryFieldError {
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+/// An element in a tower of binary field extensions.
+///
+/// Implements arithmetic in finite fields GF(2^(2^n)) constructed as
+/// successive extensions of the base field GF(2).
+///
+/// The internal representation stores polynomial coefficients as bits in a u128 integer.
 pub struct TowerFieldElement {
     /// The underlying value
     pub value: u128,
@@ -16,7 +31,6 @@ pub struct TowerFieldElement {
     pub num_level: usize,
     /// Number of bits needed for that level (2^num_levels).
     /// It's the order of the field extension that the element belongs to.
-    /// QUESTION: Is it better to have a function num_bits(){ 1<<num_levels } ?
     pub num_bits: usize,
 }
 
@@ -97,26 +111,11 @@ impl TowerFieldElement {
     /// Joins the hi and low part making a new element.
     /// For example, if a_hi = x and a_low = 1
     /// then a = xy + 1.
-    // pub fn join(&self, low: &Self) -> Self {
-    //     let joined = (self.value() << self.num_bits()) | low.value();
-    //     Self::new(joined, self.num_level() + 1)
-    // }
-
     pub fn join(&self, low: &Self) -> Self {
-        let new_level = self.num_level() + 1;
-        let safe_level = if new_level > 7 { 7 } else { new_level };
-        // Usamos la cantidad de bits correspondiente al safe_level anterior a capear
-        // Para que el desplazamiento no exceda el máximo permitido.
-        let shift = if self.num_level() < safe_level {
-            1 << (safe_level - 1)
-        } else {
-            self.num_bits()
-        };
-        let joined = (self.value() << shift) | low.value();
-        Self::new(joined, safe_level)
+        let joined = (self.value() << self.num_bits()) | low.value();
+        Self::new(joined, self.num_level() + 1)
     }
 
-    // QUESTION: Do we leave this?
     // Extend number of levels
     pub fn extend_num_levels(&mut self, new_levels: usize) {
         if self.num_level() < new_levels {
@@ -142,28 +141,9 @@ impl TowerFieldElement {
 
     /// Computes the multiplicative inverse using Fermat's little theorem.
     /// Returns an error if the element is zero
-    /// FIXME: It works until level 4.
+    // Based on Ingoyama's implementation
+    // https://github.com/ingonyama-zk/smallfield-super-sumcheck/blob/a8c61beef39bc0c10a8f68d25eeac0a7190a7289/src/tower_fields/binius.rs#L116C5-L116C6
     pub fn inv(&self) -> Result<Self, BinaryFieldError> {
-        // if self.is_zero() {
-        //     return Err(BinaryFieldError::InverseOfZero);
-        // }
-
-        // // For F, the inverse of 1 is 1
-        // if self.num_level == 0 {
-        //     return Ok(self.clone());
-        // }
-
-        // // For small fields, use Fermat's little theorem
-        // if self.num_level <= 1 || self.num_bits <= 4 {
-        //     // Use Fermat's little theorem:
-        //     // In GF(2^n), x^(2^n-1) = 1 for any non-zero x
-        //     // Therefore x^(2^n-2) is the multiplicative inverse
-        //     return Ok(self.pow((1 << self.num_bits) - 2));
-        // }
-
-        // // For larger fields, use recursive algorithm
-
-        // TODO: what if a == mod.
         if self.is_zero() {
             return Err(BinaryFieldError::InverseOfZero);
         }
@@ -171,29 +151,20 @@ impl TowerFieldElement {
             let exponent = (1 << self.num_bits()) - 2;
             Ok(Self::pow(self, exponent as u32))
         } else {
-            // Split the element into high and low parts
             let (a_hi, a_lo) = self.split();
-
-            // Compute 2^(k-1) where k = num_bits/2
-            // FIX?S
             let two_pow_k_minus_one = Self::new(1 << (self.num_bits() / 4), self.num_level() - 1);
-
-            // Apparently, there was a mistake here when we ported the implementation.
-            // let two_pow_k_minus_one =
-            //     Self::new(1 << ((self.num_bits() / 2) - 1), self.num_level() - 1);
-
             // a = a_hi * x^k + a_lo
             // a_lo_next = a_hi * x^(k-1) + a_lo
-            let a_lo_next = a_lo.clone() + a_hi.clone() * two_pow_k_minus_one;
+            let a_lo_next = a_lo + a_hi * two_pow_k_minus_one;
 
             // Δ = a_lo * a_lo_next + a_hi^2
-            let delta = a_lo.clone() * a_lo_next.clone() + a_hi.clone() * a_hi.clone();
+            let delta = a_lo * a_lo_next + a_hi * a_hi;
 
             // Compute inverse of delta recursively
             let delta_inverse = delta.inv()?;
 
             // Compute parts of the inverse
-            let out_hi = delta_inverse.clone() * a_hi;
+            let out_hi = delta_inverse * a_hi;
             let out_lo = delta_inverse * a_lo_next;
 
             // Join the parts to get the final inverse
@@ -204,47 +175,37 @@ impl TowerFieldElement {
     /// Calculate power.
     pub fn pow(&self, exp: u32) -> Self {
         let mut result = Self::one();
-        let mut base = self.clone();
+        let mut base = *self;
         let mut exp_val = exp;
 
         while exp_val > 0 {
             if exp_val & 1 == 1 {
-                result = result * base.clone();
+                result *= base;
             }
-            base = base.clone() * base.clone();
+            base = base * base;
             exp_val >>= 1;
         }
 
         result
     }
 
-    // We calculate a + b in the following way:
-    // - If both are of the same level, then a + b = a XOR b.
-    // - If a's level is larger than b's level, we take the last bits of a so that it has the same size as b,
-    // then we xor those bits with b and concatenate the rest of a with the result.
-    // Example: a = 1001 and b = 10
-    //   10 01
-    // +    10
-    // = 10 11
     fn add_elements(&self, other: &Self) -> Self {
-        if self.num_level() > other.num_level() {
-            let mask = (1 << other.num_bits()) - 1;
-            // Lsb of a: We get the last "b.num_bits" bits of a.
-            let low = self.value() & mask;
-            // Perform the addition (XOR) on the lower part.
-            let result_low = low ^ other.value();
-            // Msb of a: We get the remaining bits.
-            let high = self.value() >> other.num_bits();
-            // We concatenate both partis.
-            let result_value = (high << other.num_bits()) | result_low;
+        match self.num_level().cmp(&other.num_level()) {
+            Ordering::Greater => {
+                let mask = (1 << other.num_bits()) - 1;
+                // Lsb of a: We get the last "b.num_bits" bits of a.
+                let low = self.value() & mask;
+                // Perform the addition (XOR) on the lower part.
+                let result_low = low ^ other.value();
+                // Msb of a: We get the remaining bits.
+                let high = self.value() >> other.num_bits();
+                // We concatenate both parts.
+                let result_value = (high << other.num_bits()) | result_low;
 
-            Self::new(result_value, self.num_level())
-        } else if self.num_level() < other.num_level() {
-            // If b is larger than a, we just swap the arguments and call the same method.
-            other.add_elements(self)
-        } else {
-            // If a and b have the same size, we just XOR them.
-            Self::new(self.value() ^ other.value(), self.num_level())
+                Self::new(result_value, self.num_level())
+            }
+            Ordering::Less => other.add_elements(self),
+            Ordering::Equal => Self::new(self.value() ^ other.value(), self.num_level()),
         }
     }
 }
@@ -272,7 +233,7 @@ impl AddAssign for TowerFieldElement {
         *self = *self + other;
     }
 }
-
+#[allow(clippy::suspicious_arithmetic_impl)]
 impl Sub for TowerFieldElement {
     type Output = Self;
 
@@ -303,40 +264,51 @@ impl Mul for TowerFieldElement {
     // - if a's level is larger than b's level, we partition a until we have parts of the size of b and
     // multiply each part by b.
     fn mul(self, other: Self) -> Self {
-        if self.num_level() > other.num_level() {
-            // We split a into two parts and call the same method to multiply each part by b.
-            let (a_hi, a_lo) = self.split();
-            // Join a_hi * b and a_lo * b.
-            a_hi.mul(other).join(&a_lo.mul(other))
-        } else if self.num_level() < other.num_level() {
-            // If b is larger than a, we swap the arguments and call the same method.
-            other.mul(self)
-        } else {
-            // Base case:
-            if self.num_level() == 0 {
-                // In the binary base field, multiplication is the same as AND operation.
-                return Self::new(self.value() & other.value(), 0);
+        match self.num_level().cmp(&other.num_level()) {
+            Ordering::Greater => {
+                // We split a into two parts and call the same method to multiply each part by b.
+                let (a_hi, a_lo) = self.split();
+                // Join a_hi * b and a_lo * b.
+                a_hi.mul(other).join(&a_lo.mul(other))
             }
-            // Recursion:
-            let (a_hi, a_lo) = self.split();
-            let (b_hi, b_lo) = other.split();
-            // a_lo * b_lo
-            let al_bl = a_lo.mul(b_lo);
-            // a_hi * b_hi
-            let ah_bh = a_hi.mul(b_hi);
-            // x_{n-1}
-            let x = if self.num_level == 1 {
-                Self::new(1, 0)
-            } else {
-                Self::new(1 << ((self.num_bits()) / 4), self.num_level() - 1)
-            };
-            // a_hi * b_hi * x_{n-1}
-            let ah_bh_x = ah_bh.mul(x);
-            // We calculate (a_low + a_hi)(b_low + b_hi) to use Karatsuba.
-            let ab = (a_lo + a_hi).mul(b_lo + b_hi);
-            // b_hi * a_lo + a_hi * b_lo
-            let bhal_plus_ahbl = ab - al_bl - ah_bh;
-            (ah_bh_x + bhal_plus_ahbl).join(&(ah_bh + al_bl))
+            Ordering::Less => {
+                // If b is larger than a, we swap the arguments and call the same method.
+                other.mul(self)
+            }
+            Ordering::Equal => {
+                // Base case:
+                if self.num_level() == 0 {
+                    // In the binary base field, multiplication is the same as AND operation.
+                    return Self::new(self.value() & other.value(), 0);
+                }
+
+                // Split both elements into high and low parts
+                let (a_high, a_low) = self.split();
+                let (b_high, b_low) = other.split();
+
+                // Step 1: Compute sub-products
+                let low_product = a_low.mul(b_low); // a_low * b_low
+                let high_product = a_high.mul(b_high); // a_high * b_high
+
+                // Step 2: Get the polynomial x_{n-1} value
+                let x_value = if self.num_level() == 1 {
+                    Self::new(1, 0)
+                } else {
+                    Self::new(1 << (self.num_bits() / 4), self.num_level() - 1)
+                };
+
+                // Step 3: Compute high_product * x_{n-1}
+                let shifted_high_product = high_product.mul(x_value);
+
+                // Step 4: Karatsuba optimization for middle term
+                // Instead of computing a_high*b_low + a_low*b_high directly,
+                // we use (a_low+a_high)*(b_low+b_high) - low_product - high_product
+                let sum_product = (a_low + a_high).mul(b_low + b_high);
+                let middle_term = sum_product - low_product - high_product;
+
+                // Step 5: Join the parts according to the tower field multiplication formula
+                (shifted_high_product + middle_term).join(&(high_product + low_product))
+            }
         }
     }
 }
@@ -345,13 +317,13 @@ impl<'a> Mul<&'a TowerFieldElement> for &'a TowerFieldElement {
     type Output = TowerFieldElement;
 
     fn mul(self, other: &'a TowerFieldElement) -> TowerFieldElement {
-        self * other
+        <TowerFieldElement as Mul<TowerFieldElement>>::mul(*self, *other)
     }
 }
 
 impl MulAssign for TowerFieldElement {
     fn mul_assign(&mut self, other: Self) {
-        *self *= other;
+        *self = *self * other;
     }
 }
 
@@ -415,10 +387,10 @@ impl Default for TowerFieldElement {
     }
 }
 
-// Testing module
 #[cfg(test)]
 mod tests {
     use super::*;
+    use proptest::prelude::*;
 
     #[test]
     fn test_new_safe() {
@@ -456,6 +428,7 @@ mod tests {
     }
 
     #[test]
+    #[allow(clippy::zero_prefixed_literal)]
     fn mul_in_level_1() {
         let a = TowerFieldElement::new(00, 1); // 0
         let b = TowerFieldElement::new(01, 1); // 1
@@ -534,13 +507,11 @@ mod tests {
 
     #[test]
     fn test_inverse() {
-        // Test inverse in F
         let a0 = TowerFieldElement::new(1, 0);
         let inv_a0 = a0.inv().unwrap();
         assert_eq!(inv_a0.value, 1);
         assert_eq!(inv_a0.num_level, 0);
 
-        // Test inverse in F2
         let a1 = TowerFieldElement::new(2, 1);
         let inv_a1 = a1.inv().unwrap();
         assert_eq!(inv_a1.value, 3); // because 10 * 11 = 01.
@@ -557,14 +528,12 @@ mod tests {
         let one = TowerFieldElement::new(1, 5);
         assert_eq!(a3 * inv_a3, one);
 
-        // Test inverse of 0 returns error
         let zero = TowerFieldElement::zero();
         assert!(matches!(zero.inv(), Err(BinaryFieldError::InverseOfZero)));
     }
 
     #[test]
     fn test_multiplication_overflow() {
-        // Test that multiplication properly handles overflow and reduction
         for level in 0..7 {
             let max_value = (1u128 << (1 << level)) - 1; // Maximum value for this level
             let a = TowerFieldElement::new(max_value, level);
@@ -572,7 +541,7 @@ mod tests {
 
             let result = a * b;
 
-            // Result should be properly reduced (not exceed the field size)
+            // Result should be properly reduced
             assert!(result.value < (1u128 << result.num_bits));
         }
     }
@@ -595,5 +564,41 @@ mod tests {
         assert_eq!(a.to_binary_string(), "00000000000000000000000000001010");
         let b = TowerFieldElement::new(0b1010, 4);
         assert_eq!(b.to_binary_string(), "0000000000001010");
+    }
+
+    // Strategy to generate a TowerFieldElement with a random level between 0 and 7.
+    // For a given level:
+    // - The number of bits is computed as 1 << level.
+    // - For level 0, valid values are 0 to (1 << 1) - 1 = 1.
+    // - For level > 0, valid values are 0 to (1 << (1 << level)) - 1.
+    fn arb_tower_element_any() -> impl Strategy<Value = TowerFieldElement> {
+        (0usize..=7)
+            .prop_flat_map(|level| {
+                let max_val = if level == 0 {
+                    1
+                } else if (1usize << level) >= 128 {
+                    u128::MAX
+                } else {
+                    (1u128 << (1 << level)) - 1
+                };
+                (Just(level), 0u128..=max_val)
+            })
+            .prop_map(|(level, val)| TowerFieldElement::new(val, level))
+    }
+
+    proptest! {
+        // Test that multiplication is commutative:
+        // For any two randomly generated elements, a * b should equal b * a.
+        #[test]
+        fn test_mul_commutative(a in arb_tower_element_any(), b in arb_tower_element_any()) {
+            prop_assert_eq!(a * b, b * a);
+        }
+
+        // Test that multiplication is associative:
+        // For any three randomly generated elements, (a * b) * c should equal a * (b * c).
+        #[test]
+        fn test_mul_associative(a in arb_tower_element_any(), b in arb_tower_element_any(), c in arb_tower_element_any()) {
+            prop_assert_eq!((a * b) * c, a * (b * c));
+        }
     }
 }
