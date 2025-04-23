4ed3dbf2ef033d2e0444cea656425a06b4bed863
diegokingston <dkingston@fi.uba.ar>
2025-04-23 15:02:37 -0300

    add more comments

--- crates/provers/groth16/src/common.rs
+++ crates/provers/groth16/src/common.rs
@@ -23,8 +23,11 @@ pub type G1Point = <BLS12381Curve as IsEllipticCurve>::PointRepresentation;
 pub type G2Point = <BLS12381TwistCurve as IsEllipticCurve>::PointRepresentation;
 pub type PairingOutput = FieldElement<<Pairing as IsPairing>::OutputField>;
 
+/// Generator of the multiplicative group of Fr. Basically, the multiplicative group is obtained by taking
+/// powers of the element w: {w^0, w, w^2 , ... , w^{R - 2}} = Fr\{0}
 pub const ORDER_R_MINUS_1_ROOT_UNITY: FrElement = FrElement::from_hex_unchecked("7");
 
+/// Returns a random element in Fr
 pub fn sample_fr_elem() -> FrElement {
     let mut rng = rand_chacha::ChaCha20Rng::from_entropy();
     FrElement::new(U256 {
