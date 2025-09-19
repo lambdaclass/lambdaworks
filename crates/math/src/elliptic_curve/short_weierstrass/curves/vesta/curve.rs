f9c719ed34af6fa35dac6d733e89fcc2fd670eee
jotabulacios <jbulacios@fi.uba.ar>
2025-09-19 17:46:17 -0300

    add missing comment

--- crates/math/src/elliptic_curve/short_weierstrass/curves/vesta/curve.rs
+++ crates/math/src/elliptic_curve/short_weierstrass/curves/vesta/curve.rs
@@ -15,6 +15,7 @@ impl IsEllipticCurve for VestaCurve {
     fn generator() -> Self::PointRepresentation {
         // SAFETY:
         // - The generator point is mathematically verified to be a valid point on the curve.
+        // - `unwrap()` is safe because the provided coordinates satisfy the curve equation.
         let point = Self::PointRepresentation::new([
             -FieldElement::<Self::BaseField>::one(),
             FieldElement::<Self::BaseField>::from(2),
