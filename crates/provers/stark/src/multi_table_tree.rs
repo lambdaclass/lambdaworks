8c2d9a2fd143b58b55b85a1f67657c3332e89510
Nicole <nicole.graus@lambdaclass.com>
2025-12-02 13:04:34 -0300

    add functions for hashing nodes

--- crates/provers/stark/src/multi_table_tree.rs
+++ crates/provers/stark/src/multi_table_tree.rs
@@ -1,3 +1,4 @@
+use core::hash;
 use std::marker::PhantomData;
 
 use digest::{Digest, Output};
@@ -19,100 +20,128 @@ where
     _phantom: PhantomData<(F, D)>,
 }
 
-const ROOT: usize = 0;
-
 impl<F, D: Digest, const NUM_BYTES: usize> MultiTableTree<F, D, NUM_BYTES>
 where
     F: IsField,
     FieldElement<F>: AsBytes,
     [u8; NUM_BYTES]: From<Output<D>>,
 {
-    /// This function takes a single variable `Data` and converts it to a node.
-    fn hash_data(leaf: ) -> [u8; NUM_BYTES] {
+    /// This function takes a single row data and converts it to a node.
+    fn hash_data(row_data: &[FieldElement<F>]) -> [u8; NUM_BYTES] {
+        let mut hasher = D::new();
+        for element in row_data.iter() {
+            hasher.update(element.as_bytes());
+        }
+        let mut result_hash = [0_u8; NUM_BYTES];
+        result_hash.copy_from_slice(&hasher.finalize());
+        result_hash
+    }
 
-    };
-
-    /// This function takes the list of data from which the Merkle
+    /// This function takes a list of data (a list of rows) from which the Merkle
     /// tree will be built from and converts it to a list of leaf nodes.
-    fn hash_leaves(unhashed_leaves: &[Self::Data]) -> Vec<Self::Node> {
-        #[cfg(feature = "parallel")]
-        let iter = unhashed_leaves.par_iter();
-        #[cfg(not(feature = "parallel"))]
+    fn hash_leaves(unhashed_leaves: &[Vec<FieldElement<F>>]) -> Vec<[u8; NUM_BYTES]> {
         let iter = unhashed_leaves.iter();
-
         iter.map(|leaf| Self::hash_data(leaf)).collect()
     }
 
     /// This function takes to children nodes and builds a new parent node.
     /// It will be used in the construction of the Merkle tree.
-    fn hash_new_parent(child_1: &Self::Node, child_2: &Self::Node) -> Self::Node;
-
-    /// Create a Merkle tree from a slice of data
-    pub fn build(tables: &[Table<F>]) -> Option<Self> {
-
-        tables.sort_by_key(|t| t.height);
-        let mut iter = tables.iter().peekable();
-        
-
-        // if unhashed_leaves.is_empty() {
-        //     return None;
-        // }
-
-        // let hashed_leaves: Vec<B::Node> = B::hash_leaves(unhashed_leaves);
-
-        // //The leaf must be a power of 2 set
-        // let hashed_leaves = complete_until_power_of_two(hashed_leaves);
-        // let leaves_len = hashed_leaves.len();
-
-        // //The length of leaves minus one inner node in the merkle tree
-        // //The first elements are overwritten by build function, it doesn't matter what it's there
-        // let mut nodes = vec![hashed_leaves[0].clone(); leaves_len - 1];
-        // nodes.extend(hashed_leaves);
-
-        // //Build the inner nodes of the tree
-        // build::<B>(&mut nodes, leaves_len);
-
-        // Some(MerkleTree {
-        //     root: nodes[ROOT].clone(),
-        //     nodes,
-        // })
+    fn hash_new_parent(left: &[u8; NUM_BYTES], right: &[u8; NUM_BYTES]) -> [u8; NUM_BYTES] {
+        let mut hasher = D::new();
+        hasher.update(left);
+        hasher.update(right);
+        let mut result_hash = [0_u8; NUM_BYTES];
+        result_hash.copy_from_slice(&hasher.finalize());
+        result_hash
     }
 
-    /// Returns a Merkle proof for the element/s at position pos
-    /// For example, give me an inclusion proof for the 3rd element in the
-    /// Merkle tree
-    // pub fn get_proof_by_pos(&self, pos: usize) -> Option<Proof<B::Node>> {
-    //     let pos = pos + self.nodes.len() / 2;
-    //     let Ok(merkle_path) = self.build_merkle_path(pos) else {
-    //         return None;
-    //     };
+    /// This function takes to children nodes (left and right) and additional data (an other matrix row)
+    /// to be injected and builds a new parent node.
+    /// It will be used in the construction of the Merkle tree.
+    ///
+    /// TODO. Ask which option we should do:
+    /// 1. H(L, R, new)
+    /// 2. H(L, R, H(new))
+    /// 3. H(H(L, R), H(new))
+    fn hash_new_parent_with_injection(
+        left: &[u8; NUM_BYTES],
+        right: &[u8; NUM_BYTES],
+        data_to_inject: &[FieldElement<F>],
+    ) -> [u8; NUM_BYTES] {
+        let mut hasher = D::new();
 
-    //     self.create_proof(merkle_path)
-    // }
+        hasher.update(left);
+        hasher.update(right);
 
-    // /// Creates a proof from a Merkle pasth
-    // fn create_proof(&self, merkle_path: Vec<B::Node>) -> Option<Proof<B::Node>> {
-    //     Some(Proof { merkle_path })
-    // }
+        // // Option 1.
+        // hasher.update(data_to_inject.as_bytes());
 
-    // /// Returns the Merkle path for the element/s for the leaf at position pos
-    // fn build_merkle_path(&self, pos: usize) -> Result<Vec<B::Node>, Error> {
-    //     let mut merkle_path = Vec::new();
-    //     let mut pos = pos;
+        // Option 2.
+        let hashed_injection = Self::hash_data(data_to_inject);
+        hasher.update(hashed_injection);
 
-    //     while pos != ROOT {
-    //         let Some(node) = self.nodes.get(sibling_index(pos)) else {
-    //             // out of bounds, exit returning the current merkle_path
-    //             return Err(Error::OutOfBounds);
-    //         };
-    //         merkle_path.push(node.clone());
+        // Option 3.
+        // ...
 
-    //         pos = parent_index(pos);
-    //     }
-
-    //     Ok(merkle_path)
-    // }
+        let mut result_hash = [0_u8; NUM_BYTES];
+        result_hash.copy_from_slice(&hasher.finalize());
+        result_hash
+    }
 }
 
+#[cfg(test)]
+mod tests {
+    use lambdaworks_math::field::{
+        element::FieldElement, fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
+    };
 
+    use sha3::Sha3_256;
 
+    use super::MultiTableTree;
+
+    type F = Stark252PrimeField;
+    type FE = FieldElement<F>;
+
+    #[test]
+    fn run_hash_data() {
+        let leaves_data = [
+            vec![FE::from(1u64), FE::from(10u64)],
+            vec![FE::from(2u64), FE::from(20u64)],
+            vec![FE::from(3u64), FE::from(30u64)],
+            vec![FE::from(4u64), FE::from(40u64)],
+            vec![FE::from(5u64), FE::from(50u64)],
+            vec![FE::from(6u64), FE::from(60u64)],
+            vec![FE::from(7u64), FE::from(70u64)],
+            vec![FE::from(8u64), FE::from(80u64)],
+        ];
+
+        let leaves_hashed = MultiTableTree::<F, Sha3_256, 32>::hash_leaves(&leaves_data);
+        println!("Leaves hashed: {:?}", leaves_hashed);
+    }
+
+    #[test]
+    fn run_hash_new_parent_with_injection() {
+        let leaves_data = [
+            vec![FE::from(1u64), FE::from(10u64)],
+            vec![FE::from(2u64), FE::from(20u64)],
+            vec![FE::from(3u64), FE::from(30u64)],
+            vec![FE::from(4u64), FE::from(40u64)],
+            vec![FE::from(5u64), FE::from(50u64)],
+            vec![FE::from(6u64), FE::from(60u64)],
+            vec![FE::from(7u64), FE::from(70u64)],
+            vec![FE::from(8u64), FE::from(80u64)],
+        ];
+
+        let leaves_hashed = MultiTableTree::<F, Sha3_256, 32>::hash_leaves(&leaves_data);
+
+        let data_to_inject = &[FE::from(9u64), FE::from(90u64)];
+
+        let parent_hash = MultiTableTree::<F, Sha3_256, 32>::hash_new_parent_with_injection(
+            &leaves_hashed[0],
+            &leaves_hashed[1],
+            data_to_inject,
+        );
+
+        println!("Parent hash with injection: {:?}", parent_hash);
+    }
+}
