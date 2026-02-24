9a00b24816d6930dded2e49dcd66b957fc013361
jotabulacios <jbulacios@fi.uba.ar>
2026-02-24 10:41:30 -0300

    Add Poseidon2 hash for Goldilocks field (#1181)
    * implement poseidon2 and merkle
    
    * fix clippy
    
    * precompute round constants
    
    * inline
    
    * add fuzzer
    
    * fix missing Vec import
    
    * addres comments
    
    ---------
    
    Co-authored-by: Diego K <43053772+diegokingston@users.noreply.github.com>

--- crates/crypto/src/hash/mod.rs
+++ crates/crypto/src/hash/mod.rs
@@ -2,5 +2,6 @@ pub mod hash_to_field;
 pub mod monolith;
 pub mod pedersen;
 pub mod poseidon;
+pub mod poseidon2;
 pub mod rescue_prime;
 pub mod sha3;
--- crates/crypto/src/merkle_tree/backends/mod.rs
+++ crates/crypto/src/merkle_tree/backends/mod.rs
@@ -1,5 +1,6 @@
 pub mod field_element;
 pub mod field_element_vector;
+pub mod poseidon2;
 /// Configurations for merkle trees
 /// Setting generics to some value
 pub mod types;
